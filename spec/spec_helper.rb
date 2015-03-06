b17b50077ff50002150c0436941a8aec614db944
David Moles <david.moles@ucop.edu>
2015-03-06 15:43:48 -0800

    Add code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.start do
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::Console,
+  ]
+end
+
 require 'rspec'
 require 'dash2/harvester'
+
