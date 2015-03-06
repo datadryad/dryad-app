a93c9d2fe40577d7b5ae8c59729c3d36d86869f1
David Moles <david.moles@ucop.edu>
2015-03-06 09:56:37 -0800

    Add namespace to spec

--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -1,18 +1,24 @@
 require 'rspec'
 require 'dash2/harvester'
 
-# TODO figure out idiomatic way to be less verbose
-RSpec.describe Dash2::Harvester::Client do
-  describe '#new' do
-    it 'accepts a valid URL' do
-      valid_url = 'http://example.org/oai'
-      client = Dash2::Harvester::Client.new valid_url
-      expect(client.base_uri).to eq(URI.parse(valid_url))
-    end
+module Dash2
+  module Harvester
 
-    it 'rejects an invalid URL' do
-      invalid_url = 'I am not a valid URL'
-      expect{Dash2::Harvester::Client.new invalid_url}.to raise_error(URI::InvalidURIError)
+    describe Client do
+      describe '#new' do
+        it 'accepts a valid URL' do
+          valid_url = 'http://example.org/oai'
+          client = Client.new valid_url
+          expect(client.base_uri).to eq(URI.parse(valid_url))
+        end
+
+        it 'rejects an invalid URL' do
+          invalid_url = 'I am not a valid URL'
+          expect{Client.new invalid_url}.to raise_error(URI::InvalidURIError)
+        end
+      end
     end
+    
   end
 end
+
