a5f4f13289f11437f7ed1542b986cdc61a76b576
David Moles <david.moles@ucop.edu>
2015-03-05 17:36:42 -0800

    Initial work on Dash2::Harvester::Client, with spec

--- dash2-harvester.gemspec
+++ dash2-harvester.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.7'
   spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.2'
 
   spec.add_runtime_dependency 'oai', '~> 0.3.1'
 end
--- lib/dash2/harvester.rb
+++ lib/dash2/harvester.rb
@@ -2,6 +2,31 @@ require 'dash2/harvester/version'
 
 module Dash2
   module Harvester
-    # Your code goes here...
+
+    # A harvesting client for a single repository
+    class Client
+
+      attr_reader :base_uri
+
+      # Creates a new harvesting client for the given repository
+      #
+      # @param base_url [String] the base URL of the target repository
+      # @raise [URI::InvalidURIError] if the base URL is invalid
+      def initialize(base_url)
+        @base_uri = URI.parse base_url
+      end
+
+      # TODO does this need to be tested directly?
+      private
+      def oai_client
+        @oai_client ||= OAI::Client.new @base_url
+      end
+
+      private
+      def base_url
+        @base_url ||= @base_uri.to_s
+      end
+    end
+
   end
 end
