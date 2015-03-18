16dc765ef6e41163320c7a4f619d87555eabb622
David Moles <david.moles@ucop.edu>
2015-03-18 12:30:37 -0700

    Updated docs

--- lib/dash2/harvester/harvest_task.rb
+++ lib/dash2/harvester/harvest_task.rb
@@ -1,15 +1,43 @@
+# Class representing a single harvest operation.
+#
+# @!attribute [r] oai_base_uri
+#   @return [URI] the base URL of the repository.
+# @!attribute [r] from_time
+#   @return [Time, nil] the start (inclusive) of the datestamp range for selective harvesting.
+# @!attribute [r] until_time
+#   @return [Time, nil] the end (inclusive) of the datestamp range for selective harvesting.
+# @!attribute [r] metadata_prefix
+#   @return [String] the metadata prefix defining the metadata format requested from the repository.
+
 class Dash2::Harvester::HarvestTask
 
   DUBLIN_CORE = 'oai_dc'
+  private_constant :DUBLIN_CORE
 
   VALID_PREFIX_PATTERN = Regexp.new("^[#{URI::RFC2396_REGEXP::PATTERN::UNRESERVED}]+$")
+  private_constant :VALID_PREFIX_PATTERN
 
   attr_reader :oai_base_uri
   attr_reader :from_time
   attr_reader :until_time
   attr_reader :metadata_prefix
 
-  # TODO doc that we check time range validity to seconds, but repos may only support days
+  # Creates a new +HarvestTask+ for harvesting from the specified OAI-PMH repository, with
+  # an optional datetime range and metadata prefix.
+  #
+  # @param oai_base_url [URI, String] the base URL of the repository. *(Required)*
+  # @param from_time [Time, nil] the start (inclusive) of the datestamp range for selective harvesting.
+  #   If +from_time+ is omitted, harvesting will extend back to the earliest datestamp in the
+  #   repository. (Optional)
+  # @param until_time [Time, nil] the end (inclusive) of the datestamp range for selective harvesting.
+  #   If +until_time+ is omitted, harvesting will extend forward to the latest datestamp in the
+  #   repository. (Optional)
+  # @param metadata_prefix [String, nil] the metadata prefix defining the metadata format requested
+  #   from the repository. If +metadata_prefix+ is omitted, the prefix +oai_dc+ (Dublin Core)
+  #   will be used.
+  # @raise [RangeError] if +from_time+ is later than +until_time+.
+  # @raise [ArgumentError] if +metadata_prefix+ contains invalid characters, i.e. URI reserved
+  #   characters per {https://www.ietf.org/rfc/rfc2396.txt RFC 2396}.
   def initialize(oai_base_url:, from_time: nil, until_time: nil, metadata_prefix: DUBLIN_CORE)
 
     if from_time && until_time
