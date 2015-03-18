77620711bcd2c67925a6ab8f6a3ae455a6c95345
David Moles <david.moles@ucop.edu>
2015-03-17 17:10:42 -0700

    Initial work on new harvest_task

---
+++ lib/dash2/harvester/harvest_task.rb
@@ -0,0 +1,27 @@
+class Dash2::Harvester::HarvestTask
+
+  DUBLIN_CORE = 'oai_dc'
+
+  # VALID_PREFIX_PATTERN = Regexp.new("[#{URI::RFC2396_REGEXP::PATTERN::UNRESERVED}]+")
+
+  attr_reader :oai_base_uri
+  attr_reader :from_time
+  attr_reader :until_time
+  attr_reader :metadata_prefix
+
+  # TODO doc that we check time range validity to seconds, but repos may only support days
+  def initialize(oai_base_url:, from_time: nil, until_time: nil, metadata_prefix: DUBLIN_CORE)
+
+    if from_time && until_time
+      raise RangeError, "from_time #{from_time} must be <= until_time #{until_time}" unless from_time.to_i <= until_time.to_i
+    end
+
+    # unless URI::RFC2396_REGEXP::UNRESERVED
+
+    @oai_base_uri = (oai_base_url.kind_of? URI) ? oai_base_url : URI.parse(oai_base_url)
+    @from_time = from_time
+    @until_time = until_time
+    @metadata_prefix = metadata_prefix
+  end
+
+end
---
+++ spec/unit/harvest_task_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+module Dash2
+  module Harvester
+
+    describe HarvestTask do
+      describe 'its harvesting' do
+        it 'sends a "from" datestamp only if one is specified'
+        it 'sends a "to" datestamp only if one is specified'
+        it 'sends the specified metadata prefix'
+        it 'defaults to "oai_dc" if no metadata prefix is specified'
+      end
+
+      describe '#new' do
+        it 'accepts a valid repository URL' do
+          valid_url = 'http://example.org/oai'
+          harvest_task = HarvestTask.new oai_base_url: valid_url
+          expect(harvest_task.oai_base_uri).to eq(URI.parse(valid_url))
+        end
+
+        it 'accepts a URI object as a repository URL' do
+          uri = URI.parse('http://example.org/oai')
+          harvest_task = HarvestTask.new oai_base_url: uri
+          expect(harvest_task.oai_base_uri).to eq(uri)
+        end
+
+        it 'rejects an invalid repository URL' do
+          invalid_url = 'I am not a valid URL'
+          expect { HarvestTask.new oai_base_url: invalid_url }.to raise_error(URI::InvalidURIError)
+        end
+
+        it 'requires a repository URL' do
+          # noinspection RubyArgCount
+          expect { HarvestTask.new }.to raise_error(ArgumentError)
+        end
+
+        it 'accepts a valid "from" datestamp' do
+          time = Time.new
+          harvest_task = HarvestTask.new oai_base_url: 'http://example.org/oai', from_time: time
+          expect(harvest_task.from_time).to eq(time)
+        end
+
+        it 'accepts a valid "until" datestamp' do
+          time = Time.new
+          harvest_task = HarvestTask.new oai_base_url: 'http://example.org/oai', until_time: time
+          expect(harvest_task.until_time).to eq(time)
+        end
+
+        it 'rejects datestamps that would create an invalid range' do
+          epoch = Time.at(0)
+          now = Time.new
+
+          expect { HarvestTask.new oai_base_url: 'http://example.org/oai', from_time: now, until_time: epoch }.to raise_error(RangeError)
+        end
+
+        it 'accepts a metadata prefix' do
+          prefix = 'datacite'
+          harvest_task = HarvestTask.new oai_base_url: 'http://example.org/oai', metadata_prefix: prefix
+          expect(harvest_task.metadata_prefix).to eq(prefix)
+        end
+
+        it 'rejects an invalid metadata prefix'
+
+        # it 'rejects an invalid metadata prefix' do
+        #   invalid_prefix_chars = ':/?#[]@!$&\'()*+,;=' # prefix must be URI unreserved characters
+        #   invalid_prefix_chars.each do |c|
+        #     invalid_prefix = "oai_#{c}"
+        #     expect { HarvestTask.new oai_base_url: 'http://example.org/oai', metadata_prefix: invalid_prefix }.to raise_error(ArgumentError)
+        #   end
+        # end
+
+        it 'defaults to Dublin Core if no metadata prefix is set'
+
+      end
+    end
+
+  end
+end
+
