25e5ee5253bc325c3bb7c7acd6a42febf0c75826
David Moles <david.moles@ucop.edu>
2015-04-14 14:56:31 -0700

    Fix Rubocop complaints

--- lib/stash/harvester/engine.rb
+++ lib/stash/harvester/engine.rb
@@ -4,8 +4,8 @@ module Stash
       isolate_namespace Stash::Harvester
 
       config.generators do |g|
-        g.test_framework      :rspec,        :fixture => false
-        g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+        g.test_framework :rspec, fixture: false
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
         g.assets false
         g.helper false
       end
