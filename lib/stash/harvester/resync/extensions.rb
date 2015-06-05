f5042784eb54525f21b29e3be4420803500c2d64
David Moles <david.moles@ucop.edu>
2015-06-05 11:48:44 -0700

    Got ResourceList auto-fetch working

--- lib/stash/harvester/resync/extensions.rb
+++ lib/stash/harvester/resync/extensions.rb
@@ -1,5 +1,7 @@
 require 'resync/client'
 
+# TODO: prepend named extension modules
+# TODO: make these more idiomatic -- named lazy objects?
 module Resync
   class ResourceList
     def each_resource(&block)
@@ -8,10 +10,20 @@ module Resync
   end
 
   class ChangeList
-    def each_resource(in_range:)
+    def each_change(in_range:)
       resources.lazy.each do |r|
         next unless in_range.cover?(r.modified_time)
         yield r
+      end
+    end
+  end
+
+  class ResourceListIndex
+    def each_resource(&block)
+      @resource_lists ||= {}
+      resources.lazy.each do |r|
+        @resource_lists[r] ||= r.get_and_parse
+        @resource_lists[r].each_resource(&block)
       end
     end
   end
--- spec/unit/stash/harvester/resource_sync/extensions_spec.rb
+++ spec/unit/stash/harvester/resource_sync/extensions_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 module Resync
+
   describe ResourceList do
     describe '#each_resource' do
       it 'invokes the specified block on each resource in the list' do
@@ -38,7 +39,7 @@ module Resync
   end
 
   describe ChangeList do
-    describe '#each_resource' do
+    describe '#each_change' do
       it 'invokes the specified block on each resource in the list' do
         time = Time.new
         resources = Array.new(3) do |index|
@@ -50,7 +51,7 @@ module Resync
         end
 
         list = ChangeList.new(resources: resources)
-        list.each_resource(in_range: Time.utc(0)..Time.new) do |r|
+        list.each_change(in_range: Time.utc(0)..Time.new) do |r|
           r.get
         end
       end
@@ -67,7 +68,7 @@ module Resync
 
         range = Time.utc(2000, 1, 1)..Time.utc(2003, 1, 1)
         list = ChangeList.new(resources: resources)
-        list.each_resource(in_range: range) do |r|
+        list.each_change(in_range: range) do |r|
           r.get
         end
       end
@@ -82,10 +83,60 @@ module Resync
 
         range = Time.utc(2000, 1, 1)..Time.utc(2003, 1, 1)
         list = ChangeList.new(resources: resources)
-        list.each_resource(in_range: range) do |r|
+        list.each_change(in_range: range) do |r|
           r.get
           break
         end
+      end
+    end
+  end
+
+  describe ResourceListIndex do
+    before(:each) do
+      @helper = instance_double(Client::HTTPHelper)
+      @client = Client.new(helper: @helper)
+    end
+
+    describe '#each_resource' do
+      it 'flattens the child resourcelists' do
+        resource_index_uri = URI('http://example.com/dataset1/resourcelist.xml')
+        resource_index_data = File.read('spec/data/resync/resource-list-index.xml')
+        expect(@helper).to receive(:fetch).with(uri: resource_index_uri).and_return(resource_index_data)
+        
+        list1_uri = URI('http://example.com/resourcelist1.xml')
+        list1_data = File.read('spec/data/resync/resource-list-1.xml')
+        expect(@helper).to receive(:fetch).with(uri: list1_uri).and_return(list1_data)
+
+        list2_uri = URI('http://example.com/resourcelist2.xml')
+        list2_data = File.read('spec/data/resync/resource-list-2.xml')
+        expect(@helper).to receive(:fetch).with(uri: list2_uri).and_return(list2_data)
+
+        resource_index = @client.get_and_parse(resource_index_uri)
+        count = 0
+        resource_index.each_resource do |r|
+          count += 1
+          expect(r.uri).to eq(URI("http://example.com/res#{count}"))
+        end
+        expect(count).to eq(4)
+      end
+
+      it 'is lazy' do
+        resource_index_uri = URI('http://example.com/dataset1/resourcelist.xml')
+        resource_index_data = File.read('spec/data/resync/resource-list-index.xml')
+        expect(@helper).to receive(:fetch).with(uri: resource_index_uri).and_return(resource_index_data)
+
+        list1_uri = URI('http://example.com/resourcelist1.xml')
+        list1_data = File.read('spec/data/resync/resource-list-1.xml')
+        expect(@helper).to receive(:fetch).with(uri: list1_uri).and_return(list1_data)
+
+        resource_index = @client.get_and_parse(resource_index_uri)
+        count = 0
+        resource_index.each_resource do |r|
+          count += 1
+          expect(r.uri).to eq(URI("http://example.com/res#{count}"))
+          break if count >= 2
+        end
+        expect(count).to eq(2)
       end
     end
   end
