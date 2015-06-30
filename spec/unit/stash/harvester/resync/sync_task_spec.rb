f15f4a40ecad61f1ae345f91ac91ec7aed583f2f
David Moles <david.moles@ucop.edu>
2015-06-30 11:10:09 -0700

    Added test for ResourceDumpIndex

--- spec/unit/stash/harvester/resync/sync_task_spec.rb
+++ spec/unit/stash/harvester/resync/sync_task_spec.rb
@@ -33,9 +33,42 @@ module Stash
           it "gets resource contents from a #{::Resync::ChangeList}"
           it 'returns an Enumerator::Lazy'
         end
+
         describe "#{::Resync::ResourceDumpIndex} handling" do
-          it "gets resource contents from a #{::Resync::ResourceDumpIndex}"
-          it 'returns an Enumerator::Lazy'
+          before(:each) do
+            @resource_dump_index = ::Resync::XMLParser.parse(File.new('spec/data/resync/dumps/resourcedumpindex.xml'))
+            expect(@capability_list).to receive(:resource_dump) { @resource_dump_index }
+          end
+          it "gets resource contents from a #{::Resync::ResourceDumpIndex}" do
+            @resource_dump_index.client_delegate = @resync_client
+
+            resource_contents = []
+            (1..2).each do |d|
+              dump = ::Resync::XMLParser.parse(File.new("spec/data/resync/dumps/resourcedump#{d}/resourcedump#{d}.xml"))
+              dump.client_delegate = @resync_client
+              expect(@resync_client).to receive(:get_and_parse).with(URI("http://example.org/resourcedump#{d}/resourcedump#{d}.xml")) { dump }
+
+              (1..3).each do |p|
+                dir = "spec/data/resync/dumps/resourcedump#{d}/part#{p}"
+                file = "#{dir}.zip"
+                expect(@resync_client).to receive(:download_to_temp_file).with(URI("http://example.org/resourcedump#{d}/part#{p}.zip")) { file }
+                ((p * 2 - 1)..(p * 2)).each do |r|
+                  resource_contents << File.read("#{dir}/res#{r}")
+                end
+              end
+            end
+
+            downloaded = @sync_task.download
+            downloaded_array = downloaded.to_a
+            downloaded_array.each_with_index do |rc, i|
+              expect(rc.content).to eq(resource_contents[i])
+            end
+          end
+
+          it 'returns an Enumerator::Lazy' do
+            downloaded = @sync_task.download
+            expect(downloaded).to be_a(Enumerator::Lazy)
+          end
         end
 
         describe "#{::Resync::ResourceDump} handling" do
@@ -51,7 +84,7 @@ module Stash
             (1..3).each do |p|
               dir = "spec/data/resync/dumps/resourcedump1/part#{p}"
               file = "#{dir}.zip"
-              expect(@resync_client).to receive(:download_to_temp_file).with(URI("http://example.com/resourcedump1/part#{p}.zip")) { file }
+              expect(@resync_client).to receive(:download_to_temp_file).with(URI("http://example.org/resourcedump1/part#{p}.zip")) { file }
               ((p * 2 - 1)..(p * 2)).each do |r|
                 resource_contents << File.read("#{dir}/res#{r}")
               end
