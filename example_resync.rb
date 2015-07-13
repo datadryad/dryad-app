54d7f58e12274325c94db3fb8301b8c3c7a0ab4e
David Moles <david.moles@ucop.edu>
2015-07-13 15:22:22 -0700

    Updated Resync example for more standardized API

--- example_resync.rb
+++ example_resync.rb
@@ -5,19 +5,16 @@ $LOAD_PATH << File.dirname(__FILE__)
 require 'lib/stash/harvester'
 
 capability_list_uri = 'http://localhost:8888/capabilitylist.xml'
-# from_time = Time.utc(2015, 6, 24, 23, 20)
-# until_time = Time.utc(2015, 6, 24, 23, 29, 59)
+from_time = Time.utc(2015, 7, 13, 22, 17)
+until_time = Time.utc(2015, 7, 13, 22, 18)
 
 sync_task = Stash::Harvester::Resync::SyncTask.new(
   capability_list_uri: capability_list_uri,
-# from_time: from_time,
-# until_time: until_time
+  from_time: from_time,
+  until_time: until_time
 )
 
 response = sync_task.harvest_records
-puts response.class
-# response.each do |resource_content|
-#   puts resource_content.uri
-# end
-
-puts response.size
+response.each do |resource_content|
+  puts "#{resource_content.timestamp}\t#{resource_content.identifier}"
+end
