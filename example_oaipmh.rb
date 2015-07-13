2a604cc7db9525c50ac46c44b2edbaef1c3bce17
David Moles <david.moles@ucop.edu>
2015-07-13 16:58:27 -0700

    Inline local variables to improve readability

--- example_oaipmh.rb
+++ example_oaipmh.rb
@@ -4,21 +4,16 @@
 $LOAD_PATH << File.dirname(__FILE__)
 require 'lib/stash/harvester'
 
-oai_base_url = 'http://oai.datacite.org/oai'
-metadata_prefix = 'datacite'
-from_time = Time.utc(2013, 6, 1)
-until_time = Time.utc(2013, 6, 30)
-
 list_records_config = Stash::Harvester::OAIPMH::OAIConfig.new(
-  oai_base_url: oai_base_url,
-  metadata_prefix: metadata_prefix,
+  oai_base_url: 'http://oai.datacite.org/oai',
+  metadata_prefix: 'oai_datacite',
   set: 'REFQUALITY'
 )
 
 list_records_task = Stash::Harvester::OAIPMH::ListRecordsTask.new(
   config: list_records_config,
-  from_time: from_time,
-  until_time: until_time
+  from_time: Time.utc(2013, 6, 1),
+  until_time: Time.utc(2013, 6, 30)
 )
 
 response = list_records_task.harvest_records
