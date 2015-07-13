665d39d81a59da9de47d9f12649c02b611aeb184
David Moles <david.moles@ucop.edu>
2015-07-13 15:23:32 -0700

    Added comments

--- example_resync.rb
+++ example_resync.rb
@@ -4,6 +4,8 @@
 $LOAD_PATH << File.dirname(__FILE__)
 require 'lib/stash/harvester'
 
+# URL from resync-simulator (https://github.com/resync/resync-simulator);
+# timestamps should reflect when the simulator was started
 capability_list_uri = 'http://localhost:8888/capabilitylist.xml'
 from_time = Time.utc(2015, 7, 13, 22, 17)
 until_time = Time.utc(2015, 7, 13, 22, 18)
