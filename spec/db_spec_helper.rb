b1b3a3ea87cabb29a6cc15109d1610104e32bd40
David Moles <david.moles@ucop.edu>
2015-08-14 13:43:57 -0700

    Run migrations before test

--- spec/db_spec_helper.rb
+++ spec/db_spec_helper.rb
@@ -4,8 +4,9 @@ require 'app/models/stash/harvester/models'
 connection_info = YAML.load_file('db/config.yml')['test']
 ActiveRecord::Base.establish_connection(connection_info)
 
-RSpec.configure do |config|
+ActiveRecord::Migrator.up 'db/migrate'
 
+RSpec.configure do |config|
   config.around do |example|
     ActiveRecord::Base.transaction do
       example.run
