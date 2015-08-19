ccc9690147ce5982dc67ac95250390b9ddb35a39
David Moles <david.moles@ucop.edu>
2015-08-19 14:03:39 -0700

    Renamed methods for clarity and got #find_oldest_failed working and tested

--- app/models/stash/harvester/models/harvested_record.rb
+++ app/models/stash/harvester/models/harvested_record.rb
@@ -9,18 +9,18 @@ module Stash
         belongs_to :harvest_job
         has_many :indexed_records
 
-        def self.find_last_indexed
+        def self.find_newest_indexed
           HarvestedRecord.joins(:indexed_records).where(indexed_records: { status: Status::COMPLETED }).order(timestamp: :desc).first
         end
 
-        def self.find_first_failed
+        def self.find_oldest_failed
           query = 'SELECT harvested_records.* FROM harvested_records
                       INNER JOIN indexed_records failed_records
                         ON harvested_records.id = failed_records.harvested_record_id AND failed_records.status == 3
                       LEFT JOIN indexed_records other_records
                         ON harvested_records.id = other_records.harvested_record_id AND other_records.status <> 3
                     WHERE other_records.id IS NULL
-                    ORDER BY harvested_records.timestamp DESC LIMIT 1'
+                    ORDER BY harvested_records.timestamp LIMIT 1'
           HarvestedRecord.find_by_sql(query).first
         end
       end
--- spec/db/harvested_record_spec.rb
+++ spec/db/harvested_record_spec.rb
@@ -7,28 +7,21 @@ module Stash
       describe HarvestedRecord do
 
         before :each do
-          record_count = 3
+          @record_count = 3
 
-          @harvest_job_completed = create(:indexed_harvest_job, record_count: record_count, from_time: nil, start_time: Time.utc(2015, 7, 1))
+          @harvest_job_completed = create(:indexed_harvest_job, record_count: @record_count, from_time: nil, start_time: Time.utc(2015, 7, 1))
           @harvested_records_completed = @harvest_job_completed.harvested_records
-          @index_job_completed = @harvest_job_completed.index_jobs.first
-          @indexed_records_completed = @index_job_completed.indexed_records
 
-          @harvest_job_failed = create(:indexed_harvest_job, record_count: record_count, from_time: Time.utc(2015, 7, 1, 10), start_time: Time.utc(2015, 8, 1), index_record_status: :failed)
+          @harvest_job_failed = create(:indexed_harvest_job, record_count: @record_count, from_time: Time.utc(2015, 7, 1, 10), start_time: Time.utc(2015, 8, 1), index_record_status: :failed)
           @harvested_records_failed = @harvest_job_failed.harvested_records
-          @index_job_failed = @harvest_job_failed.index_jobs.first
-          @indexed_records_failed = @index_job_failed.indexed_records
 
-          @harvest_job_pending = create(:indexed_harvest_job, record_count: record_count, from_time: Time.utc(2015, 7, 1, 10), start_time: Time.utc(2015, 8, 1), index_job_status: :in_progress, index_record_status: :pending)
-          @harvested_records_pending = @harvest_job_pending.harvested_records
-          @index_job_pending = @harvest_job_pending.index_jobs.first
-          @indexed_records_pending = @index_job_pending.indexed_records
+          @harvest_job_pending = create(:indexed_harvest_job, record_count: @record_count, from_time: Time.utc(2015, 8, 1), start_time: Time.utc(2015, 9, 1), index_job_status: :in_progress, index_record_status: :pending)
         end
 
-        describe '#find_last_indexed' do
+        describe '#find_newest_indexed' do
 
-          it 'finds only index-completed records' do
-            last_indexed = HarvestedRecord.find_last_indexed
+          it 'finds the newest indexed record' do
+            last_indexed = HarvestedRecord.find_newest_indexed
 
             indexed_records = last_indexed.indexed_records
             expect(indexed_records.count).to eq(1)
@@ -38,14 +31,48 @@ module Stash
 
             expect(last_indexed).to eq(@harvested_records_completed.last)
           end
-
-          it 'finds only the most recent such record'
         end
 
-        describe '#find_last_indexed' do
-          it 'finds only index-failed records'
-          it 'finds only the oldest such record'
-          it 'ignores index-failed records with subsequently successful indexes'
+        describe '#find_oldest_failed' do
+          it 'finds the oldest failed record' do
+            oldest_failed = HarvestedRecord.find_oldest_failed
+
+            indexed_records = oldest_failed.indexed_records
+            expect(indexed_records.count).to eq(1)
+
+            indexed_record = indexed_records.first
+            expect(indexed_record.failed?).to be true
+
+            expect(oldest_failed).to eq(@harvested_records_failed.first)
+          end
+
+          it 'ignores index-failed records with subsequently successful indexes' do
+            reindex_job = create(
+              :index_job,
+              solr_url: 'http://solr.example.org/',
+              harvest_job: @harvest_job_completed,
+              start_time: Time.utc(2015, 9, 1),
+              end_time: Time.utc(2015, 9, 1, @record_count),
+              status: :in_progress
+            )
+
+            @harvested_records_failed.each_with_index do |hr, index|
+              create(
+                :indexed_record,
+                index_job: reindex_job,
+                harvested_record: hr,
+                submitted_time: Time.utc(2015, 9, 1, index),
+                # odd-numbered records succeed; even-numbered still failing
+                status: (index % 2 == 0 ? :completed : :failed)
+              )
+            end
+
+            # should be the first even-numbered record
+            oldest_failed = HarvestedRecord.find_oldest_failed
+
+            expect(oldest_failed).to eq(@harvested_records_failed.second)
+          end
+
         end
       end
     end
