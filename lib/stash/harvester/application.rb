dbdbe67f5d4e065f7a2f4048ce1ff621a8a09fb5
David Moles <david.moles@ucop.edu>
2015-09-02 17:01:41 -0700

    Started work on config file lookup

--- lib/stash/harvester/application.rb
+++ lib/stash/harvester/application.rb
@@ -11,6 +11,25 @@ module Stash
       def start
       end
 
+      private
+
+      def ensure_config_file(config_file)
+        config_file ||= default_config_file
+        raise ArgumentError, "No configuration file provided, and none found in default locations #{config_file_defaults.join(' or ')}" unless config_file
+        config_file
+      end
+
+      def default_config_file
+        config_file_defaults.each do |cf|
+          return cf if File.exist?(cf)
+        end
+      end
+
+      def config_file_defaults
+        [File.expand_path('stash-harvester.yml', Dir.pwd),
+         File.expand_path('.stash-harvester.yml', Dir.home)]
+      end
+
     end
   end
 end
