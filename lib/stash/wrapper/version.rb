871fd1b89e045bfbea71bf4f035ff40100526756
David Moles <david.moles@ucop.edu>
2015-09-22 09:36:53 -0700

    Initial commit

---
+++ lib/stash/wrapper/version.rb
@@ -0,0 +1,12 @@
+module Stash
+  module Wrapper
+    # The name of this gem
+    NAME = 'stash-wrapper'
+
+    # The version of this gem
+    VERSION = '0.0.1'
+
+    # The copyright notice for this gem
+    COPYRIGHT = 'Copyright (c) 2015 The Regents of the University of California'
+  end
+end
