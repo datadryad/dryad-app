cc89268e7263dbe884690795997d42defd9b85e5
David Moles <david.moles@ucop.edu>
2015-10-05 12:31:36 -0700

    Refactored to make some methods private

--- lib/stash/harvester/application.rb
+++ lib/stash/harvester/application.rb
@@ -9,37 +9,35 @@ module Stash
       def initialize(from_time: nil, until_time: nil, config_file: nil)
         @from_time = Util.utc_or_nil(from_time)
         @until_time = Util.utc_or_nil(until_time)
-        @config = ensure_config(config_file)
+        self.config = config_file
       end
 
       def start
       end
 
-      private
-
-      def ensure_config(config_file)
-        config_file = Application.ensure_config_file(config_file)
-        Config.from_file(config_file)
-      end
-
-      # TODO: Refactor tests to make these private
-
-      def self.ensure_config_file(config_file)
-        config_file ||= default_config_file
-        fail ArgumentError, "No configuration file provided, and none found in default locations #{config_file_defaults.join(' or ')}" unless config_file
-        config_file
-      end
-
-      def self.default_config_file
-        config_file_defaults.each do |cf|
-          return cf if File.exist?(cf)
-        end
-        nil
-      end
-
       def self.config_file_defaults
         [File.expand_path('stash-harvester.yml', Dir.pwd),
          File.expand_path('.stash-harvester.yml', Dir.home)]
+      end
+
+      private
+
+      def config=(value)
+        config_file = ensure_config_file(value)
+        @config = Config.from_file(config_file)
+      end
+
+      def ensure_config_file(config_file)
+        config_file ||= default_config_file
+        fail ArgumentError, "No configuration file provided, and none found in default locations #{Application.config_file_defaults.join(' or ')}" unless config_file
+        config_file
+      end
+
+      def default_config_file
+        Application.config_file_defaults.each do |cf|
+          return cf if File.exist?(cf)
+        end
+        nil
       end
 
     end
--- spec/unit/stash/harvester/application_spec.rb
+++ spec/unit/stash/harvester/application_spec.rb
@@ -19,6 +19,13 @@ module Stash
         allow(Dir).to receive(:home).and_call_original
       end
 
+      describe '#config_file_defaults' do
+        it 'prefers ./stash-harvester.yml, then ~/.stash-harvester.yml' do
+          expected = ['/stash/apps/stash-harvester/stash-harvester.yml', '/home/stash/.stash-harvester.yml']
+          expect(Application.config_file_defaults).to eq(expected)
+        end
+      end
+
       describe '#config' do
 
         after :each do
@@ -74,56 +81,6 @@ module Stash
             expect(e.message).to include "#{@wd}/stash-harvester.yml"
             expect(e.message).to include "#{@home}/.stash-harvester.yml"
           end
-        end
-      end
-
-      describe '#ensure_config_file' do
-        after :each do
-          allow(Application).to receive(:default_config_file).and_call_original
-        end
-
-        it 'returns its argument, if non-nil' do
-          config_file = '/etc/stash-harvester.yml'
-          expect(Application.ensure_config_file(config_file)).to eq(config_file)
-        end
-
-        it 'falls back to #default_config_file, if passed nil' do
-          default_config_file = '/etc/stash-harvester.yml'
-          expect(Application).to receive(:default_config_file) { default_config_file }
-          expect(Application.ensure_config_file(nil)).to eq(default_config_file)
-        end
-
-        it 'raises ArgumentError if passed nil and no default found' do
-          expect(Application).to receive(:default_config_file) { nil }
-          expect { Application.ensure_config_file(nil) }.to raise_error do |e|
-            expect(e).to be_an ArgumentError
-            expect(e.message).to include "#{@wd}/stash-harvester.yml"
-            expect(e.message).to include "#{@home}/.stash-harvester.yml"
-          end
-        end
-      end
-
-      describe '#default_config_file' do
-        it 'prefers plain file in current working directory' do
-          Dir.mktmpdir do |dir|
-            @wd = dir
-            expected = "#{dir}/stash-harvester.yml"
-            FileUtils.touch(expected)
-            expect(Application.default_config_file).to eq(expected)
-          end
-        end
-
-        it "falls back to dotfile in the user's home directory, if present" do
-          Dir.mktmpdir do |dir|
-            @home = dir
-            expected = "#{dir}/.stash-harvester.yml"
-            FileUtils.touch(expected)
-            expect(Application.default_config_file).to eq(expected)
-          end
-        end
-
-        it 'returns nil if neither default file is present' do
-          expect(Application.default_config_file).to be_nil
         end
       end
 
