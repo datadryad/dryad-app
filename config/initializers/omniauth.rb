24e04e889cf9f6de542ccdcb77b08c2a35c603b9
sfisher <scott.fisher@ucop.edu>
2015-10-14 14:31:50 -0700

    Debugging shibboleth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,13 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shibboleth,
     {
         :request_type   => :header,
-        host            => 'dash2-dev.cdlib.org',
-        callback_path   => '/stash/auth/:provider/callback',
-        uid_field       => 'eppn',
-        debug           =>  true,
-        info_fields => {
-            email               => 'mail',
-            identity_provider   => 'shib_identity_provider'
+        :host            => 'dash2-dev.cdlib.org',
+        :callback_path   => '/stash/auth/:provider/callback',
+        :uid_field       => 'eppn',
+        :debug           =>  true,
+        :info_fields => {
+            :email               => 'mail',
+            :identity_provider   => 'shib_identity_provider'
         }
     }
 end
