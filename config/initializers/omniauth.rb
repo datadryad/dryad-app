41ffdb0a9cdf4343fa30366d40b5700af3a9e971
sfisher <scott.fisher@ucop.edu>
2015-10-14 17:31:16 -0700

    Reverting changes to config for omniauth engine.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,14 @@
-#Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :shibboleth,
-#    {
-#        :request_type   => :header,
-#        :host            => 'dash2-dev.ucop.edu',
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :shibboleth,
+    {
+        :request_type   => :header,
+        :host            => 'dash2-dev.ucop.edu',
         #:callback_path   => '/stash/auth/:provider/callback',
-#        :uid_field       => 'eppn',
+        :uid_field       => 'eppn',
         #:debug           =>  true,
-#        :info_fields => {
-#            :email               => 'mail',
-#            :identity_provider   => 'shib_identity_provider'
-#        }
-#    }
-#end
+        :info_fields => {
+            :email               => 'mail',
+            :identity_provider   => 'shib_identity_provider'
+        }
+    }
+end
