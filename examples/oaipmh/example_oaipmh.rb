19d87b04a51a36a5c5623f76c3210f48bf526234
David Moles <david.moles@ucop.edu>
2015-10-20 16:27:00 -0700

    Changed commented-out version to point to Merritt dev

--- examples/oaipmh/example_oaipmh.rb
+++ examples/oaipmh/example_oaipmh.rb
@@ -9,14 +9,15 @@ config = Stash::Harvester::SourceConfig.from_yaml(
 # Alternatively:
 #
 # config = Stash::Harvester::OAI::OAISourceConfig.new(
-#   oai_base_url: 'http://oai.datacite.org/oai',
-#   metadata_prefix: 'oai_datacite',
-#   set: 'REFQUALITY'
+#   oai_base_url: 'http://uc3-mrtoai-dev.cdlib.org:37001/mrtoai/oai/v2',
+#   metadata_prefix: 'stash_wrapper',
+#   set: 'dash_cdl',
+#   seconds_granularity: true
 # )
 
 task = config.create_harvest_task(
-  from_time: Time.utc(2013, 6, 1),
-  until_time: Time.utc(2013, 6, 30)
+  from_time: Date.new(2015, 9, 30),
+  until_time: Date.new(2015, 10, 31)
 )
 
 response = task.harvest_records
