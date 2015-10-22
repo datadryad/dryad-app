bcc2f246a6329d9c6f2b315c0d487c31f99b499a
sfisher <scott.fisher@ucop.edu>
2015-10-22 13:20:49 -0700

    Troubleshooting omniauth path.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,14 @@
+application.config.middleware.use OmniAuth::Builder do
+  provider :shibboleth,
+           {
+               :request_type   => :header,
+               :host            => 'dash2-dev.ucop.edu',
+               #:callback_path   => '/auth/:provider/callback',
+               :uid_field       => 'eppn',
+               #:debug           =>  true,
+               :info_fields => {
+                   :email               => 'mail',
+                   :identity_provider   => 'shib_identity_provider'
+               }
+           }
+end
