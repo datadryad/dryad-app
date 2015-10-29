78589e5b74857d8871393c75bebe2b75af05751e
sfisher <scott.fisher@ucop.edu>
2015-10-29 15:39:04 -0700

    Trying to fix csrf problems on development machine.

--- app/controllers/stash_engine/application_controller.rb
+++ app/controllers/stash_engine/application_controller.rb
@@ -1,5 +1,7 @@
 class StashEngine::ApplicationController < ApplicationController
 
+  protect_from_forgery with: :null_session
+
   def stash_datacite
     StashDatacite::Engine.routes.url_helpers
   end
