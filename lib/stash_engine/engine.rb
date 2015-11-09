94c2901e20f51f48a2f5ae90a40d23cdacf49d07
sfisher <scott.fisher@ucop.edu>
2015-11-09 10:09:14 -0800

    Streamline tenant setting in StashEngine.

--- lib/stash_engine/engine.rb
+++ lib/stash_engine/engine.rb
@@ -6,14 +6,16 @@ module StashEngine
   # see http://stackoverflow.com/questions/20734766/rails-mountable-engine-how-should-apps-set-configuration-variables
 
   class << self
-    mattr_accessor :tenants, :app
-    self.tenants = {:key => 'value'} #this would be the default value if not set
+    mattr_accessor :app
+
+    def tenants=(t)
+      StashEngine::Tenant.tenants = t
+    end
   end
 
   # this function maps the vars from your app into your engine
   def self.setup(&block)
     yield self
-    Tenant.tenants = StashEngine.tenants
   end
 
 
