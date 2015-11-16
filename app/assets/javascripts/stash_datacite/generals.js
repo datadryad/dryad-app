c208312a8f042b08e80be5a819c68cb8623dd318
bhavi.vedula <bhavi13may@gmail.com>
2015-11-16 06:34:23 -0800

    Add jquery and jquery UI

--- app/assets/javascripts/stash_datacite/generals.js
+++ app/assets/javascripts/stash_datacite/generals.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+  $(function() {
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      heightStyle: "content",
+      collapsible: true,
+      icons: icons
+    });
+  });
