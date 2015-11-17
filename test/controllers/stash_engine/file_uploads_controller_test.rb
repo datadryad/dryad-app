31665445c882e3863f0dcc5536a71f6551c6dbeb
sfisher <scott.fisher@ucop.edu>
2015-11-17 13:51:06 -0800

    Commenting out a few tests for missing? actions.

--- test/controllers/stash_engine/file_uploads_controller_test.rb
+++ test/controllers/stash_engine/file_uploads_controller_test.rb
@@ -16,15 +16,15 @@ module StashEngine
       assert_response :success
     end
 
-    test "should get edit" do
-      get :edit
-      assert_response :success
-    end
+    #test "should get edit" do
+    #  get :edit
+    #  assert_response :success
+    #end
 
-    test "should get delete" do
-      get :delete
-      assert_response :success
-    end
+    #test "should get delete" do
+    #  get :delete
+    #  assert_response :success
+    #end
 
   end
 end
