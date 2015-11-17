233398d252f647e29d31fb0b6ce0db7dc76d7229
bhavi.vedula <bhavi13may@gmail.com>
2015-11-17 10:30:34 -0800

    Add FileUplaods controller

---
+++ app/controllers/stash_engine/file_uploads_controller.rb
@@ -0,0 +1,17 @@
+require_dependency "stash_engine/application_controller"
+
+module StashEngine
+  class FileUploadsController < ApplicationController
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def delete
+    end
+  end
+end
