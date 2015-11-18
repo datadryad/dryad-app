62bdf869a59f896ec6632a215bc7f218a359c5b9
Scott Fisher <scott.fisher@ucop.edu>
2015-11-18 11:45:49 -0800

    Adding configs for travis.ci and simplecov.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
+require 'simplecov'
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
+
+SimpleCov.start 'rails'
 
 require File.expand_path('../../test/dummy/config/environment.rb', __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
