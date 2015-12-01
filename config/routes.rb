d51d92873a8dfbf7bc1d68d716bd9d6118c9051c
bhavi.vedula <bhavi13may@gmail.com>
2015-12-01 03:51:59 -0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ StashDatacite::Engine.routes.draw do
 
   resources :titles, except: [:index]
   resources :creators, except: [:index]
+  resources :descriptions, except: [:index]
+  resources :resource_types, except: [:index]
 end
