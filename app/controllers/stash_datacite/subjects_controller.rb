363b02d47cae3942907ade0a2ae7ed89dc19c483
bhavi.vedula <bhavi13may@gmail.com>
2015-12-07 06:07:26 -0800

    Replicate ajax functionality for Subjects

--- app/controllers/stash_datacite/subjects_controller.rb
+++ app/controllers/stash_datacite/subjects_controller.rb
@@ -2,43 +2,33 @@ require_dependency 'stash_datacite/application_controller'
 
 module StashDatacite
   class SubjectsController < ApplicationController
-    before_action :set_subject, only: [:show, :edit, :update, :destroy]
-
-    # GET /subjects
-    def index
-      @subjects = Subject.all
-    end
-
-    # GET /subjects/1
-    def show
-    end
+    before_action :set_subject, only: [:update, :destroy]
 
     # GET /subjects/new
     def new
       @subject = Subject.new
     end
 
-    # GET /subjects/1/edit
-    def edit
-    end
-
     # POST /subjects
     def create
       @subject = Subject.new(subject_params)
-
-      if @subject.save
-        redirect_to @subject, notice: 'Subject was successfully created.'
-      else
-        render :new
+      respond_to do |format|
+        if @subject.save
+          format.js
+        else
+          format.html { render :new }
+        end
       end
     end
 
     # PATCH/PUT /subjects/1
     def update
-      if @subject.update(subject_params)
-        redirect_to @subject, notice: 'Subject was successfully updated.'
-      else
-        render :edit
+      respond_to do |format|
+        if @subject.update(subject_params)
+          format.js
+        else
+          format.html { render :edit }
+        end
       end
     end
 
@@ -52,12 +42,12 @@ module StashDatacite
 
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
-      @subject = Subject.find(params[:id])
+      @subject = Subject.find(subject_params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def subject_params
-      params.require(:subject).permit(:subject, :subject_scheme, :scheme_URI, :resource_id)
+      params.require(:subject).permit(:id, :subject, :subject_scheme, :scheme_URI, :resource_id)
     end
   end
 end
