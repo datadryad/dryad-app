3538c0fc8691636c748a2bea136a08c875bef94d
bhavi.vedula <bhavi13may@gmail.com>
2015-12-07 06:27:12 -0800

    Replicate ajax functionality for Related Identifiers

--- app/controllers/stash_datacite/related_identifiers_controller.rb
+++ app/controllers/stash_datacite/related_identifiers_controller.rb
@@ -2,43 +2,33 @@ require_dependency 'stash_datacite/application_controller'
 
 module StashDatacite
   class RelatedIdentifiersController < ApplicationController
-    before_action :set_related_identifier, only: [:show, :edit, :update, :destroy]
-
-    # GET /related_identifiers
-    def index
-      @related_identifiers = RelatedIdentifier.all
-    end
-
-    # GET /related_identifiers/1
-    def show
-    end
+    before_action :set_related_identifier, only: [:update, :destroy]
 
     # GET /related_identifiers/new
     def new
       @related_identifier = RelatedIdentifier.new
     end
 
-    # GET /related_identifiers/1/edit
-    def edit
-    end
-
     # POST /related_identifiers
     def create
       @related_identifier = RelatedIdentifier.new(related_identifier_params)
-
-      if @related_identifier.save
-        redirect_to @related_identifier, notice: 'Related identifier was successfully created.'
-      else
-        render :new
+      respond_to do |format|
+        if @related_identifier.save
+          format.js
+        else
+          format.html { render :new }
+        end
       end
     end
 
     # PATCH/PUT /related_identifiers/1
     def update
-      if @related_identifier.update(related_identifier_params)
-        redirect_to @related_identifier, notice: 'Related identifier was successfully updated.'
-      else
-        render :edit
+      respond_to do |format|
+        if @related_identifier.update(related_identifier_params)
+          format.js
+        else
+          format.html { render :edit }
+        end
       end
     end
 
@@ -52,12 +42,12 @@ module StashDatacite
 
     # Use callbacks to share common setup or constraints between actions.
     def set_related_identifier
-      @related_identifier = RelatedIdentifier.find(params[:id])
+      @related_identifier = RelatedIdentifier.find(related_identifier_params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def related_identifier_params
-      params.require(:related_identifier).permit(:related_identifier, :related_identifier_type_id,
+      params.require(:related_identifier).permit(:id, :related_identifier, :related_identifier_type_id,
                                                  :relation_type_id, :resource_id)
     end
   end
