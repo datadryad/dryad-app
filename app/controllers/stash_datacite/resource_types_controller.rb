50bf55aee075dfc98d2749488c771fc77aa230e5
bhavi.vedula <bhavi13may@gmail.com>
2015-12-07 05:39:21 -0800

    Replicate ajax functionality for Resource Types

--- app/controllers/stash_datacite/resource_types_controller.rb
+++ app/controllers/stash_datacite/resource_types_controller.rb
@@ -2,43 +2,34 @@ require_dependency 'stash_datacite/application_controller'
 
 module StashDatacite
   class ResourceTypesController < ApplicationController
-    before_action :set_resource_type, only: [:show, :edit, :update, :destroy]
+    before_action :set_resource_type, only: [:update, :destroy]
 
-    # GET /resource_types
-    def index
-      @resource_types = ResourceType.all
-    end
-
-    # GET /resource_types/1
-    def show
-    end
 
     # GET /resource_types/new
     def new
       @resource_type = ResourceType.new
     end
 
-    # GET /resource_types/1/edit
-    def edit
-    end
-
     # POST /resource_types
     def create
       @resource_type = ResourceType.new(resource_type_params)
-
-      if @resource_type.save
-        redirect_to @resource_type, notice: 'Resource type was successfully created.'
-      else
-        render :new
+      respond_to do |format|
+        if @resource_type.save
+          format.js
+        else
+          format.html { render :new }
+        end
       end
     end
 
     # PATCH/PUT /resource_types/1
     def update
-      if @resource_type.update(resource_type_params)
-        redirect_to @resource_type, notice: 'Resource type was successfully updated.'
-      else
-        render :edit
+      respond_to do |format|
+          if @resource_type.update(resource_type_params)
+          format.js
+        else
+          format.html { render :edit }
+        end
       end
     end
 
@@ -52,12 +43,12 @@ module StashDatacite
 
     # Use callbacks to share common setup or constraints between actions.
     def set_resource_type
-      @resource_type = ResourceType.find(params[:id])
+      @resource_type = ResourceType.find(resource_type_params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def resource_type_params
-      params.require(:resource_type).permit(:resource_type, :resource_type_general, :resource_id)
+      params.require(:resource_type).permit(:id, :resource_type, :resource_type_general, :resource_id)
     end
   end
 end
