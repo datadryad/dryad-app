450c0a450c973d15b4cd7643d340a679d9fda976
bhavi.vedula <bhavi13may@gmail.com>
2015-12-08 08:38:32 -0800

    Rubocop fixes

--- app/controllers/stash_engine/metadata_entry_pages_controller.rb
+++ app/controllers/stash_engine/metadata_entry_pages_controller.rb
@@ -4,30 +4,48 @@ module StashEngine
   class MetadataEntryPagesController < ApplicationController
     # GET/POST/PUT  /generals/find_or_create
     def find_or_create
-      @resource = Resource.find(params[:resource_id].to_i) unless params[:resource_id].blank?
-      @relation_types = StashDatacite::RelationType.all
-      @related_identifier_types = StashDatacite::RelatedIdentifierType.all
+      find_resource
       set_instances
+      set_title
+      set_description
+      set_geolocations
+      set_relations
     end
 
     private
 
-    def generals_params
-      params.require(:general).permit(:resource_id)
+    def find_resource
+      @resource = Resource.find(params[:resource_id].to_i) unless params[:resource_id].blank?
     end
 
     def set_resources
       @resources = StashDatacite.resource_class.constantize.all
     end
 
+    def set_title
+      @title = StashDatacite::Title.where(resource_id: @resource.id).first_or_initialize
+    end
+
     def set_instances
       @creator = StashDatacite::Creator.where(resource_id: @resource.id).first_or_initialize
-      @title = StashDatacite::Title.where(resource_id: @resource.id).first_or_initialize
-      @description = StashDatacite::Description.where(resource_id: @resource.id).first_or_initialize
       @contributor = StashDatacite::Contributor.where(resource_id: @resource.id).first_or_initialize
       @subject = StashDatacite::Subject.where(resource_id: @resource.id).first_or_initialize
       @resource_type = StashDatacite::ResourceType.where(resource_id: @resource.id).first_or_initialize
+    end
+
+    def set_description
+      @abstract = StashDatacite::Description.type_abstract.where(resource_id: @resource.id).first_or_initialize
+      @methods = StashDatacite::Description.type_methods.where(resource_id: @resource.id).first_or_initialize
+      @usage_notes = StashDatacite::Description.type_usage_notes.where(resource_id: @resource.id).first_or_initialize
+    end
+
+    def set_relations
       @related_identifier = StashDatacite::RelatedIdentifier.where(resource_id: @resource.id).first_or_initialize
+      @relation_types = StashDatacite::RelationType.all
+      @related_identifier_types = StashDatacite::RelatedIdentifierType.all
+    end
+
+    def set_geolocations
       @geolocation_point = StashDatacite::GeolocationPoint.where(resource_id: @resource.id).first_or_initialize
       @geolocation_box = StashDatacite::GeolocationBox.where(resource_id: @resource.id).first_or_initialize
       @geolocation_place = StashDatacite::GeolocationPlace.where(resource_id: @resource.id).first_or_initialize
