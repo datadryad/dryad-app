cd3d4d3bfb786b615ca453bceb9a9239624122a3
bhavi.vedula <bhavi13may@gmail.com>
2015-12-08 16:04:50 -0800

    Fix update view render

--- app/assets/javascripts/stash_datacite/titles.js
+++ app/assets/javascripts/stash_datacite/titles.js
@@ -1,9 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  $( "#title" ).on('blur', function(e){
-    alert("hello");
-    var form =  $("#title").parents('form');
+  $( ".title" ).on('blur', function(e){
+    var form =  $(this).parents('form');
     $(form).trigger('submit.rails');
   });
 });
