c3abdbf239e7393f80e3c8b2653131fb5960b0bf
bhavi.vedula <bhavi13may@gmail.com>
2015-12-10 13:10:43 -0800

    Add Title type enum support

--- app/models/stash_datacite/title.rb
+++ app/models/stash_datacite/title.rb
@@ -2,5 +2,8 @@ module StashDatacite
   class Title < ActiveRecord::Base
     self.table_name = 'dcs_titles'
     belongs_to :resource, class_name: StashDatacite.resource_class.to_s
+
+    enum title_type: { main: 0, subtitle: 1 }
+
   end
 end
