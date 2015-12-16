1fe6d86b28af2dde0896f75409e44248eb43b1dd
Scott Fisher <scott.fisher@ucop.edu>
2015-12-16 13:30:35 -0800

    Changes to get strange failing namespacing and gems working.

--- lib/stash_datacite.rb
+++ lib/stash_datacite.rb
@@ -1,9 +1,9 @@
 # http://stackoverflow.com/questions/5159607/rails-engine-gems-dependencies-how-to-load-them-into-the-application
 # requires all dependencies
 
-Gem.loaded_specs['stash_datacite'].dependencies.each do |d|
-  require d.name
-end
+#Gem.loaded_specs['stash_datacite'].dependencies.each do |d|
+#  require d.name
+#end
 
 require 'stash_datacite/engine'
 
--- lib/stash_datacite/engine.rb
+++ lib/stash_datacite/engine.rb
@@ -1,3 +1,9 @@
+require "mysql2"
+require 'jquery-rails'
+require 'jquery-ui-rails'
+require 'responders'
+require 'leaflet-rails'
+require 'datacite/mapping'
 module StashDatacite
   class Engine < ::Rails::Engine
     isolate_namespace StashDatacite
