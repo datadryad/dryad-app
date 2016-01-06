39f9f2202009673dc6a52e052157fc1821419f09
David Moles <david.moles@ucop.edu>
2016-01-06 15:22:43 -0800

    Started work on new metadata mappers

---
+++ lib/stash/harvester/metadata_mapper.rb
@@ -0,0 +1,24 @@
+require 'config/factory'
+
+module Stash
+  module Harvester
+
+    # Superclass of installation-specific mappers converting wrapped metadata to
+    # indexable documents.
+    class MetadataMapper
+      include ::Config::Factory
+
+      key :metadata_mapping
+
+      # Converts a Stash-wrapped metadata document to an indexable document.
+      #
+      # @param _wrapped_metadata [StashWrapper] a Stash-wrapped metadata document with
+      #   appropriate descriptive elements for this mapper.
+      # @return [Object] a document extracting information from `wrapped_metadata`
+      #   and formatting it appropriately for the index supported by this mapper.
+      def to_index_document(_wrapped_metadata)
+        fail NoMethodError, "#{self.class} should override #to_index_document to map wrapped metadata to indexable documents, but it doesn't"
+      end
+    end
+  end
+end
