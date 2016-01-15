42b5bf492985993a5a500a5fa41f759b91c25cde
David Moles <david.moles@ucop.edu>
2016-01-15 13:05:29 -0800

    First attempt at semi-realistic Solr mapping

---
+++ lib/stash/harvester/datacite_default/datacite_extensions.rb
@@ -0,0 +1,69 @@
+require 'datacite/mapping'
+require 'time'
+
+module Datacite
+  module Mapping
+
+    class Description
+      def funding?
+        # TODO: Make 'data were created with' etc. a constant or something and move it to Datacite::Mapping
+        type == DescriptionType::OTHER && value.start_with?('Data were created with funding')
+      end
+
+      def usage?
+        type == DescriptionType::OTHER && !funding?
+      end
+    end
+
+    class Resource
+
+      def creator_names
+        creators.map(&:name)
+      end
+
+      def creator_affiliations
+        creators.map(&:affiliations)
+      end
+
+      def type
+        resource_type.resource_type_general.value if resource_type
+      end
+
+      def usage_notes
+        usage = descriptions.find(&:usage?)
+        usage.value if value
+      end
+
+      def description_text_for(type:)
+        desc = descriptions.find { |d| d.type = type }
+        desc.value if value
+      end
+
+      def geo_location_places
+        geo_locations.map(&:place).compact
+      end
+
+      def geo_location_boxes
+        geo_locations.map(&:box).compact
+      end
+
+      def geo_location_points
+        geo_locations.map(&:point).compact
+      end
+
+      def embargo_type
+        embargo.type.value if embargo
+      end
+
+      def embargo_end_date
+        return nil unless embargo
+        d = embargo.end_date
+        Time.utc(d.year, d.month, d.day).xmlschema
+      end
+
+      def self.datacite?(elem)
+        elem.name == 'resource' && elem.namespace == 'http://datacite.org/schema/kernel-3'
+      end
+    end
+  end
+end
--- lib/stash/harvester/datacite_default/mapper.rb
+++ lib/stash/harvester/datacite_default/mapper.rb
@@ -1,25 +1,41 @@
 require 'stash/wrapper'
 require 'datacite/mapping'
+require_relative 'datacite_extensions'
 require_relative '../metadata_mapper'
 
 module Stash
   module Harvester
     module DataciteDefault
+      include Datacite::Mapping
+
       class Mapper < MetadataMapper
         metadata_mapping 'datacite_default'
 
-        def to_index_document(wrapped_metadata)
-          stash_descriptive = wrapped_metadata.stash_descriptive
-          datacite_xml = stash_descriptive.find { |elem| datacite?(elem) }
-          resource = Datacite::Mapping::Resource.parse_xml(datacite_xml)
-          resource.write_xml
-        end
+        def to_index_document(wrapper) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+          stash_descriptive = wrapper.stash_descriptive
+          datacite_xml = stash_descriptive.find { |elem| Resource.datacite?(elem) }
+          resource = Resource.parse_xml(datacite_xml)
 
-        private
+          # TODO: write up a real crosswalk & make sure we have all fields
+          {
+            id_s:                   wrapper.id_value,
+            dc_title_s:             resource.default_title,
+            dc_creator_sm:          resource.creator_names,
+            creator_affiliation_sm: resource.creator_affiliations.map { |a| a.join(', ') },
+            dc_type_s:              resource.type,
+            dc_description_s:       resource.description_text_for(DescriptionType::ABSTRACT),
+            methods_s:              resource.description_text_for(DescriptionType::METHODS),
+            usage_notes_s:          resource.usage_notes,
+            dct_place_sm:           resource.geo_location_places,
+            georss_box_bboxm:       resource.geo_location_boxes,
+            georss_point_ptm:       resource.geo_location_points,
+            embargo_type:           wrapper.embargo_type,
+            embargo_end_date:       wrapper.embargo_end_date,
+            dc_publisher:           resource.publisher,
+            pub_year:               resource.publication_year
+          }
 
-        # TODO: move this to Datacite::Mapping?
-        def datacite?(elem)
-          elem.name == 'resource' && elem.namespace == 'http://datacite.org/schema/kernel-3'
+          # TODO: do we need to strip nil values or does RSolr take care of that?
         end
 
       end
