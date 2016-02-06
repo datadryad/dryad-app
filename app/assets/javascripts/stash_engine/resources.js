b213f9f3d12f85f08db3043c295c0dbd954614f4
Scott Fisher <scott.fisher@ucop.edu>
2016-02-05 17:40:30 -0800

    Allowing delayed upload and also deleting of files in upload.

--- app/assets/javascripts/stash_engine/resources.js
+++ app/assets/javascripts/stash_engine/resources.js
@@ -1,23 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-/*
-jQuery(function() {
-    return $('#fileupload').fileupload({
-        dataType: "script",
-        add: function(e, data) {
-            return data.submit();
-        },
-        progress: function(e, data) {
-            var progress;
-            if (data.context) {
-                progress = parseInt(data.loaded / data.total * 100, 10);
-                return data.context.find('.bar').css('width', progress + '%');
-            }
-        }
-    });
-});*/
-
 $(function () {
     $('#fileupload').fileupload({
         dataType: 'script',
@@ -25,7 +8,7 @@ $(function () {
             console.log(data.files[0]);
             data.files[0]['id'] = generateQuickId();
             data.context = $(tmpl("upload-line", data.files[0]));
-            $('#upload_table').append(data.context);
+            $('#upload_list').append(data.context);
             $('#up_button_' + data.files[0].id ).click(function () {
                     data.submit();
             });
--- app/controllers/stash_engine/file_uploads_controller.rb
+++ app/controllers/stash_engine/file_uploads_controller.rb
@@ -2,6 +2,13 @@ require_dependency 'stash_engine/application_controller'
 
 module StashEngine
   class FileUploadsController < ApplicationController
+
+    before_action :require_login
+
+    before_action :set_file, only: [:edit, :update, :destroy]
+
+    before_action :require_file_owner, except: [:index, :new, :create]
+
     def index
     end
 
@@ -14,16 +21,44 @@ module StashEngine
     def delete
     end
 
+    def destroy
+      respond_to do |format|
+        format.js {
+          File.delete(@file.temp_file_path) if File.exist?(@file.temp_file_path)
+          @file_id = @file.id
+          @file.destroy
+        }
+      end
+
+    end
+
     def create
-      fu = params[:upload][:upload]
-      fn = FileUpload.new
-      fn.upload_file_name = fu.original_filename
-      fn.temp_file_path = fu.tempfile.path
-      fn.upload_content_type = fu.content_type
-      fn.upload_file_size = File.size(fu.tempfile.path)
-      fn.resource_id = params[:file_upload][:resource_id]
-      fn.upload_updated_at = Time.new
-      fn.save
+      respond_to do |format|
+        format.js {
+          fu = params[:upload][:upload]
+          fn = FileUpload.new
+          fn.upload_file_name = fu.original_filename
+          fn.temp_file_path = fu.tempfile.path
+          fn.upload_content_type = fu.content_type
+          fn.upload_file_size = File.size(fu.tempfile.path)
+          fn.resource_id = params[:file_upload][:resource_id]
+          fn.upload_updated_at = Time.new
+          fn.save
+        }
+      end
+    end
+
+    private
+
+    def require_file_owner
+      if current_user.id != @file.resource.user_id
+        redirect_to tenants_path
+        return false
+      end
+    end
+
+    def set_file
+      @file = FileUpload.find(params[:id])
     end
   end
 end
