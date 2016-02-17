078d2a906b16d1b3c655090d8ba389524a015ce1
bhavi.vedula <bhavi13may@gmail.com>
2016-02-17 11:10:44 -0800

    udpate title to db only on vlaue change

--- app/assets/javascripts/stash_datacite/titles.js
+++ app/assets/javascripts/stash_datacite/titles.js
@@ -1,13 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-  $("div.success").hide();
-});
-
 $(document).ready(function(){
-  $( ".title" ).on('blur', function(){
-    var form =  $(this).parents('form');
-    $(form).trigger('submit.rails');
-    $( "div.success" ).show().delay( 1000 ).fadeOut( 400 );
-  });
+  $( ".title" ).on('focus', function () {
+    previous_value = this.value;
+    }).change(function() {
+      new_value = this.value;
+      // Save when the new value is different from the previous value
+      if(new_value != previous_value) {
+        var form = $(this).parents('form');
+        $(form).trigger('submit.rails');
+      }
+    });
 });
