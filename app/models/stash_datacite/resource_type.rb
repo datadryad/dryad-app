26dccc2ecaeeabce3a9b945f91a2297ab44d1802
bhavi.vedula <bhavi13may@gmail.com>
2016-02-17 11:11:21 -0800

    set resource_type enum hash

--- app/models/stash_datacite/resource_type.rb
+++ app/models/stash_datacite/resource_type.rb
@@ -2,5 +2,8 @@ module StashDatacite
   class ResourceType < ActiveRecord::Base
     self.table_name = 'dcs_resource_types'
     belongs_to :resource, class_name: StashDatacite.resource_class.to_s
+
+    enum resource_type: { Spreadsheet: 'spreadsheet', Image: 'image', Sound: 'sound', Video: 'audiovisual',
+                                    Text: 'text', Software: 'software', MultipleTypes: 'multiple_types', Other: 'other' }
   end
 end
