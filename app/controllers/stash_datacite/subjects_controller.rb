e06096a803f818e6b6cd18811d201bd8ff7b6322
bhavi.vedula <bhavi13may@gmail.com>
2016-02-18 12:09:07 -0800

    autocomplete for keywords(subjects)

--- app/controllers/stash_datacite/subjects_controller.rb
+++ app/controllers/stash_datacite/subjects_controller.rb
@@ -22,20 +22,26 @@ module StashDatacite
     end
 
     # PATCH/PUT /subjects/1
-    def update
-      respond_to do |format|
-        if @subject.update(subject_params)
-          format.js { render template: 'stash_datacite/shared/update.js.erb' }
-        else
-          format.html { render :edit }
-        end
-      end
-    end
+    # def update
+    #   respond_to do |format|
+    #     if @subject.update(subject_params)
+    #       format.js { render template: 'stash_datacite/shared/update.js.erb' }
+    #     else
+    #       format.html { render :edit }
+    #     end
+    #   end
+    # end
 
     # DELETE /subjects/1
     def destroy
       @subject.destroy
       redirect_to subjects_url, notice: 'Subject was successfully destroyed.'
+    end
+
+    # GET /subjects
+    def autocomplete
+      @subjects = Subject.order(:subject).where("subject LIKE ?", "%#{params[:term]}%") unless params[:term].blank?
+      render json: @subjects.map(&:subject)
     end
 
     private
