58aa210286ba1c9e2fdbbe30b4624aa3908b1257
David Moles <david.moles@ucop.edu>
2016-02-19 15:32:38 -0800

    Add explicit require for sass-rails

--- lib/stash_discovery/engine.rb
+++ lib/stash_discovery/engine.rb
@@ -1,6 +1,9 @@
 require 'blacklight'
 require 'geoblacklight'
 
+# For undocumented reasons, sass-rails won't load without an explicit require
+require 'sass-rails'
+
 module StashDiscovery
   class Engine < ::Rails::Engine
   end
