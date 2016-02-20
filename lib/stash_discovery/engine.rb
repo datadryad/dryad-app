08b00ff2401d669643b827f60420e39d9c43f65b
David Moles <david.moles@ucop.edu>
2016-02-19 16:21:30 -0800

    Got migrations working

--- lib/stash_discovery/engine.rb
+++ lib/stash_discovery/engine.rb
@@ -6,5 +6,14 @@ require 'sass-rails'
 
 module StashDiscovery
   class Engine < ::Rails::Engine
+    # :nocov:
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
+    # :nocov:
   end
 end
