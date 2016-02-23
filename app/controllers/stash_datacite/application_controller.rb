ec6cbadf793aad8c0b2e6dd5d0fbd23135c757c8
Scott Fisher <scott.fisher@ucop.edu>
2016-02-23 14:47:35 -0800

    Table displaying for in progress datasets (no paging yet).

--- app/controllers/stash_datacite/application_controller.rb
+++ app/controllers/stash_datacite/application_controller.rb
@@ -1,4 +1,10 @@
 module StashDatacite
   class ApplicationController < ::ApplicationController
+    helper_method :stash_url_helpers
+
+    def stash_url_helpers
+      StashEngine::Engine.routes.url_helpers
+    end
+
   end
 end
