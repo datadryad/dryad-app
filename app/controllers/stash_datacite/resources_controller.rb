4805b6c40c34adf4ec440867d276ef4e9ec86c30
Scott Fisher <scott.fisher@ucop.edu>
2016-02-23 14:49:51 -0800

    files added with generator

---
+++ app/controllers/stash_datacite/resources_controller.rb
@@ -0,0 +1,21 @@
+require_dependency "stash_datacite/application_controller"
+
+module StashDatacite
+  # this is a class for composite (AJAX/UJS?) views starting at the resource or resources
+  class ResourcesController < ApplicationController
+
+    # get resources and composite information for in-progress table view
+    def user_in_progress
+
+      #should require current user
+
+      #respond_to do |format|
+      #  format.js{
+          @resources = StashDatacite.resource_class.where(user_id: session[:user_id])
+      #  }
+      #end
+      @in_progress_lines = @resources.map{|resource| DatasetPresenter.new(resource)}
+    end
+
+  end
+end
