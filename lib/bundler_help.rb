e7d13f3f49cfb55eac0861f07384be91437cd37d
David Moles <david.moles@ucop.edu>
2016-02-24 10:03:39 -0800

    Allow other engines to be checked out in parallel with stash_engine as well as in subdirectories

--- lib/bundler_help.rb
+++ lib/bundler_help.rb
@@ -4,7 +4,7 @@ module BundlerHelp
     paths = [File.expand_path('.'), File.expand_path('..')]
     paths.each do |path|
       path_arr = path.split(File::SEPARATOR).map {|x| x=='' ? File::SEPARATOR : x}
-      while path_arr.length > 0 do
+      until path_arr.empty? do
         test = File.join(path_arr, dir_name)
         return test if Dir.exists?(test)
         path_arr.pop
