da5ad251ff1d61b014674680c3a7874337ad94f7
bhavi.vedula <bhavi13may@gmail.com>
2016-02-23 17:38:00 -0800

    display resources on dashboard based on user logged in

--- app/controllers/stash_engine/dashboard_controller.rb
+++ app/controllers/stash_engine/dashboard_controller.rb
@@ -5,9 +5,8 @@ module StashEngine
     before_action :require_login
 
     def show
-      #@resources = Resource.where(user_id: current_user.id)
-      #@titles = metadata_engine::Title.where(resource_id: @resources.pluck(:id))
-      #byebug
+      # @resources = Resource.where(user_id: current_user.id)
+      # @titles = metadata_engine::Title.where(resource_id: @resources.pluck(:id))
     end
   end
 end
