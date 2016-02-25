cb87d25e77e46ae7a3eac635028e98b407b71f27
Scott Fisher <scott.fisher@ucop.edu>
2016-02-25 12:34:01 -0800

    current_user and current_tenant methods.  Couldn't seem to easily deligate these functions across applications.

--- app/controllers/stash_datacite/application_controller.rb
+++ app/controllers/stash_datacite/application_controller.rb
@@ -2,10 +2,23 @@ module StashDatacite
   class ApplicationController < ::ApplicationController
     helper_method :stash_url_helpers, :current_tenant, :current_user
 
-    delegate :current_tenant, :current_user, to: 'StashEngine::ApplicationController'
-
     def stash_url_helpers
       StashEngine::Engine.routes.url_helpers
+    end
+
+    # get the current tenant for customizations, also deals with login
+    def current_tenant
+      if current_user
+        StashEngine::Tenant.find(current_user.tenant_id)
+      elsif session[:test_domain]
+        StashEngine::Tenant.by_domain(session[:test_domain])
+      else
+        StashEngine::Tenant.by_domain(request.host)
+      end
+    end
+
+    def current_user
+      @current_user ||= StashEngine::User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
   end
