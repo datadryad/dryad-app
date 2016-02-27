891dd1599501563c3727d3cf66b542b3e1e5b038
Scott Fisher <scott.fisher@ucop.edu>
2016-02-26 16:15:22 -0800

    Adding associations for resource to items in stash_datacite

--- app/models/stash_datacite/subject.rb
+++ app/models/stash_datacite/subject.rb
@@ -1,6 +1,6 @@
 module StashDatacite
   class Subject < ActiveRecord::Base
     self.table_name = 'dcs_subjects'
-    has_and_belongs_to_many :resources, class_name: StashDatacite.resource_class.to_s
+    has_and_belongs_to_many :resources, class_name: StashDatacite.resource_class.to_s, through: 'StashDatacite::ResourceSubject'
   end
 end
--- lib/stash_datacite/resource_patch.rb
+++ lib/stash_datacite/resource_patch.rb
@@ -13,7 +13,24 @@ module StashDatacite
     # an engine.
     def self.associate_with_resource(resource)
       resource.instance_eval do
+        # has_many :affliations, class_name: 'StashDatacite::Affliation'
+        has_many :contributors, class_name: 'StashDatacite::Contributor'
+        has_many :creators, class_name: 'StashDatacite::Creator'
+        has_many :dates, class_name: 'StashDatacite::Date'
+        has_many :descriptions, class_name: 'StashDatacite::Description'
+        has_many :embargoes, class_name: 'StashDatacite::Embargo'
+        has_many :geolocation_boxes, class_name: 'StashDatacite::GeolocationBox'
+        has_many :geolocation_places, class_name: 'StashDatacite::GeolocationPlace'
+        has_many :geolocation_points, class_name: 'StashDatacite::GeolocationPoint'
+        has_many :publication_years, class_name: 'StashDatacite::PublicationYear'
+        has_many :publishers, class_name: 'StashDatacite::Publisher'
+        has_many :related_identifiers, class_name: 'StashDatacite::RelatedIdentifier'
+        has_many :resource_types, class_name: 'StashDatacite::ResourceType'
+        has_many :rights, class_name: 'StashDatacite::Right'
+        has_many :sizes, class_name: 'StashDatacite::Size'
+        has_and_belongs_to_many :subjects, class_name: 'StashDatacite::Subject', through: 'StashDatacite::ResourceSubject'
         has_many :titles, class_name: 'StashDatacite::Title'
+        has_many :versions, class_name: 'StashDatacite::Version'
       end
     end
   end
