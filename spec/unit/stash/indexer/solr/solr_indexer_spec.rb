a2f285712d3e2c40f9204aadb7b713ba6d9b7a50
David Moles <david.moles@ucop.edu>
2016-03-01 11:49:40 -0800

    Started testing indexer

--- spec/unit/stash/indexer/solr/solr_indexer_spec.rb
+++ spec/unit/stash/indexer/solr/solr_indexer_spec.rb
@@ -8,11 +8,50 @@ module Stash
           config = instance_double(SolrIndexConfig)
           expect(SolrIndexer.new(config: config)).to be_an(Indexer)
         end
+
         describe '#index' do
-          it 'uses the correct URL'
-          it 'uses the correct proxy URL'
-          it 'passes other options to the Solr client'
-          it 'indexes records'
+          before(:each) do
+            @url = 'http://example.org/'
+            @proxy = 'http://proxy.example.org'
+            @elvis = 'presley'
+            @config = SolrIndexConfig.new(
+              url: @url,
+              proxy: @proxy,
+              elvis: @elvis
+            )
+            @indexer = SolrIndexer.new(config: @config)
+
+            @rsolr_client = instance_double(RSolr::Client)
+            allow(@rsolr_client).to receive(:add)
+            allow(@rsolr_client).to receive(:commit)
+
+            expect(RSolr::Client).to receive(:new) do |_connection, options|
+              @rsolr_options = options
+              @rsolr_client
+            end
+          end
+
+          it 'uses the correct URL' do
+            @indexer.index([].lazy)
+            expect(@rsolr_options[:url]).to eq(@url)
+          end
+
+          it 'uses the correct proxy URL' do
+            @indexer.index([].lazy)
+            expect(@rsolr_options[:proxy]).to eq(@proxy)
+          end
+
+          it 'passes other options to the Solr client' do
+            @indexer.index([].lazy)
+            expect(@rsolr_options[:elvis]).to eq(@elvis)
+          end
+
+          it 'indexes records' do
+            records = [{ id: '10.1000/12345' }, { id: '10.1000/67890' }]
+            expect(@rsolr_client).to receive(:add).with(records)
+            @indexer.index(records.lazy)
+          end
+
           it 'respects the configured batch size'
           it 'is lazy with regard to failures'
           it 'commits partial adds'
