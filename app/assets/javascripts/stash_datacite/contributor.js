17dc4bd2f5f15b38763e178af32a86f1182aa3cb
bhavi.vedula <bhavi13may@gmail.com>
2016-03-08 13:53:45 -0800

    fix js load for contributors

--- app/assets/javascripts/stash_datacite/contributor.js
+++ app/assets/javascripts/stash_datacite/contributor.js
@@ -1,46 +1,47 @@
 
+function loadContributors() {
   // based on example at http://jqueryui.com/autocomplete/#remote-jsonp
- function loadContributors() {
-  function split( val ) {
-    return val.split( /,\s*/ );
-  }
-  function extractLast( term ) {
-    return split( term ).pop();
-  }
-
-  $( ".funders" )
-    // don't navigate away from the field on tab when selecting an item
-    .bind( "keydown", function( event ) {
-    if ( event.keyCode === $.ui.keyCode.TAB &&
-      $( this ).autocomplete( "instance" ).menu.active ) {
-      event.preventDefault();
+  $(function() {
+    function split( val ) {
+      return val.split( /,\s*/ );
     }
-    })
-    .autocomplete({
-    source: function( request, response ) {
-      $.ajax({
-      url: "https://api.crossref.org/funders?query="+ extractLast( request.term ),
-      dataType: "json",
-      success: function( data ) {
-        var arr = jQuery.map( data.message.items, function( a ) {
-        return [[ a.id, a.name, a.uri ]];
-        });
-        console.log(arr);
-        var labels = [];
-        $.each(arr, function(index, value) {
-        labels.push(value[1]);
-        });
-        response(labels);
-      }
-      });
-    },
-    minLength: 1,
-    focus: function() {
-      // prevent value inserted on focus
-      return false;
-    },
-    });
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
 
+    $( ".funders" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        source: function( request, response ) {
+          $.ajax({
+            url: "https://api.crossref.org/funders?query="+ extractLast( request.term ),
+            dataType: "json",
+            success: function( data ) {
+                var arr = jQuery.map( data.message.items, function( a ) {
+                return [[ a.id, a.name, a.uri ]];
+              });
+              console.log(arr);
+              var labels = [];
+              $.each(arr, function(index, value) {
+                labels.push(value[1]);
+              });
+              response(labels);
+            }
+          });
+        },
+        minLength: 1,
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+      });
+    });
 
     $( ".funders" ).on('focus', function () {
       previous_value = this.value;
@@ -48,15 +49,21 @@
         new_value = this.value;
         // Save when the new value is different from the previous value
         if(new_value != previous_value) {
-          var form = $(this).parents('form');
+          var form = $(this.form);
+          alert(form);
           $(form).trigger('submit.rails');
         }
-      });
-
-  // Remove link functionality
-    $('.remove').on('click', function (event) {
-      $(this).closest('form').remove();
-      $(this).closest('br').remove();
-      event.preventDefault(); // Prevent link from following its href
     });
- };
+
+    $( ".award_number" ).on('focus', function () {
+      previous_value = this.value;
+      }).change(function() {
+        new_value = this.value;
+        // Save when the new value is different from the previous value
+        if(new_value != previous_value) {
+          var form = $(this.form);
+          $(form).trigger('submit.rails');
+        }
+    });
+
+};
