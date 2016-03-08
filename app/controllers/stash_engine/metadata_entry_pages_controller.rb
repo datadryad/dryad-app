1a7851d85e087454473b55da040f8f010df2fefb
bhavi.vedula <bhavi13may@gmail.com>
2016-03-08 09:05:33 -0800

    add orcid id to creators

--- app/controllers/stash_engine/metadata_entry_pages_controller.rb
+++ app/controllers/stash_engine/metadata_entry_pages_controller.rb
@@ -15,16 +15,22 @@ module StashEngine
       # set_subjects
     end
 
-    # def metadata_callback
-    #   auth_hash = request.env['omniauth.auth']
-    #   params = request.env['omniauth.params']
-    #   creator = metadata_engine::Creator.new( resource_id: params['resource_id'],
-    #                                           creator_first_name: auth_hash.info.first_name,
-    #                                           creator_last_name: auth_hash.info.last_name,
-    #                                           orcid_id: auth_hash.uid)
-    #   creator.save
-    #   redirect_to "#{request.env['omniauth.origin']}"
-    # end
+    def metadata_callback
+      auth_hash = request.env['omniauth.auth']
+      params = request.env['omniauth.params']
+      path = request.env['omniauth.origin']
+      current_user = StashEngine::User.find(params['current_user_id'])
+      creator = metadata_engine::Creator.new( resource_id: params['resource_id'],
+                                              creator_first_name: auth_hash.info.first_name,
+                                              creator_last_name: auth_hash.info.last_name,
+                                              orcid_id: auth_hash.uid)
+      creator.save
+      if creator.creator_first_name.downcase == current_user.first_name.downcase && creator.creator_last_name.downcase == current_user.last_name.downcase
+        current_user.orcid = true
+        current_user.save
+      end
+      redirect_to "#{request.env['omniauth.origin']}"
+    end
 
     private
 
