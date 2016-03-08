4747794963eba4ae8e143d42d99c14c1e830cae9
bhavi.vedula <bhavi13may@gmail.com>
2016-03-08 09:44:34 -0800

    convert existing js to functions for ajax load

--- app/assets/javascripts/stash_datacite/map.js
+++ app/assets/javascripts/stash_datacite/map.js
@@ -1,9 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 var map;
-var test;
-$(document).ready(function() {
-
+function loadMap() {
   // create a map in the "map" div, set the view to a given place and zoom
   map = L.map('map', { zoomControl: true }).setView([36.778259, -119.417931], 6);
       mapLink = '<a href="https://openstreetmap.org">OpenStreetMap</a>';
@@ -24,31 +22,32 @@ $(document).ready(function() {
       iconAnchor: [12, 25], // point of the icon which will correspond to marker's location
       popupAnchor: [0, -25] // point from which the popup should open relative to the iconAnchor
     });
-      var geocoder = L.control.geocoder('search-OJQOSkw', {
-          placeholder: 'Add by Location Name',
-          pointIcon: false,
-          polygonIcon: false,
-          position: 'topleft',
-          markers: { icon: customIcon },
-          expanded: true
-      }).addTo(map);
 
-      var lat, lng, location_name;
-      geocoder.on('select', function (e) {
-        location_name =  e.feature.properties.label;
-        lat = e.latlng.lat;
-        lng = e.latlng.lng;
-        $.ajax({
-            type: "POST",
-            dataType: "script",
-            url: "/stash_datacite/geolocation_places/map_coordinates",
-            data: { 'geo_location_place' : location_name, 'latitude' : lat, 'longitude' : lng, 'resource_id' : $.urlParam('resource_id') }
-          });
-      });
+    var geocoder = L.control.geocoder('search-OJQOSkw', {
+        placeholder: 'Add by Location Name',
+        pointIcon: false,
+        polygonIcon: false,
+        position: 'topleft',
+        markers: { icon: customIcon },
+        expanded: true
+    }).addTo(map);
 
-      geocoder.on('reset', function (e) {
-        marker = new L.Marker([lat, lng], { icon: customIcon }).addTo(map).bindPopup(location_name).openPopup();
-      });
+    var lat, lng, location_name;
+    geocoder.on('select', function (e) {
+      location_name =  e.feature.properties.label;
+      lat = e.latlng.lat;
+      lng = e.latlng.lng;
+      $.ajax({
+          type: "POST",
+          dataType: "script",
+          url: "/stash_datacite/geolocation_places/map_coordinates",
+          data: { 'geo_location_place' : location_name, 'latitude' : lat, 'longitude' : lng, 'resource_id' : $.urlParam('resource_id') }
+        });
+    });
+
+    geocoder.on('reset', function (e) {
+      marker = new L.Marker([lat, lng], { icon: customIcon }).addTo(map).bindPopup(location_name).openPopup();
+    });
 
   // -------------------------------- //
 
@@ -348,16 +347,7 @@ $(document).ready(function() {
         }
 
     // ------------------------------------------------------------- //
-});
-
-
-$(document).ready(function(){
-    $("#location_section").click(function() {
-        setTimeout(function() {
-            map.invalidateSize();
-        }, 1000);
-    });
-});
+};
 
 $.urlParam = function(name){
   var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
