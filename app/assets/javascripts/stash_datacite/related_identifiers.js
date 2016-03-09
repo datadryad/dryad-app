2419db1275c3b49eceac012f302378d8985c8edb
bhavi.vedula <bhavi13may@gmail.com>
2016-03-09 08:54:31 -0800

    move js code back form partials intoa asset pipeline

---
+++ app/assets/javascripts/stash_datacite/related_identifiers.js
@@ -0,0 +1,36 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+function loadRelatedIdentifiers() {
+$( ".related_identifier" ).on('focus', function () {
+  previous_value = this.value;
+  }).change(function() {
+    new_value = this.value;
+    // Save when the new value is different from the previous value
+    if(new_value != previous_value) {
+      var form = $(this).parents('form');
+      $(form).trigger('submit.rails');
+    }
+  });
+
+  $( ".relation_type" ).on('focus', function () {
+  previous_value = this.value;
+  }).change(function() {
+    new_value = this.value;
+    // Save when the new value is different from the previous value
+    if(new_value != previous_value) {
+      var form = $(this).parents('form');
+      $(form).trigger('submit.rails');
+    }
+  });
+
+  $( ".related_identifier_type" ).on('focus', function () {
+  previous_value = this.value;
+  }).change(function() {
+    new_value = this.value;
+    // Save when the new value is different from the previous value
+    if(new_value != previous_value) {
+      var form = $(this).parents('form');
+      $(form).trigger('submit.rails');
+    }
+  });
+};
