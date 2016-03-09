be0c38e980294e9a2bcbd3aa69ee93a9943fe9d4
Scott Fisher <scott.fisher@ucop.edu>
2016-03-09 10:14:26 -0800

    Removing jquery and jquery_ujs from application.js to try and correct problems with double actions (probably loading it twice).

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+// require jquery  # these cause loading issues since already required by stash_engine
+// require jquery_ujs
 //
 // Required by Blacklight
 //= require blacklight/blacklight
