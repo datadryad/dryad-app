658aba655a862fd5da8a212ed2e62c23e19b3f21
bhavi.vedula <bhavi13may@gmail.com>
2016-03-09 08:11:07 -0800

    refactor

--- app/controllers/stash_datacite/metadata_entry_pages_controller.rb
+++ app/controllers/stash_datacite/metadata_entry_pages_controller.rb
@@ -2,17 +2,17 @@ require_dependency "stash_datacite/application_controller"
 
 module StashDatacite
   class MetadataEntryPagesController < ApplicationController
-    #before_action :ajax_require_current_user
+    before_action :find_resource
 
     def find_or_create
-      find_resource
-      set_instances
+      set_resource_type
       set_title
-      set_description
-      set_geolocations
-      set_relations
       set_creators
+      set_descriptions
+      set_contributors
       set_subjects
+      set_relations
+      set_geolocations
       respond_to do |format|
         format.js
       end
@@ -22,10 +22,6 @@ module StashDatacite
 
     def find_resource
       @resource = StashDatacite.resource_class.find(params[:resource_id].to_i) unless params[:resource_id].blank?
-    end
-
-    def set_resources
-      @resources = StashDatacite.resource_class.all
     end
 
     def set_creators
@@ -42,13 +38,16 @@ module StashDatacite
       @subjects = @resource.subjects
     end
 
-    def set_instances
-      @contributors = Contributor.where(resource_id: @resource.id)
-      @contributor = Contributor.new(resource_id: @resource.id)
+    def set_resource_type
       @resource_type = ResourceType.where(resource_id: @resource.id).first_or_initialize
     end
 
-    def set_description
+    def set_contributors
+      @contributors = Contributor.where(resource_id: @resource.id)
+      @contributor = Contributor.new(resource_id: @resource.id)
+    end
+
+    def set_descriptions
       @abstract = Description.type_abstract.find_or_create_by(resource_id: @resource.id)
       @methods = Description.type_methods.find_or_create_by(resource_id: @resource.id)
       @usage_notes = Description.type_usage_notes.find_or_create_by(resource_id: @resource.id)
