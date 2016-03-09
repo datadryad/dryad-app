02357f1d0c15d9a0a1a5932e8f7282b77f282b4d
bhavi.vedula <bhavi13may@gmail.com>
2016-03-09 10:08:15 -0800

    update Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218195808) do
+ActiveRecord::Schema.define(version: 20160308164307) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id",       limit: 4,   null: false
+    t.string   "user_type",     limit: 255
+    t.string   "document_id",   limit: 255
+    t.string   "title",         limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "document_type", limit: 255
+  end
+
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
 
   create_table "dcs_affliations", force: :cascade do |t|
     t.string   "short_name",   limit: 255
@@ -33,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160218195808) do
     t.integer  "resource_id",        limit: 4
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+    t.string   "award_number",       limit: 255
   end
 
   create_table "dcs_creators", force: :cascade do |t|
@@ -43,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160218195808) do
     t.integer  "resource_id",        limit: 4
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "orcid_id",           limit: 255
   end
 
   create_table "dcs_dates", force: :cascade do |t|
@@ -196,6 +210,16 @@ ActiveRecord::Schema.define(version: 20160218195808) do
     t.datetime "updated_at",              null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.text     "query_params", limit: 65535
+    t.integer  "user_id",      limit: 4
+    t.string   "user_type",    limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "searches", ["user_id"], name: "index_searches_on_user_id", using: :btree
+
   create_table "stash_engine_file_uploads", force: :cascade do |t|
     t.string   "upload_file_name",    limit: 255
     t.string   "upload_content_type", limit: 255
@@ -254,11 +278,31 @@ ActiveRecord::Schema.define(version: 20160218195808) do
     t.string   "uid",         limit: 255
     t.string   "provider",    limit: 255
     t.string   "oauth_token", limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "tenant_id",   limit: 255
+    t.boolean  "orcid",                   default: false
   end
 
   add_index "stash_engine_users", ["tenant_id"], name: "index_stash_engine_users_on_tenant_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "guest",                              default: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
