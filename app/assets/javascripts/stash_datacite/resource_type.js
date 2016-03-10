7243a00d1da68f09f96717a50f1053c81e66b3a4
bhavi.vedula <bhavi13may@gmail.com>
2016-03-10 15:52:56 -0800

    refactor js code

--- app/assets/javascripts/stash_datacite/resource_type.js
+++ app/assets/javascripts/stash_datacite/resource_type.js
@@ -1,16 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function(){
+function loadResourceTypes() {
   $( '.resource_type' ).on('focus', function () {
     previous_value = this.value;
     }).change(function() {
       new_value = this.value;
       // Save when the new value is different from the previous value
       if(new_value != previous_value) {
-        var form = $(this).parents('form');
+        var form = $(this.form);
         $(form).trigger('submit.rails');
       }
     });
-});
+};
 
 
