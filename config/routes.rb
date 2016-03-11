c75dd05361532f0793462ea9540e476fbf7c67ad
Scott Fisher <scott.fisher@ucop.edu>
2016-03-11 14:04:52 -0800

    Making the search "catalog" blacklight controller route elsewhere besides root of the site.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ require 'geoblacklight'
 
 # TODO: Should this be StashDiscovery::Engine.routes? Or just something other than root?
 Rails.application.routes.draw do
-  root to: 'catalog#index'
+  get '/search', to: 'catalog#index'
   blacklight_for :catalog
 end
