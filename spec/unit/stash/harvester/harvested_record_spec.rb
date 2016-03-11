19b3e50ac2daf10f78bf761bbfaa40fe9c2683df
David Moles <david.moles@ucop.edu>
2016-03-11 13:10:38 -0800

    Added tests

--- spec/unit/stash/harvester/harvested_record_spec.rb
+++ spec/unit/stash/harvester/harvested_record_spec.rb
@@ -5,16 +5,36 @@ module Stash
     describe HarvestedRecord do
 
       before(@each) do
-        @record = HarvestedRecord.new(identifier: 'an identifier', timestamp: Time.now.utc)
+        @timestamp = Time.now.utc
+        @record = HarvestedRecord.new(identifier: 'an identifier', timestamp: @timestamp)
       end
 
       describe '#initialize' do
-        it 'sets the identifier'
-        it 'sets the timestamp'
-        it 'sets the deleted flag'
-        it 'requires an identifier'
-        it 'requires a timestamp'
-        it 'defaults to deleted=false'
+        it 'sets the identifier' do
+          expect(@record.identifier).to eq('an identifier')
+        end
+
+        it 'requires an identifier' do
+          expect { HarvestedRecord.new(timestamp: @timestamp) }.to raise_error(ArgumentError)
+        end
+
+        it 'sets the timestamp' do
+          expect(@record.timestamp).to eq(@timestamp)
+        end
+
+        it 'sets the deleted flag' do
+          deleted_record = HarvestedRecord.new(identifier: 'an identifier', timestamp: @timestamp, deleted: true)
+          expect(deleted_record.deleted?).to be(true)
+        end
+
+        it 'defaults to deleted=false' do
+          expect(@record.deleted?).to be(false)
+        end
+
+        it 'requires a timestamp' do
+          expect { HarvestedRecord.new(identifier: 'an identifier') }.to raise_error(ArgumentError)
+        end
+
       end
 
       describe '#content' do
