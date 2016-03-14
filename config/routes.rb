bc52089c6047d3f86fae047a3ed99ed278127bee
Scott Fisher <scott.fisher@ucop.edu>
2016-03-14 10:29:13 -0700

    Moving overrides into decorators to be consistent with the rails guide.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 require 'blacklight'
 require 'geoblacklight'
 
-# TODO: Should this be StashDiscovery::Engine.routes? Or just something other than root?
 Rails.application.routes.draw do
   get '/search', to: 'catalog#index'
   blacklight_for :catalog
