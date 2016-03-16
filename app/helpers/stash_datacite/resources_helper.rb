1fb32c4b3f6f7b9b5d1b379ccdafa3ec69d1fa77
bhavi.vedula <bhavi13may@gmail.com>
2016-03-16 09:48:23 -0700

    fix nil error

--- app/helpers/stash_datacite/resources_helper.rb
+++ app/helpers/stash_datacite/resources_helper.rb
@@ -2,15 +2,15 @@ module StashDatacite
   module ResourcesHelper
     def citation(creators, title, resource_type)
       unless creators.nil?
-        creators_list = ""
+        creators_list = []
         creators.each do |creator|
           creators_list << "#{creator.creator_full_name} "
         end
       end
       publication_year  = Time.now.year
-      title = title.title || ''
-      publisher = current_tenant.long_name || ''
-      resource_type = resource_type.resource_type || ''
+      title = title.try(:title)
+      publisher = current_tenant.try(:long_name)
+      resource_type = resource_type.try(:resource_type)
       identifier = "DOI"
       return [creators_list, publication_year, title, publisher, resource_type, identifier].join(", ")
     end
