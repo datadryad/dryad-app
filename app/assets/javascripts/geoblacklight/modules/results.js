4bd3c1ab158475432797f18f894ad6e912fa7b32
Scott Fisher <scott.fisher@ucop.edu>
2016-03-18 16:10:02 -0700

    Modified geoblacklight code to display maps and bounding boxes even when they may not contain geo data.

---
+++ app/assets/javascripts/geoblacklight/modules/results.js
@@ -0,0 +1,79 @@
+Blacklight.onLoad(function() {
+  var historySupported = !!(window.history && window.history.pushState);
+
+  if (historySupported) {
+    History.Adapter.bind(window, 'statechange', function() {
+      var state = History.getState();
+      updatePage(state.url);
+    });
+  }
+
+  $('[data-map="index"]').each(function() {
+    var data = $(this).data(),
+    opts = { baseUrl: data.catalogPath },
+    geoblacklight, bbox;
+
+    if (typeof data.mapBbox === 'string') {
+      bbox = L.bboxToBounds(data.mapBbox);
+    } else {
+      $('.document [data-bbox]').each(function() {
+        var thisBbox = $(this).data().bbox;
+        // only extend if bbox set
+        if(thisBbox){
+          if (typeof bbox === 'undefined') {
+              bbox = L.bboxToBounds($(this).data().bbox);
+          } else {
+            bbox.extend(L.bboxToBounds($(this).data().bbox));
+          }
+        }
+      });
+    }
+
+    // map has no view, so set it to the world
+    if (typeof bbox === 'undefined'){
+      bbox = L.bboxToBounds("-180 -89.154124 180 89.154124");
+    }
+
+    if (!historySupported) {
+      $.extend(opts, {
+        dynamic: false,
+        searcher: function() {
+          window.location.href = this.getSearchUrl();
+        }
+      });
+    }
+
+    // instantiate new map
+    geoblacklight = new GeoBlacklight.Viewer.Map(this, { bbox: bbox });
+
+    // set hover listeners on map
+    $('#content')
+      .on('mouseenter', '#documents [data-layer-id]', function() {
+        var bounds = L.bboxToBounds($(this).data('bbox'));
+        geoblacklight.addBoundsOverlay(bounds);
+      })
+      .on('mouseleave', '#documents [data-layer-id]', function() {
+        geoblacklight.removeBoundsOverlay();
+      });
+
+    // add geosearch control to map
+    geoblacklight.map.addControl(L.control.geosearch(opts));
+  });
+
+  function updatePage(url) {
+    $.get(url).done(function(data) {
+      var resp = $.parseHTML(data);
+      $doc = $(resp);
+      $('#documents').replaceWith($doc.find('#documents'));
+      $('#sidebar').replaceWith($doc.find('#sidebar'));
+      $('#sortAndPerPage').replaceWith($doc.find('#sortAndPerPage'));
+      $('#appliedParams').replaceWith($doc.find('#appliedParams'));
+      if ($('#map').next().length) {
+        $('#map').next().replaceWith($doc.find('#map').next());
+      } else {
+        $('#map').after($doc.find('#map').next());
+      }
+    });
+  }
+
+});
