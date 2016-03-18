b1f06a0f57a23746ae99a15cf6e3b7b463057ae3
David Moles <david.moles@ucop.edu>
2016-03-18 16:55:32 -0700

    Rewrote DataciteGeoblacklight example to use pre-wrapped files, SolrIndexer

--- examples/datacite_geoblacklight/datacite_geoblacklight.rb
+++ examples/datacite_geoblacklight/datacite_geoblacklight.rb
@@ -1,32 +1,43 @@
 #! /usr/bin/env ruby
 
+require 'stash/harvester'
+require 'stash/indexer'
 require 'stash/wrapper'
-require 'stash/indexer/datacite_geoblacklight'
 
-include Stash::Wrapper
+class FileRecord < Stash::Harvester::HarvestedRecord
+  def initialize(filename)
+    @source_xml = File.read(filename)
+    @wrapper = Stash::Wrapper::StashWrapper.parse_xml(@source_xml)
+    super(identifier: @wrapper.id_value, timestamp: File.mtime(filename).utc)
+  end
 
-datacite_xml = REXML::Document.new(File.read('datacite-example-full-v3.1.xml')).root
-wrapper = StashWrapper.new(
-  identifier: Identifier.new(type: IdentifierType::DOI, value: '10.5072/example-full'),
-  version: Version.new(number: 4, date: Date.new(2014, 10, 17), note: 'Full DataCite 3.1 XML Example accessed 2014-01-06'),
-  license: License.new(
-    name: 'CC0 1.0 Universal',
-    uri: URI('http://creativecommons.org/publicdomain/zero/1.0/')
-  ),
-  embargo: Embargo.new(type: EmbargoType::DOWNLOAD, period: '1 year', start_date: Date.new(2014, 10, 17), end_date: Date.new(2015, 10, 17)),
-  inventory: Inventory.new(files: [StashFile.new(pathname: 'datacite-example-full-v3.1.xml', size_bytes: 3072, mime_type: 'application/xml')]),
-  descriptive_elements: [datacite_xml]
-)
+  def content
+    @source_xml
+  end
+end
 
-# index_config = SolrIndexConfig(url: 'http://192.168.99.100:32768/')
-# indexer = index_config.create_indexer
+records = %w(
+  wrapped-datacite-all-geodata.xml
+  wrapped-datacite-no-geodata.xml
+  wrapped-datacite-place-only.xml
+).map { |source| FileRecord.new(source) }
 
 mapper = Stash::Indexer::DataciteGeoblacklight::Mapper.new
-index_document = mapper.to_index_document(wrapper)
+records.each do |r|
+  require 'pp'
+  pp mapper.to_index_document(r.as_wrapper)
+end
 
-require 'rsolr'
-solr = RSolr.connect(url: 'http://192.168.99.100:32768/solr/geoblacklight')
-# solr = RSolr.connect(url: 'http://uc3-dash2solr-dev.cdlib.org:8983/solr/geoblacklight')
-solr.add index_document # TODO: Indexer should configure batch size
-
-solr.commit
+index_config = Stash::Indexer::Solr::SolrIndexConfig.new(url: 'http://uc3-dash2solr-dev.cdlib.org:8983/solr/geoblacklight')
+# index_config = Stash::Indexer::Solr::SolrIndexConfig.new(url: 'http://192.168.99.100:32768/')
+indexer = index_config.create_indexer(mapper)
+indexer.index(records) do |result|
+  if result.success?
+    puts "Indexed #{result.record_id} at #{result.timestamp}"
+  else
+    puts "Indexing #{result.record_id} at #{result.timestamp} failed"
+    result.errors.each do |e|
+      STDERR.puts(e.backtrace)
+    end
+  end
+end
