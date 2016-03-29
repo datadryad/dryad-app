a99a08ef318b2850cf7bb217df6c2ab02b12eff3
David Moles <david.moles@ucop.edu>
2016-03-29 12:29:25 -0700

    Keep Rubocop happy

--- lib/stash/persistence_manager.rb
+++ lib/stash/persistence_manager.rb
@@ -2,32 +2,32 @@ module Stash
   class PersistenceManager
 
     # @return [Integer] the ID of the created job
-    def begin_harvest_job(from_time:, until_time:, query_url:)
+    def begin_harvest_job(from_time:, until_time:, query_url:) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #begin_harvest_job, but it doesn't"
     end
 
     # @return [void]
-    def end_harvest_hob(harvest_job_id:, status:)
+    def end_harvest_hob(harvest_job_id:, status:) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #end_harvest_job, but it doesn't"
     end
 
     # @return [Integer] the ID of the created record
-    def record_harvested_record(harvest_job_id:, identifier:, timestamp:, deleted: false)
+    def record_harvested_record(harvest_job_id:, identifier:, timestamp:, deleted: false) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #record_harvested_record, but it doesn't"
     end
 
     # @return [Integer] the ID of the created job
-    def begin_index_job(harvest_job_id:, solr_url:)
+    def begin_index_job(harvest_job_id:, solr_url:) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #begin_index_job, but it doesn't"
     end
 
     # @return [void]
-    def end_index_job(index_job_id:, status:)
+    def end_index_job(index_job_id:, status:) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #end_index_job, but it doesn't"
     end
 
     # @return [Integer] the ID of the created record
-    def record_indexed_record(index_job_id:, harvested_record_id:, status:)
+    def record_indexed_record(index_job_id:, harvested_record_id:, status:) # rubocop:disable Lint/UnusedMethodArgument
       raise NoMethodError, "#{self.class} should implement #record_indexed_record, but it doesn't"
     end
   end
