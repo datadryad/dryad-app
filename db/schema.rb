7b040b0588117bda2b8f13167a0eb12660c8c8c7
David Moles <david.moles@ucop.edu>
2016-03-29 12:27:41 -0700

    Clean up code

--- db/schema.rb
+++ db/schema.rb
@@ -11,44 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811164526) do
+ActiveRecord::Schema.define(version: 20_150_811_164_526) do
 
-  create_table "harvest_jobs", force: :cascade do |t|
-    t.datetime "from_time"
-    t.datetime "until_time"
-    t.text     "query_url"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer  "status",     default: 0
+  create_table 'harvest_jobs', force: :cascade do |t|
+    t.datetime 'from_time'
+    t.datetime 'until_time'
+    t.text     'query_url'
+    t.datetime 'start_time'
+    t.datetime 'end_time'
+    t.integer  'status', default: 0
   end
 
-  create_table "harvested_records", force: :cascade do |t|
-    t.text     "identifier"
-    t.datetime "timestamp"
-    t.boolean  "deleted"
-    t.integer  "harvest_job_id"
+  create_table 'harvested_records', force: :cascade do |t|
+    t.text     'identifier'
+    t.datetime 'timestamp'
+    t.boolean  'deleted'
+    t.integer  'harvest_job_id'
   end
 
-  add_index "harvested_records", ["harvest_job_id"], name: "index_harvested_records_on_harvest_job_id"
+  add_index 'harvested_records', ['harvest_job_id'], name: 'index_harvested_records_on_harvest_job_id'
 
-  create_table "index_jobs", force: :cascade do |t|
-    t.integer  "harvest_job_id"
-    t.text     "solr_url"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer  "status",         default: 0
+  create_table 'index_jobs', force: :cascade do |t|
+    t.integer  'harvest_job_id'
+    t.text     'solr_url'
+    t.datetime 'start_time'
+    t.datetime 'end_time'
+    t.integer  'status', default: 0
   end
 
-  add_index "index_jobs", ["harvest_job_id"], name: "index_index_jobs_on_harvest_job_id"
+  add_index 'index_jobs', ['harvest_job_id'], name: 'index_index_jobs_on_harvest_job_id'
 
-  create_table "indexed_records", force: :cascade do |t|
-    t.integer  "index_job_id"
-    t.integer  "harvested_record_id"
-    t.datetime "submitted_time"
-    t.integer  "status",              default: 0
+  create_table 'indexed_records', force: :cascade do |t|
+    t.integer  'index_job_id'
+    t.integer  'harvested_record_id'
+    t.datetime 'submitted_time'
+    t.integer  'status', default: 0
   end
 
-  add_index "indexed_records", ["harvested_record_id"], name: "index_indexed_records_on_harvested_record_id"
-  add_index "indexed_records", ["index_job_id"], name: "index_indexed_records_on_index_job_id"
+  add_index 'indexed_records', ['harvested_record_id'], name: 'index_indexed_records_on_harvested_record_id'
+  add_index 'indexed_records', ['index_job_id'], name: 'index_indexed_records_on_index_job_id'
 
 end
