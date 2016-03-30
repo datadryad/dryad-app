c7ae14ee553c99f2df28f09a87910c5f64ec96f6
Scott Fisher <scott.fisher@ucop.edu>
2016-03-30 15:56:24 -0700

    Adding display similar to Bhavi's review as the landing page for the dataset.

--- app/controllers/stash_engine/landing_controller.rb
+++ app/controllers/stash_engine/landing_controller.rb
@@ -3,6 +3,10 @@ require_dependency "stash_engine/application_controller"
 module StashEngine
   class LandingController < ApplicationController
     def show
+      @identifiers = Identifier.where(identifier: params[:id])
+      render 'not_available' and return if @identifiers.count < 1
+      @resource_id = @identifiers.first.resource_id
     end
+
   end
 end
