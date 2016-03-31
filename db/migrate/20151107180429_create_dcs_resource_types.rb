0c8f4736f1f5f74a688cfe06366f88fdaca352e7
bhavi.vedula <bhavi13may@gmail.com>
2016-03-31 12:51:47 -0700

    fix resource type names as per Datacite doc

--- db/migrate/20151107180429_create_dcs_resource_types.rb
+++ db/migrate/20151107180429_create_dcs_resource_types.rb
@@ -1,9 +1,9 @@
 class CreateDcsResourceTypes < ActiveRecord::Migration
   def change
     create_table :dcs_resource_types do |t|
-      t.column :resource_type, "ENUM('spreadsheet', 'image', 'sound', 'audiovisual',
-                                     'text', 'software', 'multiple_types', 'other',
-                                     'collection', 'dataset', 'event', 'interactive_resource',
+      t.column :resource_type, "ENUM('dataset', 'image', 'sound', 'audiovisual',
+                                     'text', 'software', 'collection', 'other',
+                                     'event', 'interactive_resource',
                                      'model', 'physical_object', 'service', 'workflow') DEFAULT NULL"
       t.integer :resource_id
 
