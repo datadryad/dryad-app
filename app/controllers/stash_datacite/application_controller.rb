0bd4e527aed194df0345a081057541c2d04bd1c7
Scott Fisher <scott.fisher@ucop.edu>
2016-04-06 14:50:53 -0700

    Adding 'landing_url' helper to generate landing page URL.

--- app/controllers/stash_datacite/application_controller.rb
+++ app/controllers/stash_datacite/application_controller.rb
@@ -1,11 +1,11 @@
 module StashDatacite
   class ApplicationController < ::ApplicationController
-    helper_method :current_tenant, :current_user, :metadata_engine, :metadata_url_helpers, :stash_url_helpers
+    helper_method :current_tenant, :current_user, :metadata_engine, :metadata_url_helpers, :stash_url_helpers,
+                  :landing_url
 
     helper StashEngine::ApplicationHelper
 
     include StashEngine::SharedController
 
-    include StashEngine::SharedController
   end
 end
