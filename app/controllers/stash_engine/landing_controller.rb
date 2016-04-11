1c826c13e524a770f7468ae4bc76639a4878d45f
Scott Fisher <scott.fisher@ucop.edu>
2016-04-11 14:08:42 -0700

    Change to make landing page use type:identifier syntax in URL.

--- app/controllers/stash_engine/landing_controller.rb
+++ app/controllers/stash_engine/landing_controller.rb
@@ -3,10 +3,11 @@ require_dependency "stash_engine/application_controller"
 module StashEngine
   class LandingController < ApplicationController
     def show
-      @identifiers = Identifier.where(identifier: params[:id])
+      render 'not_available' and return if params[:id].blank?
+      @type, @id = params[:id].split(":", 2)
+      @identifiers = Identifier.where(identifier_type: @type).where(identifier: @id)
       render 'not_available' and return if @identifiers.count < 1
       @resource_id = @identifiers.first.resource_id
     end
-
   end
 end
