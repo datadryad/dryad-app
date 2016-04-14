cabd93e1e4b4b07944a98ac1fe14843e2de1b162
bhavi.vedula <bhavi13may@gmail.com>
2016-04-14 16:25:08 -0700

    notification settings for email

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, 'dashv2'
 set :repo_url, 'https://github.com/CDLUC3/dashv2.git'
--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
