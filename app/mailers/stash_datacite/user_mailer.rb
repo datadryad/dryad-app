b38f3fbaa9151eae9448db5e7144742f84de91b9
bhavi.vedula <bhavi13may@gmail.com>
2016-04-14 15:33:08 -0700

    enable submit dataset only if all mandatory fields are filled. Send email notification on lcoalhost

--- app/mailers/stash_datacite/user_mailer.rb
+++ app/mailers/stash_datacite/user_mailer.rb
@@ -1,24 +1,27 @@
 module StashDatacite
   class UserMailer < ApplicationMailer
-  default from: APP_CONFIG['feedback_email_from'],
-          return_path: 'dash2-dev@ucop.du'
+    default from: APP_CONFIG['feedback_email_from'],
+            return_path: 'dash2-dev@ucop.du'
 
-  #an example call:
-  # UsersMailer.notification(['catdog@mailinator.com', 'dogdog@mailinator.com'],
-  #                           'that frosty mug taste', 'test_mail').deliver
-  def notification(email_address, subject, message_template, locals)
-    if email_address.class == Array
-      email_address_array = email_address
-    else
-      email_address_array = [email_address]
+    #an example call:
+    # UserMailer.notification(['catdog@mailinator.com', 'dogdog@mailinator.com'],
+    #                           'that frosty mug taste', 'test_mail').deliver
+    def notification(email_address, subject, message_template, locals)
+      if email_address.class == Array
+        email_address_array = email_address
+      else
+        email_address_array = [email_address]
+      end
+      @vars = locals
+      mail(
+        to:             email_address_array.join(','),
+        subject:        subject.to_s,
+        from:           APP_CONFIG['feedback_email_from'],
+        reply_to:       APP_CONFIG['feedback_email_from'],
+        template_name:  message_template
+      )
     end
-    @vars = locals
-    mail( :to             => email_address_array.join(','),
-          :subject        => "#{subject}",
-          :from           => APP_CONFIG['feedback_email_from'],
-          :reply_to       => APP_CONFIG['feedback_email_from'],
-          :template_name  => message_template
-    )
-  end
   end
 end
+
+
--- test/mailers/previews/stash_datacite/user_mailer_preview.rb
+++ test/mailers/previews/stash_datacite/user_mailer_preview.rb
@@ -1,6 +1,5 @@
 module StashDatacite
   # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
   class UserMailerPreview < ActionMailer::Preview
-
   end
 end
