e1c4c938772db0f319c1badedc03fd52d23f381a
David Moles <david.moles@ucop.edu>
2016-04-19 10:05:57 -0700

    Remove unused code

--- lib/stash/indexer/datacite_default.rb
+++
@@ -1,8 +0,0 @@
-module Stash
-  module Indexer
-    # Support for indexing Datacite into the Stash default Solr schema
-    module DataciteDefault
-      Dir.glob(File.expand_path('../datacite_default/*.rb', __FILE__), &method(:require))
-    end
-  end
-end
