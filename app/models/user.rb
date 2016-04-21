da498864537dba31fd7dd12d6e5687b8798f709c
Scott Fisher <scott.fisher@ucop.edu>
2016-04-21 10:20:54 -0700

    trying to rid of devise errors, but only happening in stage.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
