bafdd6cde5d670aca9a966b43ed8eec105e2858c
David Moles <david.moles@ucop.edu>
2016-04-29 14:26:47 -0700

    First pass at auto-TOC generation

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,14 +1,3 @@
-require 'redcarpet'
-
-module MarkdownHandler
-  def self.erb
-    @erb ||= ActionView::Template.registered_template_handler(:erb)
-  end
-
-  def self.call(template)
-    compiled_source = erb.call(template)
-    "Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, autolink: true).render(begin;#{compiled_source};end).html_safe"
-  end
-end
+require 'markdown_handler'
 
 ActionView::Template.register_template_handler :md, MarkdownHandler
