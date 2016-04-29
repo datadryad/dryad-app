26e523d8992ecfa26a238c1272448be791927ae1
David Moles <david.moles@ucop.edu>
2016-04-29 14:42:32 -0700

    Clean up code, add <div class='toc'> to Help and About templates

--- lib/markdown_handler.rb
+++ lib/markdown_handler.rb
@@ -7,20 +7,7 @@ module MarkdownHandler
 
   def self.call(template)
     compiled_source = erb.call(template)
-
-    is_toc = template.locals.include?('toc')
-    renderer = is_toc ? "Redcarpet::Render::HTML_TOC" : "Redcarpet::Render::HTML.new(with_toc_data: true)"
-
+    renderer = template.locals.include?('toc') ? 'Redcarpet::Render::HTML_TOC' : 'Redcarpet::Render::HTML.new(with_toc_data: true)'
     "Redcarpet::Markdown.new(#{renderer}, no_intra_emphasis: true, autolink: true).render(begin;#{compiled_source};end).html_safe"
-
-    # "Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, autolink: true).render(begin;#{compiled_source};end).html_safe"
-    #
-
-    # <<-RUBY
-    # is_toc = template.locals.include?('toc')
-    # renderer = is_toc ? RedCarpet::Render::HTML_TOC : RedCarpet::Render::HTML(with_toc_data: true)
-    # Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true)
-    #   .render(begin;#{compiled_source};end).html_safe
-    # RUBY
   end
 end
