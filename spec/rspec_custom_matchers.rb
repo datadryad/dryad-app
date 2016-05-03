c78c6e5bfb8ad5726173844cfdf61b38d54b8cde
David Moles <david.moles@ucop.edu>
2016-05-03 16:19:21 -0700

    Fixed tests

--- spec/rspec_custom_matchers.rb
+++ spec/rspec_custom_matchers.rb
@@ -1,3 +1,15 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :request_for do |h|
+  match do |actual|
+    begin
+      !h.key?(:uri) || actual.uri == h[:uri] &&
+          !h.key?(:method) || actual.method == h[:method] &&
+          !h.key?(:headers) || actual.to_hash == h[:headers]
+    end
+  end
+end
+
 RSpec::Matchers.define :include_header do |k, v|
   def matching_key(k, actual)
     actual.keys.find { |k2| k2.to_s.downcase == k.to_s.downcase }
