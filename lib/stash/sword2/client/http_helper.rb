ff3e11260ffa08df99e815578df5a67e4b8f145a
David Moles <david.moles@ucop.edu>
2016-05-04 15:49:38 -0700

    Removed unused code

--- lib/stash/sword2/client/http_helper.rb
+++ lib/stash/sword2/client/http_helper.rb
@@ -41,31 +41,16 @@ module Stash
         # @param limit [Integer] the number of redirects to follow (defaults to {#redirect_limit})
         # @return [String] the content of the URI
         def get(uri:, limit: redirect_limit)
-          make_request(uri, limit) do |success|
+          do_get(uri, limit) do |success|
             # not 100% clear why we need an explicit return here; it
             # doesn't show up in unit tests but it does in example.rb
             return success.body
           end
         end
 
-        # Gets the content of the specified URI and saves it to a file. If no
-        # file path is provided, saves it to a temporary file.
-        # @param uri [URI] the URI to download
-        # @param path [String] the path to save the download to (optional)
-        # @return [String] the path to the downloaded file
-        def get_to_file(uri:, path: nil, limit: redirect_limit)
-          make_request(uri, limit) do |success|
-            file = path ? File.new(path, 'w+') : Tempfile.new(['resync-client', ".#{extension_for(success)}"])
-            open file, 'w' do |out|
-              success.read_body { |chunk| out.write(chunk) }
-            end
-            return file.path
-          end
-        end
-
         private
 
-        def make_request(uri, limit, &block) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+        def do_get(uri, limit, &block) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
           raise "Redirect limit (#{redirect_limit}) exceeded retrieving URI #{uri}" if limit <= 0
           req = Net::HTTP::Get.new(uri, 'User-Agent' => user_agent)
           req.basic_auth(username, password) if username && password
@@ -75,18 +60,12 @@ module Stash
               when Net::HTTPSuccess
                 yield(response)
               when Net::HTTPInformation, Net::HTTPRedirection
-                make_request(redirect_uri_for(response, uri), limit - 1, &block)
+                do_get(redirect_uri_for(response, uri), limit - 1, &block)
               else
                 raise "Error #{response.code}: #{response.message} retrieving URI #{uri}"
               end
             end
           end
-        end
-
-        def extension_for(response)
-          content_type = response['Content-Type']
-          mime_type    = MIME::Types[content_type].first || MIME::Types['application/octet-stream'].first
-          mime_type.preferred_extension || 'bin'
         end
 
         def redirect_uri_for(response, original_uri)
@@ -98,7 +77,6 @@ module Stash
             new_uri.relative? ? original_uri + location : new_uri
           end
         end
-
       end
     end
   end
