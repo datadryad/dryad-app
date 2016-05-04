efab76bb1623c85a3bddaf55b029a76f997aed07
Scott Fisher <scott.fisher@ucop.edu>
2016-05-04 11:15:15 -0700

    Removing unused tenant pages.

--- app/controllers/stash_engine/tenants_controller.rb
+++ app/controllers/stash_engine/tenants_controller.rb
@@ -2,23 +2,11 @@ require_dependency 'stash_engine/application_controller'
 
 module StashEngine
   class TenantsController < ApplicationController
-    before_action :set_tenant, only: [:show]
 
     # GET /tenants
     def index
       @tenants = Tenant.all
     end
 
-    # GET /tenants/1
-    def show
-    end
-
-    private
-
-    # Use callbacks to share common setup or constraints between actions.
-
-    def set_tenant
-      @tenant = Tenant.find(params[:id])
-    end
   end
 end
