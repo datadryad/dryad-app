a2f5ff426ed6276c62eaa42a2a8814f0fd804ffb
David Moles <david.moles@ucop.edu>
2016-05-13 14:45:18 -0700

    Factor out shared examples

--- spec/unit/stash/sword2/sequence_io_spec.rb
+++ spec/unit/stash/sword2/sequence_io_spec.rb
@@ -4,280 +4,133 @@ require 'tempfile'
 module Stash
   module Sword2
 
-    # TODO: refactor with shared examples
+    RSpec.shared_examples 'reading' do
+      it 'reads' do
+        expect(sqio.read).to eq(content)
+      end
+
+      it 'reads into a buffer' do
+        expect(sqio.read(len, outbuf)).to eq(content)
+        expect(outbuf).to eq(content)
+      end
+
+      it 'reads n < length chars' do
+        length   = len / 2
+        expected = content.slice(0, length)
+        expect(sqio.read(length, outbuf)).to eq(expected)
+        expect(outbuf).to eq(expected)
+      end
+
+      it 'reads n == length chars' do
+        expect(sqio.read(len)).to eq(content)
+      end
+
+      it 'reads n > length chars' do
+        expect(sqio.read(len * 2)).to eq(content)
+      end
+
+      it 'calculates the size' do
+        expect(sqio.size).to eq(len)
+      end
+    end
+
     describe SequenceIO do
 
       ALPHANUMERIC = Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')
 
       attr_reader :len
       attr_reader :content
-      attr_reader :stream
-      attr_reader :inputs
+      attr_reader :sqio
       attr_reader :outbuf
+      attr_reader :tempfiles
 
       before(:each) do
-        @outbuf = 'elvis'
+        @len    = 100
+        @outbuf = '[overwrite me!]'
       end
 
       after(:each) do
-        stream.close if stream
+        sqio.close if sqio
+        tempfiles.each { |f| File.delete(f) } if tempfiles
+      end
+
+      def make_alphanumeric_string(chars)
+        Array.new(chars) { ALPHANUMERIC.sample }.join
+      end
+
+      def make_tempfile(base_name, content)
+        f = Tempfile.new([base_name, 'bin'])
+        f.binmode
+        begin
+          f.write(content)
+        ensure
+          f.close
+        end
+        f.path
       end
 
       describe 'a string' do
-
         before(:each) do
-          @len = 100
-          @content = Array.new(len) { ALPHANUMERIC.sample }.join
-          @stream = SequenceIO.new(@content)
+          @content = make_alphanumeric_string(len)
+          @sqio    = SequenceIO.new(@content)
         end
-
-        it 'reads' do
-          expect(stream.read).to eq(content)
-        end
-
-        it 'reads into a buffer' do
-          expect(stream.read(len, outbuf)).to eq(content)
-          expect(outbuf).to eq(content)
-        end
-
-        it 'reads n < length chars' do
-          length = len / 2
-          expected = content.slice(0, length)
-          expect(stream.read(length, outbuf)).to eq(expected)
-          expect(outbuf).to eq(expected)
-        end
-
-        it 'reads n == length chars' do
-          expect(stream.read(len)).to eq(content)
-        end
-
-        it 'reads n > length chars' do
-          expect(stream.read(len * 2)).to eq(content)
-        end
-
-        it 'calculates the size' do
-          expect(stream.size).to eq(len)
-        end
+        include_examples 'reading'
       end
 
       describe 'a file' do
-
-        attr_reader :tempfile
-
         before(:each) do
-          @len = 100
-          @content = Random.new.bytes(len)
-
-          f = Tempfile.new(%w(array_stream-spec bin))
-          f.binmode
-          @tempfile = f.path
-          begin
-            f.write(content)
-          ensure
-            f.close
-          end
-
-          # Tempfile.new(%w(array_stream-spec bin)) do |f|
-          #   f.binwrite(content)
-          #   self.tempfile = f.path
-          # end
-
-          @inputs = File.open(@tempfile, 'rb')
-          @stream = SequenceIO.new(@inputs)
+          @content   = Random.new.bytes(len)
+          @tempfiles = [make_tempfile('array_stream-spec', content)]
+          @sqio      = SequenceIO.new(File.open(tempfiles[0], 'rb'))
         end
-
-        after(:each) do
-          File.delete(@tempfile)
-        end
-
-        it 'reads' do
-          expect(stream.read).to eq(content)
-        end
-
-        it 'reads into a buffer' do
-          expect(stream.read(len, outbuf)).to eq(content)
-          expect(outbuf).to eq(content)
-        end
-
-        it 'reads n < length chars' do
-          length = len / 2
-          expected = content.slice(0, length)
-          expect(stream.read(length, outbuf)).to eq(expected)
-          expect(outbuf).to eq(expected)
-        end
-
-        it 'reads n == length chars' do
-          expect(stream.read(len)).to eq(content)
-        end
-
-        it 'reads n > length chars' do
-          expect(stream.read(len * 2)).to eq(content)
-        end
-
-        it 'calculates the size' do
-          expect(stream.size).to eq(len)
-        end
+        include_examples 'reading'
       end
 
       describe 'an array of strings' do
-
         before(:each) do
-          @len = 100
-          @inputs = Array.new(10) { Array.new(10) { ALPHANUMERIC.sample }.join }
-          @content = @inputs.join
-          @stream = SequenceIO.new(@inputs)
+          inputs   = Array.new(10) { make_alphanumeric_string(10) }
+          @content = inputs.join
+          @sqio    = SequenceIO.new(inputs)
         end
-
-        it 'reads' do
-          expect(stream.read).to eq(content)
-        end
-
-        it 'reads into a buffer' do
-          expect(stream.read(len, outbuf)).to eq(content)
-          expect(outbuf).to eq(content)
-        end
-
-        it 'reads n < length chars' do
-          length = len / 2
-          expected = content.slice(0, length)
-          expect(stream.read(length, outbuf)).to eq(expected)
-          expect(outbuf).to eq(expected)
-        end
-
-        it 'reads n == length chars' do
-          expect(stream.read(len)).to eq(content)
-        end
-
-        it 'reads n > length chars' do
-          expect(stream.read(len * 2)).to eq(content)
-        end
-
-        it 'calculates the size' do
-          expect(stream.size).to eq(len)
-        end
+        include_examples 'reading'
       end
 
       describe 'an array of files' do
-
         before(:each) do
-          @len = 100
-          @content = ''
+          @content   = ''
           @tempfiles = Array.new(10) do |i|
-            f = Tempfile.new(%W(array_stream-spec-#{i} bin))
-            f.binmode
-            begin
-              content = Random.new.bytes(10)
-              @content << content
-              f.write(content)
-            ensure
-              f.close
-            end
-            f.path
+            content = Random.new.bytes(10)
+            @content << content
+            make_tempfile("array_stream-spec-#{i}", content)
           end
-          @inputs = @tempfiles.map { |f| File.open(f, 'rb') }
-          @stream = SequenceIO.new(inputs)
+          @sqio      = SequenceIO.new(@tempfiles.map { |f| File.open(f, 'rb') })
         end
-
-        after(:each) do
-          @tempfiles.each { |f| File.delete(f) }
-        end
-
-        it 'reads' do
-          expect(stream.read).to eq(content)
-        end
-
-        it 'reads into a buffer' do
-          expect(stream.read(len, outbuf)).to eq(content)
-          expect(outbuf).to eq(content)
-        end
-
-        it 'reads n < length chars' do
-          length = len / 2
-          expected = content.slice(0, length)
-          expect(stream.read(length, outbuf)).to eq(expected)
-          expect(outbuf).to eq(expected)
-        end
-
-        it 'reads n == length chars' do
-          expect(stream.read(len)).to eq(content)
-        end
-
-        it 'reads n > length chars' do
-          expect(stream.read(len * 2)).to eq(content)
-        end
-
-        it 'calculates the size' do
-          expect(stream.size).to eq(len)
-        end
+        include_examples 'reading'
       end
 
       describe 'a mix of strings and files' do
-
         before(:each) do
-          @len = 100
-          @content = ''
-
-          strings = Array.new(5) { Array.new(10) { ALPHANUMERIC.sample }.join }
-
-          file_contents = []
-          @tempfiles = Array.new(5) do |i|
-            f = Tempfile.new(%W(array_stream-spec-#{i} bin))
-            f.binmode
+          @content   = ''
+          @tempfiles = []
+          inputs     = (0..9).map do |i|
+            content = nil
             begin
-              content = Random.new.bytes(10)
-              file_contents << content
-              f.write(content)
+              if i.even?
+                content = make_alphanumeric_string(10)
+              else
+                content = Random.new.bytes(10)
+                @tempfiles << make_tempfile("array_stream-spec-#{i}", content)
+                File.open(tempfiles.last, 'rb')
+              end
             ensure
-              f.close
-            end
-            f.path
-          end
-
-          @inputs = (0..9).map do |i|
-            index = i / 2
-            if i.even?
-              @content << file_contents[index]
-              File.open(@tempfiles[index], 'rb')
-            else
-              @content << strings[index]
-              strings[index]
+              @content << content
             end
           end
 
-          @stream = SequenceIO.new(inputs)
+          @sqio = SequenceIO.new(inputs)
         end
 
-        after(:each) do
-          @tempfiles.each { |f| File.delete(f) }
-        end
-
-        it 'reads' do
-          expect(stream.read).to eq(content)
-        end
-
-        it 'reads into a buffer' do
-          expect(stream.read(len, outbuf)).to eq(content)
-          expect(outbuf).to eq(content)
-        end
-
-        it 'reads n < length chars' do
-          length = len / 2
-          expected = content.slice(0, length)
-          expect(stream.read(length, outbuf)).to eq(expected)
-          expect(outbuf).to eq(expected)
-        end
-
-        it 'reads n == length chars' do
-          expect(stream.read(len)).to eq(content)
-        end
-
-        it 'reads n > length chars' do
-          expect(stream.read(len * 2)).to eq(content)
-        end
-
-        it 'calculates the size' do
-          expect(stream.size).to eq(len)
-        end
+        include_examples 'reading'
       end
     end
   end
