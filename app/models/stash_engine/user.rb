44a49524efc6fd96a9c532c61732c52b6afeed15
Scott Fisher <scott.fisher@ucop.edu>
2016-05-20 15:18:59 -0700

    Adding complicated query to only show the latest version of completed plans.

--- app/models/stash_engine/user.rb
+++ app/models/stash_engine/user.rb
@@ -20,5 +20,22 @@ module StashEngine
       last = name.split(' ').last unless name.split(' ').last == first
       [first, last]
     end
+
+    #gets the latest completed resources by user, a lot of SQL since this becomes complicated
+    def latest_completed_resource_per_identifier
+      # SELECT resources.*, MAX(versions.version) as max_version FROM `stash_engine_resources` resources
+      # JOIN `stash_engine_versions` versions
+      # ON resources.`id` = versions.resource_id
+      # JOIN `stash_engine_resource_states` states
+      # ON resources.current_resource_state_id = states.id
+      # WHERE identifier_id IS NOT NULL AND states.resource_state IN ('submitted')
+      # AND resources.user_id = 7
+      # GROUP BY identifier_id
+      # ORDER BY updated_at DESC;"
+
+      Resource.select("stash_engine_resources.*, MAX(stash_engine_versions.version)").joins(:version).submitted.
+          where('identifier_id IS NOT NULL').where(user_id: id).
+          group(:identifier_id).order(:updated_at)
+    end
   end
 end
