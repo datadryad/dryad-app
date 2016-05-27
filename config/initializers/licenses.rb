5acc8ec2ada3ede5d0cf5165c4c0f2849a0b7ef9
Scott Fisher <scott.fisher@ucop.edu>
2016-05-27 12:28:33 -0700

    oops, fixing filename.

--- config/initializers/licenses.rb
+++ config/initializers/licenses.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'ostruct'
 licenses = YAML.load_file(File.join(Rails.root, 'config', 'licenses.yml'))
 
 # this will make the config available under the APP_CONFIG constant and methods like APP_CONFIG.metadata_engines
