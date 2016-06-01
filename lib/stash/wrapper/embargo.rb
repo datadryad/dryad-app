7e4d1db37ca32a345815ca0388d6e470db17f3c5
David Moles <david.moles@ucop.edu>
2016-06-01 10:53:03 -0700

    Added parameter validation for Embargo

--- lib/stash/wrapper/embargo.rb
+++ lib/stash/wrapper/embargo.rb
@@ -18,11 +18,15 @@ module Stash
       # @param period [String] The embargo period
       # @param start_date [Date] The embargo start date
       # @param end_date [Date] The embargo end date
-      def initialize(type:, period:, start_date:, end_date:)
+      def initialize(type:, period:, start_date:, end_date:) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+        fail ArgumentError, "Specified type does not appear to be an EmbargoType: #{type || 'nil'}" unless type && type.is_a?(EmbargoType)
+        fail ArgumentError, "Specified embargo period does not appear to be a non-empty string: #{period.inspect}" if period.to_s.strip.empty?
+        fail ArgumentError, "Specified start date does not appear to be a date: #{start_date || 'nil'}" unless start_date && start_date.respond_to?(:iso8601)
+        fail ArgumentError, "Specified end date does not appear to be a date: #{end_date || 'nil'}" unless end_date && end_date.respond_to?(:iso8601)
+
         self.type = type
-        self.period = period
-        self.start_date = start_date
-        self.end_date = end_date
+        self.period = period.to_s
+        self.start_date, self.end_date = valid_range(start_date, end_date)
       end
 
       # Creates a new `Embargo` instance of type {EmbargoType::NONE} with the current date as start and end.
@@ -31,7 +35,16 @@ module Stash
         today = Date.today
         new(type: EmbargoType::NONE, period: EmbargoType::NONE.value, start_date: today, end_date: today)
       end
-    end
 
+      private
+
+      def valid_range(start_date, end_date)
+        if start_date > end_date
+          fail RangeError, "start_date #{start_date} must be <= end_date #{end_date}"
+        else
+          [start_date.to_date, end_date.to_date]
+        end
+      end
+    end
   end
 end
