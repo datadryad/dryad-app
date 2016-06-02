fe117220113de64d8a29712f49b1c8809f4e2b42
Scott Fisher <scott.fisher@ucop.edu>
2016-06-02 14:58:47 -0700

    Controller for displaying the latest dash datasets in blacklight and some views.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ require 'geoblacklight'
 
 Rails.application.routes.draw do
   get '/search', to: 'catalog#index'
+  get '/latest', to: 'latest#index', as: 'latest_index'
   blacklight_for :catalog
 end
