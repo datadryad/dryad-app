ea4e90f1e0b156b4a205c2e0ffabe5b124ca1568
David Moles <david.moles@ucop.edu>
2016-06-06 14:25:22 -0700

    Fail fast when license file not found

--- config/initializers/licenses.rb
+++ config/initializers/licenses.rb
@@ -1,6 +1,5 @@
 require 'yaml'
 require 'ostruct'
 lic = File.join(Rails.root, 'config', 'licenses.yml')
-if File.exists?(lic)
-  LICENSES = YAML.load_file(lic).with_indifferent_access
-end
+raise "License configuration file #{lic} not found" unless File.exists?(lic)
+LICENSES = YAML.load_file(lic).with_indifferent_access
