44f514124086250431183765ed07bc93d3e88dfa
David Moles <david.moles@ucop.edu>
2016-06-08 15:39:34 -0700

    Change content-disposition to form-data pro tem to work around Merritt spec inconsistency

--- lib/stash/sword/header_utils.rb
+++ lib/stash/sword/header_utils.rb
@@ -5,13 +5,15 @@ module Stash
       SIMPLE_ZIP                 = 'http://purl.org/net/sword/package/SimpleZip'.freeze
       APPLICATION_ZIP            = 'application/zip'.freeze
       MULTIPART_RELATED_ATOM_XML = 'multipart/related; type="application/atom+xml"'.freeze
+      # CONTENT_DISPOSITION        = 'attachment'.freeze
+      CONTENT_DISPOSITION        = 'form-data'.freeze
 
       attr_reader :on_behalf_of
 
       def create_request_headers(zipfile, slug)
         {
           'Content-Type'        => APPLICATION_ZIP,
-          'Content-Disposition' => "attachment; filename=#{File.basename(zipfile)}",
+          'Content-Disposition' => "#{CONTENT_DISPOSITION}; filename=#{File.basename(zipfile)}",
           'Packaging'           => SIMPLE_ZIP,
           'Content-MD5'         => Digest::MD5.file(zipfile).to_s,
           'On-Behalf-Of'        => on_behalf_of,
@@ -31,7 +33,7 @@ module Stash
       def update_mime_headers(zipfile)
         {
           'Content-Type'        => APPLICATION_ZIP,
-          'Content-Disposition' => "attachment; name=payload; filename=\"#{File.basename(zipfile)}\"",
+          'Content-Disposition' => "#{CONTENT_DISPOSITION}; name=payload; filename=\"#{File.basename(zipfile)}\"",
           'Packaging'           => SIMPLE_ZIP,
           'Content-MD5'         => Digest::MD5.file(zipfile).to_s,
           'MIME-Version'        => '1.0'
