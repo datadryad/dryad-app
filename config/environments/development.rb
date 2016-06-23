12e81ecd0075ac718e6cf6c4ee19e5290fb77501
bhavi.vedula <bhavi13may@gmail.com>
2016-06-23 15:25:41 -0700

    handling action mailer on dash2-dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "https://dash2-dev.ucop.edu/stash" }
 end
