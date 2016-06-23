08d9bfec39dcc390f87d9a7bf61fc563087c8e1a
David Moles <david.moles@ucop.edu>
2016-06-23 16:01:46 -0700

    Factored shared code out of CreateResourceJob and UpdateResourceJob

--- app/jobs/stash_engine/sword_job.rb
+++ app/jobs/stash_engine/sword_job.rb
@@ -12,14 +12,21 @@ module StashEngine
       Stash::Sword::Client.new(logger: log, **sword_params)
     end
 
-    # TODO: make this less inside-out
-    def make_request(zipfile:, resource_id:, sword_params:, desc:, &block)
-      log.debug("#{self.class}: zipfile: #{zipfile}, resource_id: #{resource_id}, sword_params: #{sword_params}")
-      request_msg = "#{desc}; #{(sword_params.map { |k, v| "#{k}: #{v}" }).join(', ')}"
+    def describe(doi:, zipfile:)
+      raise NoMethodError, "#{self.class} should implement describe(), but it doesn't"
+    end
+
+    def make_request(doi:, zipfile:, resource:, client:)
+      raise NoMethodError, "#{self.class} should implement make_request(), but it doesn't"
+    end
+
+    def perform(doi:, zipfile:, resource_id:, sword_params:)
+      log.debug("#{self.class}: doi: #{doi}, zipfile: #{zipfile}, resource_id: #{resource_id}, sword_params: #{sword_params}")
+      request_msg = "#{describe(zipfile: zipfile, doi: doi)}; #{(sword_params.map { |k, v| "#{k}: #{v}" }).join(', ')}"
+
       begin
         resource = Resource.find(resource_id)
-        yield resource, client_for(sword_params)
-
+        make_request(doi: doi, zipfile: zipfile, resource: resource, client: client_for(sword_params))
         resource.update_version(zipfile)
         resource.update_submission_log(request_msg: request_msg, response_msg: 'Success')
       rescue => e
