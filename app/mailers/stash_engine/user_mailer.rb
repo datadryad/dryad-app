35c4970567340b8926337996ac4ebc1a744bed82
David Moles <david.moles@ucop.edu>
2016-07-05 17:05:46 -0700

    Added TODOs

--- app/mailers/stash_engine/user_mailer.rb
+++ app/mailers/stash_engine/user_mailer.rb
@@ -67,6 +67,8 @@ module StashEngine
 
     def identifier_for(resource)
       return unless resource
+      # TODO: add something like 'delegate :identifier_value' to Resource, cf. http://devblog.avdi.org/2011/07/05/demeter-its-not-just-a-good-idea-its-the-law/
+      # TODO: and make sure it works even if we're not talking Datacite
       resource.identifier.try(:identifier)
     end
 
