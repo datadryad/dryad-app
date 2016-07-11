ab146d6a0c2fa3ecd53eeb957e6862446af13117
Scott Fisher <scott.fisher@ucop.edu>
2016-07-11 14:41:04 -0700

    Making the references section more english-like in display.

--- app/models/stash_datacite/relation_type.rb
+++ app/models/stash_datacite/relation_type.rb
@@ -2,5 +2,10 @@ module StashDatacite
   class RelationType < ActiveRecord::Base
     self.table_name = 'dcs_relation_types'
     has_one :related_identifier
+
+    def friendly_relation_name
+      return '' if relation_type.nil?
+      relation_type.scan(/[A-Z]{1}[a-z]*/).map{|i| i.downcase}.join(' ')
+    end
   end
 end
