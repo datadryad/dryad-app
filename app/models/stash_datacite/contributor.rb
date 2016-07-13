66a2e66ecd320c09b1dd5c8ad0b169474939cd1a
Scott Fisher <scott.fisher@ucop.edu>
2016-07-13 13:11:57 -0700

    contributorTypes enum added with friendly helpers.

--- app/models/stash_datacite/contributor.rb
+++ app/models/stash_datacite/contributor.rb
@@ -3,15 +3,28 @@ module StashDatacite
     self.table_name = 'dcs_contributors'
     belongs_to :resource, class_name: StashDatacite.resource_class.to_s
     belongs_to :name_identifier
+    
+    ContributorTypes = %w(ContactPerson DataCollector DataCurator DataManager Distributor Editor Funder
+          HostingInstitution Other Producer ProjectLeader ProjectManager ProjectMember RegistrationAgency
+          RegistrationAuthority RelatedPerson ResearchGroup RightsHolder Researcher Sponsor Supervisor
+          WorkPackageLeader)
 
-    types = 'contactperson', 'datacollector', 'datacurator', 'datamanager', 'distributor', 'editor',  'funder',
-        'hostinginstitution', 'producer', 'projectleader', 'projectmanager', 'projectmember',
-        'registrationagency', 'registrationauthority', 'relatedperson', 'researcher', 'researchgroup', 'rightsholder',
-        'sponsor', 'supervisor', 'workpackageleader', 'other'
+    ContributorTypesEnum = ContributorTypes.map{|i| [i.downcase.to_sym, i.downcase]}.to_h
+    ContributorTypesStrToFull = ContributorTypes.map{|i| [i.downcase, i]}.to_h
 
-    enum contributor_type: types.map{|i| [i.to_sym, i]}.to_h
+    enum contributor_type: ContributorTypesEnum
 
     before_save :strip_whitespace
+
+    def contributor_type_friendly=(type)
+      # self required here to work correctly
+      self.contributor_type = type.to_s.downcase unless type.blank?
+    end
+
+    def contributor_type_friendly
+      return nil if contributor_type.blank?
+      ContributorTypesStrToFull[contributor_type]
+    end
 
     private
     def strip_whitespace
