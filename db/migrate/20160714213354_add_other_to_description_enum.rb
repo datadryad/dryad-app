0d815f2b7f6df459924e3175449259ea51dd9564
Scott Fisher <scott.fisher@ucop.edu>
2016-07-14 14:44:16 -0700

    Adding other and other needed description types.

--- db/migrate/20160714213354_add_other_to_description_enum.rb
+++ db/migrate/20160714213354_add_other_to_description_enum.rb
@@ -1,5 +1,7 @@
 class AddOtherToDescriptionEnum < ActiveRecord::Migration
   def change
-    t.change :description_type, "ENUM('abstract', 'methods', 'seriesinformation', 'tableofcontents', 'other', 'usage_notes') DEFAULT NULL"
+    change_table :dcs_descriptions do |t|
+      t.change :description_type, "ENUM('abstract', 'methods', 'seriesinformation', 'tableofcontents', 'other', 'usage_notes') DEFAULT NULL"
+    end
   end
 end
