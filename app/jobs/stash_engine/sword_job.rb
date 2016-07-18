5d6b4cb97bdab416c9797159dda63c87e36d99e7
David Moles <david.moles@ucop.edu>
2016-07-18 13:47:00 -0700

    Improve logging and messages, remove mail delivery deprecation warning

--- app/jobs/stash_engine/sword_job.rb
+++ app/jobs/stash_engine/sword_job.rb
@@ -30,13 +30,13 @@ module StashEngine
     end
 
     def submit
-      log.debug("#{self.class}.submit(): title: '#{title}', doi: #{doi}, zipfile: #{zipfile}, resource_id: #{resource_id}, sword_params: #{sword_params}")
-      request_msg = "Submitting #{zipfile} for '#{title}' (#{doi}): #{(sword_params.map { |k, v| "#{k}: #{v}" }).join(', ')}"
+      log.debug("#{self.class}.submit() at #{Time.now}: title: '#{title}', doi: #{doi}, zipfile: #{zipfile}, resource_id: #{resource_id}, sword_params: #{sword_params}")
+      request_msg = "Submitting #{zipfile} for '#{title}' (#{doi}) at #{Time.now}: #{(sword_params.map { |k, v| "#{k}: #{v}" }).join(', ')}"
 
       resource = nil
       begin
         resource = Resource.find(resource_id)
-        resource.update_uri ? update : create
+        resource.update_uri ? update(resource) : create(resource)
         resource.set_state('published')
         resource.update_version(zipfile)
         update_submission_log(resource_id: resource_id, request_msg: request_msg, response_msg: 'Success')
@@ -49,9 +49,9 @@ module StashEngine
         if resource
           resource.set_state('error')
           if resource.update_uri
-            UserMailer.update_failed(resource, title, request_host, request_port, e).deliver
+            UserMailer.update_failed(resource, title, request_host, request_port, e).deliver_now
           else
-            UserMailer.create_failed(resource, title, request_host, request_port, e).deliver
+            UserMailer.create_failed(resource, title, request_host, request_port, e).deliver_now
           end
         end
 
@@ -79,7 +79,7 @@ module StashEngine
       @client ||= Stash::Sword::Client.new(logger: log, **sword_params)
     end
 
-    def create
+    def create(resource)
       log.debug("invoking create(doi: #{doi}, zipfile: #{zipfile}) for resource #{resource.id} (title: '#{title}')")
       receipt = client.create(doi: doi, zipfile: zipfile)
       log.debug("create(doi: #{doi}, zipfile: #{zipfile}) for resource #{resource.id} completed with em_iri #{receipt.em_iri}, edit_iri #{receipt.edit_iri}")
@@ -87,15 +87,15 @@ module StashEngine
       resource.update_uri = receipt.edit_iri
       resource.save # save download and update URLs for this resource
       log.debug("resource #{resource.id} saved")
-      UserMailer.create_succeeded(resource, title, request_host, request_port).deliver
+      UserMailer.create_succeeded(resource, title, request_host, request_port).deliver_now
     end
 
-    def update
+    def update(resource)
       update_uri = resource.update_uri
       log.debug("invoking update(edit_iri: #{update_uri}, zipfile: #{zipfile}) for resource #{resource.id} (title: '#{title}')")
       status = client.update(edit_iri: update_uri, zipfile: zipfile)
       log.debug("update(edit_iri: #{update_uri}, zipfile: #{zipfile}) for resource #{resource.id} completed with status #{status}")
-      UserMailer.update_succeeded(resource, title, request_host, request_port).deliver
+      UserMailer.update_succeeded(resource, title, request_host, request_port).deliver_now
     end
 
     def update_submission_log(resource_id:, request_msg:, response_msg:)
@@ -126,7 +126,8 @@ module StashEngine
     end
 
     def log_success(time, value)
-      log.info("SwordJob for '#{title}' (#{doi}) succeeded at #{time}: #{value}")
+      msg = value ? value.archive_response : 'nil'
+      log.info("SwordJob for '#{title}' (#{doi}) completed at #{time}: #{msg}")
     end
   end
 end
