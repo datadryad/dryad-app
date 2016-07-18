60c61722d5cf0ca7d96e7797c330824f131b24ce
Scott Fisher <scott.fisher@ucop.edu>
2016-07-18 09:49:46 -0700

    Schema updated time.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714213354) do
+ActiveRecord::Schema.define(version: 20160715195538) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
