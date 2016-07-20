e941194fa67c74a1e644239de7eb1fce5446ba2f
Scott Fisher <scott.fisher@ucop.edu>
2016-07-20 15:15:10 -0700

    adding data transformation for many to many for contributors/creators and affiliations

---
+++ db/migrate/20160720211329_migrate_affiliation_has_and_belongs_to_many.rb
@@ -0,0 +1,21 @@
+class MigrateAffiliationHasAndBelongsToMany < ActiveRecord::Migration
+  def change
+
+    StashDatacite::Creator.where('affiliation_id IS NOT NULL').each do |c|
+      #we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
+      sql = "INSERT INTO dcs_affiliations_dcs_creators (affiliation_id, creator_id, " +
+                          "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
+      ret = ActiveRecord::Base.connection.insert(sql)
+    end
+
+    StashDatacite::Contributor.where('affiliation_id IS NOT NULL').each do |c|
+      #we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
+      sql = "INSERT INTO dcs_affiliations_dcs_contributors (affiliation_id, contributor_id, " +
+          "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
+      ret = ActiveRecord::Base.connection.insert(sql)
+    end
+
+    remove_column :dcs_creators, :affiliation_id
+    remove_column :dcs_contributors, :affiliation_id
+  end
+end
