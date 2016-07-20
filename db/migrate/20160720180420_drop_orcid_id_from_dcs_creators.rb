7c80ac51136202c5f2eb3a083475151404ba7970
Scott Fisher <scott.fisher@ucop.edu>
2016-07-20 11:24:53 -0700

    adding migration to remove orcid_id from creators.

---
+++ db/migrate/20160720180420_drop_orcid_id_from_dcs_creators.rb
@@ -0,0 +1,13 @@
+class DropOrcidIdFromDcsCreators < ActiveRecord::Migration
+  def change
+    Creator.where("orcid_id IS NOT NULL").each do |c|
+      name_id = NameIdentifier.find_or_create_by(name_identifier: c.orcid_id) do |ni|
+        ni.name_identifier_scheme = 'ORCID'
+        ni.scheme_URI = 'http://orcid.org'
+      end
+      c.name_identifier_id = name_id.id
+      c.save!
+    end
+    remove_column :dcs_creators, :orcid_id
+  end
+end
