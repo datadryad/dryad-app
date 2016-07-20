079622165b5dfc1dbaa6b7728231f711bd059e22
Scott Fisher <scott.fisher@ucop.edu>
2016-07-20 12:05:06 -0700

    Making orcid_id a convenience method on creator that uses name identifiers table values rather than a separate field in the model.

--- app/models/stash_datacite/creator.rb
+++ app/models/stash_datacite/creator.rb
@@ -20,6 +20,25 @@ module StashDatacite
       "#{creator_last_name}, #{creator_first_name}".strip
     end
 
+    # convenience methods to set orcid name identifier
+    def orcid_id=(orcid)
+      #remove if not set and an orcid exists
+      if orcid.blank? && !name_identifier_id.blank? && name_identifier.name_identifier_scheme == 'ORCID'
+        self.name_identifier_id = nil
+        return
+      end
+      name_id = NameIdentifier.find_or_create_by(name_identifier: orcid, name_identifier_scheme: 'ORCID') do |ni|
+        ni.name_identifier_scheme = 'ORCID'
+        ni.scheme_URI = 'http://orcid.org'
+      end
+      self.name_identifier_id = name_id.id
+    end
+
+    def orcid_id
+      return nil if name_identifier_id.blank? || name_identifier.name_identifier_scheme != 'ORCID'
+      name_identifier.name_identifier
+    end
+
     private
     def strip_whitespace
       self.creator_first_name = self.creator_first_name.strip unless self.creator_first_name.nil?
