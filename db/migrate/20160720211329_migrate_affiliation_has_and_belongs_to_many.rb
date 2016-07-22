af97da592888a90ca78aaf5265e49df3eb0e379f
Scott Fisher <scott.fisher@ucop.edu>
2016-07-22 14:38:28 -0700

    Fixing migration to copy data.

--- db/migrate/20160720211329_migrate_affiliation_has_and_belongs_to_many.rb
+++ db/migrate/20160720211329_migrate_affiliation_has_and_belongs_to_many.rb
@@ -3,14 +3,14 @@ class MigrateAffiliationHasAndBelongsToMany < ActiveRecord::Migration
 
     StashDatacite::Creator.where('affiliation_id IS NOT NULL').each do |c|
       #we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
-      sql = "INSERT INTO dcs_affiliations_dcs_creators (affiliation_id, creator_id, " +
+      sql = "INSERT INTO dcs_affiliations_creators (affiliation_id, creator_id, " +
                           "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
       ret = ActiveRecord::Base.connection.insert(sql)
     end
 
     StashDatacite::Contributor.where('affiliation_id IS NOT NULL').each do |c|
       #we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
-      sql = "INSERT INTO dcs_affiliations_dcs_contributors (affiliation_id, contributor_id, " +
+      sql = "INSERT INTO dcs_affiliations_contributors (affiliation_id, contributor_id, " +
           "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
       ret = ActiveRecord::Base.connection.insert(sql)
     end
