6d68dcf9c7e2309201d474a20cfcdb0da63e1203
bhavi.vedula <bhavi13may@gmail.com>
2016-08-04 11:29:02 -0700

    add state to Stash Engine File Uploads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722183102) do
+ActiveRecord::Schema.define(version: 20160804171716) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -250,6 +250,7 @@ ActiveRecord::Schema.define(version: 20160722183102) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.text     "temp_file_path",      limit: 65535
+    t.string   "file_state",          limit: 7
   end
 
   create_table "stash_engine_identifiers", force: :cascade do |t|
