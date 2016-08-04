921ea486de2c096a721c5eb11a86f50b7e76135b
bhavi.vedula <bhavi13may@gmail.com>
2016-08-04 11:49:56 -0700

    add state to Stash Engine File Uploads

--- app/models/stash_engine/file_upload.rb
+++ app/models/stash_engine/file_upload.rb
@@ -2,5 +2,7 @@ module StashEngine
   class FileUpload < ActiveRecord::Base
     belongs_to :resource, class_name: 'StashEngine::Resource'
     mount_uploader :uploader, FileUploader
+
+    enum file_state: ['created', 'copied', 'deleted'].map{|i| [i.to_sym, i]}.to_h
   end
 end
