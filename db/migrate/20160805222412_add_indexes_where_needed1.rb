c581d8cd12eafda5445eb5d5c5db2c87b8ffb54d
Scott Fisher <scott.fisher@ucop.edu>
2016-08-05 15:45:13 -0700

    fixing type-o

--- db/migrate/20160805222412_add_indexes_where_needed1.rb
+++ db/migrate/20160805222412_add_indexes_where_needed1.rb
@@ -6,7 +6,7 @@ class AddIndexesWhereNeeded1 < ActiveRecord::Migration
     add_index(:stash_engine_resource_usages, :resource_id)
     add_index(:stash_engine_resources, :identifier_id)
     add_index(:stash_engine_submission_logs, :resource_id)
-    add_index(:stash_engine_users, ;email)
+    add_index(:stash_engine_users, :email)
     add_index(:stash_engine_users, :uid)
     add_index(:stash_engine_versions, :resource_id)
   end
