bace0bac8910d85ad2122d764ad28a8beaf8bec5
JoelUCOP <joel.hagedorn@ucop.edu>
2016-08-05 16:08:32 -0700

    add ui library with gulp tool configurations

---
+++ ui-library/js/details-polyfill.js
@@ -0,0 +1,68 @@
+// ##### Details Element Polyfill ##### //
+
+$(document).ready(function(){
+  setTimeout(function() {
+    // $('details div.o-sites__group').hide();
+    if($.active > 0) {
+      $(document).ajaxStop(function () {
+        // $('details div.o-sites__group').show();
+        modernizeIt();
+        joelsReady();
+        $(this).unbind("ajaxStop"); // required since it fires everytime ajax stops after that, otherwise!
+      });
+    }else{
+      modernizeIt();
+      joelsReady();
+    }
+  }, 30);
+});
+
+function modernizeIt(){
+  // Detect via Modernizr if details element is supported in a browser:
+
+  if (Modernizr.details) {
+
+    // Details element supported:
+
+    $('details').attr('aria-expanded', 'false');
+    $('summary').attr('role', 'button');
+
+    if ($('details').is('[open]')) {
+      $('[open]').attr('aria-expanded', 'true');
+    }
+
+    $('summary').click(function () {
+      if ($(this).parent().is('[open]')) {
+        $(this).parent().attr('aria-expanded', 'false');
+      } else {
+        $(this).parent().attr('aria-expanded', 'true');
+      }
+    });
+
+  } else {
+
+    // Details element not supported:
+
+    $('details').attr('aria-expanded', 'false');
+    $('summary').attr('role', 'button');
+    $('summary').siblings().hide();
+
+    if ($('details').is('[open]')) {
+      $('[open]').children().show();
+      $('[open]').attr('aria-expanded', 'true');
+    }
+
+    // unbind('click') removed
+    $('summary').on("click", function () {
+      $(this).siblings().toggle();
+
+      if ($(this).parent().is('[open]')) {
+        $(this).parent().removeAttr('open');
+        $(this).parent().attr('aria-expanded', 'false');
+      } else {
+        $(this).parent().attr('open', '');
+        $(this).parent().attr('aria-expanded', 'true');
+      }
+    });
+  }
+  }
