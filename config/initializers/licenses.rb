901633578adf07b9c2929feab91e2058f8af00c0
Scott Fisher <scott.fisher@ucop.edu>
2016-08-15 13:35:51 -0700

    Moving licenses away.

--- config/initializers/licenses.rb
+++
@@ -1,5 +0,0 @@
-require 'yaml'
-require 'ostruct'
-lic = File.join(Rails.root, 'config', 'licenses.yml')
-raise "License configuration file #{lic} not found" unless File.exists?(lic)
-LICENSES = YAML.load_file(lic).with_indifferent_access
