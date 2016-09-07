9b599aa900879804e5b6feea5fe1d9ff7558e645
Scott Fisher <scott.fisher@ucop.edu>
2016-09-07 10:07:18 -0700

    Simplifying code slightly.

--- app/decorators/blacklight_controller_decorator.rb
+++ app/decorators/blacklight_controller_decorator.rb
@@ -4,7 +4,7 @@ module Blacklight::Controller
 
   # a fix so it only outputs path so as not to change http/https for secure/insecure problems on page
   def search_facet_url options = {}
-    opts = search_state.to_h.merge(action: "facet").merge(options).except(:page).merge({only_path: true})
+    opts = search_state.to_h.merge(action: "facet", only_path: true).merge(options).except(:page)
     url_for opts
   end
 
