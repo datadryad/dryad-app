0b83b8a925272af17358d679fc94549efc1f686b
Scott Fisher <scott.fisher@ucop.edu>
2016-09-12 13:36:48 -0700

    Adding contact_us url to text.

--- app/controllers/stash_datacite/application_controller.rb
+++ app/controllers/stash_datacite/application_controller.rb
@@ -1,7 +1,7 @@
 module StashDatacite
   class ApplicationController < ::ApplicationController
     helper_method :current_tenant, :current_user, :metadata_engine, :metadata_url_helpers, :stash_url_helpers,
-                  :landing_url, :field_suffix, :current_tenant_simple, :logo_path
+                  :landing_url, :field_suffix, :current_tenant_simple, :logo_path, :contact_us_url
 
     helper StashEngine::ApplicationHelper
 
