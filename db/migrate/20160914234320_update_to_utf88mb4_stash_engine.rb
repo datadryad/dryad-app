4828b1d335754297e48a138e81826859216839f0
Scott Fisher <scott.fisher@ucop.edu>
2016-09-15 14:08:48 -0700

    Fixing varchar columns to handle utf8mb4 and be text (since max varchar is 167 or something so may lose data if long varchar).

--- db/migrate/20160914234320_update_to_utf88mb4_stash_engine.rb
+++ db/migrate/20160914234320_update_to_utf88mb4_stash_engine.rb
@@ -1,49 +1,38 @@
 class UpdateToUtf88mb4StashEngine < ActiveRecord::Migration
 
-  TABLES = %w{ searches stash_engine_identifiers stash_engine_resources stash_engine_submission_logs stash_engine_users
-                stash_engine_versions }
+  TABLES = { 'searches' => %w{ query_params },
+             'stash_engine_identifiers' => %w{ identifier identifier_type },
+             'stash_engine_resources' => %w{ download_uri update_uri },
+             'stash_engine_submission_logs' => %w{ archive_response archive_submission_request },
+             'stash_engine_users' => %w{ first_name last_name email uid provider oauth_token tenant_id },
+             'stash_engine_versions' => %w{ zip_filename } }
 
   def up
-    TABLES.each{|t| set_utf8mb4(t) }
-    execute <<-SQL
-      ALTER TABLE stash_engine_file_uploads MODIFY
-      upload_content_type VARCHAR(255)
-      CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
-    SQL
-    execute <<-SQL
-      ALTER TABLE stash_engine_file_uploads MODIFY
-      temp_file_path TEXT
-      CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
-    SQL
+    TABLES.each do |table, v|
+      v.each do |column|
+        set_utf8mb4(table, column)
+      end
+    end
+    set_utf8mb4('stash_engine_file_uploads', 'upload_content_type')
+    set_utf8mb4('stash_engine_file_uploads', 'temp_file_path', 'utf8mb4_bin')
   end
 
   def down
-    TABLES.each{|t| set_utf8(t) }
-    execute <<-SQL
-      ALTER TABLE stash_engine_file_uploads MODIFY
-      upload_content_type VARCHAR(255)
-      CHARACTER SET utf8 COLLATE utf8_unicode_ci
-    SQL
-    execute <<-SQL
-      ALTER TABLE stash_engine_file_uploads MODIFY
-      temp_file_path TEXT
-      CHARACTER SET utf8 COLLATE utf8_unicode_ci
-    SQL
+    # nothing much since you could lose data going to smaller field (varchar) or only 3 bytes, so you
+    # really shouldn't go down
   end
 
   private
 
-  def set_utf8mb4(table_name)
+  def set_utf8mb4(table_name, col_name, collation = 'utf8mb4_unicode_ci')
+    # index_exists? only seems to work if the index was created in rails migrations and index is named a certain way
+    has_index = index_exists?(table_name.intern, col_name.intern)
+    remove_index(table_name.intern, column: col_name.intern ) if has_index
     execute <<-SQL
-      ALTER TABLE #{table_name} CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+      ALTER TABLE #{table_name} MODIFY
+      #{col_name} TEXT
+      CHARACTER SET utf8mb4 COLLATE #{collation}
     SQL
-  end
-
-  def set_utf8(table_name)
-    execute <<-SQL
-      ALTER TABLE #{table_name} CONVERT TO CHARACTER SET utf8
-      COLLATE utf8_unicode_ci;
-    SQL
+    add_index(table_name.intern, col_name.intern, length: 50) if has_index
   end
 end
