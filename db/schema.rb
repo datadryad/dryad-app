5f8b574efd7e18f11121ff392130f31d97f80b8c
Scott Fisher <scott.fisher@ucop.edu>
2016-09-15 14:30:39 -0700

    removing unused columns, schema migration changed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915211100) do
+ActiveRecord::Schema.define(version: 20160915212547) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -29,10 +29,6 @@ ActiveRecord::Schema.define(version: 20160915211100) do
     t.text     "short_name",   limit: 65535
     t.text     "long_name",    limit: 65535
     t.text     "abbreviation", limit: 65535
-    t.string   "campus",       limit: 255
-    t.string   "logo",         limit: 255
-    t.string   "url",          limit: 255
-    t.text     "url_text",     limit: 65535
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
