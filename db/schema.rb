3a273c12097607fa010053732cee92383b68422d
Scott Fisher <scott.fisher@ucop.edu>
2016-09-16 12:04:50 -0700

    resetting some things back to utf8mb4_unicode_ci since it makes case-insensitive matching/sorting break, but emojis only get inserted correctly as utfj8mb4_bin collation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916234320) do
+ActiveRecord::Schema.define(version: 20160916234420) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
