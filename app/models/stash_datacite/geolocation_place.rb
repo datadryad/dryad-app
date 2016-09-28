570b323e5708f91a091b594a723cb2013aca4b3a
bhavi.vedula <bhavi13may@gmail.com>
2016-09-28 15:07:00 -0700

    remove commented out code

--- app/models/stash_datacite/geolocation_place.rb
+++ app/models/stash_datacite/geolocation_place.rb
@@ -32,13 +32,6 @@ module StashDatacite
       end
     end
 
-    # def geo_place_boxes(resource_id)
-    #   GeolocationBox.select('dcs_geo_location_boxes.sw_longitude, dcs_geo_location_boxes.sw_latitude, dcs_geo_location_boxes.ne_longitude, dcs_geo_location_boxes.ne_latitude')
-    #                 .joins(:geolocation)
-    #                 .where('dcs_geo_locations.place_id = ?', self.id)
-    #                 .where('dcs_geo_locations.resource_id = ?', resource_id).first
-    # end
-
     #returns a bounding box string for use with Javascript
     def bounding_box_str
       return nil unless geolocation
