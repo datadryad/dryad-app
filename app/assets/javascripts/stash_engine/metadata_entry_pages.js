84c25cb36dc88beec0e6255b15d40ff32196f131
Scott Fisher <scott.fisher@ucop.edu>
2016-09-29 09:26:31 -0700

    Making ajax wait to finish before navigating away.

--- app/assets/javascripts/stash_engine/metadata_entry_pages.js
+++ app/assets/javascripts/stash_engine/metadata_entry_pages.js
@@ -1,4 +1,5 @@
 $(function () {
+  // only attach these events on metadata entry pages
   if($('body.metadata_entry_pages_find_or_create').length < 1){
     return;
   }
@@ -8,5 +9,16 @@ $(function () {
       async: false,
       url: '/stash/ajax_wait'
     });
+    waitAjax();
   });
 });
+
+// blocks until all ajax connections are closed
+var waitAjax = function(){
+  if($.active < 1){
+    return;
+  }
+  else {
+    setTimeout(waitAjax, 100); // check again in 100 ms
+  }
+}
