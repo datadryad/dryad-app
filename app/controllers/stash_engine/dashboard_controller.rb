fb338d5bbd2c935b4fc690c961c41385fc0a9b3d
Scott Fisher <scott.fisher@ucop.edu>
2016-09-29 09:28:34 -0700

    Making ajax wait to finish before navigating away.

--- app/controllers/stash_engine/dashboard_controller.rb
+++ app/controllers/stash_engine/dashboard_controller.rb
@@ -21,7 +21,7 @@ module StashEngine
     def ajax_wait
       respond_to do |format|
         format.js do
-          sleep 0.5
+          sleep 0.1
           head :ok, content_type: 'application/javascript'
         end
       end
