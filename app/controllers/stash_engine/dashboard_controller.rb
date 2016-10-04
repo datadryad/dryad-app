fe94420218f80cee31fbc6a828297bed036c2a41
bhavi.vedula <bhavi13may@gmail.com>
2016-10-04 12:28:31 -0700

    refactor getting started action controller

--- app/controllers/stash_engine/dashboard_controller.rb
+++ app/controllers/stash_engine/dashboard_controller.rb
@@ -6,17 +6,11 @@ module StashEngine
     before_action :force_to_domain, only: [:show]
 
     def getting_started
-      respond_to do |format|
-        unless current_user
-          @resources = Resource.where(user_id: current_user.id)
-            if @resources.present?
-              redirect_to stash_url_helpers.dashboard_path && return
-            else
-              format.html
-            end
-        else
-          format.html
-        end
+      @resources = Resource.where(user_id: current_user.id)
+      if @resources.present?
+        redirect_to stash_url_helpers.dashboard_path
+      else
+        render 'getting_started'
       end
     end
 
