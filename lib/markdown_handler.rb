81e5fda1c60c6972f0ed274e02460c73aee07c9e
bhavi.vedula <bhavi13may@gmail.com>
2016-10-19 13:45:25 -0700

    move options in a hash - markdown handler

--- lib/markdown_handler.rb
+++ lib/markdown_handler.rb
@@ -7,13 +7,25 @@ module MarkdownHandler
   end
 
   def self.call(template)
+    @md_options ||= {
+      autolink: true,
+      space_after_headers: true,
+      fenced_code_blocks: true,
+      underline: true,
+      no_intra_emphasis: true,
+      highlight: true,
+      footnotes: true,
+      tables: true,
+      link_attributes: {rel: 'nofollow', target: "_blank"}
+    }
+
     compiled_source = erb.call(template)
     renderer = if template.locals.include?('toc')
                  'Redcarpet::Render::HTML_TOC'
                else
                  'Redcarpet::Render::HTML.new(with_toc_data: true)'
                end
-    "Redcarpet::Markdown.new(#{renderer}, no_intra_emphasis: true, autolink: true, tables: true)."\
+    "Redcarpet::Markdown.new(#{renderer}, #{@md_options} )."\
     "render(begin;#{compiled_source};end).html_safe"
   end
 end
