2f33aef8a2ac84f740f0311fe9526d83cd4b578d
David Moles <david.moles@ucop.edu>
2016-11-23 11:36:53 -0800

    Got some tests passing

--- spec/db/stash_engine/sword_job_spec.rb
+++ spec/db/stash_engine/sword_job_spec.rb
@@ -4,13 +4,16 @@ require 'fileutils'
 require 'webmock'
 
 module StashEngine
+
   describe SwordJob do
 
     attr_reader :title
     attr_reader :doi
-    attr_reader :zipfile
     attr_reader :resource_id
+    attr_reader :zipfile
+    attr_reader :uploads_dir
     attr_reader :sword_params
+    attr_reader :sword_client
     attr_reader :request_host
     attr_reader :request_port
     attr_reader :tmpdir
@@ -33,11 +36,15 @@ module StashEngine
 
     before(:each) do
       @tmpdir = Dir.mktmpdir
-      FileUtils.cp('spec/data/archive.zip', tmpdir)
-      @zipfile = "#{tmpdir}/archive.zip"
+      @uploads_dir = File.join(@tmpdir, 'uploads')
+      FileUtils.mkdir_p(uploads_dir)
+      allow(Resource).to receive(:uploads_dir).and_return(uploads_dir)
 
       resource = Resource.create
       @resource_id = resource.id
+
+      FileUtils.cp('spec/data/archive.zip', uploads_dir)
+      @zipfile = File.join(uploads_dir, 'archive.zip')
 
       @logger = instance_double(Logger)
       allow(logger).to receive(:debug)
@@ -69,19 +76,37 @@ module StashEngine
       FileUtils.remove_entry_secure tmpdir
     end
 
+    def submit_resource
+      SwordJob.submit_async(
+          title: title,
+          doi: doi,
+          zipfile: zipfile,
+          resource_id: resource_id,
+          sword_params: sword_params,
+          request_host: request_host,
+          request_port: request_port
+      ).value!
+    end
+
     describe '#submit_async' do
-      it 'submits' do
-        future = SwordJob.submit_async(
-            title: title,
-            doi: doi,
-            zipfile: zipfile,
-            resource_id: resource_id,
-            sword_params: sword_params,
-            request_host: request_host,
-            request_port: request_port
-        )
-        future.no_error!(5)
-        expect(future.fulfilled?).to be_true
+      describe '#create' do
+        it 'creates a SWORD resource' do
+          expect(sword_client).to receive(:create).with(doi: doi, zipfile: zipfile)
+          submit_resource
+        end
+
+        it 'sets the update and download URIs' do
+          resource = submit_resource
+          expect(resource.download_uri).to eq("http://example.org/#{doi}/em")
+          expect(resource.update_uri).to eq("http://example.org/#{doi}/edit")
+        end
+
+        it 'sends an "update succeeded" email' do
+          message = instance_double(ActionMailer::MessageDelivery)
+          expect(UserMailer).to receive(:create_succeeded).with(kind_of(Resource), title, request_host, request_port).and_return(message)
+          expect(message).to receive(:deliver_now)
+          submit_resource
+        end
       end
     end
 
