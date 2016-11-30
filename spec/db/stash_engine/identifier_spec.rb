a0377b401ddbd205b55e1e11b0f62de1697a8754
David Moles <david.moles@ucop.edu>
2016-11-30 11:53:37 -0800

    Added specs for Identifier

--- spec/db/stash_engine/identifier_spec.rb
+++ spec/db/stash_engine/identifier_spec.rb
@@ -3,32 +3,64 @@ require 'db_spec_helper'
 module StashEngine
   describe Identifier do
     attr_reader :identifier
-    attr_reader :resource
-    attr_reader :download_count
-    attr_reader :view_count
+    attr_reader :usage1
+    attr_reader :usage2
+    attr_reader :res1
+    attr_reader :res2
 
     before(:each) do
       @identifier = Identifier.create(identifier_type: 'DOI', identifier: '10.123/456')
-      @resource = Resource.create(identifier_id: identifier.id)
+      @res1 = Resource.create(identifier_id: identifier.id)
+      @res2 = Resource.create(identifier_id: identifier.id)
 
-      @download_count = 7
-      @view_count = 17
-      ResourceUsage.create(
-          resource_id: resource.id,
-          downloads: download_count,
-          views: view_count
-      )
+      @usage1 = ResourceUsage.create(resource_id: res1.id, downloads: 3, views: 7)
+      @usage2 = ResourceUsage.create(resource_id: res2.id, downloads: 8, views: 6)
     end
-    
+
     describe '@download_count' do
       it 'returns the download count' do
+        download_count = usage1.downloads + usage2.downloads
         expect(identifier.download_count).to be(download_count)
       end
     end
 
     describe '@view_count' do
       it 'returns the view count' do
+        view_count = usage1.views + usage2.views
         expect(identifier.view_count).to be(view_count)
+      end
+    end
+
+    describe 'versioning' do
+      before(:each) do
+        ResourceState.create(resource_id: res1.id, resource_state: :published)
+        Version.create(resource_id: res1.id, version: 1)
+
+        ResourceState.create(resource_id: res2.id, resource_state: :in_progress)
+        Version.create(resource_id: res2.id, version: 2)
+      end
+
+      describe '#last_submitted_version' do
+        it 'returns the last submitted version' do
+          lsv = identifier.last_submitted_version
+          expect(lsv.id).to eq(res1.id)
+        end
+
+        it 'sorts by version, descending'
+      end
+
+      describe '#in_progress_version' do
+        it 'returns the in-progress version' do
+          ipv = identifier.in_progress_version
+          expect(ipv.id).to eq(res2.id)
+        end
+      end
+
+      describe '#in_progress?' do
+        it 'returns true if an in-progress version exists' do
+          expect(identifier.in_progress?).to eq(true)
+        end
+        it 'returns false if no in-progress version exists'
       end
     end
   end
