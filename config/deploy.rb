00db75ea4ffb2b836c3ba2d1f64bd2dec23c4c57
Scott Fisher <scott.fisher@ucop.edu>
2016-12-09 11:34:41 -0800

    increasing the pool idle time to 30 minutes, just in case.

--- config/deploy.rb
+++ config/deploy.rb
@@ -130,7 +130,7 @@ namespace :deploy do
           execute "cd #{deploy_to}/current; bundle install --no-deployment"
           execute "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
               "--pid-file #{fetch(:passenger_pid)} -p #{fetch(:passenger_port)} "\
-              "--log-file #{fetch(:passenger_log)} --pool-idle-time 600"
+              "--log-file #{fetch(:passenger_log)} --pool-idle-time 1800"
         end
       end
     end
