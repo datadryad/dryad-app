80c0fc74e374f3228629627f40550834b8d49711
David Moles <david.moles@ucop.edu>
2016-12-12 16:43:01 -0800

    Write DC3 in mrt-datacite.xml but DC4 in stash-wrapper.xml

--- app/models/stash_datacite/resource/resource_file_generation.rb
+++ app/models/stash_datacite/resource/resource_file_generation.rb
@@ -36,13 +36,12 @@ module StashDatacite
         )
 
         dc3_xml = datacite_xml_builder.build_datacite_xml(datacite_3: true)
-        File.open("tmp/#{doi_value.gsub('/', '-')}-dc3.xml", 'w') { |f| f.write(dc3_xml) }
         @client.update_metadata(identifier_str, dc3_xml, target_url)
 
         dc4_resource = datacite_xml_builder.build_resource
         wrapper_xml = generate_stash_wrapper(dc4_resource, @version, uploads)
 
-        [dc4_resource.write_xml, wrapper_xml]
+        [dc3_xml, wrapper_xml]
       end
 
       def generate_stash_wrapper(dc4_resource, version_number, uploads)
@@ -71,10 +70,10 @@ module StashDatacite
         purge_existing_files
 
         zipfile_name = "#{folder}/#{@resource.id}_archive.zip"
-        datacite_xml, stashwrapper_xml = generate_xml(target_url)
+        dc3_xml, stashwrapper_xml = generate_xml(target_url)
 
         File.open("#{folder}/#{@resource.id}_mrt-datacite.xml", 'w') do |f|
-          f.write datacite_xml
+          f.write dc3_xml
         end
         File.open("#{folder}/#{@resource.id}_stash-wrapper.xml", 'w') do |f|
           f.write stashwrapper_xml
