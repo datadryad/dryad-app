5e1dacda3ab52a0d62b326ab7a86dfc7c18515da
David Moles <david.moles@ucop.edu>
2016-12-20 16:42:58 -0800

    Finished covering Geolocation

--- spec/db/stash_datacite/geolocation_spec.rb
+++ spec/db/stash_datacite/geolocation_spec.rb
@@ -14,8 +14,9 @@ module StashDatacite
     end
 
     describe '#new_geolocation' do
+      attr_reader :loc
       before(:each) do
-        Geolocation.new_geolocation(
+        @loc = Geolocation.new_geolocation(
           resource_id: resource.id,
           place: 'Los Angeles',
           point: [34.2635, -118.2955],
@@ -25,6 +26,7 @@ module StashDatacite
       it 'creates a location' do
         geolocations = resource.geolocations
         expect(geolocations.count).to eq(1)
+        expect(geolocations).to contain_exactly(loc)
       end
       it 'creates a place' do
         place = resource.geolocations.first.geolocation_place
@@ -47,8 +49,91 @@ module StashDatacite
       end
     end
 
-    # describe 'amoeba copy' do
-    #
-    # end
+    describe '#amoeba_dup' do
+      attr_reader :new_loc
+      before(:each) do
+        loc = Geolocation.new_geolocation(
+          resource_id: resource.id,
+          place: 'Los Angeles',
+          point: [34.2635, -118.2955],
+          box: [32.8007, -118.9448, 34.8233, -117.6462]
+        )
+        @new_loc = loc.amoeba_dup
+      end
+      it 'duplicates the place' do
+        place = new_loc.geolocation_place
+        expect(place).not_to be_nil
+        expect(place.geo_location_place).to eq('Los Angeles')
+      end
+      it 'duplicates the point' do
+        point = new_loc.geolocation_point
+        expect(point).not_to be_nil
+        expect(point.latitude).to eq(34.2635)
+        expect(point.longitude).to eq(-118.2955)
+      end
+      it 'duplicates the box' do
+        box = new_loc.geolocation_box
+        expect(box).not_to be_nil
+        expect(box.sw_latitude).to eq(32.8007)
+        expect(box.sw_longitude).to eq(-118.9448)
+        expect(box.ne_latitude).to eq(34.8233)
+        expect(box.ne_longitude).to eq(-117.6462)
+      end
+    end
+
+    describe 'destroy methods' do
+      attr_reader :loc
+      before(:each) do
+        @loc = Geolocation.new_geolocation(
+          resource_id: resource.id,
+          place: 'Los Angeles',
+          point: [34.2635, -118.2955],
+          box: [32.8007, -118.9448, 34.8233, -117.6462]
+        )
+      end
+      it 'destroys a place' do
+        loc.destroy_place
+        expect(loc.geolocation_place).to be_nil
+      end
+      it 'destroys a point' do
+        loc.destroy_point
+        expect(loc.geolocation_point).to be_nil
+      end
+      it 'destroys a box' do
+        loc.destroy_box
+        expect(loc.geolocation_box).to be_nil
+      end
+      it 'destroys itself when empty' do
+        loc.destroy_place
+        loc.destroy_point
+        loc.destroy_box
+        expect(loc.instance_variable_get(:@destroyed)).to eq(true)
+      end
+    end
+
+    describe '#datacite_mapping_box' do
+      attr_reader :loc
+      attr_reader :box
+      before(:each) do
+        @loc = Geolocation.new_geolocation(
+          resource_id: resource.id,
+          box: [32.8007, -118.9448, 34.8233, -117.6462]
+        )
+        @box = loc.geolocation_box
+      end
+      it 'returns the box' do
+        expected = Datacite::Mapping::GeoLocationBox.new(
+          32.8007, -118.9448, 34.8233, -117.6462
+        )
+        expect(loc.datacite_mapping_box).to eq(expected)
+      end
+
+      [:sw_latitude, :sw_longitude, :ne_latitude, :ne_longitude].each do |coord|
+        it "returns nil if #{coord} is missing" do
+          box.send("#{coord}=", nil)
+          expect(loc.datacite_mapping_box).to be_nil
+        end
+      end
+    end
   end
 end
