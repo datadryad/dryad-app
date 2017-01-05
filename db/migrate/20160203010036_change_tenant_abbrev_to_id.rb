3bc73dff461454ed21571fde26bb5696606dae2f
David Moles <david.moles@ucop.edu>
2017-01-04 16:19:48 -0800

    Set index lengths explicitly to avoid utf8mb4 problems

--- db/migrate/20160203010036_change_tenant_abbrev_to_id.rb
+++ db/migrate/20160203010036_change_tenant_abbrev_to_id.rb
@@ -2,6 +2,6 @@ class ChangeTenantAbbrevToId < ActiveRecord::Migration
   def change
     remove_column :stash_engine_users, :tenant_abbrev
     add_column :stash_engine_users, :tenant_id, :string
-    add_index :stash_engine_users, :tenant_id
+    add_index :stash_engine_users, :tenant_id, length: { tenant_id: 50 }
   end
 end
