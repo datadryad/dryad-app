4f3d06a1510be49a068ff8ed4e9f61482de4bc28
David Moles <david.moles@ucop.edu>
2017-01-05 13:29:27 -0800

    Improve code style

--- lib/stash/indexer/datacite_extensions.rb
+++ lib/stash/indexer/datacite_extensions.rb
@@ -54,7 +54,7 @@ module Datacite
 
       def default_title
         title = titles.find { |t| t.type.nil? }
-        "#{title.value}".strip if title
+        title.value.to_s.strip if title
       end
 
       def doi
@@ -62,7 +62,7 @@ module Datacite
       end
 
       def type
-        "#{general_type.value}".strip if general_type
+        general_type.value.to_s.strip if general_type
       end
 
       def general_type
@@ -127,7 +127,7 @@ module Datacite
       #
       # method takes the values supplied and also adds every year for a range so people can search for
       # any of those years which may not be explicitly named
-      def dct_temporal_dates
+      def dct_temporal_dates # rubocop:disable Metrics/AbcSize
         items = dates.map(&:to_s).compact
         year_range_items = dates.map do |i|
           if i.range_start && i.range_end && i.range_start.year && i.range_end.year
--- spec/unit/stash/indexer/datacite_geoblacklight/mapper_spec.rb
+++ spec/unit/stash/indexer/datacite_geoblacklight/mapper_spec.rb
@@ -45,12 +45,12 @@ module Stash
               DM::GeoLocationPoint.new(46.82, -73.43),
               DM::GeoLocationPoint.new(-7.38, -59.52)
             ]
-            @dates = ["2000", "2001-02-03", "2010-10-01/2011-02-15", "2012/2015", "2010", "2011",
-                      "2012", "2013", "2014", "2015"]
+            @dates = ['2000', '2001-02-03', '2010-10-01/2011-02-15', '2012/2015', '2010', '2011',
+                      '2012', '2013', '2014', '2015']
 
             @descriptions = [DM::Description.new(
-                value: "Your cat has fleas. Now you get to learn to eradicate them.",
-                type: DM::DescriptionType.find_by_value('Abstract')
+              value: 'Your cat has fleas. Now you get to learn to eradicate them.',
+              type: DM::DescriptionType.find_by_value('Abstract')
             )]
 
             locations.push(*(@points.map { |pt| DM::GeoLocation.new(point: pt) }))
@@ -75,7 +75,7 @@ module Stash
             pub_year = 2015
             dates = [
               DM::Date.new(type: DM::DateType.find_by_value('Accepted'), value: '2000'),
-              DM::Date.new(type: DM::DateType.find_by_value('Created'), value: Date.new(2001,2,3)),
+              DM::Date.new(type: DM::DateType.find_by_value('Created'), value: Date.new(2001, 2, 3)),
               DM::Date.new(type: DM::DateType.find_by_value('Collected'), value: '2010-10-01/2011-02-15'),
               DM::Date.new(type: DM::DateType.find_by_value('Submitted'), value: '2012/2015')
             ]
