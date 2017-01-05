52271defecf4096ca262c6bde994da7f4f380556
David Moles <david.moles@ucop.edu>
2017-01-05 09:27:35 -0800

    Use absolute path for engines dir; check out stash_datacite as well as stash_engine

--- travis-local-deps.sh
+++ travis-local-deps.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-ENGINES_DIR=../stash_engines
+ENGINES_DIR=$(realpath ../stash_engines)
 
 mkdir ${ENGINES_DIR} && \
-  cd ${ENGINES_DIR} && \
-  git clone https://github.com/CDLUC3/stash_engine.git && \
+  cd ${ENGINES_DIR}
+
+git clone https://github.com/CDLUC3/stash_engine.git && \
   cd stash_engine && \
   git checkout ${BRANCH}
 
-REVISION=$(git rev-parse HEAD)
-echo "Checked out stash_engine branch ${BRANCH}, revision ${REVISION}"
+SE_REVISION=$(git rev-parse HEAD)
+echo "Checked out stash_engine branch ${BRANCH}, revision ${SE_REVISION}"
+
+cd ${ENGINES_DIR}
+git clone https://github.com/CDLUC3/stash_datacite.git && \
+  cd stash_datacite && \
+  git checkout ${BRANCH}
+
+SD_REVISION=$(git rev-parse HEAD)
+echo "Checked out stash_datacite branch ${BRANCH}, revision ${SD_REVISION}"
