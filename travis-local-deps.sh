e4cf0f9db6faf0ced08dc2c0601490d46b7e52f6
David Moles <david.moles@ucop.edu>
2017-01-09 09:54:18 -0800

    More debug output, add stash_discovery

--- travis-local-deps.sh
+++ travis-local-deps.sh
@@ -4,9 +4,9 @@ BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 ENGINES_DIR=$(realpath ..)/stash_engines
 
 mkdir ${ENGINES_DIR} && \
-  cd ${ENGINES_DIR}
-
-git clone https://github.com/CDLUC3/stash_engine.git && \
+  cd ${ENGINES_DIR} && \
+  echo "Cloning stash_engine into $(pwd)" && \
+  git clone https://github.com/CDLUC3/stash_engine.git && \
   cd stash_engine && \
   git checkout ${BRANCH} && \
   bundle install
@@ -14,8 +14,19 @@ git clone https://github.com/CDLUC3/stash_engine.git && \
 SE_REVISION=$(git rev-parse HEAD)
 echo "Checked out stash_engine branch ${BRANCH}, revision ${SE_REVISION}"
 
-cd ${ENGINES_DIR}
-git clone https://github.com/CDLUC3/stash_datacite.git && \
+cd ${ENGINES_DIR} && \
+  echo "Cloning stash_discovery into $(pwd)" && \
+  git clone https://github.com/CDLUC3/stash_discovery.git && \
+  cd stash_discovery && \
+  git checkout ${BRANCH}&& \
+  bundle install
+
+SD_REVISION=$(git rev-parse HEAD)
+echo "Checked out stash_discovery branch ${BRANCH}, revision ${SD_REVISION}"
+
+cd ${ENGINES_DIR} && \
+  echo "Cloning stash_datacite into $(pwd)" && \
+  git clone https://github.com/CDLUC3/stash_datacite.git && \
   cd stash_datacite && \
   git checkout ${BRANCH}&& \
   bundle install
