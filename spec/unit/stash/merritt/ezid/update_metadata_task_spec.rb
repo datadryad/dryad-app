241b5d2a372c5efda16c43487505d2168b186740
David Moles <david.moles@ucop.edu>
2017-01-18 09:37:01 -0800

    Addted test for to_s

--- spec/unit/stash/merritt/ezid/update_metadata_task_spec.rb
+++ spec/unit/stash/merritt/ezid/update_metadata_task_spec.rb
@@ -5,15 +5,24 @@ module Stash
   module Merritt
     module Ezid
       describe UpdateMetadataTask do
-        attr_reader :resource_id
         attr_reader :resource
+        attr_reader :identifier_str
+        attr_reader :landing_page_url
+        attr_reader :resource_id
         attr_reader :task
         attr_reader :tenant
         attr_reader :url_helpers
 
         before(:each) do
-          @resource_id = 17
           @resource    = double(StashEngine::Resource)
+          @identifier_str = 'doi:10.15146/R38675309'
+          allow(resource).to receive(:identifier_str).and_return(identifier_str)
+
+          @url_helpers = double(Module)
+          @landing_page_url = "http://stash.example.edu/stash/#{identifier_str}"
+          allow(url_helpers).to receive(:show_path).with(identifier_str).and_return(landing_page_url)
+
+          @resource_id = 17
           allow(StashEngine::Resource).to receive(:find).with(resource_id).and_return(resource)
 
           @tenant   = double(StashEngine::Tenant)
@@ -26,21 +35,25 @@ module Stash
           }
           allow(tenant).to receive(:identifier_service).and_return(OpenStruct.new(id_params))
 
-          @url_helpers = double(Module)
           @task = UpdateMetadataTask.new(resource_id: resource_id, tenant: tenant, url_helpers: url_helpers)
+        end
+
+        describe(:to_s) do
+          it 'describes the task' do
+            task_str = task.to_s
+            expect(task_str).to include(UpdateMetadataTask.to_s)
+            expect(task_str).to include(resource_id.to_s)
+            expect(task_str).to include(identifier_str)
+            expect(task_str).to include(landing_page_url)
+          end
         end
 
         describe :exec do
           it 'updates the metadata and landing page' do
-            identifier_str = 'doi:10.15146/R38675309'
-            allow(resource).to receive(:identifier_str).and_return(identifier_str)
 
             datacite_xml = '<resource/>'
             package = instance_double(Stash::Merritt::Package::SubmissionPackage)
             expect(package).to receive(:dc3_xml).and_return(datacite_xml)
-
-            landing_page_url = "http://stash.example.edu/stash/#{identifier_str}"
-            expect(url_helpers).to receive(:show_path).with(identifier_str).and_return(landing_page_url)
 
             ezid_client = instance_double(::Ezid::Client)
             allow(::Ezid::Client).to receive(:new)
