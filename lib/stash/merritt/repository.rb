0f2e73fa51241502b97d875ab8652bf8dc9e2513
David Moles <david.moles@ucop.edu>
2017-01-19 15:06:06 -0800

    Added spec for Repository

--- lib/stash/merritt/repository.rb
+++ lib/stash/merritt/repository.rb
@@ -5,6 +5,10 @@ module Stash
   module Merritt
     class Repository < Stash::Repo::Repository
 
+      def initialize(url_helpers:)
+        super
+      end
+
       def create_submission_job(resource_id:)
         MerrittSubmissionJob.new(resource_id: resource_id, url_helpers: url_helpers)
       end
