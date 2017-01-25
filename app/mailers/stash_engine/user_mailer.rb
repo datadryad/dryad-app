66ffcc19cc6d4f78d77d3dacdfcb7bfb1bb3e43b
David Moles <david.moles@ucop.edu>
2017-01-25 15:22:43 -0800

    Simplified email

--- app/mailers/stash_engine/user_mailer.rb
+++ app/mailers/stash_engine/user_mailer.rb
@@ -1,82 +1,13 @@
 module StashEngine
+
   # Mails users about submissions
   class UserMailer < ApplicationMailer
     # TODO: DRY these methods
 
     default from: "Dash Notifications <#{APP_CONFIG['feedback_email_from']}>",
-            return_path: (APP_CONFIG['feedback_email_from']).to_s
+      return_path: (APP_CONFIG['feedback_email_from']).to_s
 
-    def create_succeeded(resource, title, request_host, request_port)
-      warn('Unable to report successful create; nil resource') unless resource
-      return unless resource
-
-      user = resource.user
-      @to_name = "#{user.first_name} #{user.last_name}"
-      @title = title
-      @identifier_str = resource.identifier_str
-      @identifier_value = resource.identifier_value
-      @request_host = request_host
-      @request_port = request_port
-
-      to_address = to_address_list(user.email)
-      mail(to: to_address, subject: "Dataset submitted: #{@title}")
-    end
-
-    def create_failed(resource, title, request_host, request_port, error)
-      warn("Unable to report create error #{error}; nil resource") unless resource
-      return unless resource
-
-      user = resource.user
-      @to_name = "#{user.first_name} #{user.last_name}"
-      @title = title
-      @identifier_str = resource.identifier_str
-      @identifier_value = resource.identifier_value
-      @backtrace = to_backtrace(error)
-      tenant = user.tenant
-      @contact_email = to_address_list(tenant.contact_email)
-      @request_host = request_host
-      @request_port = request_port
-
-      to_address = to_address_list(user.email)
-      mail(to: to_address, subject: "Dataset submission failure: #{@title}")
-    end
-
-    def update_succeeded(resource, title, request_host, request_port)
-      warn('Unable to report successful update; nil resource') unless resource
-      return unless resource
-
-      user = resource.user
-      @to_name = "#{user.first_name} #{user.last_name}"
-      @title = title
-      @identifier_str = resource.identifier_str
-      @identifier_value = resource.identifier_value
-      @request_host = request_host
-      @request_port = request_port
-
-      to_address = to_address_list(user.email)
-      mail(to: to_address, subject: "Dataset \"#{@title}\" (doi:#{@identifier_value}) updated")
-    end
-
-    def update_failed(resource, title, request_host, request_port, error)
-      warn("Unable to report update failure #{error}; nil resource") unless resource
-      return unless resource
-
-      user = resource.user
-      @to_name = "#{user.first_name} #{user.last_name}"
-      @title = title
-      @identifier_str = resource.identifier_str
-      @identifier_value = resource.identifier_value
-      @backtrace = to_backtrace(error)
-      tenant = user.tenant
-      @contact_email = to_address_list(tenant.contact_email)
-      @request_host = request_host
-      @request_port = request_port
-
-      to_address = to_address_list(user.email)
-      mail(to: to_address, subject: "Updating dataset \"#{@title}\" (doi:#{@identifier_value}) failed")
-    end
-
-    def error_report(resource, title, error)
+    def error_report(resource, error)
       warn("Unable to report update error #{error}; nil resource") unless resource
       return unless resource
 
@@ -89,6 +20,42 @@ module StashEngine
       @backtrace = to_backtrace(error)
 
       to_address = to_address_list(APP_CONFIG['feedback_email_to'])
+      mail(to: to_address, subject: "Submitting dataset \"#{@title}\" (doi:#{@identifier_value}) failed")
+    end
+
+    def submission_succeeded(resource)
+      warn('Unable to report successful submission; nil resource') unless resource
+      return unless resource
+
+      user = resource.user
+      @to_name = "#{user.first_name} #{user.last_name}"
+      @title = resource.primary_title
+      @identifier_str = resource.identifier_str
+      @identifier_value = resource.identifier_value
+
+      tenant = user.tenant
+      @host = tenant.full_domain
+
+      mail(to: to_address, subject: "Dataset \"#{@title}\" (doi:#{@identifier_value}) submitted")
+    end
+
+    def submission_failed(resource, error)
+      warn("Unable to report submission failure #{error}; nil resource") unless resource
+      return unless resource
+
+      user = resource.user
+      @to_name = "#{user.first_name} #{user.last_name}"
+      @title = resource.primary_title
+      @identifier_str = resource.identifier_str
+      @identifier_value = resource.identifier_value
+
+      tenant = user.tenant
+      @host = tenant.full_domain
+      @contact_email = to_address_list(tenant.contact_email)
+
+      @backtrace = to_backtrace(error)
+
+      to_address = to_address_list(user.email)
       mail(to: to_address, subject: "Submitting dataset \"#{@title}\" (doi:#{@identifier_value}) failed")
     end
 
