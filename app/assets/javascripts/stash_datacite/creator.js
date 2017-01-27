fd7d7220d19d60ff304663ef44f03eb5a50e5288
Scott Fisher <scott.fisher@ucop.edu>
2017-01-27 10:25:16 -0800

    Initial fix of autofill errors with AJAX.  Needs some cleanup.

--- app/assets/javascripts/stash_datacite/creator.js
+++ app/assets/javascripts/stash_datacite/creator.js
@@ -1,27 +1,42 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 function loadCreators() {
-  $( '.js-creator_first_name' ).on('focus', function () {
-    previous_value = this.value;
-    }).change(function() {
+
+  // this uses a namespace (.mycreators) to disconnect previous events (off) before attaching them again
+  $( '.js-creator_first_name, .js-creator_last_name' )
+    .off('.mycreators')
+    .on('focus.mycreators', function () {
+      previous_value = this.value;
+    })
+    .on('change.mycreators', function() {
       new_value = this.value;
       // Save when the new value is different from the previous value
       if(new_value != previous_value) {
         var form = $(this).parents('form');
-        $(form).trigger('submit.rails');
+        // $(form).trigger('submit.rails');
+        queueAjaxFormSubmit(form);
       }
     });
 
-  $( '.js-creator_last_name' ).on('focus', function () {
-    previous_value = this.value;
-    }).change(function() {
-      new_value = this.value;
-      // Save when the new value is different from the previous value
-      if(new_value != previous_value) {
-        var form = $(this).parents('form');
-        $(form).trigger('submit.rails');
-      }
-    });
+
+  $('form.js-creator_form')
+      .off('mycreator_forms')
+      .on('ajax:beforeSend.mycreator_forms', function(event, xhr, status) {
+        console.log('ajax:beforeSend');
+      })
+      .on('ajax:complete.mycreator_forms', function(event, xhr, status) {
+        console.log('ajax:complete');
+        ajaxInProgress = false;
+        if(ajaxQueue.length > 0){
+          console.log('submitting form for next queued request:' + ajaxQueue[ajaxQueue.length-1]);
+          $(ajaxQueue.pop()).trigger('submit.rails');
+        }
+      });
+
+  /*
+  $('form.js-creator_form').on('ajax:beforeSend', function(event, xhr, settings) {
+    console.log('ajax beforesend');
+  }); */
 };
 
 
@@ -34,3 +49,16 @@ function hideRemoveLinkCreators() {
    $('.js-creator_first_name').first().parent().parent().find('.remove_record').show();
   }
 };
+
+var ajaxQueue = [];
+var ajaxInProgress = false;
+function queueAjaxFormSubmit(form){
+  console.log('using queueAjaxFormSubmit');
+  console.log("ajaxQueue.length " + ajaxQueue.length);
+  if(ajaxQueue.length < 1 && !ajaxInProgress){
+    ajaxInProgress = true;
+    $(form).trigger('submit.rails');
+  }else{
+    ajaxQueue.push(form);
+  }
+}
