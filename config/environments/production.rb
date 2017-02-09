e6a4479e99eb5ac84929f00b48facc813befd18b
Scott Fisher <scott.fisher@ucop.edu>
2017-02-08 16:58:08 -0800

    Removing force_ssl since it causes infinite redirects on stage.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.force_ssl = true
+  # force_ssl causes infinite redirects, only do in apache or rails, not both
+  #config.force_ssl = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
