5e23e5fa655af971e11a16e7669643d858140454
bhavi.vedula <bhavi13may@gmail.com>
2017-02-16 13:56:14 -0800

    add wait time for tests to comeplete upload

--- spec/features/uploads_page_spec.rb
+++ spec/features/uploads_page_spec.rb
@@ -48,7 +48,7 @@ feature "User lands on Uploads page and navigates through it" do
     click_link 'Proceed to Upload'
     # element = page.find('input[id="upload_upload"]', visible: false)
     # element.send_keys(@image_path)
-    page.attach_file('upload_upload', @image_path, :visible => false)
+    page.attach_file('upload_upload', @image_path, :visible => false, wait: Capybara.default_max_wait_time)
     page.find('#upload_all').click
     sleep 5
     expect(page).to have_content 'books.jpeg'
