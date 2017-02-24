cd2cd4a85f84cfc1ab25cc44dc591b8a5ada7300
bhavi.vedula <bhavi13may@gmail.com>
2017-02-24 12:05:54 -0800

    change end_date field to datetime from a string

--- db/migrate/20170223175726_create_stash_engine_embargoes.rb
+++ db/migrate/20170223175726_create_stash_engine_embargoes.rb
@@ -1,7 +1,7 @@
 class CreateStashEngineEmbargoes < ActiveRecord::Migration
   def change
     create_table :stash_engine_embargoes do |t|
-      t.string :end_date
+      t.datetime :end_date
       t.integer :resource_id
 
       t.timestamps null: false
