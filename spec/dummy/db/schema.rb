e03ec9220b5b1005cc064d3d25e65d7be361b604
bhavi.vedula <bhavi13may@gmail.com>
2017-03-06 09:51:40 -0800

    setup mock repo configuration for the Rails Test ENV

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215202687) do
+ActiveRecord::Schema.define(version: 20170223175726) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -297,6 +297,13 @@ ActiveRecord::Schema.define(version: 20161215202687) do
   end
 
   add_index "searches", ["user_id"], name: "index_searches_on_user_id", using: :btree
+
+  create_table "stash_engine_embargoes", force: :cascade do |t|
+    t.datetime "end_date"
+    t.integer  "resource_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
 
   create_table "stash_engine_file_uploads", force: :cascade do |t|
     t.text     "upload_file_name",    limit: 65535
