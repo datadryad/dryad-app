6d70f3868fbd5db434c339e800564b7f221e51cc
Scott Fisher <scott.fisher@ucop.edu>
2017-03-08 14:11:45 -0800

    Trying to get hiding of routing exceptions in logs working in all environments

--- config/initializers/hide_routing_exceptions.rb
+++ config/initializers/hide_routing_exceptions.rb
@@ -1,7 +1,7 @@
 class ActionDispatch::DebugExceptions
   alias_method :old_log_error, :log_error
   def log_error(env, wrapper)
-    if wrapper.exception.is_a?  ActionController::RoutingError && ['stage', 'production'].include?(Rails.env)
+    if wrapper.exception.is_a?  ActionController::RoutingError # && ['stage', 'production'].include?(Rails.env)
       return
     else
       old_log_error env, wrapper
