2b22f1d3103ee11b6eadbb56e68d0dcd33206de9
Scott Fisher <scott.fisher@ucop.edu>
2017-03-10 13:01:24 -0800

    Adding secret id instead of url for sharing.

---
+++ db/migrate/20170310200506_add_secret_id_to_shares.rb
@@ -0,0 +1,17 @@
+class AddSecretIdToShares < ActiveRecord::Migration
+  def up
+    add_column :stash_engine_shares, :secret_id, :string, after: :sharing_link
+    #transform data from old to new column
+    StashEngine::Share.reset_column_information
+    StashEngine::Share.all.each do |s|
+      if s.sharing_link && s.secret_id.nil?
+        s.update_column(:secret_id, s.sharing_link.match(/^https+\:\/\/\S+\/stash\/share\/(\S+)/)[1])
+      end
+    end
+  end
+
+  def down
+    remove_column :stash_engine_shares, :secret_id
+    # really can't transform data down
+  end
+end
