0f9090410a4da94fb3d91f42e472e584b66af005
Scott Fisher <scott.fisher@ucop.edu>
2017-03-27 12:35:05 -0700

    schema.rb changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310223319) do
+ActiveRecord::Schema.define(version: 20170324194235) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
