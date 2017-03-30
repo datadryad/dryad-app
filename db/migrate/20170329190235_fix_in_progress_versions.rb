b317388f11999d0352283c2410ef421301fe16fa
David Moles <david.moles@ucop.edu>
2017-03-30 10:58:31 -0700

    Use updated method name

--- db/migrate/20170329190235_fix_in_progress_versions.rb
+++ db/migrate/20170329190235_fix_in_progress_versions.rb
@@ -1,10 +1,10 @@
 class FixInProgressVersions < ActiveRecord::Migration
   def change
     StashEngine::Identifier.find_each do |identifier|
-      lsv = identifier.last_submitted_version
-      next unless lsv
-      lsv_stash_version = lsv.version_number
-      lsv_merritt_version = lsv.merritt_version
+      last_submitted = identifier.last_submitted_resource
+      next unless last_submitted
+      lsv_stash_version = last_submitted.version_number
+      lsv_merritt_version = last_submitted.merritt_version
 
       identifier.resources.in_progress.find_each do |resource|
         in_progress_version = resource.stash_version
