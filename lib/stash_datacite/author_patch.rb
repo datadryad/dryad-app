0f5f6aaca49c6209fc84c874183d3792fe3f0103
David Moles <david.moles@ucop.edu>
2017-04-06 10:59:48 -0700

    Fix scope

--- lib/stash_datacite/author_patch.rb
+++ lib/stash_datacite/author_patch.rb
@@ -3,16 +3,16 @@ module StashDatacite
     def self.patch!
       StashEngine::Author.instance_eval do
         has_and_belongs_to_many :affiliations, class_name: 'StashDatacite::Affiliation', join_table: 'dcs_affiliations_authors'
+      end
 
+      StashEngine::Author.class_eval do
         scope :affiliation_filled, -> {
           joins(:affiliations).where <<-SQL
             TRIM(IFNULL(dcs_affiliations.long_name,'')) <> ''
             OR TRIM(IFNULL(dcs_affiliations.short_name,'')) <> ''
           SQL
         }
-      end
 
-      StashEngine::Author.class_eval do
         #this is to simulate the bad old structure where a user can only have one affiliation
         def affiliation_id=(affil_id)
           affiliations.clear
