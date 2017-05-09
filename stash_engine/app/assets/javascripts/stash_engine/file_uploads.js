4ba3733ecd2901efa9e408fa5fee683e0e4007c0
bhavi <bhavi13may@gmail.com>
2017-05-09 12:52:28 -0700

    javascript for Files lcoation select

--- stash_engine/app/assets/javascripts/stash_engine/file_uploads.js
+++ stash_engine/app/assets/javascripts/stash_engine/file_uploads.js
@@ -1,2 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// All this logic will automatically be available in application.js.\
+
+// manifest workflow
+function validateFileUrl(){
+  $('#validate_files').on('click', function(){
+    var location_urls = $('#location_urls').val();
+    alert(location_urls);
+    var form = $(this).parents('form');
+    $(form).trigger('submit.rails');
+    $('#location_urls').val('');
+    event.preventDefault();
+    $
+  });
+}
+
+function selectFileLocation(){
+  $('#file_location_select').on('change', function() {
+    if ($(this).val() == "On this Computer") {
+      $('.files_on_computer').show();
+      $('.files_on_server').hide();
+    }
+    else {
+      $('.files_on_server').show();
+      $('.files_on_computer').hide();
+    }
+  });
+}
