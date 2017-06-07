eb665b054e40c57a501224668d6df9e6f796607e
David Moles <david.moles@ucop.edu>
2017-06-07 12:27:21 -0700

    Improve test coverage

--- stash_datacite_specs/spec/db/stash_datacite/related_identifier_spec.rb
+++ stash_datacite_specs/spec/db/stash_datacite/related_identifier_spec.rb
@@ -33,5 +33,57 @@ module StashDatacite
         end
       end
     end
+
+    describe 'relation_type_mapping_obj' do
+      it 'returns nil for nil' do
+        expect(RelatedIdentifier.relation_type_mapping_obj(nil)).to be_nil
+      end
+      it 'maps type values to enum instances' do
+        Datacite::Mapping::RelationType.each do |type|
+          value_str = type.value
+          expect(RelatedIdentifier.relation_type_mapping_obj(value_str)).to be(type)
+        end
+      end
+      it 'returns the enum instance for a model object' do
+        RelatedIdentifier::RelationTypesLimited.values.each do |rel_type|
+          related_doi_value = "10.5555/#{Time.now.nsec}"
+          rel_id = RelatedIdentifier.create(
+            resource_id: resource.id,
+            relation_type: rel_type,
+            related_identifier_type: 'doi',
+            related_identifier: related_doi_value
+          )
+          rel_type_friendly = rel_id.relation_type_friendly
+          enum_instance = Datacite::Mapping::RelationType.find_by_value(rel_type_friendly)
+          expect(rel_id.relation_type_mapping_obj).to be(enum_instance)
+        end
+      end
+    end
+
+    describe 'related_identifier_type_mapping_obj' do
+      it 'returns nil for nil' do
+        expect(RelatedIdentifier.related_identifier_type_mapping_obj(nil)).to be_nil
+      end
+      it 'maps type values to enum instances' do
+        Datacite::Mapping::RelatedIdentifierType.each do |type|
+          value_str = type.value
+          expect(RelatedIdentifier.related_identifier_type_mapping_obj(value_str)).to be(type)
+        end
+      end
+      it 'returns the enum instance for a model object' do
+        RelatedIdentifier::RelatedIdentifierTypesLimited.values.each do |rel_type|
+          related_doi_value = "10.5555/#{Time.now.nsec}"
+          rel_id = RelatedIdentifier.create(
+            resource_id: resource.id,
+            related_identifier_type: rel_type,
+            related_identifier_type: 'doi',
+            related_identifier: related_doi_value
+          )
+          rel_id_type_friendly = rel_id.related_identifier_type_friendly
+          enum_instance = Datacite::Mapping::RelatedIdentifierType.find_by_value(rel_id_type_friendly)
+          expect(rel_id.related_identifier_type_mapping_obj).to be(enum_instance)
+        end
+      end
+    end
   end
 end
