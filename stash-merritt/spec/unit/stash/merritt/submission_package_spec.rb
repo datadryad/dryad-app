743f899a26b0e7cce8927142eec9cae97be6eb5c
David Moles <david.moles@ucop.edu>
2017-06-09 12:07:04 -0700

    Improve test coverage

---
+++ stash-merritt/spec/unit/stash/merritt/submission_package_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module Stash
+  module Merritt
+    describe SubmissionPackage do
+      describe :payload do
+        it 'is abstract' do
+          resource = double(StashEngine::Resource)
+          allow(resource).to receive(:identifier_str).and_return('doi:10.123/456')
+          packaging = Stash::Sword::Packaging::SIMPLE_ZIP
+          package = SubmissionPackage.new(resource: resource, packaging: packaging)
+          expect { package.payload }.to raise_error(NoMethodError)
+        end
+      end
+    end
+  end
+end
