71a2ac78c7f80902d5fa7eee22a742a7ddaaeca2
David Moles <david.moles@ucop.edu>
2017-06-12 12:34:09 -0700

    Fix dodgy interpolation

--- stash_engine/spec/controllers/metadata_entry_controller_spec.rb
+++ stash_engine/spec/controllers/metadata_entry_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe StashEngine::MetadataEntryPagesController, type: :controller do
   routes { StashEngine::Engine.routes }
 
-  # logs_in_with "#{TEST}", "#{TESTUSER@GMAIL.COM}", "#{UCOP.EDU}"
+  logs_in_with 'TEST', 'TESTUSER@GMAIL.COM', 'UCOP.EDU'
   let(:resource) { StashEngine::Resource.create }
   describe 'GET find_or_create' do
     it 'has a 200 status code' do
