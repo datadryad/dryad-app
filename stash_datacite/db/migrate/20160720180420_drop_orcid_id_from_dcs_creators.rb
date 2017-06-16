3ef47ae629b52779fb62c19d46797c8556626324
David Moles <david.moles@ucop.edu>
2017-06-16 13:08:22 -0700

    Replace model classes (which may no longer exist) with raw SQL

--- stash_datacite/db/migrate/20160720180420_drop_orcid_id_from_dcs_creators.rb
+++ stash_datacite/db/migrate/20160720180420_drop_orcid_id_from_dcs_creators.rb
@@ -1,13 +1,37 @@
 class DropOrcidIdFromDcsCreators < ActiveRecord::Migration
   def change
-    StashDatacite::Creator.where('orcid_id IS NOT NULL').each do |c|
-      name_id = StashDatacite::NameIdentifier.find_or_create_by(name_identifier: c.orcid_id) do |ni|
-        ni.name_identifier_scheme = 'ORCID'
-        ni.scheme_URI = 'http://orcid.org'
-      end
-      c.name_identifier_id = name_id.id
-      c.save!
-    end
+
+    # Create missing name identifier records
+    execute <<-SQL
+      INSERT INTO dcs_name_identifiers
+                  (name_identifier, name_identifier_scheme, scheme_URI, created_at, updated_at)
+           SELECT creators.orcid_id, 'ORCID', 'http://orcid.org', NOW(), NOW()
+             FROM dcs_creators AS creators
+            WHERE creators.orcid_id IS NOT NULL
+              AND creators.orcid_id NOT IN
+                  (SELECT name_identifier
+                     FROM dcs_name_identifiers);
+    SQL
+
+    # Make sure any existing ORCID name identifiers have the correct scheme and scheme URI
+    execute <<-SQL
+      UPDATE dcs_name_identifiers
+         SET name_identifier_scheme = 'ORCID',
+             scheme_URI = 'http://orcid.org'
+       WHERE name_identifier IN
+             (SELECT orcid_id FROM dcs_creators);
+    SQL
+
+    # Link creators to name identifiers
+    execute <<-SQL
+      UPDATE dcs_creators
+         SET name_identifier_id =
+             (SELECT id
+                FROM dcs_name_identifiers
+               WHERE dcs_name_identifiers.name_identifier = dcs_creators.orcid_id);
+    SQL
+
+    # Remove old orcid_id column
     remove_column :dcs_creators, :orcid_id
   end
 end
--- stash_datacite/db/migrate/20160720211329_migrate_affiliation_has_and_belongs_to_many.rb
+++ stash_datacite/db/migrate/20160720211329_migrate_affiliation_has_and_belongs_to_many.rb
@@ -1,20 +1,17 @@
 class MigrateAffiliationHasAndBelongsToMany < ActiveRecord::Migration
   def change
-    StashDatacite::Creator.where('affiliation_id IS NOT NULL').each do |c|
-      # we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
-      next if c.affiliation_id.blank?
-      sql = 'INSERT INTO dcs_affiliations_creators (affiliation_id, creator_id, ' \
-            "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
-      ret = ActiveRecord::Base.connection.insert(sql)
-    end
 
-    StashDatacite::Contributor.where('affiliation_id IS NOT NULL').each do |c|
-      # we shouldn't need to worry about sanitization since theres are only integers in current db for these fields
-      next if c.affiliation_id.blank?
-      sql = 'INSERT INTO dcs_affiliations_contributors (affiliation_id, contributor_id, ' \
-            "created_at, updated_at) VALUES(#{c.affiliation_id}, #{c.id}, NOW(), NOW())"
-      ret = ActiveRecord::Base.connection.insert(sql)
-    end
+    execute <<-SQL
+      INSERT INTO dcs_affiliations_creators (affiliation_id, creator_id, created_at, updated_at)
+           SELECT dcs_creators.affiliation_id, dcs_creators.id, NOW(), NOW()
+             FROM dcs_creators;
+    SQL
+
+    execute <<-SQL
+      INSERT INTO dcs_affiliations_contributors (affiliation_id, contributor_id, created_at, updated_at)
+           SELECT dcs_contributors.affiliation_id, dcs_contributors.id, NOW(), NOW()
+             FROM dcs_contributors;
+    SQL
 
     remove_column :dcs_creators, :affiliation_id
     remove_column :dcs_contributors, :affiliation_id
