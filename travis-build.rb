77499ed288403d8a8d188ca59fd3764c36759e61
David Moles <david.moles@ucop.edu>
2017-06-19 10:40:51 -0700

    Hide non-error output

--- travis-build.rb
+++ travis-build.rb
@@ -2,6 +2,7 @@
 
 require 'bundler'
 require 'pathname'
+require 'time'
 
 PROJECTS = %w(
   stash-wrapper
@@ -15,20 +16,26 @@ PROJECTS = %w(
   stash_datacite_specs
 )
 
+def exec_command(command, log_file)
+  # preserve ANSI colors, see https://stackoverflow.com/a/27399198/27358
+  system("script -q #{log_file} #{command} > /dev/null")
+end
+
 root = Pathname.new(__dir__)
 
-def bundle(project_dir)
+def bundle(project_dir, bundle_out)
   puts "bundling #{project_dir}"
   Dir.chdir(project_dir) do
     Bundler.with_clean_env do
-      bundle_ok = system('bundle install')
+      bundle_ok = exec_command('bundle install', bundle_out)
       $stderr.puts("bundle failed: #{project_dir}") unless bundle_ok
+      system("cat #{bundle_out}") unless bundle_ok
       return bundle_ok
     end
   end
 end
 
-def prepare(project_dir)
+def prepare(project_dir, prep_out)
   prep_script = project_dir + 'travis-prep.sh'
   return true unless prep_script.exist?
 
@@ -38,21 +45,24 @@ def prepare(project_dir)
     return false
   end
 
-  prep_ok = system(prep_script.to_s, err: :out)
+  prep_ok = exec_command(prep_script, prep_out)
   $stderr.puts("prepare failed: #{prep_script}") unless prep_ok
+  system("cat #{prep_out}") unless prep_ok
   prep_ok
 end
 
-def build(project_dir)
-  puts "building #{project_dir}"
+def build(project_dir, build_out)
   Dir.chdir(project_dir) do
     begin
-      prep_ok = prepare(project_dir)
+      prep_out = build_out.sub('-build', '-prep')
+      prep_ok = prepare(project_dir, prep_out)
       return false unless prep_ok
 
+      puts "building #{project_dir}"
       Bundler.with_clean_env do
-        build_ok = system('bundle exec rake')
+        build_ok = exec_command('bundle exec rake', build_out)
         $stderr.puts("build failed: #{project_dir}") unless build_ok
+        system("cat #{build_out}") unless build_ok
         return build_ok
       end
     rescue => e
@@ -62,16 +72,21 @@ def build(project_dir)
   end
 end
 
-
+tmpdir = File.absolute_path("builds/#{Time.now.utc.iso8601}")
+FileUtils.mkdir_p(tmpdir)
+puts "logging build output to #{tmpdir}"
+tmp_path = Pathname.new(tmpdir)
 PROJECTS.each do |p|
-  bundle_ok = bundle(root + p)
+  bundle_out = tmp_path + ("#{p}-bundle.out")
+  bundle_ok = bundle(root + p, bundle_out)
   exit(1) unless bundle_ok
 end
 
 build_succeeded = []
 build_failed = []
 PROJECTS.each do |p|
-  build_ok = build(root + p)
+  build_out = tmp_path + ("#{p}-build.out")
+  build_ok = build(root + p, build_out)
   build_succeeded << p if build_ok
   build_failed << p unless build_ok
 end
