b6d47f7bb18ae7c0a5e84db3cc6862de455a6a3e
Scott Fisher <scott.fisher@ucop.edu>
2017-06-28 09:55:00 -0700

    adding propagated gems/db schema to repo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614183802) do
+ActiveRecord::Schema.define(version: 20170627163421) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -364,7 +364,7 @@ ActiveRecord::Schema.define(version: 20170614183802) do
     t.datetime "updated_at",              null: false
   end
 
-  add_index "stash_engine_shares", ["secret_id"], name: "index_stash_engine_shares_on_secret_id", length: {"secret_id"=>50}, using: :btree
+  add_index "stash_engine_shares", ["secret_id"], name: "index_stash_engine_shares_on_secret_id", using: :btree
 
   create_table "stash_engine_submission_logs", force: :cascade do |t|
     t.integer  "resource_id",                limit: 4
@@ -383,10 +383,12 @@ ActiveRecord::Schema.define(version: 20170614183802) do
     t.text     "uid",         limit: 65535
     t.text     "provider",    limit: 65535
     t.text     "oauth_token", limit: 65535
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.text     "tenant_id",   limit: 65535
     t.boolean  "orcid",                     default: false
+    t.datetime "last_login"
+    t.string   "role",        limit: 9,     default: "user"
   end
 
   add_index "stash_engine_users", ["email"], name: "index_stash_engine_users_on_email", length: {"email"=>50}, using: :btree
@@ -405,9 +407,9 @@ ActiveRecord::Schema.define(version: 20170614183802) do
   add_index "stash_engine_versions", ["resource_id"], name: "index_stash_engine_versions_on_resource_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 191, default: "",    null: false
+    t.string   "email",                  limit: 255, default: "",    null: false
     t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 191
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
