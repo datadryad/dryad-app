5e7a6ec94154b0e1ab6db13351379a9948b4edac
David Moles <david.moles@ucop.edu>
2017-06-28 16:05:24 -0700

    Improved Solr startup

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -20,39 +20,9 @@ end
 # ------------------------------------------------------------
 # Solr config
 
-# TODO: figure out how to move some of this to stash_discovery
+require 'solr_helper'
 
-require 'solr_wrapper'
-require 'colorize'
-
-def info(msg)
-  puts msg.to_s.colorize(:blue)
-end
-
-def warn(msg)
-  puts msg.to_s.colorize(:red)
-end
-
-def solr_start
-  info 'Starting Solr'
-  @solr = SolrWrapper.instance(port: '8983', version: '5.2.1')
-  @solr.start
-  info 'Solr 5.2.1 started on port 8983'
-  begin
-    @collection = @solr.create(dir: 'spec/config/solr/conf', name: 'geoblacklight')
-    info 'geoblacklight collection created'
-  rescue => ex
-    warn ex
-    @solr.stop
-    @solr = nil
-  end
-end
-
-def solr_stop
-  @solr.delete(@collection) if @collection
-ensure
-  @solr.stop if @solr
-end
+SolrHelper.start
 
 # ------------------------------------------------------------
 # Additional RSpec configuration
@@ -61,6 +31,5 @@ RSpec.configure do |config|
   # Treat specs in features/ as feature specs
   config.infer_spec_type_from_file_location!
 
-  config.before(:all) { solr_start }
-  config.after(:all) { solr_stop }
+  config.after(:all) { SolrHelper.stop }
 end
