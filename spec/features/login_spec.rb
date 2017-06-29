bdd70e38d159bc38da25621202f1565827cece33
David Moles <david.moles@ucop.edu>
2017-06-29 16:09:02 -0700

    Add login spec

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,16 @@
+require 'features_helper'
+
+describe 'login' do
+  before(:each) do
+    allow(StashEngine::Tenant).to(receive(:google_login_path)
+      .and_wrap_original) { |m| m.call.sub('https', 'http') }
+    visit('/')
+  end
+
+  it 'goes to Getting Started when no datasets exist' do
+    login_link = first(:link, 'Login')
+    expect(login_link[:href]).to eq('http://localhost:33000/stash/auth/google_oauth2')
+    login_link.click
+    expect(page).to have_content('My Datasets: Getting Started')
+  end
+end
--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -3,11 +3,11 @@ require 'capybara/dsl'
 require 'capybara/rails'
 require 'capybara/rspec'
 
-# ############################################################
-# Capybara config
+# ------------------------------------------------------------
+# Capybara
 
 Capybara.register_driver(:selenium) do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, args: ['--incognito'])
 end
 
 Capybara.javascript_driver = :chrome
@@ -15,21 +15,48 @@ Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
   config.default_max_wait_time = 10
   config.default_driver = :selenium
+  config.server_port = 33000
+  config.app_host = 'http://localhost:33000'
 end
 
 # ------------------------------------------------------------
-# Solr config
+# Solr
 
 require 'solr_helper'
 
 SolrHelper.start
 
 # ------------------------------------------------------------
-# Additional RSpec configuration
+# RSpec
 
 RSpec.configure do |config|
   # Treat specs in features/ as feature specs
   config.infer_spec_type_from_file_location!
 
+  # Mock OmniAuth login
+  config.before(:all) do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock(:shibboleth, {
+      uid: 'test@example.edu',
+      info: {
+        email: 'test@example.edu',
+        name: 'S. Testy McTestface',
+        test_domain: 'localhost'
+      }
+    })
+    OmniAuth.config.add_mock(:google_oauth2, {
+      uid: '555555555555555555555',
+      credentials: {
+        token: 'ya29.Ry4gVGVzdHkgTWNUZXN0ZmFjZQ'
+      },
+      info: {
+        email: 'test@example.edu.test-google-a.com',
+        name: 'G. Testy McTestface',
+        test_domain: 'localhost'
+      }
+    })
+  end
+
+  # Stop Solr when we're done
   config.after(:all) { SolrHelper.stop }
 end
