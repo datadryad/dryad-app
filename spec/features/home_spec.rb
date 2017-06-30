29bf1389b5c2454c7bab8a483a2c28b2f0396e2e
David Moles <david.moles@ucop.edu>
2017-06-30 11:45:28 -0700

    Add logout spec; extract shared code

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,6 @@
 require 'features_helper'
 
-describe 'GET /' do
+describe 'home' do
   before(:each) do
     visit('/')
   end
@@ -10,8 +10,6 @@ describe 'GET /' do
   end
 
   it 'is the home page' do
-    home_html_erb = File.read("#{STASH_ENGINE_PATH}/app/views/stash_engine/pages/home.html.erb")
-    title = home_html_erb[/page_title = '([^']+)'/, 1]
-    expect(page).to have_title(title)
+    expect(page).to have_title(home_page_title)
   end
 end
