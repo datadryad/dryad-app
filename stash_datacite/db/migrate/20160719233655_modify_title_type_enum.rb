8ccee5eeb61ff29c484aee50fb098787d416f7e0
Scott Fisher <scott.fisher@ucop.edu>
2017-07-05 16:42:49 -0700

    Trying to fix errors with a model that doesn't exist anymore.

--- stash_datacite/db/migrate/20160719233655_modify_title_type_enum.rb
+++ stash_datacite/db/migrate/20160719233655_modify_title_type_enum.rb
@@ -1,9 +1,15 @@
 class ModifyTitleTypeEnum < ActiveRecord::Migration
   def change
-    StashDatacite::Title.where(title_type: 'main').update_all(title_type: nil)
-    change_column_default(:dcs_titles, :title_type, nil)
-    change_table :dcs_titles do |t|
-      t.change :title_type, "ENUM('alternative_title', 'subtitle', 'translated_title') DEFAULT NULL"
-    end
+    execute QUERY1
+    execute QUERY2
   end
+
+  QUERY1 = <<-eos
+    UPDATE dcs_titles SET title_type = NULL
+    WHERE title_type = 'main'
+  eos
+
+  QUERY2 = <<-eos
+    ALTER TABLE dcs_titles MODIFY COLUMN `title_type` enum('alternativetitle','subtitle','translatedtitle') DEFAULT NULL;
+  eos
 end
