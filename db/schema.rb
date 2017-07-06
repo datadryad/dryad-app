d9d4cc1353b8c8e6865ac58ea6d8736a15c9b0fb
David Moles <david.moles@ucop.edu>
2017-07-06 11:41:47 -0700

    Update to reflect latest migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630204205) do
+ActiveRecord::Schema.define(version: 20170706174001) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
