4f6f4a554d2c565427b25eb0aa81c499a57f7c81
David Moles <david.moles@ucop.edu>
2017-07-11 11:48:21 -0700

    Double-check correct database, & clean it out before running tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'colorize'
 
 if (env = ENV['RAILS_ENV'])
   abort("Can't run tests in environment #{env}") if env != 'test'
@@ -12,6 +13,14 @@ require 'database_cleaner'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+def check_connection_config!
+  db_config = ActiveRecord::Base.connection_config
+  host = db_config[:host]
+  raise("Can't run destructive tests against non-local database #{host || 'nil'}") unless host == 'localhost'
+  msg = "Using database #{db_config[:database]} on host #{host} with username #{db_config[:username]}"
+  puts msg.colorize(:yellow)
+end
+
 RSpec.configure do |config|
   # allow test DB access from multiple connections
   config.use_transactional_fixtures = false
@@ -19,6 +28,16 @@ RSpec.configure do |config|
   # Treat specs in features/ as feature specs
   config.infer_spec_type_from_file_location!
 
-  config.before(:suite) { DatabaseCleaner.strategy = :deletion }
-  config.after(:each) { DatabaseCleaner.clean }
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :deletion
+
+    check_connection_config!
+
+    puts 'Clearing test database'.colorize(:yellow)
+    DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
