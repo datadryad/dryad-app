b979813b03d3d8444613997f04500bff33f9c019
David Moles <david.moles@ucop.edu>
2017-08-04 14:49:33 -0700

    Add spec for geolocation boxes

--- spec/features/add_geolocation_spec.rb
+++ spec/features/add_geolocation_spec.rb
@@ -33,4 +33,45 @@ describe 'add geolocation' do
       expect(new_point.longitude).to eq(longitude.to_f)
     end
   end
+
+  describe 'boxes' do
+    it 'adds by lat/long' do
+      bbox_button = first(:link_or_button, 'Bounding Box')
+      bbox_button.click
+
+      s_latitude = '37.8086'
+      w_longitude = '-122.3511'
+      
+      n_latitude = '47.6572'
+      e_longitude = '-122.2674'
+      
+      sw_latitude = find_blank_field_id('geolocation_box[sw_latitude]')
+      fill_in sw_latitude, with: s_latitude
+
+      sw_longitude = find_blank_field_id('geolocation_box[sw_longitude]')
+      fill_in sw_longitude, with: w_longitude
+
+      ne_latitude = find_blank_field_id('geolocation_box[ne_latitude]')
+      fill_in ne_latitude, with: n_latitude
+
+      ne_longitude = find_blank_field_id('geolocation_box[ne_longitude]')
+      fill_in ne_longitude, with: e_longitude
+
+      add_geo_box_button = first(:link_or_button, 'add_geo_point') # yes, this button is still 'point'
+      add_geo_box_button.click
+
+      wait_for_ajax!
+
+      locations = page.find('div .c-locations')
+      expect(locations).to have_content(/SW\s+#{s_latitude},\s+#{w_longitude}/)
+      expect(locations).to have_content(/NE\s+#{n_latitude},\s+#{e_longitude}/)
+
+      new_box = StashDatacite::GeolocationBox.from_resource_id(current_resource_id).first
+      expect(new_box).not_to be_nil
+      expect(new_box.sw_latitude).to eq(s_latitude.to_f)
+      expect(new_box.sw_longitude).to eq(w_longitude.to_f)
+      expect(new_box.ne_latitude).to eq(n_latitude.to_f)
+      expect(new_box.ne_longitude).to eq(e_longitude.to_f)
+    end
+  end
 end
