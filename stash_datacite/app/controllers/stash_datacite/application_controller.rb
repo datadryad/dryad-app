63ea5fd7c7840db3119e7f97a3c0bc02c1189d77
Scott Fisher <scott.fisher@ucop.edu>
2017-08-29 09:52:13 -0700

    trying to add catching of annoying Bing js error

--- stash_datacite/app/controllers/stash_datacite/application_controller.rb
+++ stash_datacite/app/controllers/stash_datacite/application_controller.rb
@@ -1,7 +1,11 @@
+require 'error/error_handler'
+
 module StashDatacite
   class ApplicationController < ::ApplicationController
     helper StashEngine::ApplicationHelper
 
     include StashEngine::SharedController
+
+    include Error::ErrorHandler
   end
 end
