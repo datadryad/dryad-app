be32b97012fb46e2caf1ba5f156436a6008a1eb7
Scott Fisher <scott.fisher@ucop.edu>
2017-08-29 12:15:36 -0700

    Revert "trying to add catching of annoying Bing js error" We will need to use Rack or HoneyBader to do this.
    This reverts commit 63ea5fd7c7840db3119e7f97a3c0bc02c1189d77.

--- stash_datacite/app/controllers/stash_datacite/application_controller.rb
+++ stash_datacite/app/controllers/stash_datacite/application_controller.rb
@@ -1,11 +1,7 @@
-require 'error/error_handler'
-
 module StashDatacite
   class ApplicationController < ::ApplicationController
     helper StashEngine::ApplicationHelper
 
     include StashEngine::SharedController
-
-    include Error::ErrorHandler
   end
 end
