9b9c31c767883cdafe3dcb224fbf3d37d0f4e553
Scott Fisher <scott.fisher@ucop.edu>
2017-09-01 13:10:26 -0700

    make deploy of version numbers more flexible

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,7 +78,7 @@ namespace :deploy do
   task :update_config do
     on roles(:app) do
       my_branch = fetch(:branch, 'development')
-      my_branch = "origin/#{my_branch}" unless my_branch.match(/^[v\d\.]+$/) #git acts differently with branch vs tag
+      my_branch = "origin/#{my_branch}" unless my_branch.match(/^[v\d\.]{3,}-?.*.$/) #git acts differently with tags (regex for version #s)
       execute "cd #{deploy_to}/shared; git fetch --tags; git fetch --all; git reset --hard #{my_branch}"
     end
   end
