d70df9c2f3cd4e9ab67d82ad6fae6f9315bdc068
Scott Fisher <scott.fisher@ucop.edu>
2017-11-06 13:32:29 -0800

    Cleanup of embargo and using same security methods as other ajax calls.

--- stash_engine/app/controllers/stash_engine/embargoes_controller.rb
+++ stash_engine/app/controllers/stash_engine/embargoes_controller.rb
@@ -2,64 +2,19 @@ require_dependency 'stash_engine/application_controller'
 
 module StashEngine
   class EmbargoesController < ApplicationController
-    before_action :set_embargo, only: [:delete]
+
+    before_action :ajax_require_modifiable, only: %i[changed]
 
     def messages
       @messages ||= []
     end
     helper_method :messages
 
-    # GET /embargos/new
-    def new
-      @embargo = Embargo.new(resource_id: params[:resource_id])
-    end
-
-    # GET /embargos/1/edit
-    def edit; end
-
-    # POST /embargos
-    def create
-      @embargo = Embargo.new(embargo_params)
-      respond_to do |format|
-        if @embargo.save
-          format.js
-        else
-          format.html { render :new }
-        end
-      end
-    end
-
-    # PATCH/PUT /embargos/1
-    def update # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-      @embargo = Embargo.where(resource_id: embargo_params[:resource_id]).first
-      @resource = Resource.find(embargo_params[:resource_id])
-      respond_to do |format|
-        if embargo_params[:end_date].to_date == Date.today.to_date
-          @embargo.destroy
-          format.js { render 'delete' }
-        else
-          @embargo.update(embargo_params)
-          format.js { render template: 'stash_datacite/shared/update.js.erb' }
-        end
-      end
-    end
-
-    # DELETE /embargos/1
-    def delete
-      @embargo.destroy
-      respond_to do |format|
-        format.js
-      end
-    end
-
     # this is a generic action to handle all create/modify/delete actions for an embargo since it's just one form
     # and using the rest methods in this case is overly complicated and annoying
-    def changed # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def changed
       respond_to do |format|
         format.js do
-          @resource = Resource.find(params[:resource_id])
-          return unless @resource.user.id == current_user.id
-
           if params['when-to-publish'] == 'in_future'
             set_embargo_end_date(params['yyyyEmbargo'], params['mmEmbargo'], params['ddEmbargo'])
           elsif (@embargo = @resource.embargo)
@@ -70,6 +25,10 @@ module StashEngine
     end
 
     private
+
+    def resource
+      @resource ||= Resource.find(params[:resource_id])
+    end
 
     def set_embargo_end_date(year, month, day)
       end_time = parse(year, month, day)
