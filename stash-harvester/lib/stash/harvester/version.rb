b8a5dd48f6e4ff040fdde2a5fdb5ccfd4839ad62
Scott Fisher <scott.fisher@ucop.edu>
2017-12-05 16:44:28 -0800

    Bumping version number so deploy/gemfile will do something with changes.

--- stash-harvester/lib/stash/harvester/version.rb
+++ stash-harvester/lib/stash/harvester/version.rb
@@ -4,7 +4,7 @@ module Stash
     NAME = 'stash-harvester'.freeze
 
     # The version of this gem
-    VERSION = '0.0.2'.freeze
+    VERSION = '0.0.3'.freeze
 
     # The copyright notice for this gem
     COPYRIGHT = 'Copyright (c) 2015 The Regents of the University of California'.freeze
