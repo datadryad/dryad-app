e6d6cbbbb7937f0dc6993fae2a3e4d82e6c13fba
Scott Fisher <scott.fisher@ucop.edu>
2017-12-06 13:37:57 -0800

    Adding datasets controller and switching tests to rspec rather than minitest or unit test or whatever.

--- stash_api/test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount StashApi::Engine => "/stash_api"
-end
--- stash_api/test/stash_api_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StashApiTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, StashApi
-  end
-end
