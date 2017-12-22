2af89d8c64c95e2585c89b9bd2987a7d6a61129c
Scott Fisher <scott.fisher@ucop.edu>
2017-12-22 14:04:44 -0800

    GET datasets schema/openapi changes

--- stash_api/lib/stash_api/engine.rb
+++ stash_api/lib/stash_api/engine.rb
@@ -7,6 +7,8 @@ module StashApi
     end
 
     # Initializer to combine this engines static assets with the static assets of the hosting site.
+    # this might help with annoying caching
+    # https://stackoverflow.com/questions/6962896/how-do-i-prevent-rails-3-1-from-caching-static-assets-to-rails-cache
     initializer 'static assets' do |app|
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
