24c31425d754552b8ca5ca712c86a73c868b1f39
Scott Fisher <scott.fisher@ucop.edu>
2018-01-09 12:55:01 -0800

    Initialize app_config as full ostruct

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -3,6 +3,6 @@ require 'yaml'
 ac = YAML.load_file(File.join(Rails.root, 'config', 'app_config.yml'))[Rails.env]
 
 # this will make the config available under the APP_CONFIG constant and methods like APP_CONFIG.metadata_engines
-APP_CONFIG = OpenStruct.new(ac)
+APP_CONFIG = ac.to_ostruct
 
 ENV['SSL_CERT_FILE'] = APP_CONFIG.ssl_cert_file if APP_CONFIG.ssl_cert_file
