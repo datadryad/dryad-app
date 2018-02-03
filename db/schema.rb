0da648695edecbe6e4c6e48bf250bb553c8f9b37
Scott Fisher <scott.fisher@ucop.edu>
2018-02-02 16:27:05 -0800

    Adding script to fill in missing sizes for old files.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124205901) do
+ActiveRecord::Schema.define(version: 20180202220245) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
