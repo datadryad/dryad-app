75c6348af4d89f4e541dbfccd31791c837086bc2
Scott Fisher <scott.fisher@ucop.edu>
2018-03-27 09:41:25 -0700

    adding tests, not working yet

---
+++ stash_api/spec/models/stash_api/dataset_parser_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+module StashApi
+  RSpec.describe DatasetParser do
+    before(:each) do
+    end
+
+    after(:each) do
+    end
+
+    describe :initialize do
+      it 'creates a dataset parser' do
+        # TODO: make real test
+        expect(true).to eq(true)
+      end
+    end
+  end
+end
