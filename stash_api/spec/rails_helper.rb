f276f8a4868702437ce40a0550913ecf12a08fae
Scott Fisher <scott.fisher@ucop.edu>
2018-03-27 14:57:18 -0700

    Trying to get test running at basic level

--- stash_api/spec/rails_helper.rb
+++ stash_api/spec/rails_helper.rb
@@ -1,11 +1,11 @@
-# frozen_string_literal: true
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path("../../spec/dummy/config/environment", __FILE__)
+ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
+# require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -57,3 +57,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+config.include StashEngine::Engine.routes.url_helpers
