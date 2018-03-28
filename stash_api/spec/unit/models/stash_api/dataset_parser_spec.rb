4fc0e60a9a33bdd0312624c371ec101bb22ecd88
Scott Fisher <scott.fisher@ucop.edu>
2018-03-28 11:09:15 -0700

    Got a fake test to run.  We're probably still missing things, but getting closer.

--- stash_api/spec/unit/models/stash_api/dataset_parser_spec.rb
+++ stash_api/spec/unit/models/stash_api/dataset_parser_spec.rb
@@ -5,14 +5,15 @@ module StashApi
     before(:each) do
       app = double(Rails::Application)
       allow(app).to receive(:stash_mount).and_return('/api')
-      allow(StashEngine).to receive(:app).and_return(app)
+      # TODO: We need to figure out how to load the other engines without errors (spec_helper probably)
+      # allow(StashEngine).to receive(:app).and_return(app)
     end
 
     after(:each) do
     end
 
     describe :initialize do
-      it 'creates a dataset parser' do
+      it 'creates a dataset parser--this is a fake test for now' do
         # TODO: make real test
         expect(true).to eq(true)
       end
