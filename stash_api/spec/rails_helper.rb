24b177e81bac05730e58e534ab8420e40cb1e532
Scott Fisher <scott.fisher@ucop.edu>
2018-04-02 16:57:33 -0700

    Making stash api have file uploads and display errors

--- stash_api/spec/rails_helper.rb
+++ stash_api/spec/rails_helper.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../spec/dummy/config/environment', __FILE__)
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
+ENGINE_RAILS_ROOT = ::File.join(File.dirname(__FILE__), '../')
 # require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
