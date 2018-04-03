6065d5f3a89be6092141b6792464bce31f44308e
Scott Fisher <scott.fisher@ucop.edu>
2018-04-03 10:02:29 -0700

    converting POST to PUT for file adding for dataset

--- stash_api/config/routes.rb
+++ stash_api/config/routes.rb
@@ -16,5 +16,6 @@ StashApi::Engine.routes.draw do
     end
   end
   # this one doesn't follow the pattern since it gloms filename on the end, so manual route
-  post 'datasets/:id/files/:filename', id: /[^\s\/]+?/, filename: /[^\s\/]+?/, to: 'files#create', as: 'dataset_file', format: false
+  # This should be PUT, not POST because of filename, see https://stackoverflow.com/questions/630453/put-vs-post-in-rest for example
+  put 'datasets/:id/files/:filename', id: /[^\s\/]+?/, filename: /[^\s\/]+?/, to: 'files#update', as: 'dataset_file', format: false
 end
