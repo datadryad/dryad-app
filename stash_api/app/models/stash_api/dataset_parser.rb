d54261f5c0cd46c82a3667c11f994e4e9c658b7c
Scott Fisher <scott.fisher@ucop.edu>
2018-04-19 15:20:33 -0700

    Making it take all the different pieces of metadata in the API.

--- stash_api/app/models/stash_api/dataset_parser.rb
+++ stash_api/app/models/stash_api/dataset_parser.rb
@@ -2,7 +2,7 @@ module StashApi
   # takes a dataset hash, parses it out and saves it to the appropriate places in the database
   class DatasetParser
 
-    TO_PARSE = %w[Funders Methods UsageNotes Keywords].freeze
+    TO_PARSE = %w[Funders Methods UsageNotes Keywords RelatedWorks Locations].freeze
 
     def initialize(hash: nil, id: nil, user:)
       @hash = hash
---
+++ stash_api/app/models/stash_api/dataset_parser/related_works.rb
@@ -0,0 +1,43 @@
+require_relative 'base_parser'
+
+module StashApi
+  class DatasetParser
+    class RelatedWorks < StashApi::DatasetParser::BaseParser
+
+      # Related Works look like this and use a constrainted list of relationships and identifier types
+      # "relatedWorks": [
+      #   {
+      #     "relationship": "Cites",
+      #     "identifierType": "URL",
+      #     "identifier": "http://example.org/cats"
+      #   } ]
+
+      # lists of allowed values
+      #   StashDatacite::RelatedIdentifier::RelationTypes
+      #   StashDatacite::RelatedIdentifier::RelatedIdentifierTypes
+      LowerRelationTypes = StashDatacite::RelatedIdentifier::RelationTypes.map(&:downcase)
+      LowerIdentifierTypes = StashDatacite::RelatedIdentifier::RelatedIdentifierTypes.map(&:downcase)
+
+      def parse # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        clear
+        return if @hash['relatedWorks'].blank?
+        @hash['relatedWorks'].each do |rw|
+          next if rw.blank? || !LowerRelationTypes.include?(rw['relationship']&.downcase) ||
+              !LowerIdentifierTypes.include?(rw['identifierType']&.downcase)
+          @resource.related_identifiers << StashDatacite::RelatedIdentifier.create(
+            related_identifier: rw['identifier'],
+            related_identifier_type: rw['identifierType']&.downcase,
+            relation_type: rw['relationship']&.downcase
+          )
+        end
+      end
+
+      private
+
+      def clear
+        @resource.related_identifiers.destroy_all
+      end
+
+    end
+  end
+end
