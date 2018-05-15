e7fc8ef2a9cbbb6ce1f77a865a17ecc24edb07c1
David Moles <david.moles@ucop.edu>
2018-05-15 10:37:28 -0700

    Use Travis chrome add-on instead of chromium-chromedriver

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -15,7 +15,7 @@ Capybara.register_driver(:selenium) do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: ['--incognito'])
+    options: Selenium::WebDriver::Chrome::Options.new(args: %w[incognito no-sandbox disable-gpu'])
   )
 end
 
