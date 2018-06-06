99955627a9b4a034198b7727ca2d8f68bb73288f
Scott Fisher <scott.fisher@ucop.edu>
2018-06-06 13:35:31 -0700

    In progress changes for doing citations with textual list of citations, doesn't yet affect functionality.

--- stash_engine/lib/stash/datacite_metadata.rb
+++ stash_engine/lib/stash/datacite_metadata.rb
@@ -13,20 +13,35 @@ module Stash
       @doi = doi[4..-1] if doi.downcase.start_with?('doi:')
       match_data = %r{^https?://doi.org/(\S+\/\S+)$}.match(@doi)
       @doi = match_data[1] if match_data
+      @metadata = nil
     end
 
-    def descriptive_metadata
+    def raw_metadata
+      return @metadata unless @metadata.nil?
       response = RestClient.get "#{DATACITE_BASE}#{@doi}"
-      return JSON.parse(response.body)
+      return @metadata = JSON.parse(response.body)
     rescue RestClient::Exception => ex
       logger.error("#{Time.new} Could not get response from DataCite for metadata lookcup #{@doi}")
       logger.error("#{Time.new} #{ex}")
       return nil
     end
 
+    def author_names
+      names = raw_metadata.dig('data', 'attributes', 'author')
+      return '' if names.blank?
+      names = names.map { |i| i['literal'] }
+      return "#{names.first} et al." if names.length > 4
+      names.join('; ')
+    end
+
     def logger
       Rails.logger
     end
+
+    # random list of DOIs
+    # dois = %w[10.3352/jeehp.2013.10.3 10.7280/D1NP4M 10.1130/2012.MCH101 10.7932/BDSN 10.1016/j.neuron.2018.04.031
+    #           10.1016/j.cub.2018.04.033 10.1016/j.ymeth.2015.09.003 10.13016/M2WK6V 10.3886/ICPSR36151.v5
+    #           10.1097/00002030-199011000-00007 10.1371/journal.pcbi.0030225 10.1073/pnas.1424184112 10.1101/057307]
 
     # getting citation information (where m is the metadata returned)
     # Authors: these are full names not separated last, first
