6933d29332db3a6ff1643d10dec2c480674e14cf
Scott Fisher <scott.fisher@ucop.edu>
2018-06-08 17:00:34 -0700

    schema changed for db for caching citations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530184143) do
+ActiveRecord::Schema.define(version: 20180608171320) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -322,6 +322,16 @@ ActiveRecord::Schema.define(version: 20180530184143) do
 
   add_index "stash_engine_authors", ["author_orcid"], name: "index_stash_engine_authors_on_author_orcid", length: {"author_orcid"=>20}, using: :btree
   add_index "stash_engine_authors", ["resource_id"], name: "index_stash_engine_authors_on_resource_id", using: :btree
+
+  create_table "stash_engine_counter_citations", force: :cascade do |t|
+    t.integer  "identifier_id", limit: 4
+    t.text     "citation",      limit: 65535
+    t.text     "doi",           limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "stash_engine_counter_citations", ["doi"], name: "index_stash_engine_counter_citations_on_doi", length: {"doi"=>20}, using: :btree
 
   create_table "stash_engine_counter_stats", force: :cascade do |t|
     t.integer  "identifier_id",              limit: 4
