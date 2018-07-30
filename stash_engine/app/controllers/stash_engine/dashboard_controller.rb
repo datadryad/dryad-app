b5b44ab28ff14536666a0fedba80a0481302d336
daisie_local <daisieh@gmail.com>
2018-07-30 12:10:49 -0700

    Add views, routes, controller methods for migration

--- stash_engine/app/controllers/stash_engine/dashboard_controller.rb
+++ stash_engine/app/controllers/stash_engine/dashboard_controller.rb
@@ -12,6 +12,23 @@ module StashEngine
 
     def upload_basics; end
 
+    def migrate_data_mail
+      current_user.old_dryad_email = params[:email]
+      current_user.set_migration_token
+      current_user.save
+      StashEngine::MigrationMailer.migration_email(email: current_user.old_dryad_email, code: current_user.migration_token).deliver_now
+    end
+
+    def migrate_data
+      unless User.find_by_migration_token(params[:code]).nil?
+        if User.find_by_migration_token(params[:code]).id == current_user.id
+          render 'stash_engine/dashboard/migrate_successful'
+        end
+      end
+    end
+
+    def migrate_successful; end
+
     # an AJAX wait to allow in-progress items to complete before continuing.
     def ajax_wait
       respond_to do |format|
