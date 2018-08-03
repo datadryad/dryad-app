6b4d5fc36292ff1d4a566e489a54fbb0d6a71ca4
Scott Fisher <scott.fisher@ucop.edu>
2018-08-02 17:00:13 -0700

    committing automatic schema.rb from rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180713205424) do
+ActiveRecord::Schema.define(version: 20180718223728) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
@@ -455,15 +455,17 @@ ActiveRecord::Schema.define(version: 20180713205424) do
   add_index "stash_engine_submission_logs", ["resource_id"], name: "index_stash_engine_submission_logs_on_resource_id", using: :btree
 
   create_table "stash_engine_users", force: :cascade do |t|
-    t.text     "first_name", limit: 65535
-    t.text     "last_name",  limit: 65535
-    t.text     "email",      limit: 65535
-    t.datetime "created_at",                                null: false
-    t.datetime "updated_at",                                null: false
-    t.text     "tenant_id",  limit: 65535
+    t.text     "first_name",      limit: 65535
+    t.text     "last_name",       limit: 65535
+    t.text     "email",           limit: 65535
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.text     "tenant_id",       limit: 65535
     t.datetime "last_login"
-    t.string   "role",       limit: 9,     default: "user"
-    t.string   "orcid",      limit: 191
+    t.string   "role",            limit: 9,     default: "user"
+    t.string   "orcid",           limit: 191
+    t.string   "migration_token", limit: 191
+    t.string   "old_dryad_email", limit: 191
   end
 
   add_index "stash_engine_users", ["email"], name: "index_stash_engine_users_on_email", length: {"email"=>50}, using: :btree
