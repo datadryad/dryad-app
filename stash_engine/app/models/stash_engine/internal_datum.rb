7e1715df4f276327bcf4e0b44f0f80a7bf64522e
daisie_local <daisieh@gmail.com>
2018-09-07 22:06:07 -0700

    create basic MVC in stash_engine for internal data

---
+++ stash_engine/app/models/stash_engine/internal_datum.rb
@@ -0,0 +1,12 @@
+module StashEngine
+  class InternalDatum < ActiveRecord::Base
+    belongs_to :resource
+    validates :data_type, inclusion: { in: %w[manuscriptNumber mismatchedDOI duplicateItem formerManuscriptNumber publicationName], message: '%{value} is not a valid data type'}
+    validates :data_type, presence: true
+    validates :value, presence: true
+
+    def self.data_type(type)
+      where("data_type = ?", type)
+    end
+  end
+end
