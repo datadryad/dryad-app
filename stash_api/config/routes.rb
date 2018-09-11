5533f6dd443c664d9a0e327e895bca593d508781
daisie_local <daisieh@gmail.com>
2018-09-11 12:56:15 -0700

    nesting

--- stash_api/config/routes.rb
+++ stash_api/config/routes.rb
@@ -10,12 +10,12 @@ StashApi::Engine.routes.draw do
     get 'download', on: :member
     resources :versions, shallow: true do
       get 'download', on: :member
+      resources :internal_data, shallow: true
       resources :files, shallow: true do
         resources :downloads
       end
     end
   end
-  resources :internal_data
   # this one doesn't follow the pattern since it gloms filename on the end, so manual route
   # This should be PUT, not POST because of filename, see https://stackoverflow.com/questions/630453/put-vs-post-in-rest for example
   put 'datasets/:id/files/:filename', id: /[^\s\/]+?/, filename: /[^\s\/]+?/, to: 'files#update', as: 'dataset_file', format: false
