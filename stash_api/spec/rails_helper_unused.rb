3bd6eed86a418bbdc2efadf1b4271629db2629ad
Scott Fisher <scott.fisher@ucop.edu>
2018-09-19 11:59:47 -0700

    basic factories in place

--- stash_api/spec/rails_helper_unused.rb
+++ stash_api/spec/rails_helper_unused.rb
@@ -31,8 +31,6 @@ ActiveRecord::Migration.maintain_test_schema!
 
 Dir[StashApi::Engine.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-byebug
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
