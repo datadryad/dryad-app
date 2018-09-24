bddeb0c80900ca42d09ae54b3cc3a1f2d194a61c
Scott Fisher <scott.fisher@ucop.edu>
2018-09-24 13:14:38 -0700

    Adding tests for user output generation for API for the "model" and fixing some of the FactoryBot problems (I think).

--- stash_api/spec/factories/stash_engine/users.rb
+++ stash_api/spec/factories/stash_engine/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     email { 'juanita.collins@example.org' }
     tenant_id { 'exemplia' }
     role { 'user' }
+    orcid { '1098-415-1212' }
     migration_token { 'xxxxxx' }
     old_dryad_email { 'lolinda@example.com' }
     eperson_id { 37 }
