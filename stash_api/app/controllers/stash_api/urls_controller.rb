3218e1f41341f04fb4b829055fb6669e145c28e4
Scott Fisher <scott.fisher@ucop.edu>
2018-09-28 15:28:38 -0700

    Getting basic url validations working and refactor of some of the validation code so it would be available to both UI and API more easily.

--- stash_api/app/controllers/stash_api/urls_controller.rb
+++ stash_api/app/controllers/stash_api/urls_controller.rb
@@ -1,4 +1,6 @@
 require_dependency 'stash_api/application_controller'
+require 'fileutils'
+require 'stash/url_translator'
 
 # rubocop:disable Metrics/ClassLength
 module StashApi
@@ -17,11 +19,20 @@ module StashApi
     #   mimeType: 'application/pdf' }
     #   The path will be derived from the URL and the status will be created
     def create
-      byebug
+      validation_hash = check_url(params[:url])
+      fu = StashEngine::FileUpload.create(validation_hash)
+      file = StashApi::File.new(file_id: fu.id)
       respond_to do |format|
         format.json { render json: file.metadata, status: 201 }
         format.html { render text: UNACCEPTABLE_MSG, status: 406 }
       end
+    end
+
+
+    def check_url(url)
+      url_translator = Stash::UrlTranslator.new(url)
+      validator = StashEngine::UrlValidator.new(url: url_translator.direct_download || url)
+      validator.upload_attributes_from(translator: url_translator, resource: @resource)
     end
 
   end
