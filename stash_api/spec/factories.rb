2a4a230bf7b974fa1ea37fd176700eeefd995af1
daisie_local <daisieh@gmail.com>
2018-10-19 15:54:07 -0400

    consolidate all factory definitions into one file

---
+++ stash_api/spec/factories.rb
@@ -0,0 +1,56 @@
+FactoryBot.define do
+  factory(:file_upload, class: StashEngine::FileUpload) do
+    id { nil }
+    upload_file_name { 'test.txt' }
+    upload_content_type { 'text/plain' }
+    upload_file_size { 12_345 }
+    resource_id { nil }
+    temp_file_path { '/home/ubuntu/dryad/uploads/25/test.txt' }
+    file_state { 'created' }
+    url { 'http://example.org/test.txt' }
+    status_code { 200 }
+    timed_out { 0 }
+    original_url { nil }
+    cloud_service { nil }
+  end
+
+  factory(:identifier, class: StashEngine::Identifier) do
+    identifier { '138/238/2238' }
+    identifier_type { 'DOI' }
+  end
+  
+  factory(:resource_state, class: StashEngine::ResourceState) do
+    user_id { nil }
+    resource_state { 'in_progress' }
+    resource_id { nil }
+  end
+
+  factory(:resource, class: StashEngine::Resource) do
+    user_id { nil }
+    current_resource_state_id { nil }
+    has_geolocation { 0 }
+    identifier_id { nil }
+    title { 'My Cats Have Fleas' }
+    current_editor_id { nil }
+    tenant_id { 'exemplia' }
+    skip_datacite_update { false }
+  end
+
+  factory(:user, class: StashEngine::User) do
+    first_name { 'Juanita' }
+    last_name { 'Collins' }
+    email { 'juanita.collins@example.org' }
+    tenant_id { 'exemplia' }
+    role { 'user' }
+    orcid { '1098-415-1212' }
+    migration_token { 'xxxxxx' }
+    old_dryad_email { 'lolinda@example.com' }
+    eperson_id { 37 }
+  end
+
+  factory(:version, class: StashEngine::Version) do
+    version { 1 }
+    resource_id { nil }
+    merritt_version { 1 }
+  end
+end
--- stash_api/spec/factories/stash_engine/resource_states.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory(:resource_state, class: StashEngine::ResourceState) do
-    user_id { nil }
-    resource_state { 'in_progress' }
-    resource_id { nil }
-  end
-end
