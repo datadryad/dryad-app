53b37083898e15e479b062eb9faf519b6e66e181
Scott Fisher <scott.fisher@ucop.edu>
2018-11-05 15:57:43 -0800

    Rubocop fixes for admin area.

--- stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
+++ stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
@@ -7,12 +7,10 @@ module StashEngine
 
     # the admin datasets main page showing users and stats, but slightly different in scope for superusers vs tenant admins
     def index
-      my_tenant = ( current_user.role == 'admin' ? current_user.tenant : nil )
-      @all_stats = Stats.new #
-      @seven_day_stats = Stats.new(tenant_id: my_tenant, since: (Time.new - 7.days) )
+      my_tenant = (current_user.role == 'admin' ? current_user.tenant : nil)
+      @all_stats = Stats.new
+      @seven_day_stats = Stats.new(tenant_id: my_tenant, since: (Time.new - 7.days))
     end
-
-    private
 
   end
 end
