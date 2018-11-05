629b60e3550e8f30c616baa24bdefed169e00702
Scott Fisher <scott.fisher@ucop.edu>
2018-11-05 15:55:40 -0800

    Adding layouts for the admin area.

--- stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
+++ stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
@@ -10,11 +10,6 @@ module StashEngine
       my_tenant = ( current_user.role == 'admin' ? current_user.tenant : nil )
       @all_stats = Stats.new #
       @seven_day_stats = Stats.new(tenant_id: my_tenant, since: (Time.new - 7.days) )
-
-      # prepare filter select lists to be used by options_for_select, 1st or key is the text, 2nd is the hidden value in list
-      @institution_list = StashEngine::Tenant.all.map{|item| [ item.short_name, item.tenant_id ] }
-      @status_list = CurationActivity.validators_on(:status).first.options[:in] # get the list of valid values from model
-      byebug
     end
 
     private
