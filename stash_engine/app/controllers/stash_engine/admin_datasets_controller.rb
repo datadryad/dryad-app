2ab76124885f438add351ed530b847bfc9eeda92
Scott Fisher <scott.fisher@ucop.edu>
2018-11-07 13:07:36 -0800

    in the middle of adding paging to admin tables.

--- stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
+++ stash_engine/app/controllers/stash_engine/admin_datasets_controller.rb
@@ -4,13 +4,23 @@ module StashEngine
   class AdminDatasetsController < ApplicationController
     include SharedSecurityController
     before_action :require_admin
+    before_action :setup_paging
 
     # the admin datasets main page showing users and stats, but slightly different in scope for superusers vs tenant admins
     def index
       my_tenant = (current_user.role == 'admin' ? current_user.tenant : nil)
       @all_stats = Stats.new
       @seven_day_stats = Stats.new(tenant_id: my_tenant, since: (Time.new - 7.days))
+
+      # these options are added by kaminari for paging
+      @ds_identifiers = Identifier.page(@page).per(@page_size)
     end
 
+    private
+
+    def setup_paging
+      @page = params[:page] || '1'
+      @page_size = (params[:page_size].blank? || params[:page_size] != '1000000' ? '10' : '1000000')
+    end
   end
 end
