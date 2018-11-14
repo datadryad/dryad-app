4c50a0498f7f1980b8ccbb5a26737fa7825f2f67
Scott Fisher <scott.fisher@ucop.edu>
2018-11-14 10:57:05 -0800

    Removing the old method of clearing since it's no longer working with table changes.

--- script/clear_data.rb
+++ script/clear_data.rb
@@ -1,28 +1,5 @@
 module ClearData
 
-  def self.clear
-    to_truncate = %w(bookmarks dcs_affiliations_contributors
-      dcs_alternate_identifiers dcs_contributors dcs_dates dcs_descriptions
-      dcs_formats dcs_geo_location_boxes dcs_geo_location_places dcs_geo_location_points dcs_geo_locations
-      dcs_languages dcs_name_identifiers dcs_publication_years dcs_publishers dcs_related_identifiers
-      dcs_resource_types dcs_rights dcs_sizes dcs_subjects_stash_engine_resources
-      dcs_versions searches stash_engine_authors stash_engine_embargoes stash_engine_file_uploads stash_engine_identifiers
-      stash_engine_resource_states stash_engine_resource_usages stash_engine_resources stash_engine_submission_logs
-      stash_engine_versions users)
-
-    to_truncate.each do |t|
-      query = "TRUNCATE TABLE #{t}"
-      result = ActiveRecord::Base.connection.execute(query)
-      puts "Truncating table #{t}"
-    end
-
-    query = "DELETE FROM dcs_affiliations WHERE abbreviation IS NULL AND short_name IS NULL"
-    result = ActiveRecord::Base.connection.execute(query)
-    puts "Removing extra records from affiliations"
-
-    clear_solr
-  end
-
   def self.clear_datasets
     StashEngine::Identifier.all.each do |iden|
       puts "Destroying #{iden.identifier}"
