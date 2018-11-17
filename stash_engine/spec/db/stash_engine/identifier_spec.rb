737179f5674d97e8e99071dc1c2ec3974324c0bc
Scott Fisher <scott.fisher@ucop.edu>
2018-11-16 19:08:40 -0800

    trying to get travis not to freak out. works locally

--- stash_engine/spec/db/stash_engine/identifier_spec.rb
+++ stash_engine/spec/db/stash_engine/identifier_spec.rb
@@ -127,9 +127,8 @@ module StashEngine
 
       describe '#update_search_words!' do
         before(:each) do
-          @identifier.update(latest_resource_id: res3.id)
-          @identifier.reload
-          sleep(1) # what the hell is wrong with travis?
+          @identifier.update!(latest_resource_id: res3.id)
+          @identifier = Identifier.find(@identifier.id) # why won't travis reload this record?
           res = @identifier.latest_resource
           res.update(title: 'Frolicks with the seahorses')
           Author.create(author_first_name: 'Joanna', author_last_name: 'Jones', author_orcid: '33-22-4838-3322', resource_id: res.id)
