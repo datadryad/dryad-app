98445cd2d9ea567a75f5d0e62afdccd224cd3088
Scott Fisher <scott.fisher@ucop.edu>
2018-11-28 10:09:30 -0800

    Getting lightbox modal dialog finished for changing status.

--- stash_engine/app/controllers/stash_engine/curation_activity_controller.rb
+++ stash_engine/app/controllers/stash_engine/curation_activity_controller.rb
@@ -4,7 +4,8 @@ module StashEngine
   class CurationActivityController < ApplicationController
     include SharedSecurityController
 
-    before_action :require_curator
+    before_action :require_curator, only: :index
+    before_action :ajax_require_curator, only: :status_change
 
     # GET /resources/{id}/curation_activities
     def index
@@ -16,10 +17,16 @@ module StashEngine
       end
     end
 
-    # POST /curation_activity
-    def create
-
+    # A special action for changing curation status from ajax and admin/curator area, creates new status
+    # in list for identifier, which is the ID that is passed in.  It does ajax mumbo-jumbo for admin area.
+    # this isn't RESTful
+    def status_change
+      respond_to do |format|
+        format.js do
+          @activity = CurationActivity.create(identifier_id: params[:id], status: params[:status] , user_id: current_user.id,
+                                              note: params[:note])
+        end
+      end
     end
-
   end
 end
