f870b20232811b9a10f9d4b3955111d34b708e8a
Scott Fisher <scott.fisher@ucop.edu>
2018-12-10 13:25:42 -0800

    Create and refresh the internal data.

--- stash_engine/app/controllers/stash_engine/internal_data_controller.rb
+++ stash_engine/app/controllers/stash_engine/internal_data_controller.rb
@@ -6,7 +6,9 @@ module StashEngine
 
     before_action :require_curator
 
-    # GET /resources/{id}/internal_data
+    # I don't believe the following is used except as a test, internal data list is currently in the admin datasets
+    # controller with some other things on the same page.
+    # GET /identifiers/:identifier_id/internal_data
     def index
       ident = Identifier.find_with_id(Resource.find_by!(id: params[:resource_id]).identifier_str)
       @internal_data = InternalDatum.where(identifier_id: ident.identifier_id) unless ident.blank?
@@ -16,5 +18,17 @@ module StashEngine
       end
     end
 
+    # POST /identifiers/:identifier_id/internal_data
+    def create
+      @identifier = Identifier.find(params[:identifier_id])
+      respond_to do |format|
+        format.js do
+          # right now, the only way to add is by ajax, UJS, so Javascript from the dataset admin area
+          InternalDatum.create(identifier_id: @identifier.id, data_type: params[:internal_datum][:data_type],
+                                                 value: params[:internal_datum][:value])
+          @internal_data = InternalDatum.where(identifier_id: @identifier.id)
+        end
+      end
+    end
   end
 end
