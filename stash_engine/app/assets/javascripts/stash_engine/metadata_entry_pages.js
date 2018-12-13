c82a0aa23ce8a8afd8f64585b5160486fb1038c8
Scott Fisher <scott.fisher@ucop.edu>
2018-12-12 17:18:31 -0800

    This seems to block AJAX for saving, even though the original code doesn't work as intended due to security limitations and Safari.

--- stash_engine/app/assets/javascripts/stash_engine/metadata_entry_pages.js
+++ stash_engine/app/assets/javascripts/stash_engine/metadata_entry_pages.js
@@ -1,37 +1,52 @@
-function trapNavigation(){
-  $(function () {
-    // only attach these events on metadata entry pages
-    if($('body.metadata_entry_pages_find_or_create').length < 1){
-      return;
-    }
-    $('a, #dashboard_path, #upload_path').click(function(my_evt){
+function trapNavigation() {
+    $(function () {
+        // only attach these events on metadata entry pages
+        if ($('body.metadata_entry_pages_find_or_create').length < 1) {
+            return;
+        }
 
-      if (typeof waitedOnceForNavigation === "undefined") {
-        // wait for completion and trigger again
-          console.log('waitedOnceForNavigation === undefined');
-        my_evt.preventDefault();
-        $.ajax({
-            type: 'GET',
-            async: false,
-            url: '/stash/ajax_wait'
-        }).done(function() {
-            waitAjax();
-            waitedOnceForNavigation = 1;
-            $(my_evt.target).trigger(my_evt);
-        });
-      }else{
-        console.log('waitedOnceForNavigation == 1');
-      }
+        var someThingsThatNeedToBeDoneFirst = function () {
+            $.ajax({
+                type: 'GET',
+                async: false,
+                url: '/stash/ajax_wait'
+            }).done(function () {
+                console.log('wait a second for ajax to complete');
+                waitAjax();
+                console.log('ajax completed');
+            });
+        };
+
+        $('a, #dashboard_path, #upload_path').on('click', onWithPrecondition(someThingsThatNeedToBeDoneFirst));
+
+        // blocks until all ajax connections are closed
+        var waitAjax = function () {
+            if ($.active < 1) {
+                return;
+            } else {
+                setTimeout(waitAjax, 100); // check again in 100 ms
+            }
+        }
     });
-  });
+}
 
-  // blocks until all ajax connections are closed
-  var waitAjax = function(){
-    if($.active < 1){
-      return;
+// see https://stackoverflow.com/questions/7610871/how-to-trigger-an-event-after-using-event-preventdefault
+function onWithPrecondition(callback) {
+    var isDone = false;
+
+    return function (e) {
+        if (isDone === true) {
+            isDone = false;
+            return;
+        }
+
+        // preventing default and re-triggering events seems to be problematic with security in Safari and no longer works
+        // e.preventDefault();
+
+        callback.apply(this, arguments);
+
+        isDone = true;
+
+        // $(this).click();
     }
-    else {
-      setTimeout(waitAjax, 100); // check again in 100 ms
-    }
-  }
 }
