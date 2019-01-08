2fe2548658dbb5148cdd3544d0039d27c2d7ad2c
Scott Fisher <scott.fisher@ucop.edu>
2019-01-07 16:31:46 -0800

    Add the correct domain names for each environment.  This will simplify URL generation in things like emails later.  I need to deal with this a little now because we generate URLs for the targets for DataCite when we register DOIs with metadata.

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -42,5 +42,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "https://dash2-dev.ucop.edu/stash" }
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => "https://localhost:3000/stash" }
 end
