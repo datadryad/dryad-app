6e1f89f5823f83bfa31f00c7a6de8971b2cc8c8f
Scott Fisher <scott.fisher@ucop.edu>
2019-02-05 17:15:54 -0800

    Making a duck class for the class that takes the object and makes the solr, but coming from the DB.

--- stash_engine/lib/stash/indexer/indexing_resource.rb
+++ stash_engine/lib/stash/indexer/indexing_resource.rb
@@ -1,13 +1,56 @@
+require 'datacite/mapping'
+
+
+# these patch datacite mapping modules for some extra stuff David added
+module Datacite
+  module Mapping
+
+    DATACITE_NAMESPACES = [DATACITE_3_NAMESPACE, DATACITE_4_NAMESPACE].freeze
+    DATACITE_NAMESPACE_URIS = DATACITE_NAMESPACES.map(&:uri).freeze
+
+    def self.datacite_namespace?(elem)
+      (ns = elem.namespace) && DATACITE_NAMESPACE_URIS.include?(ns)
+    end
+
+    class Description
+      def funding?
+        # TODO: Make 'data were created with' etc. a constant or something and move it to Datacite::Mapping
+        type == DescriptionType::OTHER && value.start_with?('Data were created with funding')
+      end
+
+      def usage?
+        type == DescriptionType::OTHER && !funding?
+      end
+    end
+
+    class GeoLocationBox
+      # Expresses the coordinates of this `GeoLocationBox` in [OpenGIS Well-Known Text](http://www.opengeospatial.org/standards/sfa)
+      # `ENVELOPE` format: `ENVELOPE(minX, maxX, maxY, minY)`. As the [Solr docs](https://cwiki.apache.org/confluence/display/solr/Spatial+Search)
+      # say: "The parameter ordering is unintuitive but that's what the spec calls for."
+      # @return [String] the coordinates of this box as a WKT `ENVELOPE`
+      def to_envelope
+        "ENVELOPE(#{west_longitude}, #{east_longitude}, #{north_latitude}, #{south_latitude})"
+      end
+    end
+
+    class Identifier
+      def to_doi
+        "doi:#{value}"
+      end
+    end
+  end
+end
+
 module Stash
   module Indexer
     class IndexingResource
-      # takes a database resource object
+      # takes a database resource object.
       def initialize(resource:)
         @resource = resource
       end
 
       def default_title
-        @resource&.title
+        @resource&.title&.strip
       end
 
       def doi
@@ -15,42 +58,42 @@ module Stash
       end
 
       def type
-        # TODO: find out what this is and fix
+        # This is something like 'Software'
         # general_type.value.to_s.strip if general_type
       end
 
       def general_type
-        # TODO: find out what this is and fix
+        # This is something of Datacite::Mapping::ResourceTypeGeneral
         # resource_type.resource_type_general if resource_type
       end
 
       def grant_number
-        funders = @resource.contributors.where(contributor_type: 'funder')
-        return nil unless funders.count.positive?
-        funders.first.award_number
+        @resource.contributors.where(contributor_type: 'funder').map(&:award_number).reject(&:blank?).map(&:strip).join("\r")
       end
 
       def usage_notes
-        usage = @resource.descriptions.type_abstract
-        return nil unless usage.count.positive?
-        Loofah.fragment(usage.first.description).text
+        @resource.descriptions.type_other.map(&:description).reject(&:blank?).map{|i| Loofah.fragment(i).text.strip }.join("\r")
       end
 
+      # called like resource.description_text_for(DescriptionType::ABSTRACT).to_s.strip
+      # I believe this returns the test for things besides usage notes
       def description_text_for(type)
         desc = descriptions.find { |d| d.type = type }
         desc.value if desc
       end
 
+      # gives array of names
       def geo_location_places
-        geo_locations.map(&:place).compact
+        @resource.geolocations.map(&:geolocation_place).compact.map(&:geo_location_place).reject(&:blank?)
       end
 
+      # using the icky datacite mapping objects
       def geo_location_boxes
-        geo_locations.map(&:box).compact
+        @resource.geolocations.map(&:geolocation_box).compact.map{|i| db_box_to_dc_mapping(db_box: i)}.compact
       end
 
       def geo_location_points
-        geo_locations.map(&:point).compact
+        @resource.geolocations.map(&:geolocation_point).compact.map{|i| db_point_to_dc_mapping(db_point: i)}.compact
       end
 
       def self.datacite?(elem)
@@ -92,7 +135,21 @@ module Stash
         (bbox = calc_bounding_box) ? bbox.to_envelope : nil
       end
 
+      private
 
+      # helpers to convert to datacite mapping
+      def db_box_to_dc_mapping(db_box:)
+        return nil unless db_box.sw_latitude && db_box.ne_latitude && db_box.sw_longitude && db_box.ne_longitude
+        Datacite::Mapping::GeolocationBox.new(south_latitude: db_box.sw_latitude,
+                                              west_longitude: db_box.sw_longitude,
+                                              north_latitude: db_box.ne_latitude,
+                                              east_longitude: db_box.ne_longitude)
+      end
+
+      def db_point_to_dc_mapping(db_point:)
+        return nil unless db_point.lattitude && db_point.longitude
+        Datacite::Mapping::GeolocationPoint.new(latitude: db_point.latitude, longitude: db_point.longitude)
+      end
     end
   end
 end
