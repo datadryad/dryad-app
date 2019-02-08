040e7bf03188c8799321d3e0882fab11f6658762
Ryan Scherle <ryan@scherle.org>
2019-02-08 21:08:33 +0000

    Move invoices from resource to identifier, ensure Published/Embargoed

--- stash_engine/db/migrate/20190208163055_add_stripe_payment_tracking.rb
+++ stash_engine/db/migrate/20190208163055_add_stripe_payment_tracking.rb
@@ -1,6 +1,6 @@
 class AddStripePaymentTracking < ActiveRecord::Migration
   def change
     add_column :stash_engine_users, :customer_id, :text
-    add_column :stash_engine_resources, :invoice_id, :text
+    add_column :stash_engine_identifiers, :invoice_id, :text
   end
 end
