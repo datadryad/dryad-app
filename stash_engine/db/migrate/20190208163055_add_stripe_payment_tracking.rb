eba69387708901d3ae70824f09754c0196f91571
Ryan Scherle <ryan@scherle.org>
2019-02-08 17:13:49 +0000

    Initial Stripe integration

---
+++ stash_engine/db/migrate/20190208163055_add_stripe_payment_tracking.rb
@@ -0,0 +1,6 @@
+class AddStripePaymentTracking < ActiveRecord::Migration
+  def change
+    add_column :stash_engine_users, :customer_id, :text
+    add_column :stash_engine_resources, :invoice_id, :text
+  end
+end
