6ac2ef8bfbe40567e55a97dd096b4d99b2e1e836
briri <brian.riley@ucop.edu>
2019-02-14 15:44:09 -0800

    fixed issue with build gulp task

--- stash_engine/gulpfile.js
+++ stash_engine/gulpfile.js
@@ -10,7 +10,6 @@ const gulpIf = require('gulp-if');
 const imagemin = require('gulp-imagemin');
 const jshint = require('gulp-jshint');
 const lbInclude = require('gulp-lb-include');
-const minifyCSS = require('gulp-clean-css');
 const modernizr = require('gulp-modernizr');
 const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
@@ -18,7 +17,6 @@ const scsslint = require('gulp-scss-lint');
 const shell = require('gulp-shell');
 const ssi = require('browsersync-ssi');
 const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
 const useref = require('gulp-useref');
 const validateHTML = require('gulp-w3cjs');
 
@@ -34,36 +32,30 @@ function helloTask(cb) {
 
 // copy font-awesome into fonts
 function iconsTask(cb) {
-  cb(
-    src('./ui-library/bower_components/font-awesome/fonts/**.*')
-      .pipe(dest('./ui-library/fonts'))
-  );
+  return src('./ui-library/bower_components/font-awesome/fonts/**.*')
+    .pipe(dest('./ui-library/fonts'));
 };
 
 // Minify all images during development:
 function minifyImagesTask(cb) {
-  cb(
-    src('ui-library/images/**')
-      .pipe(imagemin())
-      .pipe(dest('ui-library/images'))
-  );
+  return src('ui-library/images/**')
+    .pipe(imagemin())
+    .pipe(dest('ui-library/images'));
 };
 
 // Run 'gulp modernizr' to build a custom modernizr file based off of classes found in CSS:
 function modernizrTask(cb) {
-  src('ui-library/css/main.css') // where modernizr will look for classes
+  return src('ui-library/css/main.css') // where modernizr will look for classes
     .pipe(modernizr({
       options: ['setClasses'],
       dest: 'ui-library/js/modernizr-custombuild.js'
     }));
-  cb();
 };
 
 // Validate build HTML:
 function validateHtmlTask(cb) {
-  src('public/demo/**/*.html')
+  return src('public/demo/**/*.html')
     .pipe(validateHTML());
-  cb();
 };
 
 // ***** Plugins That Run As Part of a Combined Process ***** //
@@ -72,7 +64,7 @@ function validateHtmlTask(cb) {
 
 // Process Sass to CSS, add sourcemaps, autoprefix CSS selectors, optionally Base64 font and image files into CSS, and reload browser:
 function sassTask(cb) {
-  src('ui-library/scss/**/*.scss')
+  return src('ui-library/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
@@ -84,12 +76,11 @@ function sassTask(cb) {
     .pipe(browserSync.reload({
       stream: true
     }));
-  cb();
 };
 
 // Watch sass, html, and js and reload browser if any changes:
 function watchTask(cb) {
-  parallel(browserSyncTask, sassTask, scssLintTask, jsLintTask);
+  series(browserSyncTask, sassTask, scssLintTask, jsLintTask);
   watch('ui-library/scss/**/*.scss', sassTask);
   watch('ui-library/scss/**/*.scss', scssLintTask);
   watch('ui-library/js/**/*.js', jsLintTask);
@@ -100,7 +91,7 @@ function watchTask(cb) {
 
 // Spin up a local browser with the index.html page at http://localhost:3000/
 function browserSyncTask(cb) {
-  browserSync({
+  return browserSync({
     server: {
       baseDir: 'ui-library',
       middleware: ssi({
@@ -110,87 +101,71 @@ function browserSyncTask(cb) {
       })
     },
   });
-  cb();
 };
 
 // Concatenate and minify CSS and JavaScript from paths within useref tags during build process; include files:
 function userefTask(cb) {
-  cb(
-    src(['ui-library/**/*.html', '!ui-library/includes/*'])
-      .pipe(useref())
-      // commenting out minify and uglify since asset pipeline will do this and makes it easier to troubleshoot without
-      // .pipe(gulpIf('*.css', minifyCSS()))
-      // .pipe(gulpIf('*.js', uglify()))
-      .pipe(lbInclude()) // Process <!--#include file="" --> statements
-      .pipe(dest('public/demo'))
-  );
+  return src(['ui-library/**/*.html', '!ui-library/includes/*'])
+    .pipe(useref())
+    .pipe(lbInclude()) // Process <!--#include file="" --> statements
+    .pipe(dest('public/demo'))
 };
 
 // Delete 'demo' directory at start of build process:
 function cleanTask(cb) {
-  cb(del('public/demo'));
+  return del('public/demo');
 };
 
 // Copy images to demo directory during the build process:
 function copyImagesTask(cb) {
-  cb(
-    src('ui-library/images/**')
-      .pipe(dest('public/demo/images'))
-  );
+  return src('ui-library/images/**')
+    .pipe(dest('public/demo/images'));
 };
 
 // Copy fonts to demo directory during the build process:
 function copyFontsTask(cb) {
-  cb(
-    src('ui-library/fonts/**')
-      .pipe(dest('public/demo/fonts'))
-  );
+  return src('ui-library/fonts/**')
+    .pipe(dest('public/demo/fonts'));
 };
 
 // Copy the single CSS and JS files from UI library build to Rails asset pipeline:
 function copyToAssetsTask(cb) {
+  return src('public/demo/', { read: false })
+    .pipe(shell([
+      'cp public/demo/css/ui.css app/assets/stylesheets/stash_engine',
+      'cp public/demo/js/ui.js app/assets/javascripts/stash_engine']));
+/*
   shell.task([
     'cp public/demo/css/ui.css app/assets/stylesheets/stash_engine',
     'cp public/demo/js/ui.js app/assets/javascripts/stash_engine'
-  ]);
-  cb();
+  ], cb);
+*/
 };
 
 // Lint Sass:
 function scssLintTask(cb) {
-  cb(
-    src(['ui-library/scss/**/*.scss', '!ui-library/scss/vendor/**/*.scss'])
-      .pipe(scsslint({
-        'config': 'scss-lint-config.yml' // Settings for the linter. See: https://github.com/brigade/scss-lint/tree/master/lib/scss_lint/linter
-      }))
-  );
+  return src(['ui-library/scss/**/*.scss', '!ui-library/scss/vendor/**/*.scss'])
+    .pipe(scsslint({
+      'config': 'scss-lint-config.yml' // Settings for the linter. See: https://github.com/brigade/scss-lint/tree/master/lib/scss_lint/linter
+    }));
 };
 
 // Lint JavaScript:
 function jsLintTask(cb) {
-  cb(
-    src(['ui-library/js/**/*.js', '!ui-library/js/modernizr-custombuild.js'])
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'))
-  );
+  return src(['ui-library/js/**/*.js', '!ui-library/js/modernizr-custombuild.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 };
-
-function buildTask(cb) {
-  series(
-    cleanTask,
-    [scssLintTask, jsLintTask, sassTask, userefTask, iconsTask, copyImagesTask, copyFontsTask],
-    copyToAssetsTask
-  );
-  cb();
-};
-
 
 // Commands available via the command line. For example: `gulp hello`
 exports.hello = helloTask;
 exports.minifyImages = minifyImagesTask;
 exports.modernizr = modernizrTask;
-exports.build = buildTask;
 exports.validateHTML = validateHtmlTask;
+
+// Standard build that should be run before deploying the application
+exports.build = series(cleanTask, scssLintTask, jsLintTask, sassTask, userefTask, iconsTask,
+                       copyImagesTask, copyFontsTask, copyToAssetsTask);
 
 // Setup the default to run gulp in dev mode so that its watching our files
 exports.default = series(sassTask, browserSyncTask, watchTask);
