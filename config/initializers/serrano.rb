d5e0c64eb670e2a6a419ad603dc1ba03edddeaeb
Scott Fisher <scott.fisher@ucop.edu>
2019-04-01 16:22:43 -0700

    Trying to get serrano gem working.

--- config/initializers/serrano.rb
+++ config/initializers/serrano.rb
@@ -3,6 +3,5 @@ require 'serrano'
 # https://www.rubydoc.info/gems/serrano
 Serrano.configuration do |config|
   config.base_url = APP_CONFIG.crossref_base_url
-  byebug
   config.mailto = APP_CONFIG.crossref_mailto
 end
