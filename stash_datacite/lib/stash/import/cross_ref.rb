1862fa36d9cd24436f4b6293d797e710f3c81b0e
Scott Fisher <scott.fisher@ucop.edu>
2019-04-02 16:59:39 -0700

    A working autopopulate from CrossRef based on what I could see in the sample identifiers.

---
+++ stash_datacite/lib/stash/import/cross_ref.rb
@@ -0,0 +1,66 @@
+module Stash
+  module Import
+    class CrossRef
+
+      def initialize(resource:, serrano_message:)
+        @resource = resource
+        @sm = serrano_message # which came form crossref (x-ref)
+      end
+
+      def populate
+        populate_title
+        populate_authors
+        populate_abstract
+        populate_funders
+        populate_cited_by
+      end
+
+      def populate_title
+        return if @sm['title'].blank? || @sm['title'].first.blank?
+        @resource.update(title: @sm['title'].first )
+      end
+
+      # authors may contain given, family , affiliation, ORCID
+      def populate_authors
+        return if @sm['author'].blank?
+        @sm['author'].each do |xr_author|
+          author = @resource.authors.create(
+            author_first_name: xr_author['given'],
+            author_last_name: xr_author['family'],
+            author_orcid: ( xr_author['ORCID'] ? xr_author['ORCID'].match(/[0-9\-]{19}$/).to_s : nil ) )
+          affil_name = ( xr_author['affiliation']&.first ? xr_author['affiliation'].first['name'] : nil )
+          author.affiliation_by_name(affil_name) unless affil_name.blank?
+        end
+      end
+
+      def populate_abstract
+        return if @sm['abstract'].blank?
+        abstract = @resource.descriptions.where(description_type: 'abstract').first_or_create
+        abstract.update(description: @sm['abstract'])
+      end
+
+      def populate_funders
+        return if @sm['funder'].blank?
+        # remove any sad blank records
+        @resource.contributors.each do |item|
+          item.destroy if item.contributor_name.blank? && item.award_number.blank?
+        end
+        @sm['funder'].each do |xr_funder|
+          next if xr_funder['name'].blank?
+          if xr_funder['award'].blank?
+            @resource.contributors.create(contributor_name: xr_funder['name'], contributor_type: 'funder')
+            next
+          end
+          xr_funder['award'].each do |xr_award|
+            @resource.contributors.create(contributor_name: xr_funder['name'], contributor_type: 'funder', award_number: xr_award )
+          end
+        end
+      end
+
+      def populate_cited_by
+        return if @sm['URL'].blank?
+        @resource.related_identifiers.create(related_identifier: @sm['URL'], related_identifier_type: 'doi', relation_type: 'iscitedby')
+      end
+    end
+  end
+end
