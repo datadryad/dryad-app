77454dcce3af12072c8b31f13516f08cd9833271
briri <brian.riley@ucop.edu>
2019-04-03 15:22:54 -0700

    finished up tests for dataset versioning

--- spec/support/helpers/merritt_helper.rb
+++ spec/support/helpers/merritt_helper.rb
@@ -1,11 +1,15 @@
 module MerrittHelper
 
-  def mock_successfull_merritt_submission(resource)
+  def mock_successfull_merritt_submission!(resource)
     resource.current_state = 'submitted'
+    resource.save
+    resource.reload
   end
 
-  def mock_unsuccessfull_merritt_submission(resource)
+  def mock_unsuccessfull_merritt_submission!(resource)
     resource.current_state = 'error'
+    resource.save
+    resource.reload
   end
 
 end
