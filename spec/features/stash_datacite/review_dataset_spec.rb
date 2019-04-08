86ce63d40c822d118c3959cd350d7a713d112ca4
briri <brian.riley@ucop.edu>
2019-04-08 14:25:57 -0700

    updated ror mock

--- spec/features/stash_datacite/review_dataset_spec.rb
+++ spec/features/stash_datacite/review_dataset_spec.rb
@@ -5,10 +5,12 @@ RSpec.feature 'ReviewDataset', type: :feature do
 
   include DatasetHelper
   include Mocks::Repository
+  include Mocks::Ror
   include Mocks::RSolr
 
   before(:each) do
     mock_solr!
+    mock_ror!
     @user = create(:user)
     sign_in(@user)
     start_new_dataset
