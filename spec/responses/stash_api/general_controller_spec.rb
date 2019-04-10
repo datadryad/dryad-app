aa6162251904ab5fc143959836a0fb92bf10a3f9
Scott Fisher <scott.fisher@ucop.edu>
2019-04-10 16:39:11 -0700

    Removing extra level of nesting we don't need yet.

--- spec/responses/stash_api/general_controller_spec.rb
+++ spec/responses/stash_api/general_controller_spec.rb
@@ -5,27 +5,25 @@ require_relative 'helpers'
 module StashApi
   RSpec.describe GeneralController, type: :request do
 
-    describe 'authenticated' do
-      before(:all) do
-        @user = create(:user, role: 'superuser')
-        @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-                                                                  owner_id: @user.id, owner_type: 'StashEngine::User')
-        setup_access_token(doorkeeper_application: @doorkeeper_application)
+    before(:all) do
+      @user = create(:user, role: 'superuser')
+      @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+                                                                owner_id: @user.id, owner_type: 'StashEngine::User')
+      setup_access_token(doorkeeper_application: @doorkeeper_application)
+    end
+
+    describe '#test' do
+      it 'returns welcome message and authenticated user id for good token' do
+        post '/api/test', nil, default_authenticated_headers
+        hsh = response_body_hash
+        expect(/Welcome application owner.+$/).to match(hsh[:message])
+        expect(@user.id).to eql(hsh[:user_id])
       end
 
-      describe '#test' do
-        it 'returns welcome message and authenticated user id for good token' do
-          post '/api/test', nil, default_authenticated_headers
-          hsh = response_body_hash
-          expect(/Welcome application owner.+$/).to match(hsh[:message])
-          expect(@user.id).to eql(hsh[:user_id])
-        end
-
-        it 'returns 401 unauthorized for non-authenticated user' do
-          response_code = post '/api/test', nil, default_json_headers
-          expect(response_code).to eq(401) # unauthorized
-          expect(/invalid_token/).to match(response.headers['WWW-Authenticate'])
-        end
+      it 'returns 401 unauthorized for non-authenticated user' do
+        response_code = post '/api/test', nil, default_json_headers
+        expect(response_code).to eq(401) # unauthorized
+        expect(/invalid_token/).to match(response.headers['WWW-Authenticate'])
       end
     end
   end
