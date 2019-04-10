b415b646ecd6aaa33bc11ae0bfd4ae97b96cb45f
Scott Fisher <scott.fisher@ucop.edu>
2019-04-10 14:17:04 -0700

    Getting OAuth2 authentication to pass and basic for authentication to pass.

--- spec/responses/stash_api/general_controller_spec.rb
+++ spec/responses/stash_api/general_controller_spec.rb
@@ -1,31 +1,33 @@
 require 'rails_helper'
-require 'rest-client'
-
-RSpec.configure do |config|
-  config.render_views
-end
+require_relative 'helpers'
 
 # see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
 
 # rubocop:disable Metrics/BlockLength
 module StashApi
-
   RSpec.describe GeneralController, type: :request do
 
-    before(:all) do
-      @user = create(:user, role: 'superuser')
-      # the factorybot for this is ripped from the doorkeeper tests
-      @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-                                       owner_id: @user.id, owner_type: 'StashEngine::User')
-      @response = post "/oauth/token",
-                       { grant_type: 'client_credentials', client_id: @doorkeeper_application.uid, client_secret: @doorkeeper_application.secret },
-                       { "ACCEPT" => "application/json",  "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8" }
-      byebug
-    end
+    describe 'authenticated' do
+      before(:all) do
+        @user = create(:user, role: 'superuser')
+        @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+                                         owner_id: @user.id, owner_type: 'StashEngine::User')
+        setup_access_token(doorkeeper_application: @doorkeeper_application)
+      end
 
-    describe 'test' do
-      it 'is fun' do
-        expect(true).to eq(true)
+      describe '#test' do
+        it 'returns welcome message and authenticated user id for good token' do
+          post "/api/test", nil, default_authenticated_headers
+          hsh = response_body_hash
+          expect(/Welcome application owner.+$/).to match(hsh[:message])
+          expect(@user.id).to eql(hsh[:user_id])
+        end
+
+        it 'returns 401 unauthorized for non-authenticated user' do
+          response_code = post "/api/test", nil, default_json_headers
+          expect(response_code).to eq(401) # unauthorized
+          expect(/invalid_token/).to match(response.headers['WWW-Authenticate'])
+        end
       end
     end
   end
