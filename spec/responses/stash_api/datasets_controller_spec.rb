6967c4fde72aed1520e24dc3d82a58c39e898db9
Scott Fisher <scott.fisher@ucop.edu>
2019-04-11 10:33:28 -0700

    Building up a basic request.

---
+++ spec/responses/stash_api/datasets_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require_relative 'helpers'
+require 'fixtures/stash_api/metadata'
+
+# see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
+module StashApi
+  RSpec.describe GeneralController, type: :request do
+
+    before(:all) do
+      @user = create(:user, role: 'superuser')
+      @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+                                                                owner_id: @user.id, owner_type: 'StashEngine::User')
+      setup_access_token(doorkeeper_application: @doorkeeper_application)
+    end
+
+    # test creation of a new dataset
+    describe '#create' do
+      before(:each) do
+        @meta = StashApi::Metadata.new
+        @meta.make_minimal
+      end
+
+      it 'creates a new dataset from minimal metadata (title, author info, abstract)' do
+        expect(true).to eq(true)
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
