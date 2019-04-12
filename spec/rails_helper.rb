c2c206dd228bf7070432a90f895558e2089df673
Scott Fisher <scott.fisher@ucop.edu>
2019-04-12 12:36:51 -0700

    force a migration before running tests.  Tried all 'soluations' at https://stackoverflow.com/questions/6895179/running-rake-tasks-in-rspec-tests but it did not work in the before suite.  Probably something about load order and couldn't figure out why the rake tasks were not available, but this works at the cost of creating a shell.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,4 +74,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # force a rake run before test suite
+  config.before(:suite) do
+    output = `rake db:migrate`
+    puts output
+  end
 end
