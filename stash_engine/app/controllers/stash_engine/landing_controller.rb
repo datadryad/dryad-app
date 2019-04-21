d204c09aed04f187486d5c0778cd8acb5abb31c9
Ryan Scherle <ryan@scherle.org>
2019-04-21 21:19:14 +0000

    Remove null pointer error when viewing an item in a user workspace

--- stash_engine/app/controllers/stash_engine/landing_controller.rb
+++ stash_engine/app/controllers/stash_engine/landing_controller.rb
@@ -35,7 +35,7 @@ module StashEngine
         if params[:latest] == 'true' && current_user&.superuser? # let superusers see the latest, unpublished if they wish
           id.resources.by_version_desc.first
         # let user see his own if logged in or let superuser see non-latest-preview stuff
-        elsif (current_user && (current_user.id == id.resources.submitted.by_version_desc.first.user_id)) || current_user&.superuser?
+        elsif (current_user && (current_user.id == id.resources.submitted&.by_version_desc&.first&.user_id)) || current_user&.superuser?
           id.resources.submitted.by_version_desc.first
         else # everyone else only gets to see published or embargoed metadata latest version
           id.latest_resource_with_public_metadata
