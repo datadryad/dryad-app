a83cf09d08a21e9e9ab4732b9e2b8078a19417b9
briri <brian.riley@ucop.edu>
2019-04-23 09:12:35 -0700

    fixed minor rubocop formatting issue

--- stash_engine/app/models/stash_engine/internal_datum.rb
+++ stash_engine/app/models/stash_engine/internal_datum.rb
@@ -1,8 +1,11 @@
 module StashEngine
   class InternalDatum < ActiveRecord::Base
     belongs_to :stash_identifier, class_name: 'StashEngine::Identifier', foreign_key: 'identifier_id'
-    validates :data_type, inclusion: { in: %w[manuscriptNumber mismatchedDOI duplicateItem formerManuscriptNumber publicationISSN
-                                              publicationDOI pubmedID], message: '%{value} is not a valid data type' }
+    validates :data_type, inclusion: {
+      in: %w[manuscriptNumber mismatchedDOI duplicateItem formerManuscriptNumber publicationISSN
+             publicationDOI pubmedID],
+      message: '%{value} is not a valid data type'
+    }
     validates :data_type, presence: true
     validates :value, presence: true
 
