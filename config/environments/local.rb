d355e0ab244295f4e6ebdece1cbc2783a590723d
Ryan Scherle <ryan@scherle.org>
2019-04-23 15:27:15 +0000

    Separate host and port, so counter_logger works properly

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -42,6 +42,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  Rails.application.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  Rails.application.default_url_options = { host: 'localhost', port: 3000}
 end
