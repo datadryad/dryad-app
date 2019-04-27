528a3b2e94459b46bdeb5d4b896b26c64176f126
Scott Fisher <scott.fisher@ucop.edu>
2019-04-26 17:07:52 -0700

    substituting key parts of config from config-example so it will match the non-secure stuff run on travis

--- stash_engine/config/initializers/licenses.rb
+++ stash_engine/config/initializers/licenses.rb
@@ -3,3 +3,5 @@ require 'ostruct'
 lic = File.join(Rails.application.root, 'config', 'licenses.yml')
 raise "License configuration file #{lic} not found" unless File.exist?(lic)
 LICENSES = YAML.load_file(lic).with_indifferent_access
+# the license stuff for example config is too much of a tangle to fix right now
+# LICENSES = YAML.load(ERB.new(File.read(lic)).result)[Rails.env]
