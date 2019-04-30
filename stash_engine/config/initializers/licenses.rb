ed24419379ee9f63c88f9e9915d6983406e9845b
Scott Fisher <scott.fisher@ucop.edu>
2019-04-30 16:39:12 -0700

    Putting the licenses back to the way it is in master.

--- stash_engine/config/initializers/licenses.rb
+++ stash_engine/config/initializers/licenses.rb
@@ -1,12 +1,5 @@
 require 'yaml'
 require 'ostruct'
-example_lic = File.join(Rails.application.root, 'dryad-config-example', 'licenses.yml')
-if Rails.env == 'test' && File.exist?(example_lic)
-  lic = example_lic
-else
-  lic = File.join(Rails.application.root, 'config', 'licenses.yml')
-end
+lic = File.join(Rails.application.root, 'config', 'licenses.yml')
 raise "License configuration file #{lic} not found" unless File.exist?(lic)
 LICENSES = YAML.load_file(lic).with_indifferent_access
-# the license stuff for example config is too much of a tangle to fix right now
-# LICENSES = YAML.load(ERB.new(File.read(lic)).result)[Rails.env]
