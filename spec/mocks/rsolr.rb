48d9ed5d0c349dcfdc3fb9f2f1e79be2f5853a23
Scott Fisher <scott.fisher@ucop.edu>
2019-05-03 10:25:24 -0700

    probably works, tests do not

--- spec/mocks/rsolr.rb
+++ spec/mocks/rsolr.rb
@@ -6,14 +6,14 @@ module Mocks
 
     def mock_solr!
       # Mock the Solr connection
-      allow(::RSolr).to receive(:connect).and_return(nil)
+      # allow(::RSolr).to receive(:connect).and_return(nil)
 
       # The StashDiscovery::LatestController.index attempts to contact Solr and the Rails.cache for
       # the list of 'Recent Datasets' on the home page. We have to set one of the controller's instance
       # variables to an empty array so the view doesn't blow up
-      allow_any_instance_of(LatestController).to receive(:set_cached_latest) do |dbl|
-        dbl.instance_variable_set(:@document_list, [])
-      end
+      # allow_any_instance_of(LatestController).to receive(:set_cached_latest) do |dbl|
+      #   dbl.instance_variable_set(:@document_list, [])
+      # end
     end
 
   end
