2525069284da61e63087a913075b85ebe6cfac2f
briri <brian.riley@ucop.edu>
2019-05-15 13:42:17 -0700

    fixed ruboocop issue

--- stash_engine/app/models/stash_engine/resource.rb
+++ stash_engine/app/models/stash_engine/resource.rb
@@ -501,7 +501,7 @@ module StashEngine
       email = user.email
       affiliation = user.affiliation
       affiliation = StashDatacite::Affiliation.from_long_name(user.tenant.long_name) if affiliation.blank? &&
-        user.tenant.present? && !['dryad', 'localhost'].include?(user.tenant.abbreviation.downcase)
+        user.tenant.present? && !%w[dryad localhost].include?(user.tenant.abbreviation.downcase)
       StashEngine::Author.create(resource_id: id, author_orcid: orcid, affiliation: affiliation,
                                  author_first_name: f_name, author_last_name: l_name, author_email: email)
       # disabling because we no longer wnat this with UC Press
