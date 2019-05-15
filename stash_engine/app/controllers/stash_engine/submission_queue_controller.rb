4dc8f648133c1e012b0b4d0f589cd13fe4ee88a9
Scott Fisher <scott.fisher@ucop.edu>
2019-05-15 14:03:04 -0700

    Fixing details, but it randomly starts more than it shoulc.  :-(

--- stash_engine/app/controllers/stash_engine/submission_queue_controller.rb
+++ stash_engine/app/controllers/stash_engine/submission_queue_controller.rb
@@ -15,14 +15,20 @@ module StashEngine
       # nothing just now
     end
 
+    # rubocop:disable Metrics/AbcSize
     def refresh_table
       @queue_rows = RepoQueueState.latest_per_resource.where.not(state: 'completed').order(:hostname, :updated_at)
       @queued_count = RepoQueueState.latest_per_resource.where(state: 'enqueued').count
+      @server_queued_count = RepoQueueState.latest_per_resource.where(state: 'enqueued')
+        .where(hostname: StashEngine.repository.class.hostname).count
       @processing_count = RepoQueueState.latest_per_resource.where(state: 'processing').count
+      @server_processing_count = RepoQueueState.latest_per_resource.where(state: 'processing')
+        .where(hostname: StashEngine.repository.class.hostname).count
       @errored_count = RepoQueueState.latest_per_resource.where(state: 'errored').count
-      @day_completed_submissions = RepoQueueState.latest_per_resource.where(state: 'completed').where("created_at > ?", Time.new - 1.day).count
+      @day_completed_submissions = RepoQueueState.latest_per_resource.where(state: 'completed').where('created_at > ?', Time.new - 1.day).count
       @holding_submissions = File.exist?(HOLD_SUBMISSIONS_PATH)
     end
+    # rubocop:enable Metrics/AbcSize
 
     # it is a little weird that these are GET instead of POST requests, but we may want to make it simple to use these URLs from curl or elsewhere
     def graceful_shutdown
@@ -33,7 +39,7 @@ module StashEngine
     def graceful_start
       FileUtils.rm(HOLD_SUBMISSIONS_PATH) if File.exist?(HOLD_SUBMISSIONS_PATH)
       resource_ids = RepoQueueState.latest_per_resource.where(state: 'rejected_shutting_down')
-          .where(hostname: StashEngine.repository.class.hostname).order(:updated_at).map(&:resource_id)
+        .where(hostname: StashEngine.repository.class.hostname).order(:updated_at).map(&:resource_id)
       enqueue_submissions(resource_ids: resource_ids)
       render 'action_taken'
     end
@@ -41,7 +47,7 @@ module StashEngine
     def ungraceful_start
       FileUtils.rm(HOLD_SUBMISSIONS_PATH) if File.exist?(HOLD_SUBMISSIONS_PATH)
       resource_ids = RepoQueueState.latest_per_resource.where(state: %w[rejected_shutting_down enqueued])
-                         .where(hostname: StashEngine.repository.class.hostname).order(:updated_at).map(&:resource_id)
+        .where(hostname: StashEngine.repository.class.hostname).order(:updated_at).map(&:resource_id)
       enqueue_submissions(resource_ids: resource_ids)
       render 'action_taken'
     end
