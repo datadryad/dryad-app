64e7cda8779806ba57daaa110047f1c0701ac210
Scott Fisher <scott.fisher@ucop.edu>
2019-05-20 11:41:51 -0700

    cleanup

--- stash-merritt/spec/unit/stash/merritt/repository_spec.rb
+++ stash-merritt/spec/unit/stash/merritt/repository_spec.rb
@@ -6,7 +6,7 @@ module Stash
       describe :create_submission_job do
         it 'creates a submission job' do
           url_helpers = double(Module) # yes, apparently URL helpers are an anonymous module
-          repo = Repository.new(url_helpers: url_helpers)
+          repo = Repository.new(url_helpers: url_helpers, threads: 1)
           resource_id = 17
           job = repo.create_submission_job(resource_id: resource_id)
           expect(job).to be_a(SubmissionJob)
