6e3039b7054dc86370a7f0ff3c6d97a859939e80
Scott Fisher <scott.fisher@ucop.edu>
2019-05-28 13:29:27 -0700

    Setting to https for default url options

--- config/environments/migration.rb
+++ config/environments/migration.rb
@@ -95,6 +95,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'dryad-migration.cdlib.org' }
-  Rails.application.default_url_options = { host: 'dryad-migration.cdlib.org' }
+  Rails.application.default_url_options = { host: 'dryad-migration.cdlib.org', protocol: 'https' }
 
 end
