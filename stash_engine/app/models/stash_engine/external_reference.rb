1bd00053f7e82ee8863507767619b2ac892a8902
briri <brian.riley@ucop.edu>
2019-05-30 09:29:56 -0700

    added new table/model

---
+++ stash_engine/app/models/stash_engine/external_reference.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module StashEngine
+  # This class is currently used to store identifiers for external GenBank databases
+  # used by the LinkOut functionality. It is similar to the InternalDatum model except
+  # that the `value` is a Text field
+  #
+  # The primary difference between this model and `InternalDatum` is that a curator
+  # is not meant to be able to create/update/delete this data
+  class ExternalReference < ActiveRecord::Base
+    include StashEngine::Concerns::StringEnum
+
+    belongs_to :identifier, class_name: 'StashEngine::Identifier'
+
+    # List of possible external sources
+    enum_vals = %w[
+      bioproject
+      gene
+      nuccore
+      nucest
+      nucgss
+      nucleotide
+      protein
+      taxonomy
+    ]
+    string_enum('source', enum_vals, 'nuccore', false)
+
+    validates :source, inclusion: { in: enum_vals, message: '%{value} is not a valid source' }
+    validates :identifier, :value, presence: true
+    validates :source, uniqueness: { scope: :identifier, message: 'the dataset already has an entry for %{value}' }
+  end
+end
