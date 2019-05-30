d9d18e977e3bee48253d2458043aec359ceeb6cd
briri <brian.riley@ucop.edu>
2019-05-30 13:21:34 -0700

    added pmid and genbank sequences to dataset entry page

--- stash_datacite/app/controllers/stash_datacite/publications_controller.rb
+++ stash_datacite/app/controllers/stash_datacite/publications_controller.rb
@@ -2,6 +2,8 @@ require_dependency 'stash_datacite/application_controller'
 require 'httparty'
 require 'stash/import/cross_ref'
 require 'stash/import/dryad_manuscript'
+require 'stash/link_out/pubmed_sequence_service'
+require 'stash/link_out/pubmed_service'
 require 'cgi'
 
 module StashDatacite
@@ -17,6 +19,7 @@ module StashDatacite
             @resource = @se_id.latest_resource
             update_manuscript_metadata if params[:import_type] == 'manuscript'
             update_doi_metadata if !@doi&.value.blank? && params[:import_type] == 'published'
+            manage_pubmed_datum(identifier: @se_id, doi: @doi.value) if !@doi&.value.blank? && params[:import_type] == 'published'
           else
             render template: 'stash_datacite/shared/update.js.erb'
           end
@@ -78,6 +81,37 @@ module StashDatacite
         datum = StashEngine::InternalDatum.create(stash_identifier: identifier, data_type: data_type, value: value)
       end
       datum
+    end
+
+    def manage_pubmed_datum(identifier:, doi:)
+      pubmed_service = LinkOut::PubmedService.new
+      pmid = pubmed_service.lookup_pubmed_id(doi)
+
+p '=================================================================='
+p "DID WE FIND ANYTHING? identifier_id: #{identifier.id}, doi: #{doi} ==> #{pmid}"
+
+      return unless pmid.present?
+
+      internal_datum = StashEngine::InternalDatum.find_or_initialize_by(identifier_id: identifier.id, data_type: 'pubmedID')
+      internal_datum.value = pmid.to_s
+      return unless internal_datum.value_changed?
+
+      internal_datum.save
+      manage_genbank_datum(identifier: identifier, pmid: pmid)
+    end
+
+    def manage_genbank_datum(identifier:, pmid:)
+      pubmed_sequence_service = LinkOut::PubmedSequenceService.new
+      sequences = pubmed_sequence_service.lookup_genbank_sequences(pmid)
+      return unless sequences.any?
+
+      sequences.each do |k, v|
+        external_ref = StashEngine::ExternalReference.find_or_initialize_by(identifier_id: identifier.id, source: k)
+        external_ref.value = v.to_s
+        next unless external_ref.value_changed?
+
+        external_ref.save
+      end
     end
   end
 end
