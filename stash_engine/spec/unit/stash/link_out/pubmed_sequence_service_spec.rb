0e50911d9eaa15b5e8c286ed7a804cccc3fd9b9d
briri <brian.riley@ucop.edu>
2019-05-31 15:19:51 -0700

    fixed test issues

--- stash_engine/spec/unit/stash/link_out/pubmed_sequence_service_spec.rb
+++ stash_engine/spec/unit/stash/link_out/pubmed_sequence_service_spec.rb
@@ -6,45 +6,44 @@ require 'stash/link_out/pubmed_sequence_service'
 
 module LinkOut
   describe PubmedSequenceService do
-    TEST_FILE_DIR = "#{Dir.pwd}/spec/tmp/link_out".freeze
-
-    RESPONSE = <<~XML.freeze
-      <?xml version="1.0" encoding="UTF-8" ?>
-      <!DOCTYPE eSearchResult PUBLIC "-//NLM//DTD esearch 20060628//EN" "https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd">
-      <eLinkResult>
-        <LinkSet>
-          <DbFrom>pubmed</DbFrom>
-          <IdList>
-            <Id>21166729</Id>
-          </IdList>
-          <LinkSetDb>
-            <DbTo>nuccore</DbTo>
-            <LinkName>pubmed_nuccore</LinkName>
-            <Link>
-              <Id>316925971</Id>
-            </Link>
-            <Link>
-              <Id>316925605</Id>
-            </Link>
-          </LinkSetDb>
-        </LinkSet>
-      </eLinkResult>
-    XML
-
-    EMPTY_RESPONSE = <<~XML.freeze
-      <?xml version="1.0" encoding="UTF-8" ?>
-      <!DOCTYPE eSearchResult PUBLIC "-//NLM//DTD esearch 20060628//EN" "https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd">
-      <eLinkResult>
-        <LinkSet>
-          <DbFrom>pubmed</DbFrom>
-          <IdList>
-            <Id>21166729</Id>
-          </IdList>
-        </LinkSet>
-      </eLinkResult>
-    XML
 
     before(:each) do
+      @response = <<~XML.freeze
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <!DOCTYPE eSearchResult PUBLIC "-//NLM//DTD esearch 20060628//EN" "https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd">
+        <eLinkResult>
+          <LinkSet>
+            <DbFrom>pubmed</DbFrom>
+            <IdList>
+              <Id>21166729</Id>
+            </IdList>
+            <LinkSetDb>
+              <DbTo>nuccore</DbTo>
+              <LinkName>pubmed_nuccore</LinkName>
+              <Link>
+                <Id>316925971</Id>
+              </Link>
+              <Link>
+                <Id>316925605</Id>
+              </Link>
+            </LinkSetDb>
+          </LinkSet>
+        </eLinkResult>
+      XML
+
+      @empty_response = <<~XML.freeze
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <!DOCTYPE eSearchResult PUBLIC "-//NLM//DTD esearch 20060628//EN" "https://eutils.ncbi.nlm.nih.gov/eutils/dtd/20060628/esearch.dtd">
+        <eLinkResult>
+          <LinkSet>
+            <DbFrom>pubmed</DbFrom>
+            <IdList>
+              <Id>21166729</Id>
+            </IdList>
+          </LinkSet>
+        </eLinkResult>
+      XML
+
       # Mock the app_config.yml and Rails.application.routes since we're not loading the full
       # Rails stack
       link_out = OpenStruct.new(APP_CONFIG.link_out)
@@ -56,7 +55,7 @@ module LinkOut
 
       stub_request(:get, %r{www.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?})
         .with(headers: { 'Accept' => 'text/xml' })
-        .to_return(status: 200, body: RESPONSE.to_s, headers: {})
+        .to_return(status: 200, body: @response.to_s, headers: {})
 
       @svc = LinkOut::PubmedSequenceService.new
     end
@@ -65,13 +64,13 @@ module LinkOut
       it 'returns a nil if the API did not find a Pubmed ID' do
         stub_request(:get, %r{www.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?})
           .with(headers: { 'Accept' => 'text/xml' })
-          .to_return(status: 200, body: EMPTY_RESPONSE.to_s, headers: {})
+          .to_return(status: 200, body: @empty_response.to_s, headers: {})
 
-        expect(@svc.lookup_genbank_sequences('abcd')).to eql({})
+        expect(@svc.lookup_genbank_sequences('21166729')).to eql({})
       end
       it 'returns the Pubmed ID if the API found a match' do
         results_hash = Hash[StashEngine::ExternalReference.sources.collect { |db| [db, %w[316925971 316925605]] }]
-        expect(@svc.lookup_genbank_sequences('abcd')).to eql(results_hash)
+        expect(@svc.lookup_genbank_sequences('21166729')).to eql(results_hash)
       end
     end
 
