788b4a2db442be7f2460683d2b877c6df6e9a69e
Scott Fisher <scott.fisher@ucop.edu>
2019-06-24 16:32:13 -0700

    Trying to set up tests.  Can't get webmock to work.

--- spec/responses/stash_api/general_controller_spec.rb
+++ spec/responses/stash_api/general_controller_spec.rb
@@ -1,15 +1,64 @@
 require 'rails_helper'
 require_relative 'helpers'
+require 'byebug'
 
 # see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
 module StashApi
   RSpec.describe GeneralController, type: :request do
+
+    include Mocks::Datacite
+    include Mocks::RSolr
 
     before(:all) do
       @user = create(:user, role: 'superuser')
       @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
                                                                 owner_id: @user.id, owner_type: 'StashEngine::User')
       setup_access_token(doorkeeper_application: @doorkeeper_application)
+    end
+
+    # this is the general index of datasets
+    describe '#index' do
+
+      before(:each) do
+        mock_solr!
+        mock_datacite!
+        allow_any_instance_of(StashEngine::Resource).to receive(:submit_to_solr).and_return(true)
+        stub_request(:post, 'http://127.0.0.1:8983/solr/geoblacklight/update?wt=ruby')
+          .with(
+            body: /.+/,
+            headers: {
+              'Content-Type' => 'text/xml'
+            }
+          )
+          .to_return(status: 200, body: '', headers: {})
+      end
+
+      before(:all) do
+        @users = Array.new(5) { create(:user) }
+        @identifiers = []
+
+        0.upto(4) do |i|
+          @identifiers << create(:identifier) do |iden|
+            @res = iden.resources.create(attributes_for(:resource, :submitted, user_id: @users[i].id))
+            @res.descriptions.create(attributes_for(:description))
+            @res.authors.create(attributes_for(:author, author_first_name: @users[i].first_name,
+                                                        author_last_name: @users[i].last_name,
+                                                        author_email: @users[i].email))
+          end
+        end
+        @res = @identifiers.map { |id| id.resources.first }
+        @res[0].curation_activities.create(attributes_for(:curation_activity, :published))
+        @res[1].curation_activities.create(attributes_for(:curation_activity, :embargoed))
+        @res[2].curation_activities.create(attributes_for(:curation_activity, :submitted))
+        @res[3].curation_activities.create(attributes_for(:curation_activity, :action_required))
+        @res[4].curation_activities.create(attributes_for(:curation_activity, :curation))
+        byebug
+      end
+
+      it 'is a placeholder for now' do
+        expect(true).to eql(true)
+      end
+
     end
 
     describe '#test' do
