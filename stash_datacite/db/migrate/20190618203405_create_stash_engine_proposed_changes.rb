ee91f9318c8fd38c9fcdd657162b9fe2d5daf976
briri <brian.riley@ucop.edu>
2019-06-25 08:34:04 -0700

    added tests

---
+++ stash_datacite/db/migrate/20190618203405_create_stash_engine_proposed_changes.rb
@@ -0,0 +1,17 @@
+class CreateStashEngineProposedChanges < ActiveRecord::Migration
+  def change
+    create_table :stash_engine_proposed_changes do |t|
+      t.references :identifier, index: true
+      t.boolean :approved
+      t.references :approved_by_id, index: true
+      t.text :authors
+      t.string :provenance
+      t.datetime :publication_date
+      t.string :publication_doi, index: true
+      t.string :publication_name, index: true
+      t.float :score
+      t.string :title
+      t.timestamps null: false
+    end
+  end
+end
