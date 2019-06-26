56b7eb22bbf28778a4c283cea121b9e6a63e1b13
Scott Fisher <scott.fisher@ucop.edu>
2019-06-26 16:14:58 -0700

    Adding a test for the general root url.  Adding the curation activity killing of callbacks.

--- spec/responses/stash_api/general_controller_spec.rb
+++ spec/responses/stash_api/general_controller_spec.rb
@@ -5,47 +5,11 @@ require 'byebug'
 # see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
 module StashApi
   RSpec.describe GeneralController, type: :request do
-
-    include Mocks::CurationActivity
-
     before(:all) do
       @user = create(:user, role: 'superuser')
       @doorkeeper_application = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
                                                                 owner_id: @user.id, owner_type: 'StashEngine::User')
       setup_access_token(doorkeeper_application: @doorkeeper_application)
-    end
-
-    # this is the general index of datasets
-    describe '#index' do
-
-      before(:each) do
-        neuter_curation_callbacks!
-
-        @users = Array.new(5) { create(:user) }
-        @identifiers = []
-
-        0.upto(4) do |i|
-          @identifiers << create(:identifier) do |iden|
-            @res = iden.resources.create(attributes_for(:resource, :submitted, user_id: @users[i].id))
-            @res.descriptions.create(attributes_for(:description))
-            @res.authors.create(attributes_for(:author, author_first_name: @users[i].first_name,
-                                               author_last_name: @users[i].last_name,
-                                               author_email: @users[i].email))
-          end
-        end
-        @res = @identifiers.map { |id| id.resources.first }
-        @res[0].curation_activities.create(attributes_for(:curation_activity, :published))
-        @res[1].curation_activities.create(attributes_for(:curation_activity, :embargoed))
-        @res[2].curation_activities.create(attributes_for(:curation_activity, :submitted))
-        @res[3].curation_activities.create(attributes_for(:curation_activity, :action_required))
-        @res[4].curation_activities.create(attributes_for(:curation_activity, :curation))
-        byebug
-      end
-
-      it 'is a placeholder for now' do
-        expect(true).to eql(true)
-      end
-
     end
 
     describe '#test' do
@@ -60,6 +24,14 @@ module StashApi
         response_code = post '/api/test', nil, default_json_headers
         expect(response_code).to eq(401) # unauthorized
         expect(/invalid_token/).to match(response.headers['WWW-Authenticate'])
+      end
+    end
+
+    describe '#index' do
+      it 'has a HATEOAS link to the main entry into API, the datasets list' do
+        get '/api/', {}, default_json_headers
+        hsh = response_body_hash
+        expect(hsh['_links']['stash:datasets']['href']).to eql('/api/datasets')
       end
     end
   end
