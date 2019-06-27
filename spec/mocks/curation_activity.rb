e33a9b348b46e6f16465aea00f1ca63225b7988b
Scott Fisher <scott.fisher@ucop.edu>
2019-06-27 16:45:59 -0700

    Test showing of correct metadata based on user and curation status.

--- spec/mocks/curation_activity.rb
+++ spec/mocks/curation_activity.rb
@@ -2,6 +2,7 @@ module Mocks
 
   module CurationActivity
 
+    # rubocop:disable Metrics/AbcSize
     def neuter_curation_callbacks!
       # These callbacks cause constant grief when you're just trying to set up curation states in order to
       # do things like test dataset visibility.  Mostly we don't want these running in tests unless we're testing that
@@ -12,5 +13,6 @@ module Mocks
       allow_any_instance_of(StashEngine::CurationActivity).to receive(:email_author).and_return(true)
       allow_any_instance_of(StashEngine::CurationActivity).to receive(:email_orcid_invitations).and_return(true)
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
