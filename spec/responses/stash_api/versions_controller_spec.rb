eb21536a0ce344c41d28c350a7889852875955a4
Scott Fisher <scott.fisher@ucop.edu>
2019-07-05 15:46:31 -0700

    Adding mocks and tests for dataset version download and also rubocop fixes.

--- spec/responses/stash_api/versions_controller_spec.rb
+++ spec/responses/stash_api/versions_controller_spec.rb
@@ -4,6 +4,7 @@ require_relative 'helpers'
 require 'fixtures/stash_api/metadata'
 require 'fixtures/stash_api/curation_metadata'
 require 'cgi'
+require 'stash/download/version'
 
 # see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
 # rubocop:disable Metrics/BlockLength, Metrics/ModuleLength
@@ -192,6 +193,75 @@ module StashApi
         expect(response_code).to eq(404)
       end
 
+    end
+
+    # downloads contents by version id if authorized and able to do so, note the actual download is streamed by
+    # another class and will mock it for now since this all depends on Merritt overall.
+    # Note: doesn't have json return, but a file is returned.
+    describe '#download' do
+      before(:each) do
+        @resources[0].update(publication_date: Time.new - 24.hours)
+        @resources[0].current_state = 'submitted' # has to show submitted to merritt in order to download
+        # callbacks or something weird are adding states for this resource so, add published again as final state
+        create(:curation_activity, resource: @resources[0], status: 'published')
+
+        @resources[1].current_state = 'submitted' # has to show submitted to merritt in order to download
+        allow_any_instance_of(Stash::Download::Version).to receive(:download) do |o|
+          # o is the object instance and cc is the controller context
+          o.cc.response.headers['Content-Type'] = 'text/plain'
+          o.cc.response.headers['Content-Disposition'] = 'inline' # normally attachment for downloads, really, though
+          o.cc.response.headers['Content-Length'] = 20
+          o.cc.response.headers['Last-Modified'] = Time.now.httpdate
+          o.cc.response_body = 'This file is awesome'
+          # o.cc.render -- this isn't needed in the tests and causes a double-render which is different than the actual method
+        end
+      end
+
+      it 'downloads a public version' do
+        response_code = get "/api/versions/#{@resources[0].id}/download", {}, {}
+        expect(response_code).to eq(200)
+        expect(response.body).to eq('This file is awesome')
+      end
+
+      it 'allows owner to download private, but submitted to Merritt version' do
+        @doorkeeper_application2 = create(:doorkeeper_application, redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+                                                                   owner_id: @user1.id, owner_type: 'StashEngine::User')
+        access_token = get_access_token(doorkeeper_application: @doorkeeper_application2)
+        response_code = get "/api/versions/#{@resources[1].id}/download", {},
+                            'Accept' => '*', 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{access_token}"
+        expect(response_code).to eq(200)
+        expect(response.body).to eq('This file is awesome')
+      end
+
+      it 'allows superuser to download private, but submitted to Merritt version' do
+        response_code = get "/api/versions/#{@resources[1].id}/download", {}, default_authenticated_headers.merge('Accept' => '*')
+        expect(response_code).to eq(200)
+        expect(response.body).to eq('This file is awesome')
+      end
+
+      it 'disallows random user from downloading non-public, but submitted version' do
+        @user.update(role: 'user')
+        response_code = get "/api/versions/#{@resources[1].id}/download", {}, default_authenticated_headers.merge('Accept' => '*')
+        expect(response_code).to eq(403)
+      end
+
+      it 'disallows nil user from downloading non-public, but submitted version' do
+        response_code = get "/api/versions/#{@resources[1].id}/download", {}, default_json_headers.merge('Accept' => '*')
+        expect(response_code).to eq(403)
+      end
+
+      it 'allows admin to download private, but submitted to Merritt version' do
+        @user.update(role: 'admin', tenant_id: @resources[1].tenant_id)
+        response_code = get "/api/versions/#{@resources[1].id}/download", {}, default_authenticated_headers.merge('Accept' => '*')
+        expect(response_code).to eq(200)
+        expect(response.body).to eq('This file is awesome')
+      end
+
+      it "disallows download if it's not submitted to Merritt" do
+        @resources[1].current_state = 'in_progress'
+        response_code = get "/api/versions/#{@resources[1].id}/download", {}, default_authenticated_headers.merge('Accept' => '*')
+        expect(response_code).to eq(403)
+      end
     end
   end
 end
