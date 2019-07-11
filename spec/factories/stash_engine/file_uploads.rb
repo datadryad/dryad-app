f9244cd9b36cafe7c59298eb5bb09396f5b0f360
Scott Fisher <scott.fisher@ucop.edu>
2019-07-10 17:10:11 -0700

    Many files tests for security in the API

---
+++ spec/factories/stash_engine/file_uploads.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+
+  factory :file_upload, class: StashEngine::FileUpload do
+    resource
+
+    upload_file_name { ::File.basename(Faker::File.file_name) }
+    upload_content_type { Faker::File.mime_type }
+    upload_file_size { Faker::Number.between(1, 100000000) }
+    temp_file_path { Faker::File.file_name }
+    file_state { 'created' }
+  end
+end
