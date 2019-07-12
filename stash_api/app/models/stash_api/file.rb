6fc7d58e4c27115188bb0ba9d468b3bdde7320f5
Scott Fisher <scott.fisher@ucop.edu>
2019-07-12 11:02:27 -0700

    Adding curie link to download a file.

--- stash_api/app/models/stash_api/file.rb
+++ stash_api/app/models/stash_api/file.rb
@@ -37,7 +37,8 @@ module StashApi
         self: { href: api_url_helper.file_path(@se_file_upload.id) },
         'stash:dataset': { href: parent_version.parent_dataset.self_path },
         'stash:version': { href: parent_version.self_path },
-        'stash:files': { href: parent_version.files_path }
+        'stash:files': { href: parent_version.files_path },
+        'stash:file-download': { href: api_url_helper.download_file_path(id: @se_file_upload.id)}
       }
       add_download!(hsh)
       hsh
