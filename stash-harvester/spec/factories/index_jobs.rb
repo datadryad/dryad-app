1890696abee3a61138500e05fe2691dbd847311d
Scott Fisher <scott.fisher@ucop.edu>
2019-07-17 15:09:35 -0700

    Remove harvester from code since we use the simpler notifier now.

--- stash-harvester/spec/factories/index_jobs.rb
+++
@@ -1,8 +0,0 @@
-require 'models'
-
-FactoryGirl.define do
-  # TODO: Find a way to do modules w/o making model class name explicit
-  factory :index_job, class: Stash::Harvester::Models::IndexJob do
-
-  end
-end
