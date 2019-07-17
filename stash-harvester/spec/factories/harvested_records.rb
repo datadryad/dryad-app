3308c8548375fb0cc018596d4b06542c566892be
Scott Fisher <scott.fisher@ucop.edu>
2019-07-17 15:09:35 -0700

    Remove harvester from code since we use the simpler notifier now.

--- stash-harvester/spec/factories/harvested_records.rb
+++
@@ -1,8 +0,0 @@
-require 'models'
-
-FactoryGirl.define do
-  # TODO: Find a way to do modules w/o making model class name explicit
-  factory :harvested_record, class: Stash::Harvester::Models::HarvestedRecord do
-
-  end
-end
