317a065260d6ea8536ec6f8f4b30a0092c8ca0f9
Scott Fisher <scott.fisher@ucop.edu>
2019-07-19 16:00:50 -0700

    Got things working with datacite event data for citations (mostly) but still need to fix citation types.  In progress.

--- stash_engine/lib/stash/event_data.rb
+++ stash_engine/lib/stash/event_data.rb
@@ -1,6 +1,7 @@
 # EventData is the DataCite API for getting stats about citations and usage.
 # They currently do not have totals so we need to get large swaths of data and add it up on the client side instead
 # of doing it a database where it is probably easier and more efficient.
+require 'byebug'
 module Stash
   module EventData
     Dir.glob(File.expand_path('event_data/*.rb', __dir__)).sort.each(&method(:require))
