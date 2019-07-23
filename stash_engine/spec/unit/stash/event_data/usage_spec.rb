73e5793cc0274de4d9316ab417861cd22cdb521a
Scott Fisher <scott.fisher@ucop.edu>
2019-07-23 14:02:56 -0700

    Webmock unit tests added for stats and citations.

--- stash_engine/spec/unit/stash/event_data/usage_spec.rb
+++ stash_engine/spec/unit/stash/event_data/usage_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 require 'webmock/rspec'
-require 'byebug'
 
 module Stash
   module EventData
@@ -10,14 +9,15 @@ module Stash
         @usage = Usage.new(doi: 'doi:10.6071/m3rp49')
         WebMock.disable_net_connect!
 
-        stub_request(:get, 'https://api.datacite.org/events?doi=10.6071/m3rp49&mailto=scott.fisher@ucop.edu&page%5Bsize%5D=0&relation-type-id=unique-dataset-investigations-regular,unique-dataset-investigations-machine,unique-dataset-requests-regular,unique-dataset-requests-machine&rows&source-id=datacite-usage').
-            with(
-                headers: {
-                    'Accept'=>'*/*',
-                    'Host'=>'api.datacite.org'
-                }).
-            to_return(status: 200, body: File.read(StashEngine::Engine.root.join('spec', 'data', 'mdc-usage.json')),
-                      headers: {'Content-Type' => 'application/json'})
+        stub_request(:get, 'https://api.datacite.org/events?doi=10.6071/m3rp49&mailto=scott.fisher@ucop.edu&page%5Bsize%5D=0&relation-type-id=unique-dataset-investigations-regular,unique-dataset-investigations-machine,unique-dataset-requests-regular,unique-dataset-requests-machine&rows&source-id=datacite-usage')
+          .with(
+            headers: {
+              'Accept' => '*/*',
+              'Host' => 'api.datacite.org'
+            }
+          )
+          .to_return(status: 200, body: File.read(StashEngine::Engine.root.join('spec', 'data', 'mdc-usage.json')),
+                     headers: { 'Content-Type' => 'application/json' })
       end
 
       describe :initializes do
