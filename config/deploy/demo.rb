85b2aa73a27da084d761058d887de07cf204689c
Scott Fisher <scott.fisher@ucop.edu>
2019-08-06 10:27:43 -0700

    Fixing precompile by adding the web role in addition to the app role for the server.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -14,7 +14,7 @@ set :rails_env, 'demo'
 # To override the default host, set $SERVER_HOSTS, e.g.
 #    $ SERVER_HOSTS='localhost1 localhost2' bundle exec cap development deploy
 set :server_hosts, ENV["SERVER_HOSTS"]&.split(' ') || ['uc3-dashdemo-stg.cdlib.org']
-role :app, fetch(:server_hosts), user: 'dryad'
+role %i[app web], fetch(:server_hosts), user: 'dryad'
 
 #on roles(:all) do |host|
 #  puts "setting server host: #{host.hostname}"
