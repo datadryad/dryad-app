135a38cfb5aeedce6e6270fb54511833f39546d2
briri <brian.riley@ucop.edu>
2019-08-12 09:38:59 -0700

    extracted log file read method to abstract class

--- stash_engine/app/services/stash_engine/status_dashboard/dependency_checker_service.rb
+++ stash_engine/app/services/stash_engine/status_dashboard/dependency_checker_service.rb
@@ -5,6 +5,8 @@ module StashEngine
 
     class DependencyCheckerService
 
+      DATE_TIME_MATCHER = /[0-9]{4}\-[0-9]{2}\-[0-9]{2}T([0-9]{2}:){2}[0-9]{2}/
+
       def initialize(**args)
         @dependency = StashEngine::ExternalDependency.find_by(abbreviation: args[:abbreviation])
       end
@@ -26,6 +28,12 @@ module StashEngine
 
       def report_outage
         UserMailer.dependency_offline(@dependency).deliver_now
+      end
+
+      def extract_last_log_date(log)
+        contents = File.open(log).to_a
+        last_run_date = Time.parse(line.match(DATE_TIME_MATCHER).to_s) unless contents.empty?
+        last_run_date
       end
 
     end
--- stash_engine/app/services/stash_engine/status_dashboard/notifier_service.rb
+++ stash_engine/app/services/stash_engine/status_dashboard/notifier_service.rb
@@ -7,21 +7,16 @@ module StashEngine
 
     class NotifierService < DependencyCheckerService
 
-      LOG_FILE = '/dryad/apps/ui/shared/cron/logs/stash-notifier.log'.freeze
-      DATE_TIME_MATCHER = /[0-9]{4}\-[0-9]{2}\-[0-9]{2}T([0-9]{2}:){2}[0-9]{2}/.freeze
+      LOG_FILE = '/dryad/apps/ui/shared/cron/logs/stash-notifier.log'
 
       def ping_dependency
         super
         record_status(online: false, message: "No log file found at '#{LOG_FILE}'.") unless File.exist?(LOG_FILE)
         return false unless File.exist?(LOG_FILE)
 
-        online = true
-        contents = File.open(LOG_FILE).to_a
-        online = false if contents.empty?
-        last_run_date = Time.parse(line.match(DATE_TIME_MATCHER).to_s) if online
-        online = last_run_date >= (Time.now - 15.minutes)
-        msg = "The Notifier service has not updated its log since #{last_run_date}." if !online && last_run_date.present?
-        msg = "The Notifier service has an empty log." if !online && !last_run_date.present?
+        last_run_date = extract_last_log_date(LOG_FILE)
+        online = last_run_date.present? && last_run_date >= (Time.now - 15.minutes)
+        msg = "The Notifier service has not updated its log since '#{last_run_date}'." unless online
         record_status(online: online, message: msg)
         online
       rescue StandardError => e
