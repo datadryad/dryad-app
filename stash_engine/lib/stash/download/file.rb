4d54eafd0cb21b0ef046a87e1338a5306fb368f6
Scott Fisher <scott.fisher@ucop.edu>
2019-09-03 16:08:14 -0700

    trying to fix streaming delay

--- stash_engine/lib/stash/download/file.rb
+++ stash_engine/lib/stash/download/file.rb
@@ -1,4 +1,5 @@
 require_relative 'base'
+require 'byebug'
 
 # this doesn't add functionality to base class.  It would have added something for Merritt Express file downloads, but
 # Merritt Express doesn't support file downloads from all versions right now so no using it.
@@ -20,6 +21,27 @@ module Stash
       # url used for consistency
       def disposition_filename
         "attachment; filename=\"#{::File.basename(@file.upload_file_name)}\""
+      end
+
+      # this one overrides the base one for now because Merritt is slow to return a HEAD request on large files.
+      # See if I can just populate it from the file in the database
+      #
+      # to stream the response through this UI instead of redirecting, keep login and other stuff private
+      def stream_response(url:, tenant:)
+        # get original header info from http headers
+        client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
+
+        # headers = client.head(url, follow_redirect: true)
+
+        content_type = file.upload_content_type
+        # content_length = (file.upload_file_size.nil? || file.upload_file_size == 0) ? '' : file.upload_file_size
+        content_disposition = "attachment; filename=\"#{file.upload_file_name}\""
+        cc.response.headers['Content-Type'] = content_type if content_type
+        cc.response.headers['Content-Disposition'] = content_disposition
+        # cc.response.headers['Content-Length'] = content_length
+        cc.response.headers['Transfer-Encoding'] = 'chunked'
+        cc.response.headers['Last-Modified'] = Time.now.utc.httpdate
+        cc.response_body = Stash::Streamer.new(client, url)
       end
 
     end
