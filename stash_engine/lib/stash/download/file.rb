6a864fca77097980bbb162fa84f9040a97724573
Scott Fisher <scott.fisher@ucop.edu>
2019-09-03 16:38:01 -0700

    testing out download

--- stash_engine/lib/stash/download/file.rb
+++ stash_engine/lib/stash/download/file.rb
@@ -27,6 +27,7 @@ module Stash
       # See if I can just populate it from the file in the database
       #
       # to stream the response through this UI instead of redirecting, keep login and other stuff private
+      # rubocop:disable Metrics/AbcSize
       def stream_response(url:, tenant:)
         # get original header info from http headers
         client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
@@ -34,15 +35,17 @@ module Stash
         # headers = client.head(url, follow_redirect: true)
 
         content_type = file.upload_content_type
-        # content_length = (file.upload_file_size.nil? || file.upload_file_size == 0) ? '' : file.upload_file_size
+        content_length = file.upload_file_size.nil? || file.upload_file_size == 0 ? '' : file.upload_file_size
         content_disposition = "attachment; filename=\"#{file.upload_file_name}\""
         cc.response.headers['Content-Type'] = content_type if content_type
         cc.response.headers['Content-Disposition'] = content_disposition
-        # cc.response.headers['Content-Length'] = content_length
-        cc.response.headers['Transfer-Encoding'] = 'chunked'
+        cc.response.headers['Content-Length'] = content_length
+        # cc.response.headers['Transfer-Encoding'] = 'chunked'
+        # transfer encoding chunked makes it fail with zero bytes
         cc.response.headers['Last-Modified'] = Time.now.utc.httpdate
         cc.response_body = Stash::Streamer.new(client, url)
       end
+      # rubocop:enable Metrics/AbcSize
 
     end
   end
