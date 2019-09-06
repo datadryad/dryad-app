2f7c3cd88544a4b339306a0a27dc1ac45243ba1b
briri <brian.riley@ucop.edu>
2019-09-06 08:49:39 -0700

    fixed typo in migration

--- stash_engine/db/migrate/20190906084410_change_value_to_long_text_on_stash_engine_external_references.rb
+++ stash_engine/db/migrate/20190906084410_change_value_to_long_text_on_stash_engine_external_references.rb
@@ -1,5 +1,5 @@
 class ChangeValueToLongTextOnStashEngineExternalReferences < ActiveRecord::Migration
   def change
-    add_column :stash_engine_external_references, :value, :longtext
+    change_column :stash_engine_external_references, :value, :longtext
   end
 end
