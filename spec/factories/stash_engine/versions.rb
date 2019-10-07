26e9ca908a01221143269287336a498d85a58ad9
Scott Fisher <scott.fisher@ucop.edu>
2019-10-07 16:43:49 -0700

    Fixing rubocop formatting and other errors.

--- spec/factories/stash_engine/versions.rb
+++ spec/factories/stash_engine/versions.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
 
   factory :version, class: StashEngine::Version do
     resource
-      version { 1 }
-      merritt_version { 1 }
-      zip_filename { nil }
+    version { 1 }
+    merritt_version { 1 }
+    zip_filename { nil }
   end
 end
--- spec/support/helpers/database_helper.rb
+++ spec/support/helpers/database_helper.rb
@@ -6,15 +6,18 @@ module DatabaseHelper
     @user = create(:user, role: 'superuser')
     @identifier = create(:identifier)
     @resource = create(:resource, :submitted, identifier: @identifier, user_id: @user.id, tenant_id: @user.tenant_id,
-                       authors: [ create(:author) ], descriptions: [ create(:description) ],
-                       stash_version: create(:version, version: 1, merritt_version: 1),
-                       file_uploads: [ create(:file_upload) ] )
+                                              authors: [create(:author)], descriptions: [create(:description)],
+                                              stash_version: create(:version, version: 1, merritt_version: 1),
+                                              file_uploads: [create(:file_upload)])
   end
 
   # this essentially creates a new resource (version) to start working on for a user
   def duplicate_resource!(resource:, user: nil)
     new_res = resource.amoeba_dup
-    new_res.current_editor_id = user&.id || resource.user_id
+    # TODO: we need to upgrade the version Rubocop uses to Ruby 2.4 in this repo config, but it destroys lots of auto-generated
+    # code if I run rubocop -a with it upgraded so avoiding the upgrade for now.  (this used to use the &. construct )
+    new_res.current_editor_id = (user ? user.id : resource.user_id)
+
     new_res.curation_activities.update_all(user_id: user.id) if user
     new_res.save!
   end
