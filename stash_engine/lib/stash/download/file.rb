59c3af3a842502fb1be50e6b06d1e02d3ac355d1
Scott Fisher <scott.fisher@ucop.edu>
2019-10-12 09:47:06 -0700

    try http client instead of httprb to see if I get stalls still

--- stash_engine/lib/stash/download/file.rb
+++ stash_engine/lib/stash/download/file.rb
@@ -28,9 +28,9 @@ module Stash
       # to stream the response through this UI instead of redirecting, keep login and other stuff private
       # rubocop:disable Metrics/AbcSize
       def stream_response(url:, tenant:)
-        dl_url = 'https://images-assets.nasa.gov/image/PIA22935/PIA22935~orig.jpg'
+        # dl_url = 'https://images-assets.nasa.gov/image/PIA22935/PIA22935~orig.jpg'
         # get original header info from http headers
-        # client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
+        client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
 
         # set up header variables
         # content_length = file.upload_file_size.nil? || file.upload_file_size == 0 ? '' : file.upload_file_size
@@ -43,27 +43,24 @@ module Stash
         cc.response.headers['Content-Disposition'] = content_disposition
         cc.response.headers['Content-Length'] = content_length unless content_length.blank?
 
-        cc.response.headers["X-Accel-Buffering"] = "no"
-        cc.response.headers["Cache-Control"] = "no-cache"
-        cc.response.headers["Last-Modified"] = Time.zone.now.ctime.to_s
+        cc.response.headers['X-Accel-Buffering'] = 'no'
+        cc.response.headers['Cache-Control'] = 'no-cache'
+        cc.response.headers['Last-Modified'] = Time.zone.now.ctime.to_s
 
         # rack hijack takes a proc to run and in another thread so it frees web server thread for normal short requests
-        cc.response.headers["rack.hijack"] = proc do |stream|
-
+        cc.response.headers['rack.hijack'] = proc do |stream|
           Thread.new do
-            chunk_size = 1024 * 1024 # 1 MB
+            # chunk_size = 1024 * 1024 # 1 MB
             begin
-              # see https://twin.github.io/httprb-is-great/ or https://github.com/httprb/http/wiki
-              http = HTTP.timeout(connect: 3000, read: 3000, write: 1500).timeout(3000)
-                         .basic_auth(user: tenant.repository.username, pass: tenant.repository.password)
-              response = http.get(url)
-              while true
-                chunk = response.body.readpartial(chunk_size)
-                break if chunk.nil?
-                stream.write(chunk)
-                # stream.write(chunk.force_encoding("UTF-8")) # I don't know why this is necessary, maybe only in webrick
+              client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
+              client.receive_timeout = 7200
+              client.send_timeout = 3600
+              client.connect_timeout = 7200
+              client.keep_alive_timeout = 3600
+              client.get_content(url) do |chunk|
+                stream.write(chunk) # force_encoding('UTF-8') may be required for webrick
               end
-            rescue HTTP::Error => ex
+            rescue Error => ex
               cc.logger.error("while downloading #{ex}")
             ensure
               stream.close
