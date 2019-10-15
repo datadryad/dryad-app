5df2db3e1493738b6cfd408d5ee5f6e9405047b0
Scott Fisher <scott.fisher@ucop.edu>
2019-10-14 17:28:45 -0700

    This may get file downloading to work if I'm not firewalled out.

--- stash_engine/lib/stash/download/file.rb
+++ stash_engine/lib/stash/download/file.rb
@@ -19,7 +19,7 @@ module Stash
       # tries to make the disposition and we can do it directly from the filename since we know it
       # url used for consistency
       def disposition_filename
-        "attachment; filename=\"#{::File.basename(@file.upload_file_name)}\""
+        ::File.basename(@file.upload_file_name)
       end
 
       # this one overrides the base one for now because Merritt is slow to return a HEAD request on large files.
@@ -28,52 +28,22 @@ module Stash
       # to stream the response through this UI instead of redirecting, keep login and other stuff private
       # rubocop:disable Metrics/AbcSize
       def stream_response(url:, tenant:)
-        # dl_url = 'https://images-assets.nasa.gov/image/PIA22935/PIA22935~orig.jpg'
-        # get original header info from http headers
-        client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
+        # client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
 
-        # set up header variables
-        # content_length = file.upload_file_size.nil? || file.upload_file_size == 0 ? '' : file.upload_file_size
-        content_length = nil
-        content_type = file.upload_content_type
-        content_disposition = "attachment; filename=\"#{file.upload_file_name}\""
+        # url = 'https://www.spacetelescope.org/static/archives/images/publicationtiff40k/heic1502a.tif'
+        url = url.gsub('://', "://#{tenant.repository.username}:#{tenant.repository.password}")
 
-        # set up headers
-        cc.response.headers['Content-Type'] = content_type if content_type
-        cc.response.headers['Content-Disposition'] = content_disposition
-        cc.response.headers['Content-Length'] = content_length unless content_length.blank?
-        # cc.response.headers['Transfer-Encoding'] = 'chunked'
-        # messes it up
+        cc.request.env['rack.hijack'].call
+        stream = cc.request.env['rack.hijack_io']
 
-        cc.response.headers['X-Accel-Buffering'] = 'no'
-        cc.response.headers['Cache-Control'] = 'no-cache'
-        cc.response.headers['Last-Modified'] = Time.zone.now.ctime.to_s
-
-        # rack hijack takes a proc to run and in another thread so it frees web server thread for normal short requests
-        cc.response.headers['rack.hijack'] = proc do |stream|
-          # stream.autoclose = false
-          # stream.sync = true
-
-          Thread.new do
-            # chunk_size = 1024 * 1024 # 1 MB
-            begin
-              client = Stash::Repo::HttpClient.new(tenant: tenant, cert_file: APP_CONFIG.ssl_cert_file).client
-              client.receive_timeout = 7200
-              client.send_timeout = 3600
-              client.connect_timeout = 7200
-              client.keep_alive_timeout = 3600
-              client.get_content(url) do |chunk|
-                stream.write(chunk) #.force_encoding('UTF-8') # may be required for webrick
-              end
-            rescue StandardError => ex
-              # error reading from socket: Could not parse data entirely (1 != 0)
-              cc.logger.error("while downloading #{ex}")
-            ensure
-              stream.close
-            end
-          end
+        Thread.new do
+          # TODO: set more options for download stream
+          remote_file = Down::Wget.open(url)
+          send_headers(stream: stream, header_obj: remote_file.data[:headers], filename: disposition_filename)
+          send_stream(out_stream: stream, in_stream: remote_file)
         end
-        cc.head :ok
+
+        cc.response.close
       end
       # rubocop:enable Metrics/AbcSize
 
