a3e396ff20ba48bc97580596cbdfbeee1b569d83
Scott Fisher <scott.fisher@ucop.edu>
2019-10-16 15:12:40 -0700

    Trying to change passenger settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -142,7 +142,8 @@ namespace :deploy do
       within current_path do
         with rails_env: fetch(:rails_env) do
           execute "cd #{deploy_to}/current; bundle install --deployment"
-          execute "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
+          execute "PASSENGER_RESPONSE_BUFFER_HIGH_WATERMARK=65536"\
+              "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
               "--pid-file #{fetch(:passenger_pid)} -p #{fetch(:passenger_port)} "\
               "--log-file #{fetch(:passenger_log)} --pool-idle-time 86400"
         end
