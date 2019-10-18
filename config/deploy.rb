74db821b745b5c2e171bba8a9182fad3d25868aa
Scott Fisher <scott.fisher@ucop.edu>
2019-10-18 16:07:38 -0700

    Removing some startup options that didn't seem to make differences to passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -142,11 +142,9 @@ namespace :deploy do
       within current_path do
         with rails_env: fetch(:rails_env) do
           execute "cd #{deploy_to}/current; bundle install --deployment"
-          execute "PASSENGER_RESPONSE_BUFFER_HIGH_WATERMARK=65536; PASSENGER_BUFFER_RESPONSE=no; "\
-              "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
+          execute "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
               "--pid-file #{fetch(:passenger_pid)} -p #{fetch(:passenger_port)} "\
-              "--log-file #{fetch(:passenger_log)} --pool-idle-time 86400 --data-buffer-dir /apps/dryad-dev-shared/passenger "\
-              "--unlimited_concurrency_path /download_test/stream2"
+              "--log-file #{fetch(:passenger_log)} --pool-idle-time 86400 "
         end
       end
     end
