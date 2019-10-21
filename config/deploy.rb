0718bc6ef80e70c9e685e1c76b7912ea1877ae0b
Scott Fisher <scott.fisher@ucop.edu>
2019-10-21 13:19:52 -0700

    reverting some bad changes rubucop made

--- config/deploy.rb
+++ config/deploy.rb
@@ -144,7 +144,7 @@ namespace :deploy do
           execute "cd #{deploy_to}/current; bundle install --deployment"
           execute "cd #{deploy_to}/current; bundle exec passenger start -d --environment #{fetch(:rails_env)} "\
               "--pid-file #{fetch(:passenger_pid)} -p #{fetch(:passenger_port)} "\
-              "--log-file #{fetch(:passenger_log)} --pool-idle-time 86400 "
+              "--log-file #{fetch(:passenger_log)} --pool-idle-time 86400"
         end
       end
     end
