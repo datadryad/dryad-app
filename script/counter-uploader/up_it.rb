29b0484dd1067bae24d161dd4bc5c8063de08499
Scott Fisher <scott.fisher@ucop.edu>
2019-11-01 16:34:22 -0700

    Changes for uploading.

--- script/counter-uploader/up_it.rb
+++ script/counter-uploader/up_it.rb
@@ -10,6 +10,7 @@ require 'pp'
 require 'httparty'
 require 'fileutils'
 require 'yaml'
+require_relative 'uploader'
 
 state_hash = JSON.parse(File.read('json-state/statefile.json'))
 hub_api_token = YAML.load_file('json-state/secrets.yaml')['hub_api_token']
@@ -23,19 +24,24 @@ filenames.each do |fn|
   month_key = fn.match(/\d{4}-\d{2}/).to_s
   month_info = state_hash[month_key]
 
-  response = HTTParty.put("#{hub_base_url}/reports/#{month_info['id']}",
-    body: File.open(fn, 'r').read,
-    headers: {
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json',
-      'Authorization' => "Bearer #{hub_api_token}" },
-    timeout: 300)
+  # all this commented out stuff is for direct upload, which keeps failing for large items.  Trying the compressed upload instead
+  # response = HTTParty.put("#{hub_base_url}/reports/#{month_info['id']}",
+  #   body: File.open(fn, 'r').read,
+  #   headers: {
+  #     'Content-Type' => 'application/json',
+  #     'Accept' => 'application/json',
+  #     'Authorization' => "Bearer #{hub_api_token}" },
+  #   timeout: 300)
 
   # response.headers are useful
   # response.code should be 200
-  puts response.headers
-  puts response.code
+  # puts response.headers
+  # puts response.code
+
+  uploader = Uploader.new(report_id: month_info['id'], file_name: fn)
+  report_id = uploader.process
+
+  puts "reported id #{report_id}"
   puts "Finished #{fn}\r\n\r\n"
-  sleep 120
 end
 
