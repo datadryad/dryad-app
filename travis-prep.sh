fd27081f0abdd5c776bd01b93b515a31f7a4eb79
Scott Fisher <scott.fisher@ucop.edu>
2019-11-08 14:51:15 -0800

    Being sure main repo tests still run on travis.

--- travis-prep.sh
+++ travis-prep.sh
@@ -11,37 +11,6 @@ GITHUB_USER=`git config --get remote.origin.url | sed 's/https:\/\/github\.com\/
 set -e
 
 # ############################################################
-# Local dependencies
-
-if [ ! -d ../stash ]; then
-  BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-
-  echo "Cloning https://github.com/$GITHUB_USER/stash:"
-  cd ..
-
-  set -x
-  git clone https://github.com/$GITHUB_USER/stash
-
-
-  cd stash
-  
-  if git checkout ${BRANCH}; then
-    echo "Checking out stash branch ${BRANCH}"
-  else 
-    echo "No branch ${BRANCH}, checking out master"
-    git checkout master
-  fi
-  
-  { set +x; } 2>/dev/null
-
-  SE_REVISION=$(git rev-parse HEAD)
-  echo "Checked out stash revision ${SE_REVISION}"
-
-  cd ${PROJECT_ROOT}
-fi
-
-
-# ############################################################
 # Test database
 
 echo "Initializing database:"
