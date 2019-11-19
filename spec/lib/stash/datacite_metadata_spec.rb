b69bd4c77072c840072edba1f6df83a6706a7f55
Scott Fisher <scott.fisher@ucop.edu>
2019-11-18 17:37:14 -0800

    Adding journal to citation and adding basic test for what I added.

---
+++ spec/lib/stash/datacite_metadata_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../../../stash/stash_engine/lib/stash/datacite_metadata'
+require 'byebug'
+
+require "rails_helper"
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+end
+
+
+module Stash
+  RSpec.describe DataciteMetadata do
+    include Mocks::Datacite
+
+    # https://doi.org/10.1111/mec.13594
+
+    before(:each) do
+      mock_datacite!
+      @meta = Stash::DataciteMetadata.new(doi: '10.1111/mec.13594')
+      @meta.raw_metadata
+    end
+
+    describe '#journal' do
+      it 'returns the journal from container-title' do
+        expect(@meta.journal).to eq('Molecular Ecology')
+      end
+    end
+
+    describe '#html_citation' do
+      it 'contains the journal name' do
+        expect(@meta.html_citation).to include('Molecular Ecology')
+      end
+    end
+  end
+end
