b2718224fc5bd5c1f034268798f3930135f15655
Ryan Scherle <ryan@scherle.org>
2020-01-11 04:05:02 +0000

    rubocop

--- stash/stash_engine/db/migrate/20200111013011_add_payment_type.rb
+++ stash/stash_engine/db/migrate/20200111013011_add_payment_type.rb
@@ -7,7 +7,7 @@ class AddPaymentType < ActiveRecord::Migration
       if i.payment_id
         if i.payment_id.start_with?('in_')
           i.update_column(:payment_type, 'stripe')
-        else          
+        else
           # else, separate on the colon, set payment_type to part before colon, and payment_id to rest
           found_type, found_id = i.payment_id.match(/(.+):(.+)/).captures
           next unless found_type && found_id
@@ -20,11 +20,10 @@ class AddPaymentType < ActiveRecord::Migration
 
   def down
     StashEngine::Identifier.all.each do |i|
-      if i.payment_id
-        if i.payment_type != 'stripe'
-          new_id = "#{i.payment_type}:#{i.payment_id}"
-          i.update_column(:payment_id, new_id)
-        end
+      next unless i.payment_id
+      if i.payment_type != 'stripe'
+        new_id = "#{i.payment_type}:#{i.payment_id}"
+        i.update_column(:payment_id, new_id)
       end
     end
     rename_column :stash_engine_identifiers, :payment_id, :invoice_id
