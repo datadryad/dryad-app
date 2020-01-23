5444b14080f74a62697e7bc752e0d27e6b666576
Scott Fisher <scott.fisher@ucop.edu>
2020-01-23 15:21:06 -0800

    Test for showing stats from admin with ajax.

---
+++ spec/factories/stash_engine/counter_citations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :counter_citation, class: StashEngine::CounterCitation do
+    identifier
+
+    citation { Faker::Lorem.sentence }
+    doi { identifier { "https://doi.org/#{Faker::Pid.doi}" }}
+  end
+end
