37117f7c3772a3c4cdb4536b4fb4f113390cbbbf
Ryan Scherle <ryan@scherle.org>
2020-02-11 21:55:07 +0000

    really add the mock

---
+++ spec/mocks/crossref_funder.rb
@@ -0,0 +1,54 @@
+module Mocks
+
+  module CrossrefFunder
+
+    def mock_funders!(user = nil)
+      stub_funder_name_lookup
+    end
+
+    def stub_funder_name_lookup(name: Faker::Company.name)
+      stub_request(:get, %r{api\.crossref\.org/funders\?query(.\s)*})
+        .with(
+          headers: {
+            'Content-Type' => 'application/json'
+          }
+        ).to_return(status: 200, body:
+                                   {
+                                     "status":"ok",
+                                    "message-type":"funder-list",
+                                    "message-version":"1.0.0",
+                                    "message": {"items-per-page":20,
+                                                "query":{"start-index":0, "search-terms":"Solution"},
+                                                "total-results":2,
+                                                "items":[{"id":"501100003025",
+                                                          "location":"Canada",
+                                                          "name": name,
+                                                          "alt-names":["Alternate of #{name}"],
+                                                          "uri":"http:\/\/dx.doi.org\/10.13039\/501100003025",
+                                                          "replaces":[],
+                                                          "replaced-by":[],
+                                                          "tokens":["effigis","effigis","geo","solutions"]},
+                                                         {"id":"100009023",
+                                                          "location":null,
+                                                          "name":"WSN Environmental Solutions",
+                                                          "alt-names":[],
+                                                          "uri":"http:\/\/dx.doi.org\/10.13039\/100009023",
+                                                          "replaces":[],
+                                                          "replaced-by":[],
+                                                          "tokens":["wsn","environmental","solutions"]},
+                                                         {"id":"501100000145",
+                                                          "location":null,
+                                                          "name":"Alberta Innovates - Health Solutions",
+                                                          "alt-names":["AIHS"],
+                                                          "uri":"http:\/\/dx.doi.org\/10.13039\/501100000145",
+                                                          "replaces":[],
+                                                          "replaced-by":[],
+                                                          "tokens":["alberta","innovates","health","solutions","aihs"]}
+                                                        ]
+                                               }
+                                   }.to_json, headers: { 'Content-Type' => 'application/json' })
+    end
+    
+  end
+  
+end
