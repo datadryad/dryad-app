6114eb6355c48a60aaec98c37158fc855fcea726
Scott Fisher <scott.fisher@ucop.edu>
2020-02-25 14:31:58 -0800

    Rearranigng file download class so that it takes a filename as part of call since will do it over many filenames and don't want to make a new object every time.

--- stash/stash_engine/lib/stash/merritt_download/file.rb
+++ stash/stash_engine/lib/stash/merritt_download/file.rb
@@ -1,15 +1,12 @@
-# this is really for an individual file download from Merritt to a file and is not a user file-download and streaming class
+# this is really for an individual file download from Merritt to a file
 
-# notes on development test data
-# https://dryad-dev.cdlib.org/stash/dataset/doi:10.5061/dryad.n10d7
-# identifier_id: 761
-# resource_id: 785
-# file names: 'Madagascarophis Nexus Files.zip', 'Madagascarophis_trees.zip', 'mrt-datacite.xml', 'mrt-oaidc.xml', 'stash-wrapper.xml'
+# example file names: 'Madagascarophis Nexus Files.zip', 'Madagascarophis_trees.zip', 'mrt-datacite.xml', 'mrt-oaidc.xml', 'stash-wrapper.xml'
 #
+# example:
 # require 'stash/merritt_download'
 # resource = StashEngine::Resource.find(785)
-# smdf = Stash::MerrittDownload::File.new(resource: resource, filename: 'Madagascarophis Nexus Files.zip')
-# smdf.download_file
+# smdf = Stash::MerrittDownload::File.new(resource: resource)
+# smdf.download_file(filename: 'Madagascarophis Nexus Files.zip')
 
 # I have been favoring the 'httprb/http' gem recently since it is small, fast and pretty easy to use, similar to Python's
 # requests library. See https://twin.github.io/httprb-is-great/ .
@@ -24,11 +21,12 @@ module Stash
     # calling this class File means we need to namespace the built-in Ruby file class when calling it in here
     class File
 
+      attr_reader :path
+
       # we need to be able to download any file from Merritt, including a couple of hidden ones we don't track as user-files (mrt-datacite.xml)
       # so we will need some individual information such as resource and filename and can't use database file_id since it doesn't exist for some
-      def initialize(resource:, filename:)
+      def initialize(resource:)
         @resource = resource
-        @filename = filename
 
         # the 'upload' path is a symlinked shared EFS mount on servers
         @path = Rails.root.join('uploads', 'zenodo_replication', resource.id.to_s)
@@ -36,15 +34,15 @@ module Stash
       end
 
       # download file a and return a hash, we should be tracking success routinely since downloads are error-prone
-      def download_file
-        mrt_resp = get_url
+      def download_file(filename:)
+        mrt_resp = get_url(filename: filename)
 
         unless mrt_resp.status.success?
-          return { success: false, error: "#{mrt_resp.status.code} status code retrieving '#{@filename}' for resource #{@resource.id}" }
+          return { success: false, error: "#{mrt_resp.status.code} status code retrieving '#{filename}' for resource #{@resource.id}" }
         end
 
         # this doesn't load everything into memory at once and writes in chunks, which is good for not blowing out memory by slurping
-        ::File.open(::File.join(@path, @filename), 'wb') do |f|
+        ::File.open(::File.join(@path, filename), 'wb') do |f|
           mrt_resp.body.each do |chunk|
             f.write(chunk)
           end
@@ -52,12 +50,12 @@ module Stash
 
         { success: true }
       rescue HTTP::Error => ex
-        { success: false, error: "Error retrieving '#{@filename}' for resource #{@resource.id}\n#{ex}" }
+        { success: false, error: "Error retrieving '#{filename}' for resource #{@resource.id}\n#{ex}" }
       end
 
       # gets the file url and returns an HTTP.get(url) response object
-      def get_url(read_timeout: 30)
-        url = download_file_url
+      def get_url(filename:, read_timeout: 30)
+        url = download_file_url(filename: filename)
 
         http = HTTP.timeout(connect: 30, read: read_timeout).timeout(7200).follow(max_hops: 10)
           .basic_auth(user: @resource.tenant.repository.username, pass: @resource.tenant.repository.password)
@@ -65,12 +63,12 @@ module Stash
         http.get(url)
       end
 
-      def download_file_url
+      def download_file_url(filename:)
         # domain is not used for MerrittExpress, because the domain is different than the one given by SWORD, only for Merritt UI
         _domain, ark = @resource.merritt_protodomain_and_local_id
 
         "#{APP_CONFIG.merritt_express_base_url}/dv/#{@resource.stash_version.merritt_version}" \
-          "/#{CGI.unescape(ark)}/#{ERB::Util.url_encode(@filename).gsub('%252F', '%2F')}"
+          "/#{CGI.unescape(ark)}/#{ERB::Util.url_encode(filename).gsub('%252F', '%2F')}"
       end
 
     end
