7e9be14877e58aa8515db14c576a70269f438119
Scott Fisher <scott.fisher@ucop.edu>
2020-02-26 14:15:29 -0800

    Start of action that does the whole set of tasks

--- stash/stash_engine/lib/stash/zenodo_replicate/resource.rb
+++ stash/stash_engine/lib/stash/zenodo_replicate/resource.rb
@@ -12,33 +12,33 @@ module Stash
       end
 
       def add_to_zenodo
-
-        # download files from Merritt to local for re-uploading
-        status = download_files
-        return
+        # download files from Merritt
+        location = download_files
 
         # create new zenodo object
 
         # add files
 
         # finalize actions
-
       rescue ReplicationError => ex
-
+        # log this somewhere in the database so we can track it
+      ensure
+        # ensure clean up or other actions every time
       end
 
+      # downloads files and returns directory location
       def download_files
         smdf = Stash::MerrittDownload::File.new(resource: resource)
 
         copy_files = @resource.file_uploads.where(file_state: %w[created copied])
-                         .map(&:upload_file_name).append(%w[mrt-datacite.xml mrt-oaidc.xml stash-wrapper.xml]).flatten
+          .map(&:upload_file_name).append(%w[mrt-datacite.xml mrt-oaidc.xml stash-wrapper.xml]).flatten
 
         copy_files.each do |f|
           status = smdf.download_file(filename: f)
-          return status unless status[:success]
+          raise ReplicationError, "Download: #{status[:error]}" unless status[:success]
         end
 
-        { success: true }
+        smdf.path
       end
     end
   end
