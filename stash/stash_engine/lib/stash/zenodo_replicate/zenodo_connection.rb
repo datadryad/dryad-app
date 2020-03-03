c00b23adc1fda90ddeb02399689b08ab7c6110eb
Scott Fisher <scott.fisher@ucop.edu>
2020-03-03 14:42:42 -0800

    Updates to get full cycle working for submitting again.

--- stash/stash_engine/lib/stash/zenodo_replicate/zenodo_connection.rb
+++ stash/stash_engine/lib/stash/zenodo_replicate/zenodo_connection.rb
@@ -2,7 +2,7 @@ require 'http'
 
 # require 'stash/zenodo_replicate'
 # resource = StashEngine::Resource.find(785)
-# z = Stash::ZenodoReplicate::ZenodoConnection.new(resource: resource, path: '')
+# z = Stash::ZenodoReplicate::ZenodoConnection.new(resource: resource, file_collection:)
 
 module Stash
   module ZenodoReplicate
@@ -11,25 +11,28 @@ module Stash
 
     class ZenodoConnection
 
-      attr_reader :resource, :path, :deposit_id, :links
+      attr_reader :resource, :file_collection, :deposit_id, :links
 
-      def initialize(resource:, path:)
+      # for most actions in here, return the parsed out json and raise error if something not successful
+
+      def initialize(resource:, file_collection:)
         @resource = resource
-        @path = path # the path to where files to upload are
+        @file_collection = file_collection
 
         @http = HTTP.timeout(connect: 30, read: 60).timeout(7200).follow(max_hops: 10)
       end
 
-      # checks that can access API with token
+      # checks that can access API with token and return boolean
       def validate_access
         r = @http.get("#{base_url}/api/deposit/depositions", params: param_merge)
         return true if r.status.success?
+
         false
       rescue HTTP::Error
         false
       end
 
-      # this creates a new deposit and adds metadata at the same time
+      # this creates a new deposit and adds metadata at the same time and returns the json response if successful
       def new_deposition
         mg = MetadataGenerator.new(resource: @resource)
         r = @http.post("#{base_url}/api/deposit/depositions", params: param_merge,
@@ -37,37 +40,41 @@ module Stash
                                                               json: { metadata: mg.metadata })
 
         raise ZenodoError, "Zenodo response: #{r.status.code}" unless r.status.success?
+
         resp = r.parse.with_indifferent_access
         @deposit_id = resp[:id]
         @links = resp[:links]
-        # state = unsubmitted
+
+        # state is unsubmitted at this point
+        resp
       end
 
-      # may need to break this out into it's own class or at least track file deposits in a better way or check
-      # MD5s or other things.
       def send_files
-        path = @path.to_s
+        path = @file_collection.path.to_s
         path << '/' unless path.end_with?('/')
 
         all_files = Dir["#{path}/**/*"]
 
         all_files.each do |f|
           short_fn = f[path.length..-1]
-          # PUT /api/files/<bucket-id>/<filename>
-          resp = @http.put("#{links[:bucket]}/#{ERB::Util.url_encode(short_fn)}",
-                           params: param_merge,
-                           body: File.open(f, 'rb'))
+          r = @http.put("#{links[:bucket]}/#{ERB::Util.url_encode(short_fn)}",
+                        params: param_merge,
+                        body: File.open(f, 'rb'))
+
+          resp = r.parse.with_indifferent_access
+          # TODO: check the response digest against the known digest
         end
       end
 
       def get_files_info
         # right now this is mostly just used for internal testing
-        resp = @http.get(links[:bucket], params: param_merge)
+        r = @http.get(links[:bucket], params: param_merge)
+        r.parse.with_indifferent_access
       end
 
       def publish
-        # should be able to publish
-        resp = @http.post(links[:publish], params: param_merge)
+        r = @http.post(links[:publish], params: param_merge)
+        r.parse.with_indifferent_access
       end
 
       private
