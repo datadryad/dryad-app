39a06ef8a309c6ff7fa4a91697f9ad558467473c
Scott Fisher <scott.fisher@ucop.edu>
2020-03-30 16:19:18 -0700

    Zenodo third copies to zenodo copies

--- stash/stash_engine/app/models/stash_engine/zenodo_copy.rb
+++ stash/stash_engine/app/models/stash_engine/zenodo_copy.rb
@@ -2,7 +2,7 @@ module StashEngine
   # this class has a bit of a peculiar structure and there could be multple third copy items for an identifier,
   # however there should only be one per resource (update the status and we'll have updated timestamps which is really all we need)
   # since this set of states is very simple
-  class ZenodoThirdCopy < ActiveRecord::Base
+  class ZenodoCopy < ActiveRecord::Base
 
     include StashEngine::Concerns::StringEnum
 
