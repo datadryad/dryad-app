222cc9ba951fbe27246535ec966ae6100b17963a
Ryan Scherle <ryan@scherle.org>
2020-04-13 23:18:35 +0000

    move journal features from identifier.rb to journal model

---
+++ stash/stash_engine/spec/db/stash_engine/journal_spec.rb
@@ -0,0 +1,36 @@
+require 'db_spec_helper'
+require 'webmock/rspec'
+require_relative '../../../../spec_helpers/factory_helper'
+require 'byebug'
+
+module StashEngine
+  describe Journal do
+    
+    before(:each) do
+      @journal = Journal.create(issn: '1234-5678')
+    end
+      
+    describe '#will_pay?' do
+      it 'returns true when there is a PREPAID plan' do
+        allow(@journal).to receive('payment_plan_type').and_return('PREPAID')
+        expect(@journal.will_pay?).to be(true)
+      end
+
+      it 'returns true when there is a SUBSCRIPTION plan' do
+        allow(@journal).to receive('payment_plan_type').and_return('SUBSCRIPTION')
+        expect(@journal.will_pay?).to be(true)
+      end
+
+      it 'returns false when there is a no plan' do
+        allow(@journal).to receive('payment_plan_type').and_return(nil)
+        expect(@journal.will_pay?).to be(false)
+      end
+
+      it 'returns false when there is an unrecognized plan' do
+        allow(@journal).to receive('payment_plan_type').and_return('BOGUS-PLAN')
+        expect(@journal.will_pay?).to be(false)
+      end
+    end
+
+  end
+end
