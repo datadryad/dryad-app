665b106a2d5a45d06ecb8eb3ee0f644e030b4549
Scott Fisher <scott.fisher@ucop.edu>
2020-04-20 16:20:55 -0700

    Fixing file upload validation.

--- stash/stash_engine/config/routes.rb
+++ stash/stash_engine/config/routes.rb
@@ -36,7 +36,8 @@ StashEngine::Engine.routes.draw do
   end
 
   resources :edit_histories, only: [:index]
-  match 'file_uploads/validate_urls/:resource_id', to: 'file_uploads#validate_urls', as: 'file_uploads_validate_urls', via: %i[get post put]
+  match 'file_upload/validate_urls/:resource_id', to: 'file_uploads#validate_urls', as: 'file_upload_validate_urls', via: %i[get post put]
+  match 'software_upload/validate_urls/:resource_id', to: 'software_uploads#validate_urls', as: 'software_upload_validate_urls', via: %i[get post put]
 
   resource :file_upload do # TODO: this is wacky since it's using a resource id rather than a file id maybe this belongs in resource.
     member do
