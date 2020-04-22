683c39321216090f7616ce4a23978eaa5ce9cce0
Scott Fisher <scott.fisher@ucop.edu>
2020-04-22 14:23:28 -0700

    Adding tests for URLs for software and also adding fixes to be sure it uses correct table.

--- spec/features/stash_engine/ui_file_upload_spec.rb
+++ spec/features/stash_engine/ui_file_upload_spec.rb
@@ -28,7 +28,7 @@ RSpec.feature 'UiFileUpload', type: :feature do
     visit root_path
     click_link 'My Datasets'
     start_new_dataset
-    fill_required_fields
+    # fill_required_fields # don't need this if we're not checking metadata and just files
   end
 
   describe :file_uploads do
@@ -132,6 +132,118 @@ RSpec.feature 'UiFileUpload', type: :feature do
 
       # it put it in the database
       expect(@resource.software_uploads.count).to eq(0)
+    end
+  end
+
+  describe 'normal URL validation' do
+
+    before(:each, js: true) do
+      navigate_to_upload
+      navigate_to_upload_urls
+
+      # get resource and clean up uploads directories
+      @resource_id = page.current_path.match(%r{resources/(\d+)/up})[1].to_i
+      @resource = StashEngine::Resource.find(@resource_id)
+
+      stub_request(:head, "http://example.org/funbar.txt").
+          with(
+              headers: {
+                  'Accept'=>'*/*',
+              }).
+          to_return(status: 200, headers: {'Content-Length': 37221, 'Content-Type': 'text/plain'})
+
+      stub_request(:head, "http://example.org/foobar.txt").
+          with(
+              headers: {
+                  'Accept'=>'*/*',
+              }).
+          to_return(status: 404)
+    end
+
+    it 'validates a URL that works', js: true do
+      fill_in('location_urls', :with => 'http://example.org/funbar.txt')
+      check('confirm_to_validate')
+      click_on('validate_files')
+
+      expect(page).to have_content('37.22 kB')
+      expect(page).to have_content('funbar.txt')
+
+      # and it made it into the database
+      fu = @resource.file_uploads.first
+      expect(fu.upload_file_name).to eq('funbar.txt')
+      expect(fu.upload_content_type).to eq('text/plain')
+      expect(fu.upload_file_size).to eq(37221)
+    end
+
+    it 'shows problem with bad URL', js: true do
+      fill_in('location_urls', :with => 'http://example.org/foobar.txt')
+      check('confirm_to_validate')
+      click_on('validate_files')
+
+      expect(page).to have_content('The URL was not found')
+
+      # and it made it into the database
+      fu = @resource.file_uploads.first
+      expect(fu.upload_file_name).to be_nil
+      expect(fu.upload_content_type).to be_nil
+      expect(fu.upload_file_size).to be_nil
+      expect(fu.status_code).to eq(404)
+    end
+  end
+
+  describe 'software URL validation' do
+
+    before(:each, js: true) do
+      navigate_to_software_upload
+      navigate_to_software_upload_urls
+
+      # get resource and clean up uploads directories
+      @resource_id = page.current_path.match(%r{resources/(\d+)/up})[1].to_i
+      @resource = StashEngine::Resource.find(@resource_id)
+
+      stub_request(:head, "http://example.org/funbar.txt").
+          with(
+              headers: {
+                  'Accept'=>'*/*',
+              }).
+          to_return(status: 200, headers: {'Content-Length': 37221, 'Content-Type': 'text/plain'})
+
+      stub_request(:head, "http://example.org/foobar.txt").
+          with(
+              headers: {
+                  'Accept'=>'*/*',
+              }).
+          to_return(status: 404)
+    end
+
+    it 'validates a URL that works', js: true do
+      fill_in('location_urls', :with => 'http://example.org/funbar.txt')
+      check('confirm_to_validate')
+      click_on('validate_files')
+
+      expect(page).to have_content('37.22 kB')
+      expect(page).to have_content('funbar.txt')
+
+      # and it made it into the database
+      su = @resource.software_uploads.first
+      expect(su.upload_file_name).to eq('funbar.txt')
+      expect(su.upload_content_type).to eq('text/plain')
+      expect(su.upload_file_size).to eq(37221)
+    end
+
+    it 'shows problem with bad URL', js: true do
+      fill_in('location_urls', :with => 'http://example.org/foobar.txt')
+      check('confirm_to_validate')
+      click_on('validate_files')
+
+      expect(page).to have_content('The URL was not found')
+
+      # and it made it into the database
+      su = @resource.software_uploads.first
+      expect(su.upload_file_name).to be_nil
+      expect(su.upload_content_type).to be_nil
+      expect(su.upload_file_size).to be_nil
+      expect(su.status_code).to eq(404)
     end
   end
 end
