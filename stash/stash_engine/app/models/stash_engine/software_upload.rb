960cac9cebb52114a0d9ed7c08a132cd3ab13f2f
Scott Fisher <scott.fisher@ucop.edu>
2020-04-27 13:09:58 -0700

    fixing problems found in review

--- stash/stash_engine/app/models/stash_engine/software_upload.rb
+++ stash/stash_engine/app/models/stash_engine/software_upload.rb
@@ -1,13 +1,9 @@
 module StashEngine
-  # class SoftwareUpload < ActiveRecord::Base
   class SoftwareUpload < FileUpload
-    # belongs_to :resource, class_name: 'StashEngine::Resource'
     self.table_name = 'stash_engine_software_uploads'
 
     undef_method :download_histories, :version_file_created_in, :merritt_url, :merritt_presign_info_url,
                  :s3_presigned_url, :merritt_express_url
-
-    # seems like most methods are OK to have, but probably need to be overridden
 
     def calc_file_path
       return nil if file_state == 'copied' || file_state == 'deleted' # no current file to have a path for
