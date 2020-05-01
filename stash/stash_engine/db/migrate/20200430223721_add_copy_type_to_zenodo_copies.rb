881ee9ad50ac93fc37c9b59cb57056d0e0d85fd3
Scott Fisher <scott.fisher@ucop.edu>
2020-05-01 13:34:07 -0700

    database changes I forgot to add earlier and enable sending of items to zenodo in this branch again.

---
+++ stash/stash_engine/db/migrate/20200430223721_add_copy_type_to_zenodo_copies.rb
@@ -0,0 +1,9 @@
+class AddCopyTypeToZenodoCopies < ActiveRecord::Migration
+  def change
+    add_column :stash_engine_zenodo_copies, :copy_type, "ENUM('data', 'software', 'software_publish') DEFAULT 'data'"
+    add_index :stash_engine_zenodo_copies, :copy_type
+    # the resource_id is currently set to be only one, but we need more than one if it handles both data and software copies in this table
+    remove_index :stash_engine_zenodo_copies, :resource_id
+    add_index :stash_engine_zenodo_copies, :resource_id
+  end
+end
