3df69dce78fb42ccd030056434eb7605a74fe4ad
Scott Fisher <scott.fisher@ucop.edu>
2020-05-27 17:35:04 -0700

    Initial version of presigned download with not progressbar yet.

--- stash/stash_engine/lib/stash/download/version_presigned.rb
+++ stash/stash_engine/lib/stash/download/version_presigned.rb
@@ -15,9 +15,6 @@ module Stash
         _ignored, @local_id = @resource.merritt_protodomain_and_local_id
         @domain = @tenant.repository.domain
 
-        if @resource.blank? || @tenant.blank? || @version.blank? || @domain.blank? || @local_id.blank?
-          cc.render status: 404, text: 'Not found'
-        end
         @http = HTTP.timeout(connect: 30, read: 30).timeout(1.hour.to_i).follow(max_hops: 10)
                     .basic_auth(user: @tenant&.repository&.username, pass: @tenant&.repository&.password)
       end
@@ -26,32 +23,70 @@ module Stash
         !(@resource.blank? || @tenant.blank? || @version.blank? || @domain.blank? || @local_id.blank?)
       end
 
+      # this should return
+      # 200 for URL being present to download
+      # 202 for needing to wait with a progress bar
+      # 404 for not found
+      def download
+        assemble_hash = {}
+        assemble_hash = assemble if @resource.download_token.token.blank?
+        return { status: 404 } if assemble_hash[:status] == 404
+
+        status_hash = status
+
+        return status_hash if status_hash[:status] == 200 # it's here, do a download
+
+        if [404, 410].include?(status_hash[:status]) # not found or expired, then assemble it again
+          assemble
+        end
+
+        ready_time = @resource.download_token.available - Time.new
+
+        if ready_time.positive? && ready_time < 30.seconds
+          poll_and_download
+        else
+          status
+        end
+      end
+
+      def poll_and_download
+        puts "\n\nPOLLING FOR DOWNLOAD\n\n"
+        status_hash = {}
+        1.upto(2) do
+          sleep 7
+          status_hash = status
+          break if status_hash[:status] == 200
+        end
+        status_hash
+      end
+
+      # resp.status.success?  # resp.status == 200
+      # 200 is good and gives a token
+      # {"status"=>200, "token"=>"11d2afdd-9351-4403-8013-88a9e9284e96", "cloud-content-byte"=>1228236,
+      # "anticipated-availability-time"=>"2020-05-27T12:43:48-07:00", "message"=>"Request queued, use token to check status"}
+      # Time.parse(json['anticipated-availability-time'])
+      #
+      # otherwise 404
       def assemble
         resp = @http.get(assemble_version_url)
         if resp.status.success?
           json = resp.parse.with_indifferent_access
-          @token = json[:token]
-          @available = Time.parse(json['anticipated-availability-time'])
+          token = @resource.download_token
+          token.token = json[:token]
+          token.available = Time.parse(json['anticipated-availability-time'])
+          token.save
         end
-
-        # resp.status.success?  # resp.status == 200
-        # 200 is good and gives a token
-        # {"status"=>200, "token"=>"11d2afdd-9351-4403-8013-88a9e9284e96", "cloud-content-byte"=>1228236,
-        # "anticipated-availability-time"=>"2020-05-27T12:43:48-07:00", "message"=>"Request queued, use token to check status"}
-        # Time.parse(json['anticipated-availability-time'])
-        #
-        # otherwise 404
+        json
       end
 
-      def status(token:)
-        resp = @http.get(status_url(token: token))
-        json = resp.parse.with_indifferent_access
-        # {"status"=>202, "token"=>"ed3b8dc1-afac-4487-bfac-fb89d654e4d9", "cloud-content-byte"=>1228236, "message"=>"Object is not ready"}
-        # 200 -- ready
-        # 202 -- not ready
-        # 404 -- not found
-        # 410 -- expired
-        byebug
+      # {"status"=>202, "token"=>"ed3b8dc1-afac-4487-bfac-fb89d654e4d9", "cloud-content-byte"=>1228236, "message"=>"Object is not ready"}
+      # 200 -- ready
+      # 202 -- not ready
+      # 404 -- not found
+      # 410 -- expired
+      def status
+        resp = @http.get(status_url)
+        resp.parse.with_indifferent_access
       end
 
       def assemble_version_url
@@ -61,10 +96,10 @@ module Stash
             query: {format: 'zip', content: 'producer'}.to_query).to_s
       end
 
-      def status_url(token:)
+      def status_url
         URI::HTTPS.build(
           host: @domain,
-          path: File.join('/api', 'presign-obj-by-token', ERB::Util.url_encode(@token)),
+          path: File.join('/api', 'presign-obj-by-token', ERB::Util.url_encode(@resource.download_token.token)),
           query: {no_redirect: true, filename: filename}.to_query).to_s
       end
 
