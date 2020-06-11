b43edd8a0601a8578bb23892ae7aa5df1c52b52c
Scott Fisher <scott.fisher@ucop.edu>
2020-06-11 14:16:37 -0700

    Fixing problem with relation that got lost in the merge

--- stash/stash_engine/app/models/stash_engine/resource.rb
+++ stash/stash_engine/app/models/stash_engine/resource.rb
@@ -26,7 +26,7 @@ module StashEngine
     has_many :curation_activities, -> { order(id: :asc) }, class_name: 'StashEngine::CurationActivity', dependent: :destroy
     has_many :repo_queue_states, class_name: 'StashEngine::RepoQueueState', dependent: :destroy
     has_many :download_histories, class_name: 'StashEngine::DownloadHistory', dependent: :destroy
-    has_one :zenodo_copy, class_name: 'StashEngine::ZenodoCopy', dependent: :destroy
+    has_many :zenodo_copies, class_name: 'StashEngine::ZenodoCopy', dependent: :destroy
     has_one :download_token, class_name: 'StashEngine::DownloadToken', dependent: :destroy
 
     accepts_nested_attributes_for :curation_activities
