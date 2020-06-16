2360aead5d074469f483cef10ca8c26ce4d6a903
Scott Fisher <scott.fisher@ucop.edu>
2020-06-16 10:56:21 -0700

    This should allow this to be used with ActiveJob and also fixes small bug in re-enqueuing

--- stash/stash_engine/app/jobs/stash_engine/zenodo_software_job.rb
+++ stash/stash_engine/app/jobs/stash_engine/zenodo_software_job.rb
@@ -1,8 +1,33 @@
-require 'stash/zenodo_replicate'
+require 'stash/zenodo_software'
 
 module StashEngine
   class ZenodoSoftwareJob < ::ActiveJob::Base
     queue_as :zenodo_software
-    # TODO: this is a stub
+
+    # unlike the zenodo_copy_job, this one takes a ZenodoCopy.id instead of a resource_id since it needs a little more info
+    # and may do multiple actions on the same resource (such as sending and then publishing the same resource)
+    def perform(*args)
+      @job_entry = StashEngine::ZenodoCopy.where(id: args[0]).first
+      return if @job_entry.nil? || should_defer?
+
+      data_copy = resource&.zenodo_copies&.data&.first
+      zr = Stash::ZenodoReplicate::Copier.new(copy_id: data_copy.id)
+      zr.add_to_zenodo
+    end
+
+    def should_defer?
+      if File.exist?(ZenodoCopyJob::DEFERRED_TOUCH_FILE) # use same touch file for both software and replication for Zenodo
+        @job.update(state: 'deferred')
+        return true
+      end
+      false
+    end
+
+    def self.enqueue_deferred
+      StashEngine::ZenodoCopy.software.where(state: 'deferred').each do |zc|
+        zc.update(state: 'enqueued')
+        perform_later(zc.id)
+      end
+    end
   end
 end
