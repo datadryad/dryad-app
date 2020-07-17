6d452953c730bde3ebfcb4cf09c1bf922cdf42dd
Scott Fisher <scott.fisher@ucop.edu>
2020-07-17 13:52:09 -0700

    Simplifying the main class and making helper and adding a bunch of tests

--- stash/script/counter-uploader/main.rb
+++ stash/script/counter-uploader/main.rb
@@ -6,38 +6,37 @@ require 'byebug'
 # other classes for this
 require_relative 'submitted_reports'
 require_relative 'uploader'
+require_relative 'utility_methods'
 
 # setup variables needed
-@report_directory = ENV['REPORT_DIR'] || File.join(File.expand_path(__dir__), 'reports')
+report_directory = ENV['REPORT_DIR'] || File.join(File.expand_path(__dir__), 'reports')
 # ENV['TOKEN'] should be set, used in uploader class
 # if ENV['REPORT_IDS'] is set then just report the IDs for our reports
+# if ENV['FORCE_SUBMISSION'] is set with comma separated yyyy-mm values then those reports will be
+# submitted again, even if they already appear to have been submitted
 
-if ENV['REPORT_IDS'].nil? && (ENV['TOKEN'].nil? || ENV['REPORT_DIR'].nil?)
-  puts 'You must set environment variables for the TOKEN and REPORT_DIR to upload to DataCite'
-  exit(1)
-end
+UtilityMethods.check_env_variables
+
+force_list = UtilityMethods.force_submission_list
+
+# retrieve all the information about already submitted reports from DataCite
+submitted_reports = SubmittedReports.new
+submitted_reports.process_reports
+
+# display submitted report info and exit if that option was chosen
+UtilityMethods.output_report_table_if_requested(submitted_reports)
 
 # get the json files we have non-zero reports for and are in the correct filename format
-@json_files = Dir.glob(File.join(@report_directory, '*.json'))
-@json_files.keep_if { |f| f.match(/\d{4}-\d{2}.json$/) && File.size(f) > 0 }
-@json_files.sort!
+json_files = Dir.glob(File.join(report_directory, '*.json'))
+json_files.keep_if { |f| f.match(/\d{4}-\d{2}.json$/) && File.size(f) > 0 }
+json_files.sort!
 
-@submitted_reports = SubmittedReports.new
-@submitted_reports.process_reports
-
-if ENV['REPORT_IDS']
-  puts 'Information about submitted months'
-  puts "year-month\tresults_pages\tid"
-  @submitted_reports.reports.values.sort { |a, b| a.year_month <=> b.year_month }.each do |report|
-    puts "#{report.year_month}\t#{report.pages}\t#{report.id}"
-  end
-  exit 0
-end
-
-@json_files.each do |json_file|
+# go through our report files and try to submit unsubmitted/problem ones (and any you want to force submission for)
+json_files.each do |json_file|
   month_year = File.basename(json_file, '.json')
-  submitted_report = @submitted_reports.reports[month_year]
-  if Helper.needs_submission?(month_year: month_year, report_info: submitted_report)
+  submitted_report = submitted_reports.reports[month_year]
+  if UtilityMethods.needs_submission?(month_year: month_year, report_info: submitted_report, report_directory: report_directory,
+                                      force_list: force_list)
     puts "adding or updating #{month_year} with #{submitted_report&.pages || 'unsubmitted'} pages"
 
     report_id = (submitted_report.nil? ? nil : submitted_report.id)
---
+++ stash/script/counter-uploader/utility_methods.rb
@@ -0,0 +1,43 @@
+module UtilityMethods
+  def self.needs_submission?(month_year:, report_directory:, report_info: nil, force_list:)
+    # submit if it is being forced or no report info or no pages of results
+    return true if force_list.include?(month_year) || report_info.nil? || report_info.pages.nil? || report_info.pages == 0
+
+    # otherwise check the number of pages of results against the json file size. I did a quick analysis
+    # and the filesize-bytes/pages hovered around 10,000 for most validly submitted reports.
+    # Seemed to always be in the range of 8,000 up to about 20,000 bytes of json per page of DataCite results
+    # So if the number of bytes/page is > 50,000 then something is probably really wrong.
+    filesize = File.size(File.join(report_directory, "#{month_year}.json"))
+    return true if (filesize/report_info.pages) > 50_000
+
+    false
+  end
+
+  def self.check_env_variables
+    if ENV['REPORT_IDS'].nil? && (ENV['TOKEN'].nil? || ENV['REPORT_DIR'].nil?)
+      puts 'You must set environment variables for the TOKEN and REPORT_DIR to upload to DataCite.'
+      puts 'Optional environment variables:'
+      puts "\tREPORT_IDS -- if set, only reports the yyyy-mm and ids that have been sent to DataCite."
+      puts "\tFORCE_SUBMISSION may be set with a comma separated list of yyyy-mm values and those reports"
+      puts "\twill be sent again, even if they appear to have already been submitted successfully."
+      exit(1)
+    end
+  end
+
+  def self.output_report_table_if_requested(submitted_reports)
+    if ENV['REPORT_IDS']
+      puts 'Information about submitted months in tab separated format.'
+      puts "year-month\tresults_pages\tid"
+      submitted_reports.reports.values.sort { |a, b| a.year_month <=> b.year_month }.each do |report|
+        puts "#{report.year_month}\t#{report.pages}\t#{report.id}"
+      end
+      exit 0
+    end
+  end
+
+
+  def self.force_submission_list
+    return [] if ENV['FORCE_SUBMISSION'].nil?
+    ENV['FORCE_SUBMISSION'].split(',').map(&:strip)
+  end
+end
