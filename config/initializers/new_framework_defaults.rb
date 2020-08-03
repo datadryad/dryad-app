9195fdbb6e281ca5c66128dd438b688e6f8b35ba
Ryan Scherle <ryan@scherle.org>
2020-08-03 16:06:50 +0000

    flip new config settings to new defaults

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -12,11 +12,11 @@ Rails.application.config.action_controller.raise_on_unfiltered_parameters = true
 Rails.application.config.action_controller.per_form_csrf_tokens = true
 
 # Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+Rails.application.config.action_controller.forgery_protection_origin_check = true
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = false
+ActiveSupport.to_time_preserves_timezone = true
 
 # Require `belongs_to` associations by default. Previous versions had false.
 # Rails.application.config.active_record.belongs_to_required_by_default = false
