2f2c0e66263b1cc134daf6ac1b28f12b676718db
Scott Fisher <scott.fisher@ucop.edu>
2020-08-06 10:51:24 -0700

    Adding test for non_fos scope

--- stash/stash_datacite/app/models/stash_datacite/subject.rb
+++ stash/stash_datacite/app/models/stash_datacite/subject.rb
@@ -6,6 +6,6 @@ module StashDatacite
     has_and_belongs_to_many :resources, class_name: StashEngine::Resource.to_s,
                                         through: 'StashDatacite::ResourceSubject'
 
-    scope :non_fos, -> { where("subject_scheme <> 'fos'") }
+    scope :non_fos, -> { where("subject_scheme IS NULL OR subject_scheme <> 'fos'") }
   end
 end
