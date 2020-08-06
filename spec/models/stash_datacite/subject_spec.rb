623668a41fa5284360cd148dd363ecfa7bd83fd0
Scott Fisher <scott.fisher@ucop.edu>
2020-08-06 11:28:56 -0700

    Test for sending the subject to DataCite per ticket #776 rubocop fixes

--- spec/models/stash_datacite/subject_spec.rb
+++ spec/models/stash_datacite/subject_spec.rb
@@ -2,9 +2,9 @@ module StashDatacite
   module Resource
     describe Subject do
       it 'leaves out FOS items with .non_fos scope' do
-        items = [ create(:subject), create(:subject), create(:subject, subject_scheme: 'fos') ]
-        expect(Subject.all.length).to eq(3)
-        expect(Subject.all.non_fos.length).to eq(2)
+        items = [create(:subject), create(:subject), create(:subject, subject_scheme: 'fos')]
+        expect(Subject.all.length).to eq(items.length)
+        expect(Subject.all.non_fos.length).to eq(items.length - 1)
       end
     end
   end
