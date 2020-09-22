d1f54a792ac0673ba714d9c9b8689167f8d1fc82
Scott Fisher <scott.fisher@ucop.edu>
2020-09-22 15:02:26 -0700

    Requirements are to hide specially flagged related works from display on landing page.

---
+++ spec/responses/stash_datacite/related_identifiers_controller_spec.rb
@@ -0,0 +1,38 @@
+
+# We have no real way to test this directly in the UI since setting a related identifier to hidden can't be done in the
+# UI and testing the UI runs in a different process/database which is difficult to manipulate manually.
+module StashDatacite
+  RSpec.describe AffiliationsController, type: :request do
+
+    before(:each) do
+      user = StashEngine::User.create(
+          email: 'lmuckenhaupt@example.edu',
+          tenant_id: 'dataone'
+      )
+      @resource = create(:resource, user_id: user.id)
+      @related_identifier = create(:related_identifier, resource_id: @resource.id, work_type: 'article')
+    end
+
+    describe 'non-hidden related IDs' do
+      it 'renders a non-hidden related work' do
+        response_code = get "/stash_datacite/related_identifiers/show.js?resource_id=#{@resource.id}", xhr: true
+        expect(response_code).to eq(200)
+
+        body = response.body
+        expect(body).to include('Article')
+        expect(body).to include(@related_identifier.related_identifier)
+      end
+
+      it 'hides a hidden related work' do
+        @related_identifier.update(hidden: true)
+        response_code = get "/stash_datacite/related_identifiers/show.js?resource_id=#{@resource.id}", xhr: true
+        expect(response_code).to eq(200)
+
+        body = response.body
+        expect(body).not_to include('Article')
+        expect(body).not_to include(@related_identifier.related_identifier)
+      end
+
+    end
+  end
+end
