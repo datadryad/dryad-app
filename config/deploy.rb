de6f1d75ed69ae0459d7fbc1454770e6c9b63ed3
Ryan Scherle <ryan@scherle.org>
2020-09-23 21:32:01 +0000

    cap linking to keyfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,9 @@ set :scm, :git
 # Default value for :pty is false
 # set :pty, true
 
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push(Dir.glob('config/*.key')).flatten.uniq
+
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle',
                                                'public/system', 'uploads')
