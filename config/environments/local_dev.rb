fcab45b355d54d369c08b21c7b171c721c4bb9bb
Scott Fisher <scott.fisher@ucop.edu>
2020-09-28 14:03:43 -0700

    Change to make secret_key_base at top level and re-enable the local_dev environment

--- config/environments/local_dev.rb
+++ config/environments/local_dev.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.web_console.development_only = false
+
   # this craziness is to get logging both to console and to log file just like normal development
   normal_logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
   console_logger = ActiveSupport::Logger.new(STDOUT)
@@ -25,8 +27,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # mailer
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   # config.active_storage.service = :local     
@@ -37,6 +43,9 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -46,6 +55,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Suppress logger output for normal asset requests.
+  config.assets.quiet = true
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
@@ -54,13 +66,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # local_dev is also development so let development stuff work here
-  config.web_console.development_only = false
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'dryad-dev.cdlib.org' }
-  Rails.application.default_url_options = { host: 'dryad-dev.cdlib.org' }
+  Rails.application.default_url_options = { host: 'localhost:3000' }
 
 end
