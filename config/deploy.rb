757462e95d72a64d0a1911651f4d017d40cd95a2
Scott Fisher <scott.fisher@ucop.edu>
2020-10-13 16:04:37 -0700

    Get rid of capistrano warning about deprecation.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,9 +13,6 @@ set :branch, ENV['BRANCH'] if ENV['BRANCH']
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/apps/dryad/apps/ui'
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
