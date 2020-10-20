2d263e8e37425b2052087ccbd915b7ef9520e223
Ryan Scherle <ryan@scherle.org>
2020-10-20 11:45:41 +0000

    finish basic redirect without auth

--- stash/stash_engine/app/controllers/stash_engine/metadata_entry_pages_controller.rb
+++ stash/stash_engine/app/controllers/stash_engine/metadata_entry_pages_controller.rb
@@ -16,39 +16,44 @@ module StashEngine
 
     # GET/POST/PUT  /generals/find_or_create
     def find_or_create
-      puts 'XXXXX se find_or_create'
+      puts 'XXXXXXXXX se find_or_create'
       puts "     params #{params.permit!}"
       puts "     current_user #{current_user}"
       puts "     session #{session.to_hash}"
+      puts "     resource #{resource.id} #{@resource.id} #{@resource.submitted?}"
+      puts " -----------"
       return unless @resource.submitted? # create a new version if this is a submitted version
 
+      puts " -----------"
       redirect_to(metadata_entry_pages_new_version_path(resource_id: params[:resource_id]))
     end
 
     # GET /stash/edit/{doi}/{edit_code}
     def edit_by_doi
-      puts 'XXXXX se edit_by_doi'
+      puts 'XXXXXXXXX se edit_by_doi'
       puts "     params #{params}"
       puts "     current_user #{current_user}"
       puts "     session #{session.to_hash}"
       puts "     resource #{resource.id}"
-      #      session[:user_id] = 37178
 
       # if edit_code is present, and matches the code for the target DOI, store the edit_code in the session and redirect
       if valid_edit_code?
         # we're following an edit link and the magic code is passed in
         puts 'XXXX magic code found, allowing use'
-        puts 'XXXX redirecting to edit page'
-        redirect_to(metadata_entry_pages_new_version_path(resource: resource))
+        puts 'XXXX redirecting to edit page for resource #{resource.id}'
+        puts " -----------"
+        redirect_to(metadata_entry_pages_find_or_create_path(resource_id: resource.id))
       else
         # if the user is logged in and has permissions for the dataset, redirect and continue as normal
         puts 'XXXX redirecting to edit page, assuming logged-in user'
-        redirect_to(metadata_entry_pages_new_version_path(resource: resource))
+        puts " -----------"
+        redirect_to(metadata_entry_pages_find_or_create_path(resource_id: resource.id))
       end
     end
 
     # create a new version of this resource before editing with find or create
     def new_version
+      puts "XXXX new_version"
       duplicate_resource
 
       # redirect to find or create path
@@ -75,21 +80,27 @@ module StashEngine
     private
 
     def resource_exist
+      puts "XXXX resource_exist #{params[:resource_id]}"
       resource = Resource.find(params[:resource_id]) if params[:resource_id]
       resource = resource_from_doi if resource.nil?
       redirect_to root_path, notice: 'The dataset you are looking for does not exist.' if resource.nil?
     end
 
     def resource_from_doi
+      puts "XXXX resource_from_doi #{params[:doi]}"
       doi = params[:doi]
       doi&.match(/doi:(.*)/) do |m|
         doi = m[1]
       end
       id = Identifier.where(identifier: doi).first
       @resource = id&.latest_resource
+      params[:resource_id] = @resource.id
+      puts "     setting resource id #{@resource.id}"
+      @resource
     end
 
     def duplicate_resource
+      puts "XXXX duplicate_resource"
       @new_res = @resource.amoeba_dup
       @new_res.current_editor_id = current_user.id
       # The default curation activity gets set to the `Resource.user_id` but we want to use the current user here
@@ -98,6 +109,7 @@ module StashEngine
     end
 
     def require_can_duplicate
+      puts "XXXX r_c_duplicate"
       return false unless (@identifier = resource.identifier)
 
       set_return_to_path_from_referrer # needed for dropping into edit (and back) from various places in the ui
