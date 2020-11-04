9569bdd2fd8d9118e4b302130f707388e309cd1a
Ryan Scherle <ryan@scherle.org>
2020-11-04 22:05:52 +0000

    ignore insert errors

--- stash/stash_engine/db/migrate/20201102215917_add_admin_user.rb
+++ stash/stash_engine/db/migrate/20201102215917_add_admin_user.rb
@@ -1,6 +1,6 @@
 class AddAdminUser < ActiveRecord::Migration[5.2]
   def up
-    execute "INSERT INTO stash_engine_users (id, first_name, last_name, role, created_at, updated_at) VALUES (0, 'Dryad', 'System', 'user', '2020-01-01', '2020-01-01')"
+    execute "INSERT IGNORE INTO stash_engine_users (id, first_name, last_name, role, created_at, updated_at) VALUES (0, 'Dryad', 'System', 'user', '2020-01-01', '2020-01-01')"
   end
 
   def down
