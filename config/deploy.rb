8abce0f20767f61048e0e937ed6bbf1349051b0f
Scott Fisher <scott.fisher@ucop.edu>
2020-12-15 14:07:53 -0800

    Setting variables per environment because the ones set in .bash_profile are not available.

--- config/deploy.rb
+++ config/deploy.rb
@@ -179,11 +179,15 @@ namespace :deploy do
   desc 'Setup ENV Variables'
   task :env_setup do
     on roles(:app), wait: 1 do
-      json = capture ('aws ssm get-parameter --name "${SSM_ROOT_PATH}master_key" --region "$AWS_REGION"')
-      json = JSON.parse(json)
-      master_key = json['Parameter']['Value']
-      # ssm = Uc3Ssm::ConfigResolver.new
-      # master_key = ssm.parameter_for_key('master_key')
+      # ssm_root_path = capture('echo $SSM_ROOT_PATH')
+      # region = capture('echo $AWS_REGION')
+      # puts "ssm_root_path: #{ssm_root_path}"
+      # puts "region: #{region}"
+      # json = capture ('aws ssm get-parameter --name "${SSM_ROOT_PATH}master_key" --region "$AWS_REGION"')
+      # json = JSON.parse(json)
+      # master_key = json['Parameter']['Value']
+      ssm = Uc3Ssm::ConfigResolver.new(ssm_root_path: fetch(:ssm_root_path), region: fetch(:aws_region))
+      master_key = ssm.parameter_for_key('master_key')
       f = File.open("#{release_path}/config/master.key", 'w')
       f.puts master_key
       f.close
