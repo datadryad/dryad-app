513a15f8d55f59d894540db198ccc3005e6230d8
Scott Fisher <scott.fisher@ucop.edu>
2021-02-19 13:43:02 -0800

    Lots of annoying testing of S3 streaming

--- stash/stash_engine/lib/stash/zenodo_software/file_collection.rb
+++ stash/stash_engine/lib/stash/zenodo_software/file_collection.rb
@@ -16,24 +16,24 @@ module Stash
         @path = resource.software_upload_dir
       end
 
-      def ensure_local_files
-        @resource.software_uploads.newly_created.each do |upload|
-          zen_file = Stash::ZenodoSoftware::FileDownload.new(file_obj: upload)
-          zen_file.download unless upload.url.blank?
-          zen_file.check_file_exists
-          zen_file.check_digest
-        end
-      end
+      # def ensure_local_files
+      #   @resource.software_uploads.newly_created.each do |upload|
+      #     zen_file = Stash::ZenodoSoftware::FileDownload.new(file_obj: upload)
+      #     zen_file.download unless upload.url.blank?
+      #     zen_file.check_file_exists
+      #     zen_file.check_digest
+      #   end
+      # end
 
-      def cleanup_files
-        FileUtils.rm_rf(@path)
-      end
+      # def cleanup_files
+      #   FileUtils.rm_rf(@path)
+      # end
 
       # from the response o loaded dataset's json response[:links][:bucket]
       def synchronize_to_zenodo(bucket_url:)
         @zenodo_file = ZenodoFile.new(bucket_url: bucket_url)
         remove_files
-        upload_files
+        upload_files(zenodo_bucket_url: bucket_url)
       end
 
       def remove_files
@@ -42,9 +42,11 @@ module Stash
         end
       end
 
-      def upload_files
+      def upload_files(zenodo_bucket_url:)
         @resource.software_uploads.newly_created.each do |upload|
-          @zenodo_file.upload(file_model: upload)
+          # @zenodo_file.upload(file_model: upload)
+          streamer = Streamer.new(file_model: upload, zenodo_bucket_url: zenodo_bucket_url)
+          streamer.stream
         end
       end
     end
