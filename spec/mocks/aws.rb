8fc53e3132f02002253bca9df8832fd3a457feaf
Ryan Scherle <ryan@scherle.org>
2021-02-25 19:50:02 +0000

    Fix test

---
+++ spec/mocks/aws.rb
@@ -0,0 +1,24 @@
+module Mocks
+
+  module Aws
+
+    def mock_aws!
+      mock_s3!
+    end
+
+    def mock_s3!
+      allow_any_instance_of(::Aws::S3::Object).to receive(:delete).and_return(nil)
+      allow_any_instance_of(::Aws::S3::Object).to receive(:exists?).and_return(nil)
+      allow_any_instance_of(::Aws::S3::Object).to receive(:put).and_return(nil)
+      allow_any_instance_of(::Aws::S3::Object).to receive(:presigned_url).and_return(
+        "https://somebucket.amazonaws.com/presignedURL-#{Faker::Alphanumeric.alphanumeric(number: 10)}"
+      )
+      allow_any_instance_of(::Aws::S3::ObjectSummary::Collection).to receive(:batch_delete!).and_return(nil)
+
+      # Add a default database configuration, which is needed by Resource.s3_dir_name
+      allow(Rails).to receive(:configuration).and_return(OpenStruct.new(database_configuration: { 'test' => { 'host' => 'localhost' } }))
+    end
+
+  end
+
+end
