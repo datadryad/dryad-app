cb583d481f5cbc2e527e99c35aec0a9efcb743ab
Scott Fisher <scott.fisher@ucop.edu>
2021-03-01 11:16:09 -0800

    Fixing minor bug for zenodo merritt uploads and adding test.

--- stash/stash_engine/app/models/stash_engine/file_upload.rb
+++ stash/stash_engine/app/models/stash_engine/file_upload.rb
@@ -98,13 +98,10 @@ module StashEngine
       Stash::Aws::S3.presigned_download_url(s3_key: "#{resource.s3_dir_name(type: 'data')}/#{upload_file_name}")
     end
 
-    # the URL we use for replication from other source (Presigned or URL) up to Zenodo
+    # the URL we use for replication to zenodo, for software it's always the merritt url, but for software we have the same
+    # method but switches between S3 and external URL depending on source
     def zenodo_replication_url
-      if url.blank?
-        merritt_s3_presigned_url
-      else
-        url
-      end
+      merritt_s3_presigned_url
     end
 
     # makes list of directories with numbers. not modified for > 7 days, and whose corresponding resource has been successfully submitted
