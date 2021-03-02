260a326bf9a43dfef727e34487710cad7022317b
Scott Fisher <scott.fisher@ucop.edu>
2021-03-02 14:30:22 -0800

    Fixing unnecessary things in test

---
+++ spec/lib/stash/zenodo_software/streamer_spec.rb
@@ -0,0 +1,13 @@
+require 'stash/zenodo_software'
+
+require 'rails_helper'
+
+RSpec.configure(&:infer_spec_type_from_file_location!)
+
+module Stash
+  module ZenodoSoftware
+    RSpec.describe Streamer do
+
+    end
+  end
+end
