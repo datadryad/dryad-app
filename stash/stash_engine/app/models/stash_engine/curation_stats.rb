be079e6bb20b4f81a8761c0510746541533e1967
Ryan Scherle <ryan@scherle.org>
2021-03-09 23:46:44 +0000

    more stats,tests

--- stash/stash_engine/app/models/stash_engine/curation_stats.rb
+++ stash/stash_engine/app/models/stash_engine/curation_stats.rb
@@ -83,41 +83,56 @@ module StashEngine
       update(new_datasets_to_peer_review: datasets_found.size)
     end
 
-    # Number that were sent from curation to a given status on the target day
-    def datasets_curation_to_status(status: nil)
-      return 0 unless status
+    # Number that were transitioned directly from one status to another on the target day
+    def datasets_transitioned(from_status: nil, to_status: nil)
+      return 0 unless from_status && to_status
 
       datasets_found = Set.new
       # for each dataset that received the target status on the given day
-      cas = CurationActivity.where(created_at: date..(date + 1.day), status: status)
+      cas = CurationActivity.where(created_at: date..(date + 1.day), status: to_status)
       cas.each do |ca|
-        # if the previous ca was `curation`, add the identifier to datasets_found
+        # if the previous ca was from_status, add the identifier to datasets_found
         prev_ca = CurationActivity.where(resource_id: ca.resource_id, id: 0..ca.id - 1).last
-        datasets_found.add(ca.resource.identifier) if prev_ca.curation?
+        datasets_found.add(ca.resource.identifier) if prev_ca.status == from_status
       end
       datasets_found.size
     end
 
     # The number AAR'd that day (status change from 'curation' to 'action_required')
     def populate_datasets_to_aar
-      update(datasets_to_aar: datasets_curation_to_status(status: 'action_required'))
+      update(datasets_to_aar: datasets_transitioned(from_status: 'curation', to_status: 'action_required'))
     end
 
     # The number published by a curator that day (status change from 'curation' to 'published' by a curator and not the system)
     def populate_datasets_to_published
-      update(datasets_to_published: datasets_curation_to_status(status: 'published'))
+      update(datasets_to_published: datasets_transitioned(from_status: 'curation', to_status: 'published'))
     end
 
     # The number embargoed that day (status change from 'curation' to 'embargoed' per day)
     def populate_datasets_to_embargoed
-      update(datasets_to_embargoed: datasets_curation_to_status(status: 'embargoed'))
+      update(datasets_to_embargoed: datasets_transitioned(from_status: 'curation', to_status: 'embargoed'))
     end
 
     # The number that come back to us after an Author Action Required
     # (so they change status from 'action_required' to 'curation')
-    def populate_author_revised; end
+    def populate_author_revised
+      datasets_found = Set.new
+      # for each dataset that received the target status on the given day
+      cas = CurationActivity.where(created_at: date..(date + 1.day), status: 'curation')
+      cas.each do |ca|
+        # action_required is either a previous status in this version, or the last status of the previous version
+        this_ver_aar = CurationActivity.where(resource_id: ca.resource_id, id: 0..ca.id - 1, status: 'action_required').present?
+        prev_resource = ca.resource.identifier.resources.where(id: 0..ca.resource_id - 1).last
+        prev_ver_aar = prev_resource&.current_curation_status == 'action_required'
 
-    # The number resubmitted that day (were 'published' or 'embargoed' before, and change status to 'submitted')
+        datasets_found.add(ca.resource.identifier) if this_ver_aar || prev_ver_aar
+      end
+      datasets_found.size
+
+      update(author_revised: datasets_found.size)
+    end
+
+    # The number resubmitted that day (were 'published' or 'embargoed' before, and changed status to 'submitted')
     def populate_author_versioned; end
 
   end
