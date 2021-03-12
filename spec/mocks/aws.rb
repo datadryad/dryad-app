9f110f9d13b9eee3df64149b91647ce765e1a486
Scott Fisher <scott.fisher@ucop.edu>
2021-03-12 13:19:30 -0800

    restoring broken webmock

--- spec/mocks/aws.rb
+++ spec/mocks/aws.rb
@@ -6,6 +6,7 @@ module Mocks
       mock_s3!
     end
 
+    # rubocop:disable Metrics/AbcSize
     def mock_s3!
       allow_any_instance_of(::Aws::S3::Object).to receive(:delete).and_return(nil)
       allow_any_instance_of(::Aws::S3::Object).to receive(:exists?).and_return(nil)
@@ -13,11 +14,14 @@ module Mocks
       allow_any_instance_of(::Aws::S3::Object).to receive(:presigned_url).and_return(
         "https://somebucket.amazonaws.com/presignedURL-#{Faker::Alphanumeric.alphanumeric(number: 10)}"
       )
+      allow_any_instance_of(::Aws::S3::Object).to receive(:size).and_return(Faker::Number.number(digits: 6))
+      allow_any_instance_of(::Aws::S3::Object).to receive(:upload_stream).and_return(nil)
       allow_any_instance_of(::Aws::S3::ObjectSummary::Collection).to receive(:batch_delete!).and_return(nil)
 
       # Add a default database configuration, which is needed by Resource.s3_dir_name
       allow(Rails).to receive(:configuration).and_return(OpenStruct.new(database_configuration: { 'test' => { 'host' => 'localhost' } }))
     end
+    # rubocop:enable Metrics/AbcSize
 
   end
 
