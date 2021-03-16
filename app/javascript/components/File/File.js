0425f8aef7222fd3d0ff221487e32fc429784c0b
cassiano <cacods21@gmail.com>
2021-03-16 15:22:02 -0300

    Adds form to ModalUrl component; adds styles to ModalUrl component; npm install axios; includes format.html in controller for responses to assyncronous requests; adds treatment for manifest files via ModalUrl in UploadFiles component

--- app/javascript/components/File/File.js
+++ app/javascript/components/File/File.js
@@ -4,8 +4,8 @@ const file = (props) => {
     return (
         <tr>
             <td>{props.file.name}</td>
-            <td>Pending</td>
-            <td/>
+            <td>{props.file.status}</td>
+            <td>{props.file.url}</td>
             <td>{props.file.typeId}</td>
             <td>{props.file.sizeKb}</td>
             <td><a href="#" onClick={props.click}>Remove</a></td>
