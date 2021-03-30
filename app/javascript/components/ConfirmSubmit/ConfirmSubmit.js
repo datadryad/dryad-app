9e8e3929039a57e507bdff40797b40242c08166e
cassiano <cacods21@gmail.com>
2021-03-30 08:05:12 -0300

    Adds ids for catching html elements by ids in capybara tests; fixes test; adds test for json response for bad urls made from React component; recover test marked with xit

--- app/javascript/components/ConfirmSubmit/ConfirmSubmit.js
+++ app/javascript/components/ConfirmSubmit/ConfirmSubmit.js
@@ -4,15 +4,15 @@ const confirm_submit = (props) => {
     return (
         <div>
             <input
-                type="checkbox" className={classes.ConfirmPersonalHealth}
+                type="checkbox" id="confirm_to_validate" className={classes.ConfirmPersonalHealth}
                 onChange={props.changed} />
-            <label htmlFor="confirm_not_personal_health">
+            <label htmlFor="confirm_to_validate">
                 <span className={classes.MandatoryField}>{'\u00A0\u00A0\u00A0\u00A0'}* </span>
                 I confirm that no Personal Health Information or
                 Sensitive Data are being uploaded with this submission.
             </label>
             <input
-                className={classes.Submit} type="submit" value={props.buttonLabel}
+                className={classes.Submit} type="submit" id="validate_files" value={props.buttonLabel}
                 disabled={props.disabled} />
         </div>
     )
