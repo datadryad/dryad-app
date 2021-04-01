2d6c2ef11882524aaf7826418ab8bec213d0ce8e
Scott Fisher <scott.fisher@ucop.edu>
2021-04-01 14:42:23 -0700

    Fixing uploading by URL.

--- spec/responses/stash_engine/data_files_controller_spec.rb
+++ spec/responses/stash_engine/data_files_controller_spec.rb
@@ -4,7 +4,7 @@ require 'byebug'
 
 # see https://relishapp.com/rspec/rspec-rails/v/3-8/docs/request-specs/request-spec
 module StashEngine
-  RSpec.describe FileUploadsController, type: :request do
+  RSpec.describe DataFilesController, type: :request do
     include Mocks::Tenant
 
     before(:each) do
@@ -16,12 +16,12 @@ module StashEngine
       @resource.reload
 
       # HACK: in session because requests specs don't allow session in rails 4
-      allow_any_instance_of(FileUploadsController).to receive(:session).and_return({ user_id: @user.id }.to_ostruct)
+      allow_any_instance_of(DataFilesController).to receive(:session).and_return({ user_id: @user.id }.to_ostruct)
     end
 
     describe '#presign_upload' do
       before(:each) do
-        @url = StashEngine::Engine.routes.url_helpers.file_upload_presign_url_path(resource_id: @resource.id)
+        @url = StashEngine::Engine.routes.url_helpers.data_file_presign_url_path(resource_id: @resource.id)
         @json_hash = { 'to_sign' => "AWS4-HMAC-SHA256\n20210213T001147Z\n20210213/us-west-2/s3/aws4_request\n" \
                                   '98fd9689d64ec7d84eb289ba859a122f07f7944e802edc4d5666d3e2df6ce7d6',
                        'datetime' => '20210213T001147Z' }
@@ -47,7 +47,7 @@ module StashEngine
     describe '#upload_complete' do
 
       before(:each) do
-        @url = StashEngine::Engine.routes.url_helpers.file_upload_complete_path(resource_id: @resource.id)
+        @url = StashEngine::Engine.routes.url_helpers.data_file_complete_path(resource_id: @resource.id)
         @json_hash =  { 'name' => 'lkhe_hg.jpg', 'size' => 1_843_444, 'type' => 'image/jpeg', 'original' => 'lkhe*hg.jpg' }.with_indifferent_access
       end
 
