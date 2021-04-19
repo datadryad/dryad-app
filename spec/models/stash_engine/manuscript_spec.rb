25a65340c4421b17f61e422fe2848e34bd2c4018
Ryan Scherle <ryan@scherle.org>
2021-04-19 23:05:36 +0000

    author parsing

--- spec/models/stash_engine/manuscript_spec.rb
+++ spec/models/stash_engine/manuscript_spec.rb
@@ -4,21 +4,35 @@ require 'byebug'
 module StashEngine
   describe Manuscript, type: :model do
     before(:each) do
-    
+      @journal = create(:journal)
     end
 
     describe '#from_message_content' do
       it 'turns a basic message into a manuscript' do
-        content = "Journal Name: Royal Society Open Science\n" \
+        ms_number = "ms-#{Faker::Number.number(digits: 4)}"
+        title = Faker::Lorem.sentence
+        content = "Journal Name: #{@journal.title}\n" \
                   "Journal Code: RSOS\n" \
-                  "Online ISSN: 2054-5703" \
-                  "MS Reference Number: abc123" \
-                  "MS Title: Some great title" \
-                  "MS Authors: "
+                  "Online ISSN: #{@journal.issn}\n" \
+                  "MS Reference Number: #{ms_number}\n" \
+                  "MS Title: #{title}\n" \
+                  'MS Authors: Lastname, Firstname; McOtherLastname, Somename'
+        result = Manuscript.from_message_content(content: content)
+        expect(result).not_to be_nil
+        expect(result.success?).to be_truthy
+        expect(result.payload).not_to be_nil
+        expect(result.payload.manuscript_number).to eq(ms_number)
+        puts("PAYLmmet #{result.payload.metadata} -- #{title}")
+        expect(result.payload.metadata['ms title']).to eq(title)
       end
 
       it 'provides an error when parsing fails' do
-        assert(false)
+        content = 'abc'
+        result = Manuscript.from_message_content(content: content)
+        expect(result).not_to be_nil
+        expect(result.success?).to be_falsey
+        expect(result.payload).to be_nil
+        expect(result.error).not_to be_nil
       end
     end
   end
--- stash/stash_engine/app/services/stash_engine/email_parser.rb
+++ stash/stash_engine/app/services/stash_engine/email_parser.rb
@@ -1,30 +1,20 @@
 module StashEngine
 
+  # rubocop:disable Metrics/ClassLength, Metrics/MethodLength
   class EmailParser
+    attr_reader :journal, :identifier
 
     def initialize(content:)
       @content = content
       parse
-    end      
-
-    def journal
-      #TODO
-      nil
-    end
-
-    def identifier
-      #TODO
-      nil
     end
 
     def manuscript_number
-      #TODO
-      nil
+      @hash['ms reference number']
     end
 
     def article_status
-      #TODO
-      nil
+      @hash['article status']
     end
 
     def metadata_hash
@@ -32,71 +22,71 @@ module StashEngine
     end
 
     private
-    
+
     def parse
-      @lines = content_to_lines
-      
-      # convert the lines to a hash
-      @hash = lines_to_hash
-      puts "HASH #{hash}"
-      
+      parse_content_to_lines
+      lines_to_hash
+      puts "HASH #{@hash}"
+
+      find_journal
+      puts "JOURNAL #{@journal}"
+
       # clean authors (and any other parts of the hash that need cleaning)
-      # TODO clean_authors(hash)      
-      
-      j = find_journal
-      puts "JOURNAL #{j}"
-      
-      # TODO ident = find_associated_identifier(hash)
+      clean_authors
+      puts "AUTH #{@hash['ms authors']}"
+
+      # TODO: ident = find_associated_identifier(hash)
     end
 
     # Breaks a large string of content into an array of lines,
     # stripping any material after the ending tag
-    def content_to_lines
-      lines = @content.split(%r{\n+|\r+|<br/>|<br />|<BR/>|<BR />})
-      puts "LIN #{lines}"
+    def parse_content_to_lines
+      @lines = @content.split(%r{\n+|\r+|<br/>|<br />|<BR/>|<BR />})
+      puts "LIN #{@lines}"
       # remove any lines after EndDryadContent
       last_dryad_line = 0
-      lines.each_with_index do |val, index|
+      @lines.each_with_index do |val, index|
         puts "#{val} => #{index}"
         if val.include?('EndDryadContent')
           last_dryad_line = index
           break
         end
       end
-      lines = lines[0..last_dryad_line - 1] if last_dryad_line > 0
-      @lines = lines
+      @lines = @lines[0..last_dryad_line - 1] if last_dryad_line > 0
+      @lines
     end
-    
+
     def lines_to_hash
       # Although journal emails may contain many fields, we only use the fields that are listed here
-      allowed_tags = ["journal code", "journal name", "ms reference number", "ms title", "ms authors", "article status",
-                      "publication doi", "keywords", "dryad data doi", "print issn", "online issn"]
-      @hash = {}
+      allowed_tags = ['journal code', 'journal name', 'ms reference number', 'ms title', 'ms authors', 'article status',
+                      'publication doi', 'keywords', 'dryad data doi', 'print issn', 'online issn']
+      @hash = {}.with_indifferent_access
       @lines.each_with_index do |line, index|
         puts "-- ln #{index} -- #{line}"
         next unless line.include?(':')
+
         colon_index = line.index(':')
-        tag = line[0..colon_index-1].downcase
+        tag = line[0..colon_index - 1].downcase
         # if the line starts with a valid tag, add it to the hash
         if allowed_tags.include?(tag)
-          value = line[colon_index+1..].strip
+          value = line[colon_index + 1..].strip
           puts "  -- tag |#{tag}|#{value}|"
           @hash[tag] = value
         end
         # if the line starts the abstract, add all of the remaining lines, and break from the loop
-        if tag == "abstract"
-          value = line[colon_index+1..]
-          remaining_lines = @lines[index+1..]
-          value += ' ' + remaining_lines.join(' ') if remaining_lines.present?
-          puts "  -- tag |#{tag}|#{value}|"
-          @hash[tag] = value.strip
-          break
-        end
-        
+        next unless tag == 'abstract'
+
+        value = line[colon_index + 1..]
+        remaining_lines = @lines[index + 1..]
+        value += " #{remaining_lines.join(' ')}" if remaining_lines.present?
+        puts "  -- tag |#{tag}|#{value}|"
+        @hash[tag] = value.strip
+        break
+
       end
       @hash
     end
-    
+
     def find_journal
       puts "HASH #{@hash}"
       @journal = nil
@@ -104,11 +94,11 @@ module StashEngine
       @journal = StashEngine::Journal.where(journal_code: @hash['journal code'].downcase).first if @hash['journal code']
       puts "    -- j1 #{@journal}"
       return if @journal
-            
+
       @journal = StashEngine::Journal.where(issn: @hash['print issn']).first if @hash['print issn']
       puts "    -- j2 #{@journal}"
       return if @journal
-      
+
       @journal = StashEngine::Journal.where(issn: @hash['online issn']).first if @hash['online issn']
       puts "    -- j3 #{@journal}"
       return if @journal
@@ -119,5 +109,81 @@ module StashEngine
       @journal
     end
 
+    # Convert the author string into an array of names
+    # Author lists generally come as either "last, first; last, first"
+    # or as "first last, first last and first last" (sometimes with the Oxford comma before the 'and' token)
+    def clean_authors
+      author_string = @hash['ms authors']
+      return unless author_string
+
+      authors = []
+
+      # first check to see if the authors are semicolon-separated
+      split_string = author_string.split(';')
+      puts "SPLIT #{split_string}"
+
+      # if it didn't have semicolons, it must have commas
+      if split_string.size == 1
+        split_string = author_string.split(',')
+
+        # although, if there was only one author and it was listed as lastname, firstname, it would have a comma too...
+        if split_string.length == 2 && !split_string[1].include?(' and ')
+          authors << [split_string[0], split_string[1]]
+          split_string = []
+        end
+      end
+
+      split_string.each do |auth|
+        if auth.include?(' and ')
+          # It's the end of the list, so we parse both names
+          two_auths = auth.split(' and ')
+          authors << parse_author_name(two_auths[0])
+          authors << parse_author_name(two_auths[1])
+        else
+          authors << parse_author_name(auth)
+        end
+      end
+
+      @hash['ms authors'] = authors
+    end
+
+    # Parse the name of a single author
+    # @return {family_name:, given_name:}]
+    def parse_author_name(auth)
+      # Remove any leading title, like Dr., Mrs.
+      auth.strip!
+      auth.gsub!(/^[DM]+r*s*\.*\s+/, '')
+      suffix = ''
+
+      # is there a comma in the name?
+      # it could either be lastname, firstname, or firstname lastname, title
+      # rubocop:disable Style/CaseLikeIf
+      comma_match = auth.match(/^(.+),\s*(.+)$/)
+      if comma_match
+        if comma_match[2].match(/(Jr\.*|Sr\.*|III)/)
+          # if it's a suffix situation, then part 1 is "firstname lastname"
+          # the last name will be the last word in group 1 + ", " + suffix
+          suffix = ", #{comma_match[2].strip}"
+          auth = comma_match[1]
+        elsif comma_match[2].match(/(Ph|J)\.*D\.*|M\.*[DAS]c*\.*/)
+          # if it's a title situation, throw the title away and assume the part before is "firstname lastname"
+          auth = comma_match[1]
+        else
+          # it's simply "lastname, firstname", so return it in that order
+          return { family_name: comma_match[1], given_name: comma_match[2] }.with_indifferent_access
+        end
+      end
+      # rubocop:enable Style/CaseLikeIf
+
+      space_match = auth.match(/^(.+) +(.*)$/)
+      if space_match
+        { family_name: "#{space_match[2]}#{suffix}", given_name: space_match[1] }.with_indifferent_access
+      else
+        # there is only one word in the name: assign it to the familyName
+        { family_name: auth, given_name: '' }.with_indifferent_access
+      end
+    end
+
   end
 end
+# rubocop:enable Metrics/ClassLength, Metrics/MethodLength
