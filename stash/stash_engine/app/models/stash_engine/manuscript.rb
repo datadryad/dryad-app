c33a24e7e639651cb588c9c515ff2d80a6855145
Ryan Scherle <ryan@scherle.org>
2021-04-21 18:36:43 +0000

    add notification of parsing errors

--- stash/stash_engine/app/models/stash_engine/manuscript.rb
+++ stash/stash_engine/app/models/stash_engine/manuscript.rb
@@ -11,21 +11,40 @@ module StashEngine
       return result unless content
 
       parser = EmailParser.new(content: content)
+      parsing_error = check_parsing_errors(parser)
 
-      manu = Manuscript.create(journal: parser.journal,
-                               identifier: parser.identifier,
-                               manuscript_number: parser.manuscript_number,
-                               status: parser.article_status,
-                               metadata: parser.metadata_hash)
+      if !parsing_error
+        manu = Manuscript.create(journal: parser.journal,
+                                 identifier: parser.identifier,
+                                 manuscript_number: parser.manuscript_number,
+                                 status: parser.article_status,
+                                 metadata: parser.metadata_hash)
 
-      if manu.present? && manu.id.present?
-        result.delete_field('error')
-        result[:success?] = true
-        result[:payload] = manu
+        if manu.present? && manu.id.present?
+          result.delete_field('error')
+          result[:success?] = true
+          result[:payload] = manu
+        end
+      else
+        result[:error] = parsing_error
       end
 
       result
     end
 
+    def self.check_parsing_errors(parser)
+      return 'Unable to locate Journal -- either through Journal Code or an ISSN' unless parser.journal
+      return 'Article Status not found' unless parser.article_status
+      unless parser.manuscript_number || parser.identifier
+        return 'Unable to identify manuscript -- either through MS Reference Number or Dryad Data DOI'
+      end
+
+      hash = parser.metadata_hash
+      return 'Unable to create metadata hash' unless hash.present?
+      return 'Unable to parse MS Authors' unless hash['ms authors'].present?
+      return 'Unable to locate MS Title' unless hash['ms title'].present?
+
+      nil
+    end
   end
 end
