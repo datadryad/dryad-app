e90ea33312fc71ce771550b00dfc74f94705de8f
Scott Fisher <scott.fisher@ucop.edu>
2021-04-23 14:41:47 -0700

    Fixing migration and making query for updating (in ticket)

--- stash/stash_datacite/db/migrate/20210423203902_add_added_by_to_related_identifiers.rb
+++ stash/stash_datacite/db/migrate/20210423203902_add_added_by_to_related_identifiers.rb
@@ -1,5 +1,5 @@
 class AddAddedByToRelatedIdentifiers < ActiveRecord::Migration[5.2]
   def change
-    add_column :dcs_related_identifiers, :status, :integer, default: 0 # default
+    add_column :dcs_related_identifiers, :added_by, :integer, default: 0 # default
   end
 end
