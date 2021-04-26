dd069755c2540eb57e2c88d1e039802c558ae27a
Ryan Scherle <ryan@scherle.org>
2021-04-26 23:20:47 +0000

    connect manuscripts to the submission system

--- stash/stash_engine/app/models/stash_engine/manuscript.rb
+++ stash/stash_engine/app/models/stash_engine/manuscript.rb
@@ -4,6 +4,16 @@ module StashEngine
     belongs_to :identifier, optional: true
     serialize :metadata
 
+    def accepted?
+      accepted_statuses = %w[accepted published]
+      accepted_statuses.include?(status)
+    end
+
+    def rejected?
+      rejected_statuses = ['rejected', 'transferred', 'rejected w/o review']
+      rejected_statuses.include?(status)
+    end
+
     # Create a new Manuscript from the content of an email message
     def self.from_message_content(content:)
       result = OpenStruct.new(success?: false, error: 'No content')
@@ -20,6 +30,7 @@ module StashEngine
                                  metadata: parser.metadata_hash)
 
         if manu.present? && manu.id.present?
+          update_existing_dataset_status(manu)
           result.delete_field('error')
           result[:success?] = true
           result[:payload] = manu
@@ -29,6 +40,35 @@ module StashEngine
       end
 
       result
+    end
+
+    def self.update_existing_dataset_status(manuscript)
+      return unless manuscript.identifier
+
+      target_status = nil
+      target_note = 'setting manuscript status based on notification from journal'
+      if manuscript.accepted?
+        target_status = 'submitted'
+      elsif manuscript.rejected?
+        target_status = 'withdrawn'
+      end
+
+      resource = manuscript.identifier.latest_resource
+
+      # if the status is being updated based on notification from a journal, DON'T go backwards in workflow,
+      # that is, don't change a status other than submitted or peer_review
+      unless %w[submitted peer_review].include?(resource.current_curation_status)
+        target_note = "received notification from journal module that the associated manuscript is #{manuscript.status}, " \
+                      "but the dataset is #{resource.current_curation_status}, so it will retain that status"
+        target_status = resource.current_curation_status
+      end
+
+      return unless target_status
+
+      StashEngine::CurationActivity.create(resource: resource,
+                                           status: target_status,
+                                           user_id: 0,  # system user
+                                           note: target_note)
     end
 
     def self.check_parsing_errors(parser)
