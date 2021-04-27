895441f773d5fbecbaa14406d3188a9b469ab804
Ryan Scherle <ryan@scherle.org>
2021-04-27 18:51:44 +0000

    move cron every 1 to every 5

--- cron/every_5.sh
+++ cron/every_5.sh
@@ -7,4 +7,4 @@ PATH=$PATH:/apps/dryad/local/bin
 cd /apps/dryad/apps/ui/current/
 
 bundle exec rails status_dashboard:check RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/status_dashboard_check.log 2>&1
-
+bundle exec rails journal_email:process RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/journal_email.log 2>&1
