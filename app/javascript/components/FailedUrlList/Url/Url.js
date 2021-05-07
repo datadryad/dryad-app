dff201dd7f7d32192d5fc46b453c200ba39bd3f6
caco <cacods21@gmail.com>
2021-05-07 14:43:37 -0300

    Gets rid of the remaining React CSS modules; adds some classes to scss for upload files; adds some inline css styles like the inline styles in UI -- exception for ModalUrl component: in that case inline styles were used to expand the modal width/height and the position relative to the top

--- app/javascript/components/FailedUrlList/Url/Url.js
+++ app/javascript/components/FailedUrlList/Url/Url.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import classes from './Url.module.css';
-
 const url = (props) => {
     return (
-        <div className={classes.ManifestItem}>
-            <div className={classes.ManifestUrl}>{props.url.url}</div>
-            <div className={classes.ManifestError}>{props.url.error_message}</div>
-            <div className={classes.ManifestAction}>
-                {/*<a className={classes.ManifestAction} href="#!">Edit</a>*/}
-                <a className={classes.ManifestAction} href="#!" onClick={props.click}>Remove</a>
+        <div className="c-manifest__item">
+            <div className="c-manifest__url">{props.url.url}</div>
+            <div className="c-manifest__error">{props.url.error_message}</div>
+            <div className="c-manifest__action">
+                {/*<a href="#!">Edit</a>*/}
+                <a href="#!" onClick={props.click}>Remove</a>
             </div>
         </div>
     )
