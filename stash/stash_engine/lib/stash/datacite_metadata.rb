d3c4ebfa87089ba6a1aeeccf394b4c97fff12e71
Scott Fisher <scott.fisher@ucop.edu>
2021-05-13 14:55:24 -0700

    Adding text saying unavailable with URL when DataCite cannot give the citation text.

--- stash/stash_engine/lib/stash/datacite_metadata.rb
+++ stash/stash_engine/lib/stash/datacite_metadata.rb
@@ -27,7 +27,7 @@ module Stash
       response = RestClient.get "#{DATACITE_BASE}#{CGI.escape(@doi)}", accept: 'application/citeproc+json'
       @metadata = JSON.parse(response.body)
     rescue RestClient::Exception, Errno::ECONNREFUSED => e
-      logger.error("#{Time.new.utc} Could not get response from DataCite for metadata lookcup #{@doi}")
+      logger.error("#{Time.new.utc} Could not get response from DataCite for metadata lookup #{@doi}")
       logger.error("#{Time.new.utc} #{e}")
       @metadata = false
       nil
