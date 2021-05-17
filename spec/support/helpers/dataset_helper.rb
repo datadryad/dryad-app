f4e730ee4a69b6e933e8aa4c208ad3ced64833a1
Scott Fisher <scott.fisher@ucop.edu>
2021-05-17 13:59:07 -0700

    rubocop fix

--- spec/support/helpers/dataset_helper.rb
+++ spec/support/helpers/dataset_helper.rb
@@ -33,9 +33,7 @@ module DatasetHelper
 
   def navigate_to_review
     click_link 'Review and Submit'
-    if page.has_content?('We notice you do not have any data files in this submission.')
-      page.send_keys :escape
-    end
+    page.send_keys :escape if page.has_content?('We notice you do not have any data files in this submission.')
     expect(page).to have_content('Review Description')
   end
 
