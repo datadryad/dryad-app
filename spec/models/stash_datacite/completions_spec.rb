84050373e1ddafa5b025c757872bb54baa131fe2
Scott Fisher <scott.fisher@ucop.edu>
2021-05-17 11:20:53 -0700

    Rubocop doens't like the word has with its dogma

--- spec/models/stash_datacite/completions_spec.rb
+++ spec/models/stash_datacite/completions_spec.rb
@@ -203,14 +203,14 @@ module StashDatacite
           end
         end
 
-        describe 'has_data?' do
+        describe 'contains_data?' do
           it 'returns false if no data files present' do
             @resource.data_files.destroy_all
-            expect(@completions.has_data?).to eq(false)
+            expect(@completions.contains_data?).to eq(false)
           end
 
           it 'returns true if data files are present' do
-            expect(@completions.has_data?).to eq(true)
+            expect(@completions.contains_data?).to eq(true)
           end
         end
       end
