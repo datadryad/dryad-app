9cf3e79ef3a8bb5dbd70b3d27ffa68831ce76bd1
caco <cacods21@gmail.com>
2021-05-24 17:07:25 -0300

    Just to run github workflow again for testing Net::ReadTimeout issue again

--- app/javascript/containers/UploadFiles.js
+++ app/javascript/containers/UploadFiles.js
@@ -15,7 +15,6 @@ import WarningMessage from '../components/WarningMessage/WarningMessage';
 // TODO: check if this is the best way to refer to stash_engine files.
 import '../../../stash/stash_engine/app/assets/javascripts/stash_engine/resources.js';
 
-
 /**
  * Constants
  */
