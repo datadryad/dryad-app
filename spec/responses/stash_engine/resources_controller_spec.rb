9ff04addd31c37a6d24503007ec23d20b8022a94
Scott Fisher <scott.fisher@ucop.edu>
2021-05-27 17:33:55 -0700

    Updating failing test.

--- spec/responses/stash_engine/resources_controller_spec.rb
+++ spec/responses/stash_engine/resources_controller_spec.rb
@@ -15,7 +15,7 @@ module StashEngine
         assert_react_component 'UploadFiles' do |props|
           assert_equal @resource.id, props[:resource_id]
           assert_equal @resource.data_files.first.attributes, props[:file_uploads].first.stringify_keys
-          assert_equal APP_CONFIG[:s3].to_h, props[:app_config_s3][:table]
+          assert_equal APP_CONFIG[:s3].to_h.except(:secret), props[:app_config_s3][:table]
           assert_equal @resource.s3_dir_name(type: 'base'), props[:s3_dir_name]
         end
       end
