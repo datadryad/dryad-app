80086c7b34f5d1a3b6e91a2c7125367f4865f6a4
Scott Fisher <scott.fisher@ucop.edu>
2021-06-04 15:07:35 -0700

    Fixing rubocop.

--- spec/models/stash_engine/counter_citation_spec.rb
+++ spec/models/stash_engine/counter_citation_spec.rb
@@ -9,7 +9,7 @@ module StashEngine
       it 'returns nil if citation unable to be generated' do
         doi = '10.1010/12345.67890'
 
-        expect(Stash::DataciteMetadata).to receive(:new).with(doi: doi).and_return({html_citation: nil}.to_ostruct)
+        expect(Stash::DataciteMetadata).to receive(:new).with(doi: doi).and_return({ html_citation: nil }.to_ostruct)
         expect(CounterCitation.citation_metadata(doi: doi, stash_identifier: @identifier)).to be_nil
       end
     end
