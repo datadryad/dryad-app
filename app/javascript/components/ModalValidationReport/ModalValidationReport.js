ac2013f870106419d3a614264974ef4de15c31f3
caco <cacods21@gmail.com>
2021-06-08 15:52:50 -0300

    Gives a title to the Validation Modal indicating the file issues beeing viewed; changes the status from "Issues found" to "View Issues"

--- app/javascript/components/ModalValidationReport/ModalValidationReport.js
+++ app/javascript/components/ModalValidationReport/ModalValidationReport.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import classes from './ModalValidationReport.module.css';
+
 import '@cacods/frictionless-components/lib/styles';
 import {render} from '@cacods/frictionless-components/lib/render';
 import {Report} from '@cacods/frictionless-components/lib/components/Report';
 
+
 class ModalValidationReport extends React.Component {
     componentDidMount() {
         const element = document.getElementById('validation_report');
-        render(Report, JSON.parse(this.props.report), element);
+        render(Report, JSON.parse(this.props.file.frictionless_report.report), element);
     }
 
     render () {
@@ -19,12 +22,8 @@ class ModalValidationReport extends React.Component {
                             'z-index': '100149', 'top': '50%'}}
                         open>
                     <div className="c-uploadmodal__header">
-                        {/* TODO: this div just while don't have UI classes defined.
-                            So we can place it here for applying "c-uploadmodal__header"
-                            class and have the close button right aligned before
-                            the title of the modal goes here */}
-                        <div/>
-                        <button className="c-uploadmodal__button-close-modal js-uploadmodal__button-close-modal"
+                        <h2 className={classes.FileTitle}>Formatting Report: {this.props.file.sanitized_name}</h2>
+                        <button className={classes.CloseButton}
                                 aria-label="close"
                                 type="button"
                                 onClick={(event) => this.props.clickedClose(event)} />
