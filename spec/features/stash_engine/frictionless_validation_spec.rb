2dfc8daa7dfb115a1603cafa7d5187488cd9a2ba
Scott Fisher <scott.fisher@ucop.edu>
2021-06-30 15:45:03 -0700

    Adding display of how many errors in the table itself.

--- spec/features/stash_engine/frictionless_validation_spec.rb
+++ spec/features/stash_engine/frictionless_validation_spec.rb
@@ -85,7 +85,7 @@ RSpec.feature 'UploadFiles', type: :feature, js: true do
       end
     end
 
-    it 'shows "View issues" if file is tabular and status is "issues"' do
+    it 'shows "issues" if file is tabular and status is "issues"' do
       @report = StashEngine::FrictionlessReport.create!(
         report: '[{errors: errors}]', generic_file: @file, status: 'issues'
       )
@@ -93,7 +93,7 @@ RSpec.feature 'UploadFiles', type: :feature, js: true do
       click_link 'Upload Files'
 
       within(:xpath, '//table/tbody/tr/td[2]') do
-        expect(text).to eq('View issues')
+        expect(text).to include('issues')
       end
     end
 
