1b39bf2075390a6bb976cc18568bd1d3ebdb57a7
Scott Fisher <scott.fisher@ucop.edu>
2021-07-13 11:47:45 -0700

    Found a few more calls to this function that didn't come up in my search before (IDK why, maybe indexes out of date).  removing these references to the function fix the tests.

--- stash/stash_datacite/app/assets/javascripts/stash_datacite/contributor.js
+++ stash/stash_datacite/app/assets/javascripts/stash_datacite/contributor.js
@@ -60,3 +60,15 @@ function loadContributors() {
     });
 };
 
+/*
+function hideRemoveLinkContributors() {
+  if($('.js-funders').length < 2)
+  {
+   $('.js-funders').first().parent().parent().find('.remove_record').hide();
+  }
+  else
+  {
+   $('.js-funders').first().parent().parent().find('.remove_record').show();
+  }
+};
+*/
