277440b111093f74c947ef33c445b2966f9c7a3c
Ryan Scherle <ryan@scherle.org>
2021-08-20 22:08:23 +0000

    add ionice

--- cron/every_30.sh
+++ cron/every_30.sh
@@ -7,4 +7,4 @@ PATH=$PATH:/apps/dryad/local/bin
 cd /apps/dryad/apps/ui/current/
 
 # Opting not to record output in logs since it would display the command in clear text
-nice bundle exec rails dev_ops:backup RAILS_ENV=$1
+nice -n 19 ionice -c 3 bundle exec rails dev_ops:backup RAILS_ENV=$1
