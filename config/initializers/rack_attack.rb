43962381cfad90948f472dd2a662023879161ee7
Ryan Scherle <ryan@scherle.org>
2021-08-31 03:54:27 +0000

    rework entirely

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -6,7 +6,8 @@
 # ------------------
 
 # IPs to allow outright
-# Rack::Attack.safelist_ip("98.27.57.189")
+Rack::Attack.safelist_ip('127.0.0.1')
+Rack::Attack.safelist_ip('::1')
 
 # Blocks
 # -------------------
@@ -15,12 +16,12 @@
 # Rack::Attack.blocklist_ip("5.6.7.8")
 
 # Set a long block period for any client that is explicitly looking for security holes
-Rack::Attack.blocklist("fail2ban") do |req|
-  Rack::Attack::Fail2Ban.filter("fail2ban-pentest-#{req.ip}", maxretry: 1, findtime: 1.day, bantime: 1.day) do
+Rack::Attack.blocklist('malicious_clients') do |req|
+  Rack::Attack::Fail2Ban.filter("fail2ban_malicious_#{req.ip}", maxretry: 1, findtime: 1.day, bantime: 1.day) do
     CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-      req.path.include?("/etc/passwd") ||
-      req.path.include?("wp-admin") ||
-      req.path.include?("wp-login") ||
+      req.path.include?('/etc/passwd') ||
+      req.path.include?('wp-admin') ||
+      req.path.include?('wp-login') ||
       /\S+\.php/.match?(req.path)
   end
 end
@@ -28,29 +29,43 @@ end
 # Throttling
 # -------------------
 
-# The number of requests allowed within each time limit
-limit_proc = proc { |req|
-  puts "XXXXX req.path #{req.path}"
-  if req.path.include?('/stash/downloads/download_resource')
-    # Throttle zip downloads to a slower rate than other requests,
-    # since it is expensive for Merritt to asemble the zip files
-    10
-  else    
-    120
-  end
-}
+# Throttling allows no more than `limit` requests per `period`
+# Each throttle takes a block that returns a "discriminator" for the user and type of call.
+# Rack-attack counts the requests for each discriminator within the period and manages the
+# throttle notifications.
+#
+# Each throttle is independent, so it is possible for a single request to be counted in multiple
+# discriminators.
 
-# Baseline throttle of all requests by IP
-# allow no more than `limit` requests per `period`
-# Returns the IP address, which will be tracked with a count of how many requests are made by each IP
-# but ignore requests for assets, which are just part of each page
-Rack::Attack.throttle('requests by IP', limit: limit_proc, period: 1.minute) do |req|
+# Baseline throttle all requests by IP
+# But don't return anything for /assets, which are just part of each page and should not be tracked.
+Rack::Attack.throttle('all_requests_by_IP', limit: 120, period: 1.minute) do |req|
   req.ip unless req.path.start_with?('/assets')
 end
 
+# Zip downloads have a much lower limit than other requests,
+# since it is expensive for Merritt to asemble the zip files.
+Rack::Attack.throttle('zip_downloads', limit: 10, period: 1.minute) do |req|
+  "zip_download_#{req.ip}" if req.path.start_with?('/stash/downloads/download_resource') ||
+                              req.path.match(/api.*(version|dataset).*download/)
+end
 
-# When a client is throttled, return useful information to them
-Rack::Attack.throttled_response = lambda do |env|
+# Registered API users get preferential treatment over anonymous users. Assume API users have
+# a valid auth code. If the auth data is bad, it will be caught and blocked later in the process.
+Rack::Attack.throttle('API_requests_by_registered_users', limit: 120, period: 1.minute) do |req|
+  "api-user-#{req.ip}" if req.path.start_with?('/api') && req.has_header?('HTTP_AUTHORIZATION')
+end
+
+Rack::Attack.throttle('API_requests_by_anonymous_users', limit: 30, period: 1.minute) do |req|
+  "anon-api-user-#{req.ip}" if req.path.start_with?('/api') && !req.has_header?('HTTP_AUTHORIZATION')
+end
+
+Rack::Attack.throttle('v1_resource_requests', limit: 30, period: 1.minute) do |req|
+  "v1-api-user-#{req.ip}" if req.path.start_with?('/resource')
+end
+
+# When a client is throttled, return useful information in the response
+Rack::Attack.throttled_response = ->(env) do
   match_data = env['rack.attack.match_data']
   now = match_data[:epoch_time]
 
@@ -60,7 +75,12 @@ Rack::Attack.throttled_response = lambda do |env|
     'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
   }
 
-  [ 429, headers, ["This request has been denied due to exceeding the rate limit.\n"]]
+  [429, headers, ["Request rejected due to rate limits.\n"]]
 end
 
-#  
+# Log the blocked requests
+ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, _start, _finish, _request_id, payload|
+  req = payload[:request]
+  Rails.logger.info "[Rack::Attack][Blocked] name: #{name}, rule: #{req.env['rack.attack.matched']} remote_ip: #{req.ip}, " \
+                    "path: #{req.path}, agent: #{req.user_agent}"
+end
