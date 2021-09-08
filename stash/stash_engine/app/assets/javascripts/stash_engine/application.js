69df4bf21891d25091652dbb9e73a84cf0878090
Scott Fisher <scott.fisher@ucop.edu>
2021-09-08 09:39:49 -0700

    Intermediate code for creating special y/n dialog for rails

--- stash/stash_engine/app/assets/javascripts/stash_engine/application.js
+++ stash/stash_engine/app/assets/javascripts/stash_engine/application.js
@@ -14,7 +14,46 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery-fileupload/basic
-//= require jquery-fileupload/vendor/tmpl
 //= require ckeditor/init
 //= require_tree .
+
+console.log($.rails.allowAction);
+
+$.rails.confirm = function(message) {
+  console.log("in confirmer");
+
+  // see https://dev.to/boyum/creating-a-simple-confirm-modal-in-vanilla-js-2lcl which gives useful example
+
+  const prom = new Promise((resolve, reject) => {
+    const somethingWentWrongUponCreation = !document.getElementById('railsConfirmDialog') ||
+        !document.getElementById('railsConfirmDialogYes') ||
+        !document.getElementById('railsConfirmDialogNo');
+
+    if (somethingWentWrongUponCreation) {
+      reject("Something is wrong with modal in html");
+    }
+
+    document.getElementById('railsConfirmDialog').showModal();
+
+    const yBtn = document.getElementById('railsConfirmDialogYes');
+    const yHandler = yBtn.addEventListener("click", () => {
+      resolve(true);
+      document.getElementById('railsConfirmDialog').close();
+      yBtn.removeEventListener('click', yHandler, false);
+    });
+
+    const nBtn = document.getElementById('railsConfirmDialogNo');
+    const nHandler = nBtn.addEventListener("click", () => {
+      resolve(false);
+      document.getElementById('railsConfirmDialog').close();
+      nBtn.removeEventListener('click', yHandler, false);
+    });
+  });
+
+  prom.then(result => {
+    console.log(result);
+    return result;
+  });
+  console.log('after promise.then');
+  // return confirm(message);
+};
