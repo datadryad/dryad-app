f96302242c4bb1d0ad8a8b537c088558a79bdd3c
Scott Fisher <scott.fisher@ucop.edu>
2021-09-08 09:39:49 -0700

    Intermediate code for creating special y/n dialog for rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,37 +13,7 @@
 
 // This is renamed "papplication.js" because otherwise it clobbers the one needed by
 // geoblacklight to load it's junk and then nothing works.
-
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-// see https://thelazylog.com/custom-dialog-for-data-confirm-in-rails/
-//Override the default confirm dialog by rails
-$.rails.allowAction = function(link){
-  if (link.data("confirm") == undefined){
-    return true;
-  }
-  $.rails.showConfirmationDialog(link);
-  return false;
-}
-//User click confirm button
-$.rails.confirmed = function(link){
-  link.data("confirm", null);
-  link.trigger("click.rails");
-}
-//Display the confirmation dialog
-$.rails.showConfirmationDialog = function(link){
-  /*
-  var message = link.data("confirm");
-  $.fn.SimpleModal({
-    model: "modal",
-    title: "Please confirm",
-    contents: message
-  }).addButton("Confirm", "button alert", function(){
-    $.rails.confirmed(link);
-    this.hideModal();
-  }).addButton("Cancel", "button secondary").showModal();
-   */
-}
