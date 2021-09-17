f0a5dfa08b5f5fa6aae6a1e7178b3ba438723fc7
Scott Fisher <scott.fisher@ucop.edu>
2021-09-16 18:24:16 -0700

    A couple of changes I had to add back to get tests to run (solr-wrapper and get rid of user class).

--- stash/stash_discovery/app/models/user.rb
+++ stash/stash_discovery/app/models/user.rb
@@ -1,17 +1,18 @@
-class User < ApplicationRecord
-
-  attr_accessible :email, :password, :password_confirmation if Blacklight::Utils.needs_attr_accessible?
-  # Connects this user object to Blacklights Bookmarks.
-  include Blacklight::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Method added by Blacklight; Blacklight uses #to_s on your
-  # user class to get a user-displayable login/identifier for
-  # the account.
-  def to_s
-    email
-  end
-end
+# I think david put this in to not track users
+# class User < ApplicationRecord
+#
+#   attr_accessible :email, :password, :password_confirmation if Blacklight::Utils.needs_attr_accessible?
+#   # Connects this user object to Blacklights Bookmarks.
+#   include Blacklight::User
+#   # Include default devise modules. Others available are:
+#   # :confirmable, :lockable, :timeoutable and :omniauthable
+#   devise :database_authenticatable, :registerable,
+#          :recoverable, :rememberable, :trackable, :validatable
+#
+#   # Method added by Blacklight; Blacklight uses #to_s on your
+#   # user class to get a user-displayable login/identifier for
+#   # the account.
+#   def to_s
+#     email
+#   end
+# end
