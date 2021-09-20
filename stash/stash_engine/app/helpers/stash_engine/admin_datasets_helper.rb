d810b5f00fc952f9c44ddcec35c4221c21e9f1b2
Ryan Scherle <ryan@scherle.org>
2021-09-20 15:22:41 +0000

    adding real links

--- stash/stash_engine/app/helpers/stash_engine/admin_datasets_helper.rb
+++ stash/stash_engine/app/helpers/stash_engine/admin_datasets_helper.rb
@@ -27,5 +27,21 @@ module StashEngine
         [c.name, c.id]
       end
     end
+
+    def format_external_references(instring)
+      return '' unless instring.present?
+
+      # Turn salesforce references into hyperlinks
+      matchdata = instring.match(/(.*)SF ?#? ?(\d+)(.*)/)
+      return instring unless matchdata
+
+      render inline: matchdata[1] + link_to("SF #{matchdata[2]}", salesforce_link(case_num: matchdata[2])) + matchdata[3]
+    end
+
+    def salesforce_link(case_num: '')
+      # @sf.query("SELECT Id FROM Case Where CaseNumber = '00006729'").first['Id']
+      
+      "https#{case_id}"
+    end
   end
 end
