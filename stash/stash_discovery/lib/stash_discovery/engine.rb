601b7650ef2ebdd705dbf3f09bc9a1f813494299
Scott Fisher <scott.fisher@ucop.edu>
2021-09-21 11:13:46 -0700

    This gets bad display working.

--- stash/stash_discovery/lib/stash_discovery/engine.rb
+++ stash/stash_discovery/lib/stash_discovery/engine.rb
@@ -1,4 +1,4 @@
-# require 'blacklight'
+require 'blacklight'
 require 'geoblacklight'
 # these devise lines must be required otherwise geoblacklight barfs, but only on stage.
 # require 'devise'
@@ -6,10 +6,15 @@ require 'geoblacklight'
 # require 'rsolr'
 
 # For undocumented reasons, sass-rails won't load without an explicit require
-# require 'sass-rails'
+require 'bootstrap'
+# require 'bootstrap-sass'
+require 'sass-rails'
+require 'twitter-typeahead-rails'
 
 module StashDiscovery
   class Engine < ::Rails::Engine
+    # assets are not loading from subdirectories of blacklight/geoblacklight so trying to add them to asset path
+    # config.assets.paths << Blacklight::Engine.root.join('app', 'assets', 'stylesheets', 'blacklight')
     # :nocov:
     initializer :append_migrations do |app|
       unless app.root.to_s.match?(root.to_s)
