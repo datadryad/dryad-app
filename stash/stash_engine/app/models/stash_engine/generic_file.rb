345ff3ddcc2433175240ceabb8f27ecd5df82506
Scott Fisher <scott.fisher@ucop.edu>
2021-10-19 10:20:48 -0700

    Output timing message added more info.

--- stash/stash_engine/app/models/stash_engine/generic_file.rb
+++ stash/stash_engine/app/models/stash_engine/generic_file.rb
@@ -239,7 +239,7 @@ module StashEngine
       result = `#{cmd}`
       logger.debug("Frictionless validation:\n  #{cmd}\n  #{result}")
       ending = Time.new
-      F      logger.debug("#{File.basename(file.path)}: Frictionless validation took: #{ending-starting} seconds")
+      logger.debug("generic_file.id = #{id}, #{File.basename(file.path)}: Frictionless validation took #{ending-starting} seconds")
       file.close!
       result
     end
