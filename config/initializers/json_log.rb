7214f9c648b761a790c39c6084b5f862533a1a65
Ashley Gould <agould@ucop.edu>
2021-10-20 16:01:41 -0700

    rails-semantic-logger: debugging

--- config/initializers/json_log.rb
+++ config/initializers/json_log.rb
@@ -5,10 +5,10 @@
 
 Rails.application.config.rails_semantic_logger.format = :json
 Rails.application.config.rails_semantic_logger.add_file_appender = false
-#Rails.application.config.semantic_logger.add_appender(file_name: 'log/json.log', formatter: :json)
-Rails.application.config.semantic_logger.add_appender(file_name: "log/#{Rails.env}.log", formatter: :json)
+Rails.application.config.semantic_logger.add_appender(file_name: 'log/json.log', formatter: :json)
 
+# this doesnt work. I suppose we can't yet access Rails.env???
+#Rails.application.config.semantic_logger.add_appender(file_name: "log/#{Rails.env}.log", formatter: :json)
 
-#SemanticLogger.add_appender(file_name: 'json_formatter.json', formatter: :json)
 
 
