525d5630009cc26e56fe409a435c962635e9c239
Scott Fisher <scott.fisher@ucop.edu>
2021-10-20 15:55:53 -0700

    Make a migration to add triggers for last resource

---
+++ stash/stash_engine/db/migrate/20211020221956_add_triggers_for_latest_resource.rb
@@ -0,0 +1,65 @@
+class AddTriggersForLatestResource < ActiveRecord::Migration[5.2]
+  def up
+    # this is sort of large, but just 1) updates all the latest_resource_ids to be correct in stash_engine_identifiers
+    # and then 2) defines create, update, delete triggers that will keep them up to date in MySQL
+    execute <<-SQL
+      UPDATE stash_engine_identifiers
+      INNER JOIN
+        (SELECT max(id) as max_resource_id, identifier_id
+        FROM stash_engine_resources
+        GROUP BY identifier_id) max_resources
+      ON stash_engine_identifiers.id = max_resources.identifier_id
+      SET stash_engine_identifiers.latest_resource_id = max_resources.max_resource_id;
+    SQL
+
+    execute <<-SQL
+      CREATE TRIGGER trigger_resource_insert AFTER INSERT ON stash_engine_resources
+      FOR EACH ROW
+      BEGIN
+        UPDATE stash_engine_identifiers
+        SET latest_resource_id = (
+	        SELECT max(id) as max_id
+	        FROM stash_engine_resources
+	        WHERE identifier_id = NEW.identifier_id)
+        WHERE id = NEW.identifier_id;
+      END;
+    SQL
+
+    execute <<-SQL
+      CREATE TRIGGER trigger_resource_update AFTER UPDATE ON stash_engine_resources
+      FOR EACH ROW
+      BEGIN
+        IF !(NEW.identifier_id <=> OLD.identifier_id) THEN
+          UPDATE stash_engine_identifiers
+          SET latest_resource_id = (
+            SELECT max(id) as max_id
+            FROM stash_engine_resources
+            WHERE identifier_id = NEW.identifier_id)
+          WHERE id = NEW.identifier_id;
+        END IF;
+      END;
+    SQL
+
+
+
+    execute <<-SQL
+      CREATE TRIGGER trigger_resource_delete AFTER DELETE ON stash_engine_resources
+      FOR EACH ROW
+      BEGIN
+        UPDATE stash_engine_identifiers
+        SET latest_resource_id = (
+          SELECT max(id) as max_id
+          FROM stash_engine_resources
+          WHERE identifier_id = OLD.identifier_id)
+        WHERE id = OLD.identifier_id;
+      END;
+    SQL
+  end
+
+  def down
+    execute 'DROP TRIGGER IF EXISTS trigger_resource_insert'
+    execute 'DROP TRIGGER IF EXISTS trigger_resource_update'
+    execute 'DROP TRIGGER IF EXISTS trigger_resource_delete'
+    SQL
+  end
+end
