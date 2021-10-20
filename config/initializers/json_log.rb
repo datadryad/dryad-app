8f8dc301f5bc0891015db20b0bba44d28605f1fb
Ashley Gould <agould@ucop.edu>
2021-10-20 16:31:23 -0700

    rails-semantic-logger: debugging

--- config/initializers/json_log.rb
+++ config/initializers/json_log.rb
@@ -3,7 +3,7 @@
 # per https://github.com/reidmorrison/rails_semantic_logger/issues/73
 #
 
-Rails.application.config.rails_semantic_logger.format = :json
+#Rails.application.config.rails_semantic_logger.format = :json
 Rails.application.config.rails_semantic_logger.add_file_appender = false
 Rails.application.config.semantic_logger.add_appender(file_name: 'log/json.log', formatter: :json)
 
