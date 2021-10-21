83b5ad88aa3c6d1828cdd8c76fff98b0a0e4f671
Ashley Gould <agould@ucop.edu>
2021-10-21 07:54:52 -0700

    rails-semantic-logger: debugging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker   
 
+  # Disable default file_appender to fix issue with using multiple appenders.
+  # See: https://github.com/reidmorrison/rails_semantic_logger/issues/73
+  config.rails_semantic_logger.add_file_appender = false
+
   # Set up rails_semantic_logger file_appender instances for both local text log
   # and a json log for indexing into OpenSearch.
   config.semantic_logger.add_appender(file_name: "log/#{Rails.env}.log")
--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -3,7 +3,7 @@
 #
 # Disable default file_appender to fix issue with using multiple appenders.
 # See: https://github.com/reidmorrison/rails_semantic_logger/issues/73
-Rails.application.config.rails_semantic_logger.add_file_appender = false
+#Rails.application.config.rails_semantic_logger.add_file_appender = false
 
 # Additional 'log_tag' fields
 Rails.application.config.log_tags = {
