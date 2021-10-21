5ce48412e09efb1b8d04fe36e4ec33cf20aa8c0e
Ashley Gould <agould@ucop.edu>
2021-10-21 08:09:49 -0700

    rails-semantic-logger: debugging

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -1,13 +1,6 @@
-# Initialize rails_semantic_logger configs.
-# See: https://logger.rocketjob.io/rails.html
 #
 # Disable default file_appender to fix issue with using multiple appenders.
 # See: https://github.com/reidmorrison/rails_semantic_logger/issues/73
 #Rails.application.config.rails_semantic_logger.add_file_appender = false
 
-# Additional 'log_tag' fields
-Rails.application.config.log_tags = {
-  request_id: :request_id,
-  ip:         :remote_ip,
-} 
 
