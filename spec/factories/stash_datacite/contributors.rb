ed2e7ccadb8519b033ae2bc94733e790b3517723
Scott Fisher <scott.fisher@ucop.edu>
2021-10-29 14:04:28 -0700

    Add the identifier_type to contributors for identifying a type of identifier

--- spec/factories/stash_datacite/contributors.rb
+++ spec/factories/stash_datacite/contributors.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
 
     contributor_name    { Faker::Company.name }
     contributor_type    { 'funder' }
+    identifier_type     { 'crossref_funder_id' }
     name_identifier_id  { Faker::Pid.doi }
     award_number        { Faker::Alphanumeric.alphanumeric(number: 8, min_alpha: 2, min_numeric: 4) }
   end
