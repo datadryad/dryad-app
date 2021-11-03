1eed7767f32a931c2aee6176dd64fe8491622923
Scott Fisher <scott.fisher@ucop.edu>
2021-11-03 15:14:51 -0700

    Research facility autofill.

---
+++ spec/features/stash_datacite/research_facility_autofill_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+RSpec.feature 'ResearchFacilityAutofill', type: :feature do
+
+  include DatasetHelper
+  include Mocks::RSolr
+  include Mocks::Ror
+  include Mocks::Tenant
+
+  before(:each) do
+    mock_solr!
+    mock_ror!
+    mock_tenant!
+    @user = create(:user)
+    sign_in(@user)
+  end
+
+  context :ror_api do
+
+    before(:each) do
+      start_new_dataset
+    end
+
+    it 'displays affiliation choices from the ROR API', js: true do
+      fill_in 'contributor_research_facility', with: 'Testing'
+      expect(page).to have_text('University of Testing')
+      expect(page).to have_text('University of Testing v2')
+    end
+
+    it 'sets the ROR id when user selects an option', js: true do
+      stub_ror_id_lookup(university: 'University of Testing v2')
+      fill_in 'contributor_research_facility', with: 'Testing'
+      first('.ui-menu-item-wrapper', wait: 1).click
+      expect(find('#facility_name_identifier_id', visible: false).value).to eql('https://ror.org/TEST2')
+    end
+
+    it 'allows entries that are not registered with ROR', js: true do
+      fill_in 'contributor_research_facility', with: 'Testing a non-ROR organization'
+      expect(find('#facility_name_identifier_id', visible: false).value).to eql('')
+    end
+
+    it 'allows changing from a ROR-based value to a non-ROR value', js: true do
+      stub_ror_id_lookup(university: 'University of Testing v2')
+      fill_in 'contributor_research_facility', with: 'Testing'
+      first('.ui-menu-item-wrapper', wait: 1).click
+      expect(find('#facility_name_identifier_id', visible: false).value).to eql('https://ror.org/TEST2')
+      fill_in 'contributor_research_facility', with: 'Testing a non-ROR organization'
+      expect(find('#facility_name_identifier_id', visible: false).value).to eql('')
+    end
+  end
+end
