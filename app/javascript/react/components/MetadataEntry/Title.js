0279aea5cbdad14cf93c0cb6d28938189bca7eaa
Scott Fisher <scott.fisher@ucop.edu>
2022-01-11 17:15:10 -0800

    Adding form that successfully submits to controller

--- app/javascript/react/components/MetadataEntry/Title.js
+++ app/javascript/react/components/MetadataEntry/Title.js
@@ -1,27 +1,37 @@
 import React from 'react';
+// see https://formik.org/docs/tutorial for basic tutorial, yup is easy default for validation w/ formik
+import {Field, Form, Formik} from 'formik';
+import axios from 'axios';
 
-const Title = ({ resource }) => {
+const Title = ({ resource, path }) => {
   // see https://stackoverflow.com/questions/54808071/cant-verify-csrf-token-authenticity-rails-react for other options
   const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 
-  // the title should ALWAYS be a patch request for sending data, since it's a field in the resource model
-  // which should always be created before any metadata entry for title is performed
   return (
-    <form id="resource_title_REPLACE_ME"
-          className="c-input"
-          action="/stash_datacite/titles/update#REPLACE_ME" acceptCharset="UTF-8"
-          data-remote="true" method="post" _lpchecked="1">
-      <input name="utf8" type="hidden" value="✓" />
-      <input type="hidden" name="_method" value="patch" />
-      <input type="hidden" name="authenticity_token" value={csrf} />
-      <strong>
-        <label className="required c-input__label" htmlFor={`title__${resource.id}`} >Dataset Title</label>
-      </strong><br />
-      <input type="text" name="title" id={`title__${resource.id}`} value={resource.title} className="title c-input__text" size="130" />
-      <input type="hidden" name="id" id="id" value={resource.id} />
-      <input type="hidden" name="form_id" id="form_id" value={`resource_title_${resource.id}`} />
-    </form>
+    <Formik
+      initialValues={{ title: resource.title, id: resource.id, authenticity_token: csrf }}
+
+      onSubmit={(values, { setSubmitting }) => {
+        axios.patch(path, values, { headers: {'Content-Type': 'application/json; charset=utf-8', 'Accept': 'application/json'} })
+          .then((data) =>
+            {
+              console.log(data);
+            }
+          )
+      }}
+    >
+      <Form>
+        <strong>
+          <label className="required c-input__label" htmlFor="title">Dataset Title</label>
+        </strong><br />
+        <Field name="title" type="text" className="title c-input__text" size="130" maxLength="255" />
+        <Field name="id" type="hidden" />
+        <Field name="authenticity_token" type="hidden" />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
-}
+};
 
 export default Title;
