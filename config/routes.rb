496c974f23c218e18d020ec6c3e1cd12dda9fc3d
Ryan Scherle <ryan@scherle.org>
2022-01-19 22:31:42 +0000

    move more files

--- config/routes.rb
+++ config/routes.rb
@@ -101,6 +101,45 @@ Rails.application.routes.draw do
       constraints: {host: 'wiki.datadryad.org'},
       to: redirect('https://github.com/CDL-Dryad/dryad-app/tree/main/documentation/v1_wiki_content.md')
 
+  ############################# API support ######################################
+
+  match '/api/v2/test', to: 'general#test', via: %i[get post]
+  match '/api/v2/search', to: 'datasets#search', via: %i[get]
+
+  # Support for the Editorial Manager API
+  match '/api/v2/em_submission_metadata(/:id)', constraints: { id: /\S+/ }, to: 'datasets#em_submission_metadata', via: %i[post put]
+
+  resources :datasets, shallow: true, id: %r{[^\s/]+?}, format: /json|xml|yaml/ path: '/api/v2/datasets' do
+    member do
+      get 'download'
+    end
+    member do
+      post 'set_internal_datum'
+    end
+    member do
+      post 'add_internal_datum'
+    end
+    resources :internal_data, shallow: true, path: '/api/v2/internal_data'
+    resources :curation_activity, shallow: false, path: '/api/v2/curation_activity'
+    resources :versions, shallow: true, path: '/api/v2/versions' do
+      get 'download', on: :member
+      resources :files, shallow: true do
+        get :download, on: :member
+      end
+    end
+    resources :urls, shallow: true, path: '/api/v2/urls', only: [:create]
+  end
+  # this one doesn't follow the pattern since it gloms filename on the end, so manual route
+  # This should be PUT, not POST because of filename, see https://stackoverflow.com/questions/630453/put-vs-post-in-rest for example
+  put '/api/v2/datasets/:id/files/:filename', id: %r{[^\s/]+?}, filename: %r{[^\s/]+?}, to: 'files#update', as: 'dataset_file', format: false
+
+  resources :users, path: '/api/v2/users', only: %i[index show]
+
+  get '/api/v2/queue_length', to: 'submission_queue#length'
+
+  
+  ########################## Dryad v1 support ######################################
+  
   # Routing to redirect old Dryad URLs to their correct locations in this system
   get '/pages/faq', to: redirect('stash/faq')
   get '/pages/jdap', to: redirect('docs/JointDataArchivingPolicy.pdf')
