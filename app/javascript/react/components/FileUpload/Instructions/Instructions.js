0bed7f2969478d6288c411f4f676055f7f4ef64a
Scott Fisher <scott.fisher@ucop.edu>
2022-01-19 11:53:51 -0800

    Autofixing and turning off a couple of rules

--- app/javascript/react/components/FileUpload/Instructions/Instructions.js
+++ app/javascript/react/components/FileUpload/Instructions/Instructions.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const instructions = (props) => {
-  return (
-    <div>
-      <p>
-        You may upload data via two mechanisms: directly from your computer, or from a URL
-        on an external server (e.g., Box, Dropbox, AWS, lab server). We do not recommend
-        using Google Drive.
-      </p>
-      <p>
-        We require that you include a README file to provide key information for understanding
-        and reuse of your data. The file should be uploaded to the Data category and the file
-        name must contain and begin with "README" in all caps (i.e. README_file.txt). 
-      </p>
-      <p>
-        Software and Supplemental Information can be uploaded for publication at <a href="https://zenodo.org" target="_blank">Zenodo</a>.
-        You will have the opportunity to choose a separate license for your software on the
-        review page.
-      </p>
-    </div>
-    )
-}
+const instructions = (props) => (
+  <div>
+    <p>
+      You may upload data via two mechanisms: directly from your computer, or from a URL
+      on an external server (e.g., Box, Dropbox, AWS, lab server). We do not recommend
+      using Google Drive.
+    </p>
+    <p>
+      We require that you include a README file to provide key information for understanding
+      and reuse of your data. The file should be uploaded to the Data category and the file
+      name must contain and begin with "README" in all caps (i.e. README_file.txt).
+    </p>
+    <p>
+      Software and Supplemental Information can be uploaded for publication at
+      {' '}
+      <a href="https://zenodo.org" target="_blank" rel="noreferrer">Zenodo</a>
+      .
+      You will have the opportunity to choose a separate license for your software on the
+      review page.
+    </p>
+  </div>
+);
 
 export default instructions;
