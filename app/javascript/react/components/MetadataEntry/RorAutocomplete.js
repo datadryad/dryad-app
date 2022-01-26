3e74a61823c5f6fba278da19fd8ef71d2d141208
Scott Fisher <scott.fisher@ucop.edu>
2022-01-26 10:59:23 -0800

    This is a working, but ugly demo of the combobox using downshift

--- app/javascript/react/components/MetadataEntry/RorAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/RorAutocomplete.js
@@ -1,125 +1,56 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+import React, {useState} from 'react';
+import {useCombobox} from 'downshift';
+import {comboboxStyles, items, menuStyles} from './shared';
 
-class RorAutocomplete extends Component {
-  static propTypes = {
-    options: PropTypes.instanceOf(Array)
-  };
+export default function RorAutocomplete() {
+  const [inputItems, setInputItems] = useState(items);
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({inputValue}) => {
+      setInputItems(
+          items.filter((item) =>
+              item.toLowerCase().startsWith(inputValue.toLowerCase()),
+          ),
+      )
+    },
+  });
 
-  static defaultProps = {
-    options: []
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      userInput: ""
-    };
-  }
-
-  onChange = (e) => {
-    const { options } = this.props;
-    const userInput = e.currentTarget.value;
-
-    const filteredOptions = options.filter(
-        (option) => option.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-    );
-
-    this.setState({
-      activeOption: 0,
-      filteredOptions,
-      showOptions: true,
-      userInput: e.currentTarget.value
-    });
-  };
-
-  onClick = (e) => {
-    this.setState({
-      activeOption: 0,
-      filteredOptions: [],
-      showOptions: false,
-      userInput: e.currentTarget.innerText
-    });
-  };
-
-  onKeyDown = (e) => {
-    const { activeOption, filteredOptions } = this.state;
-
-    if (e.keyCode === 13) {
-      this.setState({
-        activeOption: 0,
-        showOptions: false,
-        userInput: filteredOptions[activeOption]
-      });
-    } else if (e.keyCode === 38) {
-      if (activeOption === 0) {
-        return;
-      }
-
-      this.setState({ activeOption: activeOption - 1 });
-    } else if (e.keyCode === 40) {
-      if (activeOption - 1 === filteredOptions.length) {
-        return;
-      }
-
-      this.setState({ activeOption: activeOption + 1 });
-    }
-  };
-
-  render() {
-    const {
-      onChange,
-      onClick,
-      onKeyDown,
-      state: { activeOption, filteredOptions, showOptions, userInput }
-    } = this;
-
-    let optionsListComponent;
-
-    if (showOptions && userInput) {
-      if (filteredOptions.length) {
-        optionsListComponent = (
-            <ul class="options">
-              {filteredOptions.map((option, index) => {
-                let className;
-
-                if (index === activeOption) {
-                  className = "option-active";
-                }
-
-                return (
-                    <li className={className} key={option} onClick={onClick}>
-                      {option}
-                    </li>
-                );
-              })}
-            </ul>
-        );
-      } else {
-        optionsListComponent = (
-            <div class="no-options">
-              <em>No options!</em>
-            </div>
-        );
-      }
-    }
-
-    return (
-        <Fragment>
-          <input
-              type="text"
-              onChange={onChange}
-              onKeyDown={onKeyDown}
-              value={userInput}
-          />
-          {optionsListComponent}
-        </Fragment>
-    );
-  }
-}
-
-export default RorAutocomplete;
+  return (
+      <div>
+        <label {...getLabelProps()}>Choose an element:</label>
+        <div style={comboboxStyles} {...getComboboxProps()}>
+          <input {...getInputProps()} />
+          <button
+              type="button"
+              {...getToggleButtonProps()}
+              aria-label="toggle menu"
+          >
+            &#8595;
+          </button>
+        </div>
+        <ul {...getMenuProps()} style={menuStyles}>
+          {isOpen &&
+          inputItems.map((item, index) => (
+              <li
+                  style={
+                    highlightedIndex === index ? {backgroundColor: '#bde4ff'} : {}
+                  }
+                  key={`${item}${index}`}
+                  {...getItemProps({item, index})}
+              >
+                {item}
+              </li>
+          ))}
+        </ul>
+      </div>
+  );
+};
