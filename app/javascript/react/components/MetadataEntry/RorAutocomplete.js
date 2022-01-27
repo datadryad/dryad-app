f12f42b25c4ddc3ccad0cd70713f0530d3b36bf6
Scott Fisher <scott.fisher@ucop.edu>
2022-01-26 18:20:50 -0800

    Getting autocomplete to work based on examples and axios, but still looks pretty ugly.

--- app/javascript/react/components/MetadataEntry/RorAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/RorAutocomplete.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {useCombobox} from 'downshift';
 import {comboboxStyles, items, menuStyles} from './shared';
+import axios from "axios";
 
 export default function RorAutocomplete() {
   const [inputItems, setInputItems] = useState(items);
@@ -16,19 +17,35 @@ export default function RorAutocomplete() {
   } = useCombobox({
     items: inputItems,
     onInputValueChange: ({inputValue}) => {
-      setInputItems(
+      const result = [];
+      // URL = https://api.ror.org/organizations
+      // query="name"
+      axios.get('https://api.ror.org/organizations', { params: {query: "University of California"},
+            headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'} } )
+          .then((data) => {
+            if (data.status !== 200) {
+              console.log('Response failure not a 200 response');
+            }else{
+              // const myList = data.data.items.map((item) => item.name);
+              const myList = data.data.items;
+              console.log(myList);
+              setInputItems(myList,)
+            }
+          });
+      /*setInputItems(
           items.filter((item) =>
               item.toLowerCase().startsWith(inputValue.toLowerCase()),
           ),
-      )
+      )*/
     },
+    itemToString: (item) => item?.name || '',
   });
 
   return (
       <div>
         <label {...getLabelProps()}>Choose an element:</label>
         <div style={comboboxStyles} {...getComboboxProps()}>
-          <input {...getInputProps()} />
+          <input className='c-input__text' {...getInputProps()} />
           <button
               type="button"
               {...getToggleButtonProps()}
@@ -41,13 +58,13 @@ export default function RorAutocomplete() {
           {isOpen &&
           inputItems.map((item, index) => (
               <li
+                  {...getItemProps({item, index})}
                   style={
                     highlightedIndex === index ? {backgroundColor: '#bde4ff'} : {}
                   }
-                  key={`${item}${index}`}
-                  {...getItemProps({item, index})}
+                  key={item.id}
               >
-                {item}
+                {item.name}
               </li>
           ))}
         </ul>
