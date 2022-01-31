b6f6512f3ceb653e8e4d66a2b7d973eda1a4b131
Scott Fisher <scott.fisher@ucop.edu>
2022-01-31 15:36:30 -0800

    Better appearance for autocomplete list

--- app/javascript/react/components/MetadataEntry/GenericAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/GenericAutocomplete.js
@@ -1,13 +1,15 @@
 import React, {useCallback, useState} from 'react';
 import {useCombobox} from 'downshift';
-import {comboboxStyles, menuStyles} from './shared';
+import {menuStyles} from './shared';
 import axios from "axios";
 import _debounce from 'lodash/debounce';
 import stringSimilarity from 'string-similarity';
 
 
-export default function GenericAutocomplete({acText, setAcText, setAcID, setAutoBlurred}) {
+export default function GenericAutocomplete({acText, setAcText, acID, setAcID, setAutoBlurred}) {
   const [inputItems, setInputItems] = useState([]);
+
+  let lastItemText = '';
 
   // see https://stackoverflow.com/questions/36294134/lodash-debounce-with-react-input
   const debounceFN = useCallback(_debounce(supplyLookupList, 500), []);
@@ -48,10 +50,14 @@ export default function GenericAutocomplete({acText, setAcText, setAcID, setAuto
     getComboboxProps,
     highlightedIndex,
     getItemProps,
+    closeMenu,
   } = useCombobox({
     items: inputItems,
     onInputValueChange: ({inputValue}) => {
       setAcText(inputValue);
+      if (inputValue !== lastItemText) {
+        setAcID(''); // reset any identifiers if they've changed input text, but otherwise leave alone
+      }
       // only autocomplete with 3 or more characters so as not to waste queries
       if (!inputValue || inputValue.length < 4){
         setInputItems([],);
@@ -61,30 +67,41 @@ export default function GenericAutocomplete({acText, setAcText, setAcID, setAuto
     },
     onSelectedItemChange: ({selectedItem}) => {
       setAcID(selectedItem.id);
+      lastItemText = stringItem(selectedItem);
+      console.log(`lastItemText=${lastItemText}`);
     },
     itemToString: (item) => stringItem(item),
   });
 
   return (
       <>
-        <label {...getLabelProps()}>Choose an element:</label>
-        <div style={comboboxStyles} {...getComboboxProps()}>
-          <input className='c-input__text' {...getInputProps()} value={acText} onBlur={ () => { setAutoBlurred(true) } } />
+        <label {...getLabelProps()} className="c-input__label required">Institutional Affiliation:</label>
+        <div {...getComboboxProps()} style={{position: 'relative'}}>
+          <input className='c-input__text' {...getInputProps()} value={acText}
+                 onBlur={ () => {
+                   setAutoBlurred(true);
+                   closeMenu(); // by default this library leaves the menu open all over the page if you tab out
+                 } }
+          />
+          {acID
+              ? ''
+              : <span title="Institution not found. Select it from the auto-complete list if it's available.">&#x2753;</span>
+          }
+          <ul {...getMenuProps()} style={menuStyles}>
+            {isOpen &&
+            inputItems.map((item, index) => (
+                <li
+                    style={
+                      highlightedIndex === index ? {backgroundColor: '#bde4ff', marginBottom: '0.5em'} : { marginBottom: '0.5em' }
+                    }
+                    key={item.id}
+                    {...getItemProps({item, index})}
+                >
+                  {item.name}
+                </li>
+            ))}
+          </ul>
         </div>
-        <ul {...getMenuProps()} style={menuStyles}>
-          {isOpen &&
-          inputItems.map((item, index) => (
-              <li
-                  style={
-                    highlightedIndex === index ? {backgroundColor: '#bde4ff', marginBottom: '0.5em'} : { marginBottom: '0.5em' }
-                  }
-                  key={item.id}
-                  {...getItemProps({item, index})}
-              >
-                {item.name}
-              </li>
-          ))}
-        </ul>
       </>
   );
 };
