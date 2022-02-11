482dbb27ad681e4d94eb35d9b59c26063abd8df8
Ryan Scherle <ryan@scherle.org>
2022-02-11 23:46:45 +0000

    update criteria for selecting journals

--- stash/stash_engine/app/controllers/stash_engine/journals_controller.rb
+++ stash/stash_engine/app/controllers/stash_engine/journals_controller.rb
@@ -10,7 +10,14 @@ module StashEngine
       params.permit(:q)
       params[:sort] = 'title' if params[:sort].blank?
       @all_journals = Journal.all
-      @journals = Journal.joins(:sponsor).where.not(payment_plan_type: [nil, '']).order(helpers.sortable_table_order, title: :asc)
+
+      metadata_journal_clause = 'stash_engine_journals.id IN ' \
+                                "(select distinct journal_id from stash_engine_manuscripts where created_at > '#{1.year.ago.iso8601}')"
+      metadata_journals = Journal.where(metadata_journal_clause).map(&:id)
+      sponsoring_journals = Journal.where.not(payment_plan_type: [nil, '']).map(&:id)
+      display_journals = metadata_journals | sponsoring_journals
+
+      @journals = Journal.joins(:sponsor).where(id: display_journals).order(helpers.sortable_table_order, title: :asc)
 
       respond_to do |format|
         format.html
