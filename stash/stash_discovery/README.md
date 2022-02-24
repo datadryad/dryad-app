f85709bd56ca4e146db4cab87b99eec12759b1e8
Ryan Scherle <ryan@scherle.org>
2022-02-24 20:36:02 +0000

    decorators

--- stash/stash_discovery/README.md
+++
@@ -1,3 +0,0 @@
-# stash_discovery
-
-Placeholder GeoBlacklight app for Stash Solr integration testing.
--- stash/stash_discovery/test/stash_discovery_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StashDiscoveryTest < ActiveSupport::TestCase
-  test 'truth' do
-    assert_kind_of Module, StashDiscovery
-  end
-end
