9f3036bcdd5efc475be9934ffcd0115a386e26e8
Scott Fisher <scott.fisher@ucop.edu>
2022-02-25 11:17:56 -0800

    Getting some tests working.  Sorry couldn't get actual dropdown to work in test.

--- spec/javascript/react/components/MetadataEntry/RorAutocomplete.test.js
+++ spec/javascript/react/components/MetadataEntry/RorAutocomplete.test.js
@@ -1,45 +1,13 @@
 import React from "react";
-import {act, fireEvent, render, screen} from '@testing-library/react';
+import {act, fireEvent, render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import RorAutocomplete from "../../../../../app/javascript/react/components/MetadataEntry/RorAutocomplete.js";
 import axios from 'axios';
+import {acData} from "./rorTestData"
+
+jest.mock('axios');
 
 describe('RorAutocomplete', () => {
-
-  const acData =
-  {
-    "number_of_results": 3333,
-    "time_taken": 17,
-    "items": [
-      {
-        "id": "https://ror.org/04gyf1771",
-        "name": "University of California, Irvine"
-      },
-      {
-        "id": "https://ror.org/00d9ah105",
-        "name": "University of California, Merced"
-      },
-      {
-        "id": "https://ror.org/05rrcem69",
-        "name": "University of California, Davis"
-      },
-      {
-        "id": "https://ror.org/056d3gw71",
-        "name": "Dominican University of California"
-      },
-      {
-        "id": "https://ror.org/03taz7m60",
-        "name": "University of Southern California"
-      },
-      {
-        "id": "https://ror.org/01an7q238",
-        "name": "University of California, Berkeley"
-      },
-      {
-        "id": "https://ror.org/00pjdza24",
-        "name": "University of California System"
-      }
-    ]
-  };
 
   it("renders the basic autocomplete form", () => {
 
@@ -56,31 +24,52 @@ describe('RorAutocomplete', () => {
     expect(container.getElementsByClassName('js-affil-id')[0].value).toEqual(info.id);
   });
 
-  it('should return list of institutions in dropdown', async () => {
-    const info = {name: '', id: '',
+  it('allows changes to dd text input and resets the hidden name and ID fields after change', async () => {
+    const info = {name: 'Institute for Fermentation', id: 'https://ror.org/05nq89q24',
       'controlOptions': { htmlId: "instit_affil_1234", labelText: 'Institutional Affiliation', isRequired: true } }
+
+    /*
+    // The promise stuff in here was trying to mock axios to return drop-down list, but can't get it to work for anything
 
     const promise = Promise.resolve({
       data: acData
-    })
-
-    jest.mock('axios');
+    });
 
     axios.get.mockImplementationOnce(() => promise);
+    */
+
+    axios.get.mockResolvedValueOnce({"data": acData});
 
     const { container } = render(<RorAutocomplete {...info} />);
 
-    const labeledElements = screen.getAllByLabelText(info.controlOptions.labelText, { exact: false })
+    const labeledElements = screen.getAllByLabelText(info.controlOptions.labelText, { exact: false });
 
-    screen.debug();
+    expect(container.getElementsByClassName('js-affil-longname')[0].value).toEqual(info.name);
+    expect(container.getElementsByClassName('js-affil-id')[0].value).toEqual(info.id);
 
-    fireEvent.change(labeledElements[0], {target: {value: 'University of California'}});
+    // There seems to be some kind of bug where it will not reset this value to an empty string (docs say it should)
+    // also if I don't reset it then the userEvent.type below doesn't clear the previous string.
 
+    fireEvent.change(labeledElements[0], {target: {value: 'p'}});
+
+    fireEvent.focus(labeledElements[0]);
+    await act(async () => {
+      // info at: https://testing-library.com/docs/ecosystem-user-event/
+      await userEvent.type(labeledElements[0], '{backspace}University of California', {delay: 20});
+    });
+
+    await waitFor(() => expect(labeledElements[0]).toHaveValue('University of California'));
+
+    expect(container.getElementsByClassName('js-affil-longname')[0].value).toEqual('University of California');
+    expect(container.getElementsByClassName('js-affil-id')[0].value).toEqual('');
+
+    /*
     await act(() => promise);
 
-    screen.debug();
-  });
-
-  it('should select institution from dropdown and fill hidden fields', () => {
+    expect(axios.get).toHaveBeenCalled();
+    await waitFor(() => {
+      expect(getByText('University of California, Irvine').toBeInTheDocument());
+    });
+    */
   });
 });
