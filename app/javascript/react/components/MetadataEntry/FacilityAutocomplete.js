2045671e05ddb33d3c93f35e867830231f7f8820
Scott Fisher <scott.fisher@ucop.edu>
2022-02-28 13:05:48 -0800

    Fixing Eslint warning

--- app/javascript/react/components/MetadataEntry/FacilityAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/FacilityAutocomplete.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 import stringSimilarity from 'string-similarity';
 import PropTypes from 'prop-types';
 import GenericNameIdAutocomplete from './GenericNameIdAutocomplete';
-import {showSavedMsg, showSavingMsg} from "../../../lib/utils";
+import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 
-export default function FacilityAutocomplete({name, rorId, contribId, resourceId, createPath, updatePath, controlOptions}) {
+export default function FacilityAutocomplete({
+  name, rorId, contribId, resourceId, createPath, updatePath, controlOptions,
+}) {
   // control options: htmlId, labelText, isRequired (t/f)
 
   // in order to use this component, we need to track the state of the autocomplete text and the autocomplete id
@@ -19,35 +21,37 @@ export default function FacilityAutocomplete({name, rorId, contribId, resourceId
   const [contributorId, setContributorId] = useState(contribId);
   const nameRef = useRef(null);
 
-
   const submitData = () => {
     showSavingMsg();
     const csrf = document.querySelector("meta[name='csrf-token']")?.getAttribute('content');
 
-    const values =  { 'authenticity_token': csrf,
+    const values = {
+      authenticity_token: csrf,
       contributor: {
-        'id': contributorId,
-        'resource_id': resourceId,
-        'identifier_type': 'ror',
-        'contributor_type': 'sponsor',
-        'contributor_name': acText,
-        'name_identifier_id': acID
-      }
-    }
+        id: contributorId,
+        resource_id: resourceId,
+        identifier_type: 'ror',
+        contributor_type: 'sponsor',
+        contributor_name: acText,
+        name_identifier_id: acID,
+      },
+    };
 
-    let url, method;
-    if(contributorId){
+    let url; let
+      method;
+    if (contributorId) {
       url = updatePath;
       method = 'patch';
-    }else{
+    } else {
       url = createPath;
       method = 'post';
     }
 
-    axios({method: method,
-      url: url,
+    axios({
+      method,
+      url,
       data: values,
-      headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'}
+      headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'},
     }).then((data) => {
       if (data.status !== 200) {
         console.log('Response failure not a 200 response from research facility save');
@@ -55,7 +59,7 @@ export default function FacilityAutocomplete({name, rorId, contribId, resourceId
       setContributorId(data.data.id);
       showSavedMsg();
     });
-  }
+  };
 
   // do something when blurring from the autocomplete, passed up here, probably want to save on blur, but save
   // action may be different depending on autocomplete context inside another form or may save directly.
@@ -93,20 +97,20 @@ export default function FacilityAutocomplete({name, rorId, contribId, resourceId
       params: {query: qt},
       headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'},
     })
-        .then((data) => {
-          if (data.status !== 200) {
-            return [];
-            // raise an error here if we want to catch it and display something to user or do something else
-          }
+      .then((data) => {
+        if (data.status !== 200) {
+          return [];
+          // raise an error here if we want to catch it and display something to user or do something else
+        }
 
-          const list = data.data.items.map((item) => {
-            // add one point if starts with the same string, sends to top
-            const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
-            return {...item, similarity};
-          });
-          list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
-          return list;
+        const list = data.data.items.map((item) => {
+          // add one point if starts with the same string, sends to top
+          const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
+          return {...item, similarity};
         });
+        list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
+        return list;
+      });
   }
 
   // Given a js object from list (supplyLookupList above) it returns the string name
@@ -127,21 +131,21 @@ export default function FacilityAutocomplete({name, rorId, contribId, resourceId
   // in fact some sources say to do it to avoid repeating components (like https://www.youtube.com/watch?v=yH5Z-lSeV9Y ).
   // So IDK what the real guidance is for this and it seems to work fine.
   return (
-      <>
-        <GenericNameIdAutocomplete
-            acText={acText || ''}
-            setAcText={setAcText}
-            acID={acID}
-            setAcID={setAcID}
-            setAutoBlurred={setAutoBlurred}
-            supplyLookupList={supplyLookupList}
-            nameFunc={nameFunc}
-            idFunc={idFunc}
-            controlOptions={controlOptions}
-        />
-        <input ref={nameRef} type="hidden" value={acText} className="js-affil-longname" name="contributor[name_identifier_id]" />
-        <input type="hidden" value={acID} className="js-affil-id" name="author[affiliation][ror_id]" />
-      </>
+    <>
+      <GenericNameIdAutocomplete
+        acText={acText || ''}
+        setAcText={setAcText}
+        acID={acID}
+        setAcID={setAcID}
+        setAutoBlurred={setAutoBlurred}
+        supplyLookupList={supplyLookupList}
+        nameFunc={nameFunc}
+        idFunc={idFunc}
+        controlOptions={controlOptions}
+      />
+      <input ref={nameRef} type="hidden" value={acText} className="js-affil-longname" name="contributor[name_identifier_id]" />
+      <input type="hidden" value={acID} className="js-affil-id" name="author[affiliation][ror_id]" />
+    </>
   );
   /* eslint-enable react/jsx-no-bind */
 }
@@ -156,3 +160,5 @@ FacilityAutocomplete.propTypes = {
   updatePath: PropTypes.string.isRequired,
   controlOptions: PropTypes.object.isRequired,
 };
+
+FacilityAutocomplete.defaultProps = {contribId: ''};
