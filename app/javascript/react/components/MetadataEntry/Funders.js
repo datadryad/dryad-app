b97cb21118343a1b0c64ac41c1cd638e9c5c0aa0
Scott Fisher <scott.fisher@ucop.edu>
2022-03-04 18:18:47 -0800

    Basics to delete and add rows for funders

--- app/javascript/react/components/MetadataEntry/Funders.js
+++ app/javascript/react/components/MetadataEntry/Funders.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import FunderForm from "./FunderForm";
 import {nanoid} from 'nanoid';
+import axios from "axios";
+import {showSavedMsg, showSavingMsg} from "../../../lib/utils";
 
-function Funders({resourceId, contributors, createPath, updatePath}){
+function Funders({resourceId, contributors, createPath, updatePath, deletePath}){
 
   const blankFunder = () => {
      return {id: `new${nanoid()}`, contributor_name: '', contributor_type: 'funder', identifier_type: '',  name_identifier_id: ''};
@@ -16,6 +18,33 @@ function Funders({resourceId, contributors, createPath, updatePath}){
 
   const removeItem = (id) => {
     console.log(`removing ${id}`);
+    const trueDelPath = deletePath.replace('id_xox', id);
+    const csrf = document.querySelector("meta[name='csrf-token']")?.getAttribute('content');
+    showSavingMsg();
+
+    // TODO: requiring the resource like this is weird in a controller for a model that isn't a resource
+    if (!`${id}`.startsWith('new')) {
+      const submitVals = {
+        authenticity_token: csrf,
+        contributor: {
+          id: id,
+          resource_id: resourceId
+        }
+      }
+      axios.delete(trueDelPath, {
+        data: submitVals,
+        headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'}
+      })
+          .then((data) => {
+            if (data.status !== 200) {
+              console.log('Response failure not a 200 response from funders save');
+            } else {
+              console.log('deleted from funders');
+            }
+            showSavedMsg();
+          });
+    }
+
     setFunders(funders.filter((item) => (item.id !== id)));
   }
 
