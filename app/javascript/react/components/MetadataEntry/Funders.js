51bf7bfb78f0b833943f305be883ff4e7fbf65f9
Scott Fisher <scott.fisher@ucop.edu>
2022-03-04 16:56:10 -0800

    making the delete and add do stuff.  Delete doesn't show right ones.

--- app/javascript/react/components/MetadataEntry/Funders.js
+++ app/javascript/react/components/MetadataEntry/Funders.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FunderForm from "./FunderForm";
 
 function Funders({resourceId, contributors, createPath, updatePath}){
 
-  const blankFunder = {id: null, contributor_name: '', contributor_type: 'funder', identifier_type: '',  name_identifier_id: ''}
+  const blankFunder = {id: '', contributor_name: '', contributor_type: 'funder', identifier_type: '',  name_identifier_id: ''}
 
-  if(contributors.length < 1){
-    contributors = [ blankFunder ];
+  const [funders, setFunders] = useState(contributors);
+
+  if(funders.length < 1){
+    setFunders([ blankFunder ]);
+  }
+
+  const removeItem = (i) => {
+    console.log(`removing ${i}`);
+    setFunders(funders.filter((item, idx) => (idx !== i)));
   }
 
   return (
     <>
-      {contributors.map((contrib, i) => (
+      { console.log(funders) }
+      {funders.map((contrib, i) => (
           <FunderForm
             key={i}
             resourceId={resourceId}
             contributor={contrib}
             createPath={createPath}
             updatePath={updatePath}
+            removeFunction={ () => { removeItem(i) } } // hacky since I want to call this method with this value and not a closure
           />
         ))}
+      <a href="#"
+         className="t-describe__add-funder-button o-button__add"
+         role="button"
+         onClick={(e) => {
+           e.preventDefault();
+           setFunders( prev => [...prev, blankFunder]);
+         }}
+      >
+        add another funder
+      </a>
     </>
   );
 }
