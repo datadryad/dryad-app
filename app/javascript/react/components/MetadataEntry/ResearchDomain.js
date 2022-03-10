f3defb354ee77bdaf9ebad4ad252173c921807ef
Scott Fisher <scott.fisher@ucop.edu>
2022-03-10 10:28:11 -0800

    Saving of research domain working

--- app/javascript/react/components/MetadataEntry/ResearchDomain.js
+++ app/javascript/react/components/MetadataEntry/ResearchDomain.js
@@ -2,11 +2,12 @@ import React, {useRef} from 'react';
 // see https://formik.org/docs/tutorial for basic tutorial, yup is easy default for validation w/ formik
 import {Field, Form, Formik} from 'formik';
 import {makeId, showSavingMsg} from '../../../lib/utils';
+import axios from 'axios';
 
 function ResearchDomain({resourceId, subject, subjectList, updatePath}) {
 
   const frmSuffix = makeId(resourceId);
-
+  const csrf = document.querySelector("meta[name='csrf-token']")?.getAttribute('content');
   const formRef = useRef();
 
   /*
@@ -22,17 +23,35 @@ function ResearchDomain({resourceId, subject, subjectList, updatePath}) {
 
   return (
       <Formik
-          initialValues={{fos_subject: subject}}
+          initialValues={{fos_subjects: subject}}
           innerRef={formRef}
           onSubmit={(values, {setSubmitting}) => {
             showSavingMsg();
-            // other stuff here for saving
+            const vals = {
+              authenticity_token: csrf,
+                fos_subjects: values.fos_subjects,
+                id: resourceId
+            }
+            axios.patch(updatePath,
+                vals,
+                {headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'}})
+                .then((data) => {
+                  if (data.status !== 200) {
+                    // console.log('Response failure not a 200 response');
+                  }
+                  showSavedMsg();
+                  setSubmitting(false);
+                });
           }}
       >{(formik) => (
         <Form className="c-input" id={`dc_fos_subjects_${frmSuffix}`}>
           <label className="c-input__label required" htmlFor={`fos_subjects__${frmSuffix}`}>Research Domain</label>
-          <Field type="text" name="fos_subject" id={`fos_subjects__${frmSuffix}`} list={`fos_subject__${frmSuffix}`}
-              className="fos-subjects js-change-submit c-input__text" />
+          <Field type="text" name="fos_subjects" id={`fos_subjects__${frmSuffix}`} list={`fos_subject__${frmSuffix}`}
+                 className="fos-subjects js-change-submit c-input__text"
+                 onBlur={() => { // formRef.current.handleSubmit();
+                   formik.handleSubmit();
+                 }}
+          />
 
           <datalist id={`fos_subject__${frmSuffix}`} className="c-input__text">
             {subjectList.map((subj) => (
