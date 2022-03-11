8fd8f057c606d96123d9a71957e000a3e6550cd5
Scott Fisher <scott.fisher@ucop.edu>
2022-03-11 14:07:16 -0800

    Moving subject display into its own component in jsx

--- app/javascript/react/components/MetadataEntry/Keywords.js
+++ app/javascript/react/components/MetadataEntry/Keywords.js
@@ -8,29 +8,38 @@ import {showModalYNDialog, showSavedMsg, showSavingMsg} from '../../../lib/utils
 
 function Keywords({resourceId, subjects, createPath, deletePath}) {
 
+  function SubjDisplay({subj}){
+
+    console.log(subj);
+
+    return (
+        <span className="c-keywords__keyword">
+          {subj.subject}
+          <span className="delete_keyword">
+            <a id={`sub_remove_${subj.id}`}
+               aria-label="Remove this keyword"
+               role="button"
+               className="c-keywords__keyword-remove"
+               rel="nofollow"
+            ></a>
+          </span>
+        </span>
+    );
+  }
+
   return (
       <div className='c-keywords'>
         <label className="c-input__label" htmlFor="keyword">Keywords:</label>
         &nbsp;&nbsp;Adding keywords improves the findability of your dataset. E.g. scientific names, method type
 
         <div id="js-keywords__container" className="c-keywords__container c-keywords__container--has-blur">
-          {subjects.map((subj) => {
-            return (<span className="c-keywords__keyword">
-            {subj.subject}
-            <span className="delete_keyword">
-              <a id={`sub_remove_${subj.id}`}
-                 aria-label="Remove this keyword"
-                 role="button"
-                 className="c-keywords__keyword-remove"
-                 rel="nofollow"
-              ></a>
-            </span>
-          </span>);
-          })}
+          {subjects.map(subj => <SubjDisplay subj={subj}/> )}
         </div>
       </div>
   );
 }
+
+
 
 export default Keywords;
 
