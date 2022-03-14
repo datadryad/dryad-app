9947fe57cc3f3bbe325900c32f868297cceb700d
Scott Fisher <scott.fisher@ucop.edu>
2022-03-14 13:28:30 -0700

    Some eslint fixes.

--- app/javascript/react/components/MetadataEntry/KeywordAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/KeywordAutocomplete.js
@@ -4,7 +4,9 @@ import stringSimilarity from 'string-similarity';
 import PropTypes from 'prop-types';
 import GenericNameIdAutocomplete from './GenericNameIdAutocomplete';
 
-export default function KeywordAutocomplete({name, id, saveFunction, controlOptions}) {
+export default function KeywordAutocomplete({
+  name, id, saveFunction, controlOptions,
+}) {
   // control options: htmlId, labelText, isRequired (t/f)
 
   // in order to use this component, we need to track the state of the autocomplete text and the autocomplete id
@@ -45,20 +47,20 @@ export default function KeywordAutocomplete({name, id, saveFunction, controlOpti
       params: {term: qt},
       headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'},
     })
-        .then((data) => {
-          if (data.status !== 200) {
-            return [];
-            // raise an error here if we want to catch it and display something to user or do something else
-          }
+      .then((data) => {
+        if (data.status !== 200) {
+          return [];
+          // raise an error here if we want to catch it and display something to user or do something else
+        }
 
-          const list = data.data.map((item) => {
-            // add one point if starts with the same string, sends to top
-            const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
-            return {...item, similarity};
-          });
-          list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
-          return list;
+        const list = data.data.map((item) => {
+          // add one point if starts with the same string, sends to top
+          const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
+          return {...item, similarity};
         });
+        list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
+        return list;
+      });
   }
 
   // Given a js object from list (supplyLookupList above) it returns the string name
@@ -79,21 +81,21 @@ export default function KeywordAutocomplete({name, id, saveFunction, controlOpti
   // in fact some sources say to do it to avoid repeating components (like https://www.youtube.com/watch?v=yH5Z-lSeV9Y ).
   // So IDK what the real guidance is for this and it seems to work fine.
   return (
-      <>
-        <GenericNameIdAutocomplete
-            acText={acText || ''}
-            setAcText={setAcText}
-            acID={acID}
-            setAcID={setAcID}
-            setAutoBlurred={setAutoBlurred}
-            supplyLookupList={supplyLookupList}
-            nameFunc={nameFunc}
-            idFunc={idFunc}
-            controlOptions={controlOptions}
-        />
-        <input ref={nameRef} type="hidden" value={acText} className="js-affil-longname" name="author[affiliation][long_name]" />
-        <input type="hidden" value={acID} className="js-affil-id" name="author[affiliation][ror_id]" />
-      </>
+    <>
+      <GenericNameIdAutocomplete
+        acText={acText || ''}
+        setAcText={setAcText}
+        acID={acID}
+        setAcID={setAcID}
+        setAutoBlurred={setAutoBlurred}
+        supplyLookupList={supplyLookupList}
+        nameFunc={nameFunc}
+        idFunc={idFunc}
+        controlOptions={controlOptions}
+      />
+      <input ref={nameRef} type="hidden" value={acText} className="js-affil-longname" name="author[affiliation][long_name]" />
+      <input type="hidden" value={acID} className="js-affil-id" name="author[affiliation][ror_id]" />
+    </>
   );
   /* eslint-enable react/jsx-no-bind */
 }
@@ -101,5 +103,6 @@ export default function KeywordAutocomplete({name, id, saveFunction, controlOpti
 KeywordAutocomplete.propTypes = {
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  saveFunction: PropTypes.func.isRequired,
   controlOptions: PropTypes.object.isRequired,
 };
