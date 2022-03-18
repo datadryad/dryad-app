ca592da3d1663b24543c1dd994384b992fcc10be
Scott Fisher <scott.fisher@ucop.edu>
2022-03-18 16:05:10 -0700

    This works for reordering, but deleting and re-ordering messes it all up.

--- app/javascript/react/components/MetadataEntry/DragonTest1.js
+++ app/javascript/react/components/MetadataEntry/DragonTest1.js
@@ -6,38 +6,72 @@ import './Dragon.css';
 
 export default function DragonTest1(){
 
-  const nameArr = new Array(20).fill( true ).map((item, idx) => {
+  const nameArr = new Array(10).fill( true ).map((item, idx) => {
     return { id: idx + 1000, name: faker.name.findName(), order: idx }
   });
+
+  const [authors, setAuthors] = useState(nameArr);
+  // const [dragonDrop, setDragonDrop] = useState(null);
+
+  // function relies on css class dd-list-item and data-id items in the dom for info, so render should make those
+  function updateOrderFromDom(container) {
+    const items = Array.from(container.querySelectorAll('li.dd-list-item'));
+
+    const newOrder = items.map((item, idx) => {
+      return { id: parseInt(item.getAttribute('data-id')), order: idx }
+    });
+
+    // make into key/values with object id as key and order as value for fast lookup
+    const newOrderObj = newOrder.reduce( (obj, item) => {
+      obj[item.id] = item.order;
+      return obj;
+    }, {});
+    // console.log("new order object:", newOrderObj);
+
+    // duplicate authors list with updated order values reflecting new order
+    const newAuth = authors.map((item) => {
+      return {...item, order: newOrderObj[item.id]}
+    });
+
+    // replace
+    setAuthors(newAuth);
+  }
+
+  function deleteItem(id){
+    setAuthors(authors.filter((item) => (item.id !== id)));
+  }
 
   useEffect(() => {
     // do this after first render
     const ddAuthors = document.getElementById('dd-authors');
-    const dragonDrop = new DragonDrop(ddAuthors, {
+
+    // the "announcement section below is to announce changes to screen readers
+    const dragon = new DragonDrop(ddAuthors, {
       handle: '.handle',
       announcement: {
-        grabbed: (el) => {
-          console.log(`${el.querySelector('span').innerText} grabbed`);
-          return `${el.querySelector('span').innerText} grabbed`;
-        },
-        dropped: (el) => {
-          console.log(`${el.querySelector('span').innerText} dropped`);
-          return `${el.querySelector('span').innerText} dropped`;
-        },
+        grabbed: el => `${el.querySelector('span').innerText} grabbed`,
+        dropped: el => `${el.querySelector('span').innerText} dropped`,
         reorder: (el, items) => {
           console.log('reordered');
           const pos = items.indexOf(el) + 1;
           const text = el.querySelector('span').innerText;
-          console.log(`The rankings have been updated, ${text} is now ranked #${pos} out of ${items.length}`);
           return `The rankings have been updated, ${text} is now ranked #${pos} out of ${items.length}`;
         },
-        cancel: () => {
-          console.log('Reranking canceled');
-          return 'Reranking cancelled.';
-        }
+        cancel: 'Reranking cancelled.'
       }
     });
+
+    dragon.on('dropped', function (container, item) {
+      // console.log('dropped: ', item);
+      // console.log('container: ', container);
+      updateOrderFromDom(container);
+    });
+
   }, []);
+
+  useEffect(() => {
+    console.log('author list:', authors);
+  }, [authors])
 
   return (
     <section>
@@ -47,9 +81,18 @@ export default function DragonTest1(){
         drag/reorder. Press escape to cancel the reordering.
         <span className="offscreen">Ensure screen reader is in focus mode.</span>
       </p>
-      <ul class="dragon-drop-list" id="dd-authors" aria-labelledby="authors-head">
-        {nameArr.map((name) => (
-          <li key={name.id} className="dd-list-item">
+      <ul className="dragon-drop-list" id="dd-authors" aria-labelledby="authors-head">
+        {authors
+            .sort((a, b) => {
+              // sorts by id if order not present and gets around 0 being falsey in javascript
+              if (a.order === undefined || a.order === null || b.order === undefined || b.order === null) {
+                return a.order - b.order;
+              }else{
+                return a.id - b.id;
+              }
+            })
+            .map((name) => (
+          <li key={name.id} className="dd-list-item" data-id={name.id}>
             <button aria-describedby="global-help"
                     type="button"
                     className="fa fa-bars handle"
@@ -57,10 +100,12 @@ export default function DragonTest1(){
                     id={`author-button-${name.id}`}>
               <div className="offscreen">Reorder</div>
             </button>
-            <span id={`author-text-${name.id}`}>{name.name}</span>
+            <span id={`author-text-${name.id}`}>{name.name}</span>&nbsp;
+            <a href="#" onClick={() => deleteItem(name.id)}>delete</a>
           </li>
         ))}
       </ul>
+      <div>{authors.length} authors</div>
     </section>
   );
 }
