3708fe6df4f269721931cd87466525e27b8ef214
Scott Fisher <scott.fisher@ucop.edu>
2022-03-29 13:09:30 -0700

    This should fix the autocomplete problems with clicking on items and it sending two blur requests.

--- app/javascript/react/components/MetadataEntry/GenericNameIdAutocomplete.js
+++ app/javascript/react/components/MetadataEntry/GenericNameIdAutocomplete.js
@@ -1,6 +1,6 @@
 // Spreading below is how downshift sets things up and is part of their "good" examples.  Annoying and hard to change
 /* eslint-disable react/jsx-props-no-spreading */
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useState, useRef} from 'react';
 import {useCombobox} from 'downshift';
 import _debounce from 'lodash/debounce';
 import PropTypes from 'prop-types';
@@ -25,14 +25,15 @@ import menuStyles from './AcMenuStyles';
        See the file RorAutocomplete.js for a real example.
  */
 export default function GenericNameIdAutocomplete(
-  {
-    acText, setAcText, acID, setAcID, setAutoBlurred, supplyLookupList, nameFunc, idFunc,
-    controlOptions: {htmlId, labelText, isRequired},
-  },
+    {
+      acText, setAcText, acID, setAcID, setAutoBlurred, supplyLookupList, nameFunc, idFunc,
+      controlOptions: {htmlId, labelText, isRequired},
+    },
 ) {
   const [inputItems, setInputItems] = useState([]);
 
-  let lastItemText = '';
+  const lastItemText = useRef(acText);
+  const completionClick = useRef(false);
 
   function wrapLookupList(qt) {
     supplyLookupList(qt).then((items) => {
@@ -52,12 +53,12 @@ export default function GenericNameIdAutocomplete(
     getComboboxProps,
     highlightedIndex,
     getItemProps,
-    closeMenu,
+    // closeMenu,
   } = useCombobox({
     items: inputItems,
     onInputValueChange: ({inputValue}) => {
       setAcText(inputValue);
-      if (inputValue !== lastItemText) {
+      if (inputValue !== lastItemText.current) {
         setAcID(''); // reset any identifiers if they've changed input text, but otherwise leave alone
       }
       // only autocomplete with 3 or more characters so as not to waste queries
@@ -69,69 +70,78 @@ export default function GenericNameIdAutocomplete(
     },
     onSelectedItemChange: ({selectedItem}) => {
       setAcID(idFunc(selectedItem));
-      lastItemText = nameFunc(selectedItem);
-      setAutoBlurred(true); // this indicates a blur so that it can trigger save or whatever action blur takes
+      lastItemText.current = nameFunc(selectedItem);
+      setAutoBlurred(true); // this notifies parent component to save
     },
     itemToString: (item) => nameFunc(item),
   });
 
   return (
-    <>
-      <label
-        {...getLabelProps()}
-        className={`c-input__label ${(isRequired ? 'required' : '')}`}
-        id={`label_${htmlId}`}
-        htmlFor={htmlId}
-      >
-        {labelText}:
-      </label>
-      <div
-        {...getComboboxProps()}
-        aria-owns={`menu_${htmlId}`}
-        style={{position: 'relative', display: 'flex'}}
-      >
-        <input
-          className="c-input__text"
-          {...getInputProps()}
-          id={htmlId}
-          style={{flex: 1}}
-          value={acText}
-          aria-controls={`menu_${htmlId}`}
-          aria-labelledby={`label_${htmlId}`}
-          onBlur={(e) => {
-            /* workaround: We don't want to set blur unless relatedTarget exists as a good element.
-                    It is null when clicking on an autocomplete menu and we don't want to trigger the autoBlur flag for that
-                    */
-            if (e.relatedTarget) {
-              setAutoBlurred(true);
-              closeMenu(); // by default this library leaves the menu open all over the page if you tab out
-            }
-          }}
-        />
-        { !acID && isRequired
-          ? <span title={`${labelText} not found. Select the correct ${labelText} from the auto-complete list.`}>&#x2753;</span>
-          : ''}
-        <ul
-          {...getMenuProps()}
-          style={menuStyles}
-          id={`menu_${htmlId}`}
-          aria-labelledby={`label_${htmlId}`}
+      <>
+        { labelText
+            ? (
+                <label
+                    {...getLabelProps()}
+                    className={`c-input__label ${(isRequired ? 'required' : '')}`}
+                    id={`label_${htmlId}`}
+                    htmlFor={htmlId}
+                >
+                  {labelText}:
+                </label>
+            ) : '' }
+        <div
+            {...getComboboxProps()}
+            aria-owns={`menu_${htmlId}`}
+            style={{position: 'relative', display: 'flex'}}
         >
-          {isOpen
+          <input
+              className="c-input__text"
+              {...getInputProps(
+                  {
+                    onBlur: () => {
+                      if (completionClick.current) {
+                        // don't fire a save after a mousedown state set on clicking a completion item, it's not a real blur
+                        completionClick.current = false;
+                      } else {
+                        setAutoBlurred(true); // set this to notify the parent component to save or do whatever
+                      }
+                    },
+                  },
+              )}
+              id={htmlId}
+              style={{flex: 1}}
+              value={acText}
+              aria-controls={`menu_${htmlId}`}
+              aria-labelledby={`label_${htmlId}`}
+          />
+          { !acID && isRequired
+              ? <span title={`${labelText} not found. Select the correct ${labelText} from the auto-complete list.`}>&#x2753;</span>
+              : ''}
+          <ul
+              {...getMenuProps()}
+              style={menuStyles}
+              id={`menu_${htmlId}`}
+              aria-labelledby={`label_${htmlId}`}
+          >
+            {isOpen
             && inputItems.map((item, index) => (
-              <li
-                style={
+                <li
+                    style={
                       highlightedIndex === index ? {backgroundColor: '#bde4ff', marginBottom: '0.5em'} : {marginBottom: '0.5em'}
                     }
-                key={idFunc(item)}
-                {...getItemProps({item, index})}
-              >
-                {nameFunc(item)}
-              </li>
+                    key={idFunc(item)}
+                    {...getItemProps({
+                      item,
+                      index,
+                      onMouseDown: () => { completionClick.current = true; },
+                    })}
+                >
+                  {nameFunc(item)}
+                </li>
             ))}
-        </ul>
-      </div>
-    </>
+          </ul>
+        </div>
+      </>
   );
 }
 
