c34435fb422033b81d5f3547a75aec7e0da2d42b
Scott Fisher <scott.fisher@ucop.edu>
2022-03-29 18:46:14 -0700

    Got collisions on Github creating ror_orgs

--- spec/factories/stash_engine/ror_orgs.rb
+++ spec/factories/stash_engine/ror_orgs.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
   factory :ror_org, class: StashEngine::RorOrg do
 
     name { Faker::Company.name }
-    ror_id { "https://ror.org/#{Faker::Number.number(digits: 4)}" }
+    ror_id { "https://ror.org/#{Faker::Number.number(digits: 7)}" }
     # Don't default to a real country, because we don't want it to accidentally match when testing
-    country { 'The Undiscovered Country' }
+    country { Faker::Address.country }
 
   end
 
