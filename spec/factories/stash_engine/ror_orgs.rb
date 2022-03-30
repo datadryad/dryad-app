ff927cd52210fb146a44bfaa982bb80af90d5840
Scott Fisher <scott.fisher@ucop.edu>
2022-03-29 22:05:51 -0700

    fixing one test payment

--- spec/factories/stash_engine/ror_orgs.rb
+++ spec/factories/stash_engine/ror_orgs.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     name { Faker::Company.name }
     ror_id { "https://ror.org/#{Faker::Number.number(digits: 7)}" }
     # Don't default to a real country, because we don't want it to accidentally match when testing
-    country { Faker::Address.country }
+    country { 'The Undiscovered Country' }
 
   end
 
