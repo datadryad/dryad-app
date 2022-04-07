04200a518c10f6be3128338973e82b08ee100f87
Scott Fisher <scott.fisher@ucop.edu>
2022-04-07 16:05:11 -0700

    Fixing author fill

--- spec/support/helpers/dataset_helper.rb
+++ spec/support/helpers/dataset_helper.rb
@@ -79,9 +79,9 @@ module DatasetHelper
   end
 
   def fill_in_author
-    fill_in 'author[author_first_name]', with: Faker::Name.unique.first_name
-    fill_in 'author[author_last_name]', with: Faker::Name.unique.last_name
-    fill_in 'author[author_email]', with: Faker::Internet.safe_email
+    fill_in 'author_first_name', with: Faker::Name.unique.first_name
+    fill_in 'author_last_name', with: Faker::Name.unique.last_name
+    fill_in 'author_email', with: Faker::Internet.safe_email
     # just fill in results of name dropdown (react) in hidden field and test this separately
     page.execute_script("document.getElementsByClassName('js-affil-longname')[0].value = '#{Faker::Educator.university}'")
   end
