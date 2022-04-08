d63936f3a0fade1ac127893e420c08f30f1beafe
Scott Fisher <scott.fisher@ucop.edu>
2022-04-08 13:12:27 -0700

    reformatting by eslint

--- app/javascript/react/components/MetadataEntry/AuthorForm.js
+++ app/javascript/react/components/MetadataEntry/AuthorForm.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from 'react';
 import {Field, Form, Formik} from 'formik';
 import axios from 'axios';
 import {showModalYNDialog, showSavedMsg, showSavingMsg} from '../../../lib/utils';
-import RorAutocomplete from "./RorAutocomplete"
+import RorAutocomplete from './RorAutocomplete';
 
 // dryadAuthor below has nested affiliation
 export default function AuthorForm({dryadAuthor, removeFunction}) {
@@ -27,20 +27,20 @@ export default function AuthorForm({dryadAuthor, removeFunction}) {
         author_last_name: values.author_last_name,
         author_email: values.author_email,
         resource_id: dryadAuthor.resource_id,
-        affiliation: { long_name: acText, ror_id: acID }
+        affiliation: {long_name: acText, ror_id: acID},
       },
     };
 
     // submit by json
     return axios.patch(
-        '/stash_datacite/authors/update',
-        submitVals,
-        {
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            Accept: 'application/json',
-          },
+      '/stash_datacite/authors/update',
+      submitVals,
+      {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Accept: 'application/json',
         },
+      },
     ).then((data) => {
       if (data.status !== 200) {
         console.log('Response failure not a 200 response from author save');
@@ -53,11 +53,9 @@ export default function AuthorForm({dryadAuthor, removeFunction}) {
     });
   };
 
-
-
   return (
-      <Formik
-          initialValues={
+    <Formik
+      initialValues={
             {
               author_first_name: (dryadAuthor.author_first_name || ''),
               author_last_name: (dryadAuthor.author_last_name || ''),
@@ -65,84 +63,85 @@ export default function AuthorForm({dryadAuthor, removeFunction}) {
               id: (dryadAuthor.id),
             }
           }
-          innerRef={formRef}
-          onSubmit={(values, {setSubmitting}) => {
-            submitForm(values).then(() => { setSubmitting(false); });
-            console.log(values);
-          }}
-      >
-        {(formik) => (
-            <Form className="c-input__inline">
-              <Field name="id" type="hidden" />
-              <div className="c-input">
-                <label className="c-input__label required" htmlFor={`author_first_name__${dryadAuthor.id}`}>
-                  First Name
-                </label>
-                <Field
-                    id={`author_first_name__${dryadAuthor.id}`}
-                    name="author_first_name"
-                    type="text"
-                    className="c-input__text"
-                    onBlur={() => { // defaults to formik.handleBlur
-                      formik.handleSubmit();
-                    }}
-                />
-              </div>
-              <div className="c-input">
-                <label className="c-input__label required" htmlFor={`author_last_name__${dryadAuthor.id}`}>
-                  Last Name
-                </label>
-                <Field
-                    id={`author_last_name__${dryadAuthor.id}`}
-                    name="author_last_name"
-                    type="text"
-                    className="c-input__text"
-                    onBlur={() => { // defaults to formik.handleBlur
-                      formik.handleSubmit();
-                    }}
-                />
-              </div>
-              <div className="c-input">
-                <RorAutocomplete
-                    formRef={formRef}
-                    acText={acText}
-                    setAcText={setAcText}
-                    acID={acID}
-                    setAcID={setAcID}
+      innerRef={formRef}
+      onSubmit={(values, {setSubmitting}) => {
+        submitForm(values).then(() => { setSubmitting(false); });
+        console.log(values);
+      }}
+    >
+      {(formik) => (
+        <Form className="c-input__inline">
+          <Field name="id" type="hidden" />
+          <div className="c-input">
+            <label className="c-input__label required" htmlFor={`author_first_name__${dryadAuthor.id}`}>
+              First Name
+            </label>
+            <Field
+              id={`author_first_name__${dryadAuthor.id}`}
+              name="author_first_name"
+              type="text"
+              className="c-input__text"
+              onBlur={() => { // defaults to formik.handleBlur
+                formik.handleSubmit();
+              }}
+            />
+          </div>
+          <div className="c-input">
+            <label className="c-input__label required" htmlFor={`author_last_name__${dryadAuthor.id}`}>
+              Last Name
+            </label>
+            <Field
+              id={`author_last_name__${dryadAuthor.id}`}
+              name="author_last_name"
+              type="text"
+              className="c-input__text"
+              onBlur={() => { // defaults to formik.handleBlur
+                formik.handleSubmit();
+              }}
+            />
+          </div>
+          <div className="c-input">
+            <RorAutocomplete
+              formRef={formRef}
+              acText={acText}
+              setAcText={setAcText}
+              acID={acID}
+              setAcID={setAcID}
                     // name={dryadAuthor.affiliation.long_name}
                     // id={dryadAuthor.affiliation.ror_id}
-                    controlOptions={{ 'htmlId': `instit_affil_${dryadAuthor.id}`, 'labelText': 'Institutional Affiliation', 'isRequired': true }} />
-              </div>
-              <div className="c-input">
-                <label className="c-input__label required" htmlFor={`author_email__${dryadAuthor.id}`}>
-                  Author Email
-                </label>
-                <Field
-                    id={`author_email__${dryadAuthor.id}`}
-                    name="author_email"
-                    type="text"
-                    className="c-input__text"
-                    onBlur={() => { // defaults to formik.handleBlur
-                      formik.handleSubmit();
-                    }}
-                />
-              </div>
-              <a
-                  role="button"
-                  className="t-describe__remove-button o-button__remove remove_record"
-                  rel="nofollow"
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    showModalYNDialog('Are you sure you want to remove this author?', () => {
-                      removeFunction(dryadAuthor.id, dryadAuthor.resource_id);
-                      // deleteItem(auth.id);
-                    });
-                  }}
-              >remove
-              </a>
-            </Form>
-        )}
-      </Formik>
+              controlOptions={{htmlId: `instit_affil_${dryadAuthor.id}`, labelText: 'Institutional Affiliation', isRequired: true}}
+            />
+          </div>
+          <div className="c-input">
+            <label className="c-input__label required" htmlFor={`author_email__${dryadAuthor.id}`}>
+              Author Email
+            </label>
+            <Field
+              id={`author_email__${dryadAuthor.id}`}
+              name="author_email"
+              type="text"
+              className="c-input__text"
+              onBlur={() => { // defaults to formik.handleBlur
+                formik.handleSubmit();
+              }}
+            />
+          </div>
+          <a
+            role="button"
+            className="t-describe__remove-button o-button__remove remove_record"
+            rel="nofollow"
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              showModalYNDialog('Are you sure you want to remove this author?', () => {
+                removeFunction(dryadAuthor.id, dryadAuthor.resource_id);
+                // deleteItem(auth.id);
+              });
+            }}
+          >remove
+          </a>
+        </Form>
+      )}
+    </Formik>
   );
 }
