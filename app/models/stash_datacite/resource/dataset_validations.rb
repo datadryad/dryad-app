6d20b18f39bd42d2c710a01d1be287a938ee4ea7
Ryan Scherle <ryan@scherle.org>
2022-04-11 21:24:54 +0000

    require at least one funder

--- app/models/stash_datacite/resource/dataset_validations.rb
+++ app/models/stash_datacite/resource/dataset_validations.rb
@@ -60,6 +60,7 @@ module StashDatacite
         err << title
         err << authors
         err << research_domain
+        err << funder
         err << abstract
 
         err << s3_error_uploads
@@ -124,6 +125,17 @@ module StashDatacite
           return ErrorItem.new(message: 'Fill in a {research domain}',
                                page: metadata_page(@resource),
                                ids: ["fos_subjects__#{@resource.id}"])
+        end
+        []
+      end
+
+      def funder
+        funder_require_date = '2022-04-13'
+        if (@resource.contributors.blank? || @resource.contributors.first.contributor_name.blank?) &&
+           @resource.identifier.created_at > funder_require_date
+          return ErrorItem.new(message: 'Fill in a {funder}. Use "N/A" if there is no funder associated with the dataset.',
+                               page: metadata_page(@resource),
+                               ids: ['funder_fieldset'])
         end
         []
       end
