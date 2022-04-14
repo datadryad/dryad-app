e03ad9b6f574d63bf36f499e0cfabdc819d185a0
Ryan Scherle <ryan@scherle.org>
2022-04-14 13:37:04 +0000

    fix identifier reference

--- app/models/stash_datacite/resource/dataset_validations.rb
+++ app/models/stash_datacite/resource/dataset_validations.rb
@@ -133,7 +133,7 @@ module StashDatacite
         funder_require_date = '2022-04-14'
         if (@resource.contributors.blank? || @resource.contributors.first.contributor_name.blank?) &&
            @resource.identifier.created_at > funder_require_date &&
-           @resource.identifer.pub_state == 'unpublished'
+           @resource.identifier.pub_state == 'unpublished'
           return ErrorItem.new(message: 'Fill in a {funder}. Use "N/A" if there is no funder associated with the dataset.',
                                page: metadata_page(@resource),
                                ids: ['funder_fieldset'])
