f977effb1a72be237e788ab7f72961ff1f8132d1
Scott Fisher <scott.fisher@ucop.edu>
2022-04-20 11:01:01 -0700

    redirect from www.datadryad.org to datadryad.org .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
+
   use_doorkeeper
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rails routes".
