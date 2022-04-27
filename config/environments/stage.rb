d8106f12abec29f9b007c81d114b84da6ee293b4
Scott Fisher <scott.fisher@ucop.edu>
2022-04-27 11:17:19 -0700

    meaningless change to test the deploy again

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files). x
   config.require_master_key = true
 
   config.public_file_server.enabled = true
