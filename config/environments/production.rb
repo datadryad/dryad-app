4905363abbd50ea7e9166ab9772477d41ed71e88
Scott Fisher <scott.fisher@ucop.edu>
2022-04-27 10:48:58 -0700

    try this for deploying to stage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.css_compressor = :uglifier
+  config.assets.css_compressor = :terser
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
