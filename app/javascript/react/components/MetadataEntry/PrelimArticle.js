5e5ada5b8dc4e891f65748958b6b0b7a88438859
Scott Fisher <scott.fisher@ucop.edu>
2022-04-28 16:57:14 -0700

    Get HTML output showing on screen for each option, doesn't really do much yet.

--- app/javascript/react/components/MetadataEntry/PrelimArticle.js
+++ app/javascript/react/components/MetadataEntry/PrelimArticle.js
@@ -5,7 +5,33 @@ import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 
 function PrelimArticle(){
   return (
-      <p>You&apos;ll see article information here</p>
+      <div className="c-import__form-section">
+        <p>Please provide the following information. You may either enter the information and leave it or choose to
+          autofill your dataset based on the information you supply below.</p>
+
+        <div className="c-input__inline">
+          <div className="c-input">
+            <label className="c-input__label required" htmlFor="publication">Journal Name</label>
+            <input className="c-input__text ui-autocomplete-input" type="text" name="publication" id="publication" />
+            <input value="419" type="hidden" name="identifier_id" id="identifier_id" />
+            <input value="3621" type="hidden" name="resource_id" id="resource_id" />
+            <input type="hidden" name="publication_issn" id="publication_issn" />
+            <input type="hidden" name="publication_name" id="publication_name" />
+          </div>
+          <div className="c-input" style={{display: 'flex'}}>
+            <label className="c-input__label required" htmlFor="primary_article_doi">DOI</label>
+            <input className="c-input__text" placeholder="5702.125/qlm.1266rr" type="text"
+                   name="primary_article_doi" id="primary_article_doi" />
+          </div>
+        </div>
+        <div>
+          <input type="submit" name="commit" value="Import Article Metadata" method="post"
+                 className="o-button__import-manuscript" />
+        </div>
+        <div id="population-warnings" className="o-metadata__autopopulate-message">
+          Sample warning here.
+        </div>
+      </div>
   );
 }
 
