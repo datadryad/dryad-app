c53f55ee9e5d5619dd39cfde0e89418607ce6de3
Scott Fisher <scott.fisher@ucop.edu>
2022-04-28 16:32:05 -0700

    A switch to display one of three different components based on selection.

---
+++ app/javascript/react/components/MetadataEntry/PrelimArticle.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
+
+function PrelimArticle(){
+  return (
+      <p>You&apos;ll see article information here</p>
+  );
+}
+
+export default PrelimArticle;
