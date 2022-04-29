a63572d867bee91e01f1c60b6fab18ca656cef10
Scott Fisher <scott.fisher@ucop.edu>
2022-04-29 13:41:00 -0700

    Formik enclosed both forms.

--- app/javascript/react/components/MetadataEntry/PrelimArticle.js
+++ app/javascript/react/components/MetadataEntry/PrelimArticle.js
@@ -1,10 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import axios from 'axios';
+import {Field, Form, Formik} from 'formik';
 import PropTypes from 'prop-types';
 import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 
 function PrelimArticle(){
+  const formRef = useRef();
+
   return (
+      <Formik
+          initialValues={
+            {
+              publication: 'great journal of testing',
+              primary_article_doi: 'test'
+            }
+          }
+          innerRef={formRef}
+          onSubmit={(values, {setSubmitting}) => {
+            // submitForm(values).then(() => { setSubmitting(false); });
+          }}
+      >
+        {(formik) => (
+            <Form className="c-input__inline">
       <div className="c-import__form-section">
         <p>Please provide the following information. You may either enter the information and leave it or choose to
           autofill your dataset based on the information you supply below.</p>
@@ -12,7 +29,7 @@ function PrelimArticle(){
         <div className="c-input__inline">
           <div className="c-input">
             <label className="c-input__label required" htmlFor="publication">Journal Name</label>
-            <input className="c-input__text ui-autocomplete-input" type="text" name="publication" id="publication" />
+            <input className="c-input__text" type="text" name="publication" id="publication" />
             <input value="419" type="hidden" name="identifier_id" id="identifier_id" />
             <input value="3621" type="hidden" name="resource_id" id="resource_id" />
             <input type="hidden" name="publication_issn" id="publication_issn" />
@@ -32,6 +49,9 @@ function PrelimArticle(){
           Sample warning here.
         </div>
       </div>
+            </Form>
+        )}
+      </Formik>
   );
 }
 
