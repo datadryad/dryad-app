b30c77ea89f71574c93e6bb7ec62a857bb91991e
Scott Fisher <scott.fisher@ucop.edu>
2022-04-29 13:59:16 -0700

    Making more Formik

--- app/javascript/react/components/MetadataEntry/PrelimArticle.js
+++ app/javascript/react/components/MetadataEntry/PrelimArticle.js
@@ -4,7 +4,7 @@ import {Field, Form, Formik} from 'formik';
 import PropTypes from 'prop-types';
 import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 
-function PrelimArticle(){
+function PrelimArticle() {
   const formRef = useRef();
 
   return (
@@ -22,33 +22,37 @@ function PrelimArticle(){
       >
         {(formik) => (
             <Form className="c-input__inline">
-      <div className="c-import__form-section">
-        <p>Please provide the following information. You may either enter the information and leave it or choose to
-          autofill your dataset based on the information you supply below.</p>
+              <div className="c-import__form-section">
+                <p>Please provide the following information. You may either enter the information and leave it or choose
+                  to
+                  autofill your dataset based on the information you supply below.</p>
 
-        <div className="c-input__inline">
-          <div className="c-input">
-            <label className="c-input__label required" htmlFor="publication">Journal Name</label>
-            <input className="c-input__text" type="text" name="publication" id="publication" />
-            <input value="419" type="hidden" name="identifier_id" id="identifier_id" />
-            <input value="3621" type="hidden" name="resource_id" id="resource_id" />
-            <input type="hidden" name="publication_issn" id="publication_issn" />
-            <input type="hidden" name="publication_name" id="publication_name" />
-          </div>
-          <div className="c-input" style={{display: 'flex'}}>
-            <label className="c-input__label required" htmlFor="primary_article_doi">DOI</label>
-            <input className="c-input__text" placeholder="5702.125/qlm.1266rr" type="text"
-                   name="primary_article_doi" id="primary_article_doi" />
-          </div>
-        </div>
-        <div>
-          <input type="submit" name="commit" value="Import Article Metadata" method="post"
-                 className="o-button__import-manuscript" />
-        </div>
-        <div id="population-warnings" className="o-metadata__autopopulate-message">
-          Sample warning here.
-        </div>
-      </div>
+                <div className="c-input__inline">
+                  <div className="c-input">
+                    <label className="c-input__label required" htmlFor="publication">Journal Name</label>
+                    <input className="c-input__text" type="text" name="publication" id="publication"/>>
+                    <input type="hidden" name="publication_issn" id="publication_issn"/>
+                    <input type="hidden" name="publication_name" id="publication_name"/>
+                  </div>
+                  <div className="c-input" style={{display: 'flex'}}>
+                    <label className="c-input__label required" htmlFor="primary_article_doi">DOI</label>
+                    <Field
+                        className="c-input__text"
+                        placeholder="5702.125/qlm.1266rr"
+                        type="text"
+                        name="primary_article_doi"
+                        id="primary_article_doi"/>
+                  </div>
+                </div>
+                <div>
+                  <button type="submit" name="commit" className="o-button__import-manuscript">
+                    Import Article Metadata
+                  </button>
+                </div>
+                <div id="population-warnings" className="o-metadata__autopopulate-message">
+                  Sample warning here.
+                </div>
+              </div>
             </Form>
         )}
       </Formik>
--- app/javascript/react/components/MetadataEntry/PrelimManu.js
+++ app/javascript/react/components/MetadataEntry/PrelimManu.js
@@ -4,7 +4,7 @@ import {Field, Form, Formik} from 'formik';
 import PropTypes from 'prop-types';
 import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 
-function PrelimManu(){
+function PrelimManu() {
   const formRef = useRef();
 
   return (
@@ -20,40 +20,44 @@ function PrelimManu(){
             // submitForm(values).then(() => { setSubmitting(false); });
           }}
       >
-    {(formik) => (
-    <Form className="c-input__inline">
-      <div className="c-import__form-section">
-        <p>Please provide the following information. You may either enter the information and leave it or choose to
-          autofill your dataset based on the information you supply below.</p>
+        {(formik) => (
+            <Form className="c-input__inline">
+              <div className="c-import__form-section">
+                <p>Please provide the following information. You may either enter the information and leave it or choose
+                  to
+                  autofill your dataset based on the information you supply below.</p>
 
-        <div className="c-input__inline">
-          <div className="c-input">
-            <label className="c-input__label required" htmlFor="publication">Journal Name</label>
-            <input className="c-input__text ui-autocomplete-input" type="text" name="publication"
-                   id="publication" />
-            <input value="419" type="hidden" name="identifier_id" id="identifier_id" />
-            <input value="3621" type="hidden" name="resource_id" id="resource_id" />
-            <input type="hidden" name="publication_issn" id="publication_issn" />
-            <input type="hidden" name="publication_name" id="publication_name" />
-            <input value="false" type="hidden" name="do_import" id="do_import" />
-          </div>
-          <div className="c-input">
-            <label className="c-input__label required" htmlFor="msid">
-              Manuscript Number
-            </label>
-            <input className="c-input__text" placeholder="APPS-D-17-00113" type="text" name="msid" id="msid" />
-          </div>
-        </div>
-        <div>
-          <input type="submit" name="commit" value="Import Manuscript Metadata" method="post"
-                 className="o-button__import-manuscript" />
-        </div>
-        <div id="population-warnings" className="o-metadata__autopopulate-message">
-          Some warnings here.
-        </div>
-      </div>
-    </Form>
-    )}
+                <div className="c-input__inline">
+                  <div className="c-input">
+                    <label className="c-input__label required" htmlFor="publication">Journal Name</label>
+                    <input className="c-input__text ui-autocomplete-input" type="text" name="publication"
+                           id="publication"/>
+                    <input type="hidden" name="publication_issn" id="publication_issn"/>
+                    <input type="hidden" name="publication_name" id="publication_name"/>
+                  </div>
+                  <div className="c-input">
+                    <label className="c-input__label required" htmlFor="msid">
+                      Manuscript Number
+                    </label>
+                    <Field
+                        className="c-input__text"
+                        placeholder="APPS-D-17-00113"
+                        type="text"
+                        name="msid"
+                        id="msid"/>
+                  </div>
+                </div>
+                <div>
+                  <button type="submit" name="commit" className="o-button__import-manuscript">
+                    Import Manuscript Metadata
+                  </button>
+                </div>
+                <div id="population-warnings" className="o-metadata__autopopulate-message">
+                  Some warnings here.
+                </div>
+              </div>
+            </Form>
+        )}
       </Formik>
   );
 }
