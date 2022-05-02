2ac71409e623ec0ceb277237adc378c4687a311d
Ryan Scherle <ryan@scherle.org>
2022-05-02 16:18:14 +0000

    comment out counter process

--- cron/monthly.sh
+++ cron/monthly.sh
@@ -14,7 +14,7 @@ export REPORT_DIR="/apps/dryad/apps/ui/shared/cron/counter-json"
 export FORCE_SUBMISSION="`date --date="$(date +%Y-%m-15) - 1 month" "+%Y-%m"`"
 
 # run the script with the above settings (RAILS_ENV, REPORT_DIR, FORCE_SUBMISSION)
-bundle exec rails counter:datacite_pusher >> /apps/dryad/apps/ui/shared/cron/logs/counter-uploader.log 2>&1
+# bundle exec rails counter:datacite_pusher >> /apps/dryad/apps/ui/shared/cron/logs/counter-uploader.log 2>&1
 
 # Clean outdated content from the database and temporary S3 store
 bundle exec rails identifiers:remove_old_versions >> /apps/dryad/apps/ui/shared/cron/logs/remove_old_versions.log 2>&1
