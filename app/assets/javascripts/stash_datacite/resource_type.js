1bbb5012782a0ac7d5166ab5cb5c6e67b73f1428
Scott Fisher <scott.fisher@ucop.edu>
2022-05-06 16:45:38 -0700

    Removed a bunch of javascript to modify forms and also many of the forms that were in Rails since metadata entry is in react now.

--- app/assets/javascripts/stash_datacite/resource_type.js
+++ app/assets/javascripts/stash_datacite/resource_type.js
@@ -1,16 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-function loadResourceTypes() {
-  $( '.js-resource_type' ).on('focus', function () {
-    previous_value = this.value;
-    }).change(function() {
-      new_value = this.value;
-      // Save when the new value is different from the previous value
-      if(new_value != previous_value) {
-        var form = $(this.form);
-        $(form).trigger('submit.rails');
-      }
-    });
-};
+
 
 
