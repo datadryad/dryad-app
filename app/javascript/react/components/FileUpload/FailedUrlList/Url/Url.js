8856995e36248a925f0ed9ecbb373a88745a0fd0
Scott Fisher <scott.fisher@ucop.edu>
2022-06-02 14:46:30 -0700

    Blocks the upload of HTML files since it usually means something is wrong.

--- app/javascript/react/components/FileUpload/FailedUrlList/Url/Url.js
+++ app/javascript/react/components/FileUpload/FailedUrlList/Url/Url.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import parse from 'html-react-parser';
+
 
 const url = (props) => (
   <div className="c-manifest__item">
     <div className="c-manifest__url">{props.url.url}</div>
-    <div className="c-manifest__error">{props.url.error_message}</div>
+    <div className="c-manifest__error">{parse(props.url.error_message)}</div>
     <div className="c-manifest__action">
       {/* <a href="#!">Edit</a> */}
       <a href="#!" onClick={props.click}>Remove</a>
