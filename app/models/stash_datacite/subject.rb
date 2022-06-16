c1973118f38678e1f0223d5ec0f692f4765f7ecb
Scott Fisher <scott.fisher@ucop.edu>
2022-06-16 15:55:24 -0700

    Fixes weird behavior deleting subjects and when people enter a field of science "subject"

--- app/models/stash_datacite/subject.rb
+++ app/models/stash_datacite/subject.rb
@@ -6,6 +6,10 @@ module StashDatacite
     has_and_belongs_to_many :resources, class_name: StashEngine::Resource.to_s,
                                         through: 'StashDatacite::ResourceSubject'
 
+    # non_fos isn't a field of science
+    # fos is a field of science, but only standard ones
+    # permissive_fos is a field of science entry but could be good or a free-form one
+    # bad_fos is a non-standard field entry that someone typed in
     scope :non_fos, -> { where("subject_scheme IS NULL OR subject_scheme NOT IN ('fos', 'bad_fos')") }
     scope :fos, -> { where("subject_scheme = 'fos'") }
     scope :permissive_fos, -> { where("subject_scheme IN ('fos', 'bad_fos')") }
