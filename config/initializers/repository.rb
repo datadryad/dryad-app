6af9573a8839ddf3039cbe3e49faec349beb6503
Scott Fisher <scott.fisher@ucop.edu>
2022-06-24 11:15:09 -0700

    Remove the yaml config for storage repo since we've never used anything but Merritt in 6 years and changing config requires code changes anyway.
    Also mark Merritt state as 'processing' right away on submission (instead of waiting for it to enqueue and mark later) in order to prevent multiple submissions in the time between going to queue and starting.

--- config/initializers/repository.rb
+++ config/initializers/repository.rb
@@ -3,16 +3,12 @@ require 'stash/repo'
 module StashEngine
   def self.repository
     @repository ||= begin
-      repository_class_name = APP_CONFIG.repository
-      Rails.logger.debug("APP_CONFIG.repository = #{repository_class_name}")
-
-      repository_class = repository_class_name.constantize
       url_helpers = Rails.application.routes.url_helpers
 
-      Rails.logger.debug("Initializing new instance of repository #{repository_class}")
-      repository_instance = repository_class.new(url_helpers: url_helpers, threads: APP_CONFIG.merritt_max_submission_threads)
+      Rails.logger.debug("Initializing new instance of repository Stash::Merritt::Repository")
+      repository_instance = Stash::Merritt::Repository.new(url_helpers: url_helpers, threads: APP_CONFIG.merritt_max_submission_threads)
       unless repository_instance.respond_to?(:submit)
-        raise ArgumentError, "Repository #{repository_instance.class} does not appear to be a #{Stash::Repo::Repository}"
+        raise ArgumentError, "Repository Stash::Merritt::Repository does not appear to be a #{Stash::Repo::Repository}"
       end
 
       repository_instance
