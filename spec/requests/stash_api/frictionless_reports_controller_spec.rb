4023ccc82be8cf4c1e561bb03a299616248465da
Scott Fisher <scott.fisher@ucop.edu>
2022-08-23 09:25:14 -0700

    Tests for the frictionless report view and update and added code for update

--- spec/requests/stash_api/frictionless_reports_controller_spec.rb
+++ spec/requests/stash_api/frictionless_reports_controller_spec.rb
@@ -58,8 +58,63 @@ module StashApi
         expect(response_code).to eq(404)
         expect(response_body_hash).to eq({"error"=>"not-found"})
       end
-
     end
 
+    describe '#update' do
+      before(:each) do
+        @generic_file2 = create(:generic_file, resource: @resource)
+        @frict_report2 = create(:frictionless_report, generic_file: @generic_file2)
+        @path = Rails.application.routes.url_helpers.file_frictionless_report_path(@generic_file.id)
+      end
+
+      it "adds a report that doesn't exist yet" do
+        response_code = put @path,
+                            params: {status: 'noissues', report: @frict_report2.report}.to_json, # same as report2 json
+                            headers: default_authenticated_headers
+        expect(response_code).to eq(200)
+        hsh = response_body_hash
+        expect(hsh['status']).to eq('noissues')
+        expect(hsh['report']).to eq(@frict_report2.report)
+      end
+
+      it "updates a report that does exist" do
+        @frict_report = create(:frictionless_report, generic_file: @generic_file)
+        report_id = @frict_report.id
+        response_code = put @path,
+                            params: {status: 'noissues', report: @frict_report2.report}.to_json, # same as report2 json
+                            headers: default_authenticated_headers
+        expect(response_code).to eq(200)
+        hsh = response_body_hash
+        expect(hsh['status']).to eq('noissues')
+        expect(hsh['report']).to eq(@frict_report2.report)
+        expect(StashEngine::FrictionlessReport.find(report_id).report).to eq(@frict_report2.report) # the id is the same
+      end
+
+      it "doesn't allow updating without the correct permissions" do
+        @user.update(role: 'user')
+        response_code = put @path,
+                            params: {status: 'noissues', report: @frict_report2.report}.to_json, # same as report2 json
+                            headers: default_authenticated_headers
+        expect(response_code).to eq(401)
+        hsh = response_body_hash
+        expect(hsh['error']).to eq('unauthorized')
+      end
+
+      it "doesn't allow updating unless logged in" do
+        response_code = put @path,
+                            params: {status: 'noissues', report: @frict_report2.report}.to_json, # same as report2 json
+                            headers: default_json_headers
+        expect(response_code).to eq(401)
+      end
+
+      it "doesn't allow updating unless a status is correct from the list" do
+        response_code = put @path,
+                            params: {status: 'squid_cats', report: @frict_report2.report}.to_json,
+                            headers: default_authenticated_headers
+        expect(response_code).to eq(400)
+        hsh = response_body_hash
+        expect(hsh['error']).to eq('incorrect status set')
+      end
+    end
   end
 end
