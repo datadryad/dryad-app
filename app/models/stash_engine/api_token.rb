41d2009ca655be85d1d2e2d96db6869232584877
Scott Fisher <scott.fisher@ucop.edu>
2022-08-24 13:46:19 -0700

    Fixes to retries and adding testing

--- app/models/stash_engine/api_token.rb
+++ app/models/stash_engine/api_token.rb
@@ -1,6 +1,8 @@
 require 'http'
 
 module StashEngine
+  class ApiTokenStatusError < StandardError; end
+
   # A class for allowing access through tokens for our own API
   # Why do this?  We can pass in the token to sn AWS Lambda and then the lambda can access our API
   # It's easier if we make lambda's mostly stateless and limit the amount of code they contain rather making them "fat"
@@ -37,14 +39,11 @@ module StashEngine
                                client_secret: secret,
                                grant_type: 'client_credentials' })
 
-      if resp.status > 399
-        sleep 3
-        retry if (attempts += 1) < 10
-      end
+      raise StashEngine::ApiTokenStatusError, "Received #{resp.status} code from API" if resp.status > 399
 
       json = resp.parse
-      update(token: json['access_token'], expires_at: Time.new + json['expires_in'].seconds)
-    rescue HTTP::Error, HTTP::TimeoutError => e
+      update(token: json['access_token'], expires_at: (Time.new + json['expires_in'].seconds))
+    rescue HTTP::Error, HTTP::TimeoutError, StashEngine::ApiTokenStatusError => e
       logger.warn("Http error getting API key: #{e.full_message}")
       sleep 3
       retry if (attempts += 1) < 10
