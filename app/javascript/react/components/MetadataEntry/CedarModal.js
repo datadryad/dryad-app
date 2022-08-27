e9c728204b65e2463fb0db1b98da32a4cba229a9
Ryan Scherle <ryan@scherle.org>
2022-08-27 21:48:30 +0000

    simplify modal handling

--- app/javascript/react/components/MetadataEntry/CedarModal.js
+++ app/javascript/react/components/MetadataEntry/CedarModal.js
@@ -5,15 +5,7 @@ import ReactDom from "react-dom";
 export const CedarModal = ({ setShowModal, template, config }) => {
     console.log("CedarModal ssm");
    
-    // close the modal when clicking outside the modal.
     const modalRef = useRef();
-    const closeModal = (e) => {
-	console.log("CedarModal.closeModal");
-	if (e.target === modalRef.current) {
-	    console.log("CedarModal.closeModal true");
-	    setShowModal(false);
-	}
-    };
 
     function configCedar() {
 	console.log("Loading CEDAR config");
@@ -22,8 +14,9 @@ export const CedarModal = ({ setShowModal, template, config }) => {
     }
 
     function initCedar() {
-	console.log("CedarModal. init the modal for template", template);
-	
+	console.log("CedarModal.init the modal for template", template);
+
+	// Inject the cedar editor into the modal and open it
 	document.querySelector('#genericModalContent').classList.replace('c-modal-content__normal', 'c-modal-content__cedar');
 	$('#genericModalContent').html("<script src=\"" + config.table.editor_url + "\"></script>" +
 				       "<cedar-embeddable-editor />");
@@ -39,7 +32,6 @@ export const CedarModal = ({ setShowModal, template, config }) => {
 	initCedar();
     }
 
-    console.log('Finding form val', document.querySelector('#cedarTemplate'))
     // return null because the CEDAR editor fits in the #genericModalDialog, not
     // in the space on the page where this component lives
     return null;
