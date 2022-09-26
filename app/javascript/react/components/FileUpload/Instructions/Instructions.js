cc6ceb275ba072cb9ca9a47b43d2e9263b4268ce
Scott Fisher <scott.fisher@ucop.edu>
2022-09-26 10:10:09 -0700

    eslint fixes.

--- app/javascript/react/components/FileUpload/Instructions/Instructions.js
+++ app/javascript/react/components/FileUpload/Instructions/Instructions.js
@@ -8,18 +8,18 @@ const instructions = () => (
       using Google Drive.
     </p>
     <p>
-      We require that you include a <a href="/docs/README.md" target="_blank">README.md</a> file based on our template in
-      order to provide key
+      We require that you include a <a href="/docs/README.md" target="_blank" rel="noreferrer">README.md</a> file based
+      on our template in order to provide key
       information for understanding and reuse of your data.
     </p>
     <p>If you prefer, you can edit the Markdown online
-      at <a href="https://hackmd.io/JgxUwkEdS9uOOcgadhe29w?both=" target="_blank">hackmd.io</a>.
+      at <a href="https://hackmd.io/JgxUwkEdS9uOOcgadhe29w?both=" target="_blank" rel="noreferrer">hackmd.io</a>.
     </p>
 
     <ol>
       <li>Open the link (above)</li>
       <li>Copy and paste the text into a new note (create new by clicking the plus sign)</li>
-      <li>When you're done, save into a README.md and upload the file under the <em>Data</em> category</li>
+      <li>When you&apos;re done, save into a README.md and upload the file under the <em>Data</em> category</li>
     </ol>
 
     <p>
