3d54c08c911a2c96f27d7d5afd83227024eabd02
Audrey Hamelers <audrey@datadryad.org>
2022-10-06 16:17:41 +0200

    fix prop binding

--- app/javascript/react/components/MetadataEntry/KeywordAutocomplete.jsx
+++ app/javascript/react/components/MetadataEntry/KeywordAutocomplete.jsx
@@ -42,44 +42,31 @@ export default function KeywordAutocomplete({
      autocompletes for a generic case.
    */
 
-  function supplyLookupList(qt) {
-    return axios.get('/stash_datacite/subjects/autocomplete', {
-      params: {term: qt},
-      headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'},
-    })
-      .then((data) => {
-        if (data.status !== 200) {
-          return [];
-          // raise an error here if we want to catch it and display something to user or do something else
-        }
+  const supplyLookupList = (qt) => axios.get('/stash_datacite/subjects/autocomplete', {
+    params: {term: qt},
+    headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'},
+  })
+    .then((data) => {
+      if (data.status !== 200) {
+        return [];
+        // raise an error here if we want to catch it and display something to user or do something else
+      }
 
-        const list = data.data.map((item) => {
-          // add one point if starts with the same string, sends to top
-          const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
-          return {...item, similarity};
-        });
-        list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
-        return list;
+      const list = data.data.map((item) => {
+        // add one point if starts with the same string, sends to top
+        const similarity = stringSimilarity.compareTwoStrings(item.name, qt) + (item.name.startsWith(qt) ? 1 : 0);
+        return {...item, similarity};
       });
-  }
+      list.sort((x, y) => ((x.similarity < y.similarity) ? 1 : -1));
+      return list;
+    });
 
   // Given a js object from list (supplyLookupList above) it returns the string name
-  function nameFunc(item) {
-    return (item?.name || '');
-  }
+  const nameFunc = (item) => (item?.name || '');
 
   // Given a js object from list (supplyLookupList above) it returns the unique identifier
-  function idFunc(item) {
-    return item.id;
-  }
+  const idFunc = (item) => item.id;
 
-  /* eslint-disable react/jsx-no-bind */
-  // I'm passing in functions for getting name, id and lookup list.  None require any state from this component and are static functions
-  // eslint hates it, but what's the point of higher order functions or passing functions to separate concerns if you can't use it?
-  // I don't think this is actually a problem and if it causes re-loading of functions, IDK what a good alternative is.
-  // The information I can find regarding why not to pass a function through props in react is conflicting and unclear and
-  // in fact some sources say to do it to avoid repeating components (like https://www.youtube.com/watch?v=yH5Z-lSeV9Y ).
-  // So IDK what the real guidance is for this and it seems to work fine.
   return (
     <>
       <GenericNameIdAutocomplete
