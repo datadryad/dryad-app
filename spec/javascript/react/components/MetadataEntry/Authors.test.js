b9c81653a4b8b0c2568a3771f28f5d03250527a9
Audrey Hamelers <audrey@datadryad.org>
2022-10-06 17:37:16 +0200

    adjust tests

--- spec/javascript/react/components/MetadataEntry/Authors.test.js
+++ spec/javascript/react/components/MetadataEntry/Authors.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {faker} from '@faker-js/faker';
-import Authors from "../../../../../app/javascript/react/components/MetadataEntry/Authors.js";
+import Authors from "../../../../../app/javascript/react/components/MetadataEntry/Authors";
 import axios from 'axios';
 
 jest.mock('axios');
--- spec/javascript/react/components/MetadataEntry/Funders.test.js
+++ spec/javascript/react/components/MetadataEntry/Funders.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {faker} from '@faker-js/faker';
-import Funders from "../../../../../app/javascript/react/components/MetadataEntry/Funders.js";
+import Funders from "../../../../../app/javascript/react/components/MetadataEntry/Funders";
 import axios from 'axios';
 
 jest.mock('axios');
