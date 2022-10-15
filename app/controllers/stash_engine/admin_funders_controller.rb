175e5a9cdbcb1b4bd1eb7729c1279b16c393901b
Scott Fisher <scott.fisher@ucop.edu>
2022-10-14 17:32:55 -0700

    Beginning of basic page layout

--- app/controllers/stash_engine/admin_funders_controller.rb
+++ app/controllers/stash_engine/admin_funders_controller.rb
@@ -1,4 +1,15 @@
-class StashEngine::AdminFundersController < ApplicationController
-  def index
+require_dependency 'stash_engine/application_controller'
+
+module StashEngine
+  class AdminFundersController < ApplicationController
+
+    include SharedSecurityController
+    helper SortableTableHelper
+
+    before_action :require_admin
+
+    def index
+    end
+
   end
 end
