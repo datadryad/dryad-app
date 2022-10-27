0df25cf7ea95c6a1e9a88e79b62bb2a4d16b3ead
Scott Fisher <scott.fisher@ucop.edu>
2022-10-27 13:57:06 -0700

    display table and fixing query that was subtly wrong on a join

--- app/controllers/stash_engine/admin_funders_controller.rb
+++ app/controllers/stash_engine/admin_funders_controller.rb
@@ -12,8 +12,6 @@ module StashEngine
       rep = Report.new
       rep.add_limit(offset: 0, rows: 100)
       @funder_table = rep.do_query
-      byebug
-      # :-(  it's not working right for dates
     end
 
   end
