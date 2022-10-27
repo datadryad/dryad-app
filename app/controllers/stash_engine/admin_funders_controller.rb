adb70f2255fbc8788c4b5b925b3833215ca70af2
Scott Fisher <scott.fisher@ucop.edu>
2022-10-27 14:47:03 -0700

    Fixing column names, get sorting working.

--- app/controllers/stash_engine/admin_funders_controller.rb
+++ app/controllers/stash_engine/admin_funders_controller.rb
@@ -2,15 +2,22 @@ require_dependency 'stash_engine/application_controller'
 
 module StashEngine
   class AdminFundersController < ApplicationController
-
     include SharedSecurityController
     helper SortableTableHelper
 
     before_action :require_admin
+    # before_action :setup_paging, only: [:index]
 
     def index
       rep = Report.new
       rep.add_limit(offset: 0, rows: 100)
+
+      # order the list only by the whitelist, which creates partial SQL and eliminates injection
+      ord = helpers.sortable_table_order(whitelist: %w[title authors identifier contributor_name award_number
+                init_sub_date publication_date1 publication_date2])
+      ord.gsub!(/publication_date\d/, 'publication_date')
+      rep.order(order_str: ord)
+
       @funder_table = rep.do_query
     end
 
