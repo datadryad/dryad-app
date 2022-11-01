04fea837b0bb11c39916bcc47bc696f422f59430
Scott Fisher <scott.fisher@ucop.edu>
2022-11-01 10:32:15 -0700

    Tests for filters on Dataset funder dashboard

--- spec/requests/stash_engine/admin_dataset_funders_controller_spec.rb
+++ spec/requests/stash_engine/admin_dataset_funders_controller_spec.rb
@@ -30,16 +30,29 @@ module StashEngine
         end
       end
 
-      it 'shows a submission date' do
-
+      it 'shows the submission date' do
+        @resources.first.resource_states.first.update(resource_state: 'submitted', updated_at: Time.new(2006, 6, 6, 12))
+        response_code = get '/stash/ds_admin_funders'
+        expect(response_code).to eq(200)
+        expect(body).to include('06/06/2006')
       end
 
       it 'shows an embargo date' do
-
+        res = @resources.first
+        res.identifier.update(pub_state: 'embargoed')
+        res.update(meta_view: true, publication_date: Time.new(2291, 10, 11, 12))
+        response_code = get '/stash/ds_admin_funders'
+        expect(response_code).to eq(200)
+        expect(body).to include('10/11/2291')
       end
 
       it 'shows a publication date' do
-
+        res = @resources.first
+        res.identifier.update(pub_state: 'published')
+        res.update(meta_view: true, file_view: true, publication_date: Time.new(2021, 3, 17, 12))
+        response_code = get '/stash/ds_admin_funders'
+        expect(response_code).to eq(200)
+        expect(body).to include('03/17/2021')
       end
     end
 
@@ -57,5 +70,55 @@ module StashEngine
       end
     end
 
+    describe 'limit report to funder' do
+
+      it 'limits to a single, simple funder (not NIH)' do
+        response_code = get '/stash/ds_admin_funders', params: { funder_name: @resources.first.contributors.first.contributor_name }
+        expect(response_code).to eq(200)
+        expect(body).to include(@resources.first.title)
+        expect(body).not_to include(@resources.second.title)
+      end
+
+      it 'limits to a compound funder with sub-units' do
+        create(:contributor_grouping) # sets up example NIH sub-units
+        @resources.first.contributors.first.update(contributor_name: "National Institute on Minority Health and Health Disparities",
+                                                   name_identifier_id: "http://dx.doi.org/10.13039/100006545")
+        @resources.second.contributors.first.update(contributor_name: "Eunice Kennedy Shriver National Institute of Child Health and Human Development",
+                                                   name_identifier_id: "http://dx.doi.org/10.13039/100009633")
+
+        cg = StashDatacite::ContributorGrouping.first
+        response_code = get '/stash/ds_admin_funders', params: { funder_name: cg.contributor_name, funder_id: cg.name_identifier_id }
+        expect(response_code).to eq(200)
+
+        expect(body).to include(@resources[0].title)
+        expect(body).to include(@resources[1].title)
+        expect(body).not_to include(@resources[2].title)
+      end
+    end
+
+    describe 'limit to dates' do
+      it 'limits to an initial submission date' do
+        @resources.first.resource_states.first.update(resource_state: 'submitted', updated_at: Time.new(2011, 11, 11, 12))
+        response_code = get '/stash/ds_admin_funders', params: { date_type: 'initial',
+                                                                 start_date: '2011-11-01',
+                                                                 end_date: '2011-12-01' }
+        expect(response_code).to eq(200)
+        expect(body).to include('11/11/2011')
+        expect(body).to include(@resources[0].title)
+        expect(body).not_to include(@resources[2].title)
+      end
+
+      it 'limits to an embargo/publication date' do
+        res = @resources.first
+        res.identifier.update(pub_state: 'published')
+        res.update(meta_view: true, file_view: true, publication_date: Time.new(2016, 8, 22, 12))
+        response_code = get '/stash/ds_admin_funders', params: { date_type: 'published',
+                                                                 start_date: '2016-08-01',
+                                                                 end_date: '2016-09-01' }
+        expect(body).to include('08/22/2016')
+        expect(body).to include(@resources[0].title)
+        expect(body).not_to include(@resources[1].title)
+      end
+    end
   end
 end
