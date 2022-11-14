23f0bab174ae1019eb6a49eb1f73334c8eecac24
Audrey Hamelers <audrey@datadryad.org>
2022-11-14 16:33:52 +0100

    new rails 6.1 config defaults, form_with change

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,9 @@ Bundler.require(*Rails.groups)
 
 module Dash2
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # Initialize configuration defaults for the Rails version.
+    config.load_defaults 6.1
+
     config.eager_load_paths << Rails.root.join("lib")
     config.autoload_paths << Rails.root.join("lib")
     Rails.autoloaders.main.ignore("#{Rails.root}/app/overrides")
