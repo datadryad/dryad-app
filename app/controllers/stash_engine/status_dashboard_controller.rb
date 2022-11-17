620bfc51f904c631b1dbceaef9de7af4d8d28f61
Scott Fisher <scott.fisher@ucop.edu>
2022-11-17 15:01:08 -0800

    Adding merritt submission status to status dashboard in database

--- app/controllers/stash_engine/status_dashboard_controller.rb
+++ app/controllers/stash_engine/status_dashboard_controller.rb
@@ -6,6 +6,11 @@ module StashEngine
 
     include SharedController
 
+    # note that this is a bit confusing since it service checks tied in from various strange places.
+
+    # An item must have something entered into the stash_engine_external_dependencies to run checks.  It also uses the
+    # similar model ExternalDependency in StashEngine.  It also has specific checks for the dependency in the directory
+    # app/services/stash_engine/status_dashboard/<service-name>.rb that does the actual check and sets a value for it.
     def show
       @main_documentation = 'https://confluence.ucop.edu/display/Stash/Dryad+Operations'
       @managed_dependencies = StashEngine::ExternalDependency.where(internally_managed: true).order(:name)
