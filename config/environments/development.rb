47e569e9f72da0184545e4a19b0cf1e6c9fc9612
Audrey Hamelers <audrey@datadryad.org>
2022-11-17 18:40:58 +0100

    add hosts to environment configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << 'dryad-dev.cdlib.org'
   Rails.application.default_url_options = { host: 'dryad-dev.cdlib.org' }
 
 end
