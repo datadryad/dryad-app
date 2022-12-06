d35c2bf3d4ea2f358612e1c20329618c14e06f72
Scott Fisher <scott.fisher@ucop.edu>
2022-12-05 17:26:02 -0800

    fixing problems with linkout service

--- lib/stash/link_out/labslink_service.rb
+++ lib/stash/link_out/labslink_service.rb
@@ -57,8 +57,8 @@ module Stash
             locals: {
               id: @ftp.ftp_provider_id,
               name: 'Dryad Data Platform',
-              description: 'Dryad is a nonprofit organization and an international repository ' +
-                           'of data underlying scientific and medical publications.',
+              description:
+                'Dryad is a nonprofit organization and an international repository of data underlying scientific and medical publications.',
               email: 'linkout@datadryad.org'
             }
           ), nil, 'UTF-8')
