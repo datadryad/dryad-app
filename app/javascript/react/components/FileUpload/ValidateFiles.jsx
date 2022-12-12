774044ddf68241bae41c4cc8282a09244d351d59
Audrey Hamelers <audrey@datadryad.org>
2022-12-12 18:29:19 +0100

    Closes #2163

--- app/javascript/react/components/FileUpload/ValidateFiles.jsx
+++ app/javascript/react/components/FileUpload/ValidateFiles.jsx
@@ -8,13 +8,18 @@ const validate_files = (props) => {
         <input id={props.id} type="checkbox" name="confirm_to_upload" onChange={props.changed} checked={!props.disabled} />
         <strong style={{color: 'red'}}> *</strong>
         <label htmlFor={props.id}>
-          I confirm that no Personal Health Information or Sensitive Data are being uploaded with this submission.
+          I confirm that no personal health information or sensitive data are being uploaded with this submission.
         </label>
       </div>
     );
   }
   return (
     <div>
+      <p>Data archived in Dryad are publicly available, and any human subjects data or species data must be
+        properly anonymized and prepared under applicable legal and ethical guidelines. Please see our
+      {' '}<a href="/docs/HumanSubjectsData.pdf">human subjects guidance</a> and
+      {' '}<a href="/docs/EndangeredSpeciesData.pdf">species conservation guidance</a>.
+      </p>
       {checkConfirm}
       <input
         type="submit"
