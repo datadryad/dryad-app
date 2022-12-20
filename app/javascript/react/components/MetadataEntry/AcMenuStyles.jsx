8a19ba0df80bf1b11f43521ae76da1b48e00d23c
Audrey Hamelers <audrey@datadryad.org>
2022-12-20 16:14:57 +0100

    additional dropdown for more funder information

--- app/javascript/react/components/MetadataEntry/AcMenuStyles.jsx
+++ app/javascript/react/components/MetadataEntry/AcMenuStyles.jsx
@@ -1,11 +1,12 @@
 export default {
   maxHeight: '180px',
   overflowY: 'auto',
-  maxWidth: '300px',
+  width: 'calc(100% - 16px)',
   minWidth: '200px',
   backgroundColor: '#fafafa',
   position: 'absolute',
   top: '1em',
+  right: '16px',
   zIndex: 1000,
   padding: 0,
   listStyle: 'none',
