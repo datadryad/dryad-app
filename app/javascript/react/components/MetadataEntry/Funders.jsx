edd11e0880fcd1e3e776cb698f96b3ce72e28d6c
Audrey Hamelers <audrey@datadryad.org>
2023-01-12 17:42:28 +0100

    improve movement of no funding to checkbox

--- app/javascript/react/components/MetadataEntry/Funders.jsx
+++ app/javascript/react/components/MetadataEntry/Funders.jsx
@@ -7,27 +7,21 @@ import {showSavedMsg, showSavingMsg} from '../../../lib/utils';
 function Funders({
   resourceId, contributors, createPath, updatePath, deletePath, groupings,
 }) {
-  const csrf = document.querySelector("meta[name='csrf-token']")?.getAttribute('content');
-
-  const blankContrib = {
-    contributor_name: '',
-    contributor_type: 'funder',
-    identifier_type: 'crossref_funder_id',
-    name_identifier_id: '',
-    resource_id: resourceId,
-  };
-
-  const noContrib = {...blankContrib, name_identifier_id: '0'};
+  const authenticity_token = document.querySelector("meta[name='csrf-token']")?.getAttribute('content');
 
   const [funders, setFunders] = useState(contributors);
   const [disabled, setDisabled] = useState(contributors[0]?.name_identifier_id === '0');
 
-  const addNewFunder = (contributor) => {
+  const addNewFunder = () => {
     console.log(`${(new Date()).toISOString()}: Adding funder`);
-    const contribJson = {
-      authenticity_token: csrf,
-      contributor,
+    const contributor = {
+      contributor_name: '',
+      contributor_type: 'funder',
+      identifier_type: 'crossref_funder_id',
+      name_identifier_id: '',
+      resource_id: resourceId,
     };
+    const contribJson = {authenticity_token, contributor};
 
     axios.post(createPath, contribJson, {headers: {'Content-Type': 'application/json; charset=utf-8', Accept: 'application/json'}})
       .then((data) => {
@@ -38,9 +32,7 @@ function Funders({
       });
   };
 
-  if (funders.length < 1) {
-    addNewFunder(blankContrib);
-  }
+  if (funders.length < 1) addNewFunder();
 
   // delete a funder from the list
   const removeItem = (id) => {
@@ -51,7 +43,7 @@ function Funders({
     // requiring the resource like this is weird in a controller for a model that isn't a resource, but it's how it is set up
     if (id && !`${id}`.startsWith('new')) {
       const submitVals = {
-        authenticity_token: csrf,
+        authenticity_token,
         contributor: {
           id,
           resource_id: resourceId,
@@ -79,16 +71,28 @@ function Funders({
     setFunders((prevState) => prevState.map((funder) => (updatedContributor.id === funder.id ? updatedContributor : funder)));
   };
 
-  const setNoFunders = (e) => {
-    setDisabled(e.currentTarget.checked);
-    if (e.currentTarget.checked) {
-      contributors.forEach((c) => { if (c.name_identifier_id !== '0') removeItem(c.id); });
-      if (!contributors.some((c) => c.name_identifier_id === '0')) {
-        addNewFunder(noContrib);
+  const setNoFunders = async (e) => {
+    const noFunders = e.currentTarget.checked;
+    const [contributor] = contributors;
+    setDisabled(noFunders);
+    contributor.name_identifier_id = noFunders ? '0' : '';
+    // submit by json
+    return axios.patch(
+      updatePath,
+      {authenticity_token, contributor},
+      {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Accept: 'application/json',
+        },
+      },
+    ).then((data) => {
+      if (data.status !== 200) {
+        console.log('Response failure not a 200 response from funders save');
       }
-    } else {
-      contributors.forEach((c) => removeItem(c.id));
-    }
+      // forces data update in the collection containing me
+      updateFunder(data.data);
+    });
   };
 
   return (
@@ -111,7 +115,7 @@ function Funders({
             className="t-describe__add-funder-button o-button__add"
             type="button"
             disabled={disabled}
-            onClick={() => addNewFunder(blankContrib)}
+            onClick={addNewFunder}
             style={{marginRight: '2em'}}
           >
           Add another funder
