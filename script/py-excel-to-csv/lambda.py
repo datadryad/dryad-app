42744da492578302d1a72a856cb756a0162d19fb
Scott Fisher <scott.fisher@ucop.edu>
2023-01-12 15:02:22 -0800

    Very much in progress, but I need to switch to another branch.

--- script/py-excel-to-csv/lambda.py
+++ script/py-excel-to-csv/lambda.py
@@ -2,23 +2,42 @@ import json
 import pandas as pd
 import requests
 
-# event json has these params passed in: download_url, filename, callback_url, token
+# event json has these params passed in: download_url, filename, callback_url, token, processor_obj
 # the filename is tricky to parse out of http request (sometimes in content-disposition and sometimes not), so just pass it from our DB
 def lambda_handler(event, context):
-  # TODO implement
+  update_processor_results(event['processor_obj'], event['callback_url'], event['token'])
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lambda!')
+    'body': event
   }
 
+def update_processor_results(processor_obj, callback_url, token):
+  headers = {'Authorization': f'Bearer {token}'}
+  update = processor_obj.copy()
+  # the following setting are just for testing
+  update['completion_state'] = 'processing'
+  update['message'] = 'This is a new message'
+  update['structured_info'] = processor_obj
+
+  for i in range(0,5):
+    r = requests.put(callback_url, headers=headers, json=update)
+    if r.status_code < 400:
+      break
+    else:
+      time.sleep(5)
+
+  return r.status_code
+
+
+
 class ExcelToCsv:
-  def __init__(self, download_url, filename, callback_url, token):
+  def __init__(self, download_url, filename, callback_url, token, processor_object):
     self.download_url = download_url
     self.filename = filename
     self.callback_url = callback_url
     self.token = token
 
-  def download_excel(url):
+  def download_excel():
     r = requests.get(url, allow_redirects=True)
 
   # tries to upload it to our API, TODO: needs to be fixed
