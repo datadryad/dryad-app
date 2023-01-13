54ec82d668c99cf08d867f436a52b0d068bbaf9e
Scott Fisher <scott.fisher@ucop.edu>
2023-01-13 15:07:40 -0800

    Update lambda code to download, extract, log things, hit our api

--- script/py-excel-to-csv/lambda.py
+++ script/py-excel-to-csv/lambda.py
@@ -1,7 +1,10 @@
 import json
 import time
-import pandas as pd
+import pandas
 import requests
+from pathlib import Path
+from datetime import datetime
+import re
 
 # event json has these params passed in: download_url, filename, callback_url, token, processor_obj
 # the filename is tricky to parse out of http request (sometimes in content-disposition and sometimes not), so just pass it from our DB
@@ -11,6 +14,14 @@ def lambda_handler(event, context):
 
   e2c = ExcelToCsv(event['download_url'], event['filename'], event['callback_url'], event['token'], event['processor_obj'])
   e2c.update_processor_start()
+
+  mypath = e2c.download_excel()
+  if(mypath is None): return
+
+  new_files = e2c.extract_worksheets(mypath)
+  if(new_files is None): return
+
+  e2c.update_processor_end()
 
   return {
     'statusCode': 200,
@@ -32,36 +43,70 @@ def update_processor_results(processor_obj, callback_url, token):
 
   return r.status_code
 
-
+def fix_fn(fn):
+  return re.sub(r'[^-a-zA-Z0-9_.()]+', '_', fn)
 
 class ExcelToCsv:
   def __init__(self, download_url, filename, callback_url, token, processor_obj):
     self.download_url = download_url
-    self.filename = filename
+    self.filename = fix_fn(filename)
     self.callback_url = callback_url
     self.token = token
     self.processor_obj = processor_obj
 
-  def update_processor_start(self):
+  def update_processor(self, state, message, struct_info):
     update = self.processor_obj.copy()
-    update['completion_state'] = 'processing'
-    update['message'] = 'This is a new message'
-    update['structured_info'] = json.dumps(update, indent = 2)
+    update['completion_state'] = state
+    update['message'] = message
+    update['structured_info'] = struct_info
     update_processor_results(update, self.callback_url, self.token)
 
+  # set processor message as started
+  def update_processor_start(self):
+    self.update_processor('processing', f'started lambda at {datetime.now().isoformat()}', '')
+    print(f'Started processing at {datetime.now().isoformat()}')
 
-#   def download_excel():
-#     r = requests.get(url, allow_redirects=True)
-#
-#   # tries to upload it to our API, TODO: needs to be fixed
-#   def update_status(token, status, report, callback):
-#     headers = {'Authorization': f'Bearer {token}'}
-#     update = { 'status': status, 'report': report }
-#     for i in range(0,5):
-#       r = requests.put(callback, headers=headers, json=update)
-#       if r.status_code < 400:
-#         break
-#       else:
-#         time.sleep(5)
-#
-#    return r.status_code
+  def update_processor_end(self):
+    self.update_processor('success', f'ended processing lambda at {datetime.now().isoformat()}', '')
+    print(f'Finished processing at {datetime.now().isoformat()}')
+
+  def update_processor_error(self, message):
+    self.update_processor('error', f'error lambda at {datetime.now().isoformat()} with message: {message}', '')
+    print(f'Finished processing at {datetime.now().isoformat()}')
+
+  # attempt downloading the Excel file as 'excelfile.{ext}'
+  def download_excel(self):
+    # ext = Path(self.filename).suffix
+    for i in range(0,5):
+      r = requests.get(self.download_url, allow_redirects=True)
+      if r.status_code < 400:
+        open(f'/tmp/{self.filename}', 'wb').write(r.content)
+        print(f'saved to /tmp/{self.filename}')
+        return f'/tmp/{self.filename}'
+      else:
+        print(f'Error from server: code {r.status_code} downloading {self.filename}, try {i}')
+        time.sleep(5)
+
+      update = self.processor_obj.copy()
+      update['completion_state'] = 'error'
+      update['message'] = f'unable to download {self.filename} at {self.download_url} after 5 attempts'
+      update_processor_results(update, self.callback_url, self.token)
+      return None
+
+  def extract_worksheets(self, mypath):
+    try:
+      data_frames = pandas.read_excel(mypath, sheet_name=None)
+    except ValueError as inst:
+      print(f'File {mypath} could not be parsed as an Excel file.  Is the format correct?')
+      self.update_processor_error(f'File {mypath} could not be parsed as an Excel file.  Is the format correct?')
+      return None
+
+    new_files = []
+    for x in data_frames.keys():
+      new_fn = f'{Path(mypath).stem}_{fix_fn(x)}.csv'
+      data_frames[x].to_csv(f'/tmp/{new_fn}', index=None, header=True)
+      print(f'    Created new csv file {new_fn}')
+      new_files.append(new_fn)
+
+    return new_files
+
