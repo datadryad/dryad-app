acf53812907ab67242af9986700b7ffb7e10a695
Scott Fisher <scott.fisher@ucop.edu>
2023-01-18 13:04:00 -0800

    Make a lambda work with trigger and output result in json

--- script/py-excel-to-csv/lambda.py
+++ script/py-excel-to-csv/lambda.py
@@ -12,6 +12,10 @@ from urllib.parse import urlparse, quote
 def lambda_handler(event, context):
   # update_processor_results(event['processor_obj'], event['callback_url'], event['token'])
   # TODO: validation of correct events passed in
+  set1 = set(event.keys())
+  if len(set1.intersection({'download_url', 'callback_url', 'processor_obj', 'doi', 'token', 'filename'})) != 6:
+    print('Incorrect elements passed into lambda for ExcelToCsv')
+    return
 
   e2c = ExcelToCsv(event['download_url'], event['filename'], event['callback_url'], event['token'], event['doi'], event['processor_obj'])
   e2c.update_processor_start()
@@ -24,17 +28,9 @@ def lambda_handler(event, context):
 
   e2c.upload_new_files(new_files)
 
-  e2c.update_processor_end()
-
-  return {
-    'statusCode': 200,
-    'body': event
-  }
-
+# standalone methods not part of the ExcelToCsv object
 def update_processor_results(processor_obj, callback_url, token):
   headers = {'Authorization': f'Bearer {token}'}
-  # print(processor_obj)
-  # print (processor_obj.__class__.__name__)
 
   for i in range(0,5):
     r = requests.put(callback_url, headers=headers, json=processor_obj)
@@ -58,6 +54,7 @@ class ExcelToCsv:
     self.doi = doi
     self.processor_obj = processor_obj
 
+  # updates the processor results in our API, convenience method
   def update_processor(self, state, message, struct_info):
     update = self.processor_obj.copy()
     update['completion_state'] = state
@@ -65,20 +62,23 @@ class ExcelToCsv:
     update['structured_info'] = struct_info
     update_processor_results(update, self.callback_url, self.token)
 
-  # set processor message as started
+  # set processor message as started in our API
   def update_processor_start(self):
     self.update_processor('processing', f'started lambda at {datetime.now().isoformat()}', '')
     print(f'Started processing at {datetime.now().isoformat()}')
 
-  def update_processor_end(self):
-    self.update_processor('success', f'ended processing lambda at {datetime.now().isoformat()}', '')
+  # set processor as completed successfully in our API
+  def update_processor_success(self, filenames):
+    json_out = json.dumps({ "csv_files": filenames })
+    self.update_processor('success', f'ended processing lambda at {datetime.now().isoformat()}', json_out)
     print(f'Finished processing at {datetime.now().isoformat()}')
 
+  # set a processor error in our API
   def update_processor_error(self, message):
     self.update_processor('error', f'error lambda at {datetime.now().isoformat()} with message: {message}', '')
     print(f'Finished processing at {datetime.now().isoformat()}')
 
-  # attempt downloading the Excel file as 'excelfile.{ext}'
+  # attempt downloading the Excel file
   def download_excel(self):
     # ext = Path(self.filename).suffix
     for i in range(0,5):
@@ -114,7 +114,6 @@ class ExcelToCsv:
 
     return new_files
 
-  # TODO: extra logging and sending information back, but this works!
   def upload_new_files(self, new_files):
     domain = urlparse(self.callback_url).netloc
     headers = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'text/csv'}
@@ -127,8 +126,14 @@ class ExcelToCsv:
           break
         else:
           print(f'Error from server while uploading {new_file}: code {r.status_code}, try {i}')
+          if i == 4:
+            print(f'File {new_file} could not be uploaded to the Dryad API with status code: {r.status_code}')
+            self.update_processor_error(f'File {new_file} could not be uploaded to the Dryad API with status code: {r.status_code}')
+            return None
           time.sleep(5)
 
+    # got through all the uploads of CSVs to our API, now write success
+    self.update_processor_success(new_files)
     return
 
 
