19f155e5dcfee4a16e12394c301da3abde474512
Scott Fisher <scott.fisher@ucop.edu>
2023-01-18 17:07:55 -0800

    Final touches for excel process triggering

--- app/models/stash_engine/generic_file.rb
+++ app/models/stash_engine/generic_file.rb
@@ -199,6 +199,8 @@ module StashEngine
 
       h = Rails.application.routes.url_helpers
 
+      # Don't create multiple entries for all the processing steps, just overwrite this one (will save last step).
+      # We can move to a more full log of every step in the future if we need it.
       pr = ProcessorResult.where(resource_id: resource_id, parent_id: id)&.first ||
         ProcessorResult.create(resource: resource, processing_type: 'excel_to_csv', parent_id: id, completion_state: 'not_started')
 
