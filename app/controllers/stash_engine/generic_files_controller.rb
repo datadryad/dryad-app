b316aac802adce33bc2e0296be06bdec7f2ca263
Scott Fisher <scott.fisher@ucop.edu>
2023-01-23 17:45:26 -0800

    Another change if the SQL query doesn't return case insensitive

--- app/controllers/stash_engine/generic_files_controller.rb
+++ app/controllers/stash_engine/generic_files_controller.rb
@@ -61,7 +61,8 @@ module StashEngine
       respond_to do |format|
         format.any(:json, :html) do
           # It can maintain previous deletions for the file (both same and different case), but delete non-deletions
-          @resource.send(@resource_assoc).where(upload_file_name: params[:name]).where.not(file_state: 'deleted').destroy_all
+          @resource.send(@resource_assoc).where('lower(upload_file_name) = ?', params[:name]&.downcase)
+                   .where.not(file_state: 'deleted').destroy_all
 
           db_file =
             @file_model.create(
