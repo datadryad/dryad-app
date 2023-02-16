c7bfe89f80369e4a3e8955e06b7c7be3811c8a2e
Audrey Hamelers <audrey@datadryad.org>
2023-02-16 18:37:40 +0100

    redesign all tables

--- app/assets/javascripts/stash_engine/ui.js
+++ app/assets/javascripts/stash_engine/ui.js
@@ -1480,24 +1480,6 @@ function joelsReady(){
     });
   }
 
-  // ***** Toggle Table Cell Details ***** //
-
-  $('.js-table-heading__button-hide').hide();
-
-  $('.js-table-heading__button-show').click(function(){
-    $(this).hide();
-    $(this).siblings('.js-table-heading__button-hide').show();
-    $(this).parents('.c-table-heading').next().find('.js-table-progress').toggleClass('c-table-progress c-table-progress--details');
-    $(this).parents('.c-table-heading').next().find('.js-table-submitted').toggleClass('c-table-submitted c-table-submitted--details');
-  });
-
-  $('.js-table-heading__button-hide').click(function(){
-    $(this).hide();
-    $(this).siblings('.js-table-heading__button-show').show();
-    $(this).parents('.c-table-heading').next().find('.js-table-progress').toggleClass('c-table-progress--details c-table-progress');
-    $(this).parents('.c-table-heading').next().find('.js-table-submitted').toggleClass('c-table-submitted--details c-table-submitted');
-  });
-
   // ***** Select Content Object ***** //
 
   $('.o-select__select').change(function(){
