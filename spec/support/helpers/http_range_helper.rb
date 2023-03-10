9fea5d0aff33f5d937911ff8f3ba814c6b5f2ede
Scott Fisher <scott.fisher@ucop.edu>
2023-03-09 17:46:36 -0800

    rubocop

--- spec/support/helpers/http_range_helper.rb
+++ spec/support/helpers/http_range_helper.rb
@@ -1,8 +1,8 @@
 # a helper to do webmock range requests without being so verbose and annoying in tests
 module HttpRangeHelper
   def stub_range_request(url:, r_start:, r_end:, body:, file_size:)
-    stub_request(:get, url).
-      with(headers: { 'Range' => "bytes=#{r_start}-#{r_end}" }).
-      to_return(status: 206, body: body, headers: { 'Content-Range' => "bytes #{r_start}-#{r_end}/#{file_size}" })
+    stub_request(:get, url)
+      .with(headers: { 'Range' => "bytes=#{r_start}-#{r_end}" })
+      .to_return(status: 206, body: body, headers: { 'Content-Range' => "bytes #{r_start}-#{r_end}/#{file_size}" })
   end
 end
