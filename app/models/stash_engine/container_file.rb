5a6cc5d08d0c0c07371683c282c21b735fad914c
Scott Fisher <scott.fisher@ucop.edu>
2023-03-23 15:06:36 -0700

    Found correct settings in order to manually get items to duplicate without taking 20 minutes for 12,000 files.  Also deletes more quickly.

--- app/models/stash_engine/container_file.rb
+++ app/models/stash_engine/container_file.rb
@@ -2,10 +2,6 @@ module StashEngine
   class ContainerFile < ApplicationRecord
     self.table_name = 'stash_engine_container_files'
     belongs_to :data_file, class_name: 'StashEngine::DataFile'
-
-    amoeba do
-      enable
-    end
   end
 end
 
