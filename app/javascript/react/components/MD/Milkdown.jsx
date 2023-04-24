104d592fdc78798a0be79e6b3ae642201e203032
Scott Fisher <scott.fisher@ucop.edu>
2023-04-24 14:00:20 -0700

    A few working examples and some that don't work.

---
+++ app/javascript/react/components/MD/Milkdown.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Editor, rootCtx } from '@milkdown/core';
+import { nord } from '@milkdown/theme-nord';
+import { Milkdown, MilkdownProvider, useEditor } from '@milkdown/react';
+import { commonmark } from '@milkdown/preset-commonmark';
+
+const MilkdownEditor: React.FC = () => {
+  const { editor } = useEditor((root) =>
+      Editor.make()
+          .config(nord)
+          .config((ctx) => {
+            ctx.set(rootCtx, root);
+          })
+          .use(commonmark),
+  );
+
+  return <Milkdown />;
+};
+
+export const MilkdownEditorWrapper: React.FC = () => {
+  return (
+      <MilkdownProvider>
+        <MilkdownEditor />
+      </MilkdownProvider>
+  );
+};
---
+++ app/javascript/react/components/MD/Toast.jsx
@@ -0,0 +1,27 @@
+import React, {useRef, useEffect} from 'react';
+
+import Editor from "@toast-ui/editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import classes from './Toast.module.css';
+
+export default function Toast(){
+  const editorRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const editor = new Editor({
+      el: editorRef.current,
+      initialEditType: "wysiwyg",
+      previewStyle: "vertical",
+      height: "500px",
+      initialValue: "# Hello World",
+    });
+  }, []);
+
+  return (
+      <>
+        <div>
+          <div ref={editorRef} />
+        </div>
+      </>
+  );
+}
