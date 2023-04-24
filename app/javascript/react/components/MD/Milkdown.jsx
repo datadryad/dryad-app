471c288e532e2c23b10328dcb6aaab296be81668
Scott Fisher <scott.fisher@ucop.edu>
2023-04-24 14:26:16 -0700

    Annoying trying to get it to ignore these files.

--- app/javascript/react/components/MD/Milkdown.jsx
+++ app/javascript/react/components/MD/Milkdown.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { Editor, rootCtx } from '@milkdown/core';
 import { nord } from '@milkdown/theme-nord';
--- app/javascript/react/components/MD/Toast.jsx
+++ app/javascript/react/components/MD/Toast.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, {useRef, useEffect} from 'react';
 
 import Editor from "@toast-ui/editor";
