2c947e523c282f787a05a486e1f365e944c981bb
Scott Fisher <scott.fisher@ucop.edu>
2023-06-15 16:26:31 -0700

    Interim commit with lots of issues and byebugs I'll need to remove later.

--- stash/stash-merritt/lib/stash/merritt/submission_package.rb
+++ stash/stash-merritt/lib/stash/merritt/submission_package.rb
@@ -27,6 +27,7 @@ module Stash
       end
 
       def dc4_builder
+        byebug
         @dc4_builder ||= begin
           datacite_xml_factory = Datacite::Mapping::DataciteXMLFactory.new(
             doi_value: resource.identifier_value,
