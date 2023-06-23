574e3aea74fd98582eba2687775a3cde06b958f0
Ryan Scherle <ryan@scherle.org>
2023-06-23 21:44:33 +0000

    add an index of reports and some documentation

--- config/routes.rb
+++ config/routes.rb
@@ -88,6 +88,7 @@ Rails.application.routes.draw do
     get '/', to: 'api#index'
     match '/test', to: 'api#test', via: %i[get post]
     match '/search', to: 'datasets#search', via: %i[get]
+    get '/reports', to: 'api#reports_index'
     get '/reports(/:report_name)', to: 'api#reports'
     
     # Support for the Editorial Manager API
