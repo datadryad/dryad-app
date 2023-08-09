44bef6277e24a0e8a5bb8f245b12244fa3630c8f
Audrey Hamelers <audrey@datadryad.org>
2023-08-09 21:00:39 +0200

    preload template or previously uploaded file content

--- app/javascript/react/components/ReadMe.jsx
+++ app/javascript/react/components/ReadMe.jsx
@@ -1,18 +1,26 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 import {Editor} from '@toast-ui/react-editor';
 import subsubPlugin from '../../lib/subsup_plugin';
 
 import '@toast-ui/editor/dist/toastui-editor.css';
+import '../../lib/toastui-editor.css';
 
-export default function ReadMe() {
+export default function ReadMe({readmeUrl}) {
   const editorRef = useRef();
+
+  useEffect(async () => {
+    if (readmeUrl) {
+      const response = await fetch(readmeUrl);
+      const value = await response.text();
+      editorRef.current.getInstance().setMarkdown(value);
+    }
+  }, [readmeUrl]);
 
   return (
     <Editor
       ref={editorRef}
       initialEditType="wysiwyg"
-      height="700px"
-      initialValue="# Hello World"
+      height="800px"
       toolbarItems={[
         ['heading', 'bold', 'italic', 'strike'],
         ['hr', 'quote', 'link'],
