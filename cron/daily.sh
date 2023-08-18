6b56cf616eeaa23ac3bb0ce96223d6449fb680ec
Scott Fisher <scott.fisher@ucop.edu>
2023-08-18 13:43:48 -0700

    Add to cron

--- cron/daily.sh
+++ cron/daily.sh
@@ -11,6 +11,7 @@ bundle exec rails identifiers:peer_review_reminder RAILS_ENV=$1 >> /apps/dryad/a
 bundle exec rails identifiers:doi_linking_invitation RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/doi_linking_invitation.log 2>&1
 bundle exec rails identifiers:in_progess_reminder RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/in_progess_reminder.log 2>&1
 bundle exec rails identifiers:update_missing_search_words RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/update_search_words.log 2>&1
+bundle exec rails identifiers:identifiers:action_required_reminder RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/action_required_reminders.log 2>&1
 bundle exec rails dev_ops:retry_zenodo_errors RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/retry_zenodo_errors.log 2>&1
 bundle exec rails curation_stats:update_recent RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/curation_stats.log 2>&1
 bundle exec rails journal_email:clean_old_manuscripts RAILS_ENV=$1 >> /apps/dryad/apps/ui/shared/cron/logs/manuscripts_clean.log 2>&1
