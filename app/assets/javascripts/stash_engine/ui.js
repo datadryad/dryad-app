d34a4e4ed9d779aff0f4df9c23d50126a3263c94
Audrey Hamelers <audrey@datadryad.org>
2023-08-22 19:41:12 +0200

    Closes https://github.com/CDL-Dryad/dryad-product-roadmap/issues/2677

--- app/assets/javascripts/stash_engine/ui.js
+++ app/assets/javascripts/stash_engine/ui.js
@@ -1459,6 +1459,24 @@ function modernizeIt(){
 
 // ##### Main JavaScript ##### //
 
+function awaitSelector(selector) {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
+
 function joelsReady(){
 
   // ***** Upload Modal Component ***** //
--- ui-library/js/main.js
+++ ui-library/js/main.js
@@ -1,5 +1,23 @@
 // ##### Main JavaScript ##### //
 
+function awaitSelector(selector) {
+  return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+      const observer = new MutationObserver(mutations => {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+      observer.observe(document.body, {
+          childList: true,
+          subtree: true
+      });
+  });
+}
+
 function joelsReady(){
 
   // ***** Upload Modal Component ***** //
