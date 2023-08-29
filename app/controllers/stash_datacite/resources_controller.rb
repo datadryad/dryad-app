bc725d9502ea3e6579089c8eb821f6956eed4b2f
Scott Fisher <scott.fisher@ucop.edu>
2023-08-28 18:13:46 -0700

    rubocop

--- app/controllers/stash_datacite/resources_controller.rb
+++ app/controllers/stash_datacite/resources_controller.rb
@@ -67,7 +67,7 @@ module StashDatacite
       StashEngine.repository.submit(resource_id: @resource_id)
 
       @resource.curation_activities << StashEngine::CurationActivity.create(status: 'processing', note: 'Repository processing data',
-                                                                           user_id: current_user.id)
+                                                                            user_id: current_user.id)
 
       @resource.reload
 
