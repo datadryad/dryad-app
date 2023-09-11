6d5e7fb969f944704b8013aa32f99a1b4163d104
Ryan Scherle <ryan@scherle.org>
2023-09-11 16:55:02 -0400

    Add common prepositions to "nondescriptive" words for titles

--- app/models/stash_datacite/resource/dataset_validations.rb
+++ app/models/stash_datacite/resource/dataset_validations.rb
@@ -307,7 +307,8 @@ module StashDatacite
 
       def nondescript_title?
         dict = ['raw', 'data', 'dataset', 'dryad', 'fig', 'figure', 'figures', 'table', 'tables', 'file', 'supp', 'suppl',
-                'supplement', 'supplemental', 'extended', 'supplementary', 'supporting', 'et al']
+                'supplement', 'supplemental', 'extended', 'supplementary', 'supporting', 'et al', 
+                'the' 'of' 'for' 'in', 'from']
         regex = dict.join('|')
         remainder = @resource.title.gsub(/[^a-z0-9\s]/i, '').gsub(/(#{regex}|s\d|f\d|t\d)\b/i, '').strip
         remainder.split.size < 3
