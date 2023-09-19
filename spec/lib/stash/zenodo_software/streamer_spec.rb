dcd01b91e38d681f9e85d30c19317c7593a01c37
Scott Fisher <scott.fisher@ucop.edu>
2023-09-19 13:03:27 -0700

    fixing streamer test for zenodo

--- spec/lib/stash/zenodo_software/streamer_spec.rb
+++ spec/lib/stash/zenodo_software/streamer_spec.rb
@@ -95,7 +95,13 @@ module Stash
         end
 
         it 'raises Stash::ZenodoReplicate::ZenodoError for handling with S3CustomErrors' do
-          stub_request(:get, /merritt-fake/).to_return(status: 404, body: '', headers: {})
+          stub_request(:get, %r{https://a-merritt-test-bucket.s3.us-west-2.amazonaws.com/ark+.})
+            .to_return(status: 200, body: '', headers: {})
+
+          stub_request(:put, %r{https://example.org/my/great/test/bucket/.+})
+            .to_return(status: 404, body: "", headers: {})
+
+          # stub_request(:get, /merritt-fake/).to_return(status: 404, body: '', headers: {})
           @resource.data_files << create(:data_file)
           data_file = @resource.data_files.first
 
