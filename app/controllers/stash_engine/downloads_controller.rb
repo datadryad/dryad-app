4ab2e2cc70f3b3966bf3cf13858d4bdaaf08d4b7
Scott Fisher <scott.fisher@ucop.edu>
2023-10-20 15:49:47 -0700

    Also search after a file that is supposed to exist. Do not load deleted files for download.

--- app/controllers/stash_engine/downloads_controller.rb
+++ app/controllers/stash_engine/downloads_controller.rb
@@ -97,7 +97,7 @@ module StashEngine
 
     # uses presigned
     def file_stream
-      data_file = DataFile.find(params[:file_id])
+      data_file = DataFile.where(id: params[:file_id]).present_files.first
       if data_file&.resource&.may_download?(ui_user: current_user)
         CounterLogger.general_hit(request: request, file: data_file)
         @file_presigned.download(file: data_file)
