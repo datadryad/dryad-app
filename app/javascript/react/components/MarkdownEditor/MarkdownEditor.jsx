8849d964445054896975ab6986a8ac8e002ae114
Audrey Hamelers <audrey@datadryad.org>
2023-11-09 16:12:14 +0100

    working toolbar menu highlighting

--- app/javascript/react/components/MarkdownEditor/MarkdownEditor.jsx
+++ app/javascript/react/components/MarkdownEditor/MarkdownEditor.jsx
@@ -1,27 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {
-  Editor, rootCtx, defaultValueCtx, editorViewCtx, serializerCtx,
+  Editor, rootCtx, defaultValueCtx, editorViewCtx, serializerCtx, schemaCtx,
 } from '@milkdown/core';
 import {
   Milkdown, MilkdownProvider, useEditor, useInstance,
 } from '@milkdown/react';
 import {history} from '@milkdown/plugin-history';
 import {listener as listen, listenerCtx} from '@milkdown/plugin-listener';
+import {trailing} from '@milkdown/plugin-trailing';
 import {commonmark} from '@milkdown/preset-commonmark';
 import {gfm} from '@milkdown/preset-gfm';
 import {replaceAll} from '@milkdown/utils';
+import dryadConfig from './dryadConfig';
+import {selectionListener, selectionCtx} from './selectionListener';
 import Button from './Button';
 
-import './milkdown_editor.css';
-
-function MilkdownCore({initialValue, onChange, onBlur}) {
+function MilkdownCore({
+  initialValue, onChange, onBlur, setSelection,
+}) {
   useEditor((root) => Editor
     .make()
+    .config(dryadConfig)
     .config((ctx) => {
       ctx.set(rootCtx, root);
       ctx.set(defaultValueCtx, initialValue);
-
       const listener = ctx.get(listenerCtx);
       listener.markdownUpdated((_ctx, markdown, prevMarkdown) => {
         if (markdown !== prevMarkdown) onChange(markdown);
@@ -32,34 +35,67 @@ function MilkdownCore({initialValue, onChange, onBlur}) {
         const markdown = serializer(view.state.doc);
         onBlur(markdown);
       });
+      const slistener = ctx.get(selectionCtx);
+      slistener.selection((ctxx, selection, doc) => {
+        const schema = ctxx.get(schemaCtx);
+        setSelection({doc, selection, schema});
+      });
     })
-    .use([listen, commonmark, gfm, history]));
+    .use([listen, commonmark, gfm, history, trailing, selectionListener]));
   return (
     <Milkdown />
   );
 }
 
-const defaultButtons = ['heading', 'bold', 'emph', 'link', 'code', 'spacer', 'ul', 'ol', 'quote', 'block', 'table', 'spacer', 'undo', 'redo'];
+const defaultButtons = ['heading', 'strong', 'emphasis', 'link', 'inlineCode', 'spacer',
+  'bullet_list', 'ordered_list', 'blockquote', 'code_block', 'table', 'spacer', 'undo', 'redo'];
 
 function MilkdownEditor({
   id, initialValue, replaceValue, onChange, onBlur, buttons = defaultButtons,
 }) {
   const [loading, editor] = useInstance();
 
+  const [selection, setSelection] = useState(null);
+  const [active, setActive] = useState([]);
+  const [headingLevel, setHeadingLevel] = useState(0);
+
   useEffect(() => {
     if (editor && replaceValue) editor()?.action(replaceAll(replaceValue));
   }, [editor, replaceValue]);
+
+  useEffect(() => {
+    if (selection) {
+      setActive([]);
+      const list = [];
+      const {doc, selection: sel, schema} = selection;
+      const {from, to} = sel;
+      const {path, parent} = doc.resolve(from);
+      path.forEach((i) => i.type && list.push(i.type.name));
+      Object.keys(schema.marks).forEach((m) => {
+        if (doc.rangeHasMark(from === to ? from - 1 : from, to, schema.marks[m])) list.push(schema.marks[m].name);
+      });
+      setActive(list);
+      setHeadingLevel(parent.attrs?.level || 0);
+    }
+  }, [selection]);
 
   return (
     <>
       {!loading && (
         <div className="md_editor-buttons" role="menubar">
           {buttons.map((button, i) => (
-            <Button editorId={id} key={button + buttons.slice(0, i).filter((b) => b === button).length} type={button} editor={editor} />
+            <Button
+              active={active.includes(button)}
+              headingLevel={headingLevel}
+              editorId={id}
+              key={button + buttons.slice(0, i).filter((b) => b === button).length}
+              type={button}
+              editor={editor}
+            />
           ))}
         </div>
       )}
-      <MilkdownCore initialValue={initialValue} onChange={onChange} onBlur={onBlur} />
+      <MilkdownCore initialValue={initialValue} onChange={onChange} onBlur={onBlur} setSelection={setSelection} />
     </>
   );
 }
