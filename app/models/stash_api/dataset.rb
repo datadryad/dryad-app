ea3de4c395289f7b67dc279080808e6d0e4a4b12
Audrey Hamelers <audrey@datadryad.org>
2023-12-07 00:38:00 +0100

    Close https://github.com/CDL-Dryad/dryad-product-roadmap/issues/2990, other API adjustments

--- app/models/stash_api/dataset.rb
+++ app/models/stash_api/dataset.rb
@@ -7,12 +7,13 @@ module StashApi
   class Dataset
     include Presenter
 
-    def initialize(identifier:, user: nil, item_view: false) # item view means not in list and may show more info for individual record
+    def initialize(identifier:, user: nil, item_view: false, post: false) # item view means not in list and may show more info for individual record
       id_type, iden = identifier.split(':', 2)
       @identifier_s = identifier
       @se_identifier = StashEngine::Identifier.where(identifier_type: id_type, identifier: iden).first
       @user = user
       @item_view = item_view
+      @post = post
     end
 
     def last_se_resource
@@ -23,7 +24,7 @@ module StashApi
       res = last_se_resource
       return nil unless res
 
-      Version.new(resource_id: res.id, item_view: @item_view)
+      Version.new(resource_id: res.id, item_view: @item_view, post: @post)
     end
 
     def metadata
