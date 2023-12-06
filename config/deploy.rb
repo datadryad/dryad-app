d71875acbb94f4621ee0f42547a71b0e9b03707f
Scott Fisher <scott.fisher@ucop.edu>
2023-12-06 12:15:05 -0800

    See if order makes a difference

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,13 @@ set :version_number, `git describe --tags`
 
 set :migration_role, :app
 
+# this copies these files over from shared if they exist, but doesn't error if they don't exist (so can be the same in all envs)
+set :optional_linked_files, %w{
+  config/master.key
+  config/credentials/production.key
+  config/credentials/stage.key
+}
+
 # Default value for linked_dirs is []
 append :linked_dirs,
        "log",
@@ -26,12 +33,7 @@ append :linked_dirs,
        "uploads",
        "reports"
 
-# this copies these files over from shared if they exist, but doesn't error if they don't exist (so can be the same in all envs)
-set :optional_linked_files, %w{
-  config/master.key
-  config/credentials/production.key
-  config/credentials/stage.key
-}
+
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
