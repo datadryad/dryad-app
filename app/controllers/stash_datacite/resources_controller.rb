23d4892a77a632651f5edbe3aa9f3d604949cca5
dragos <dragos@creativepill.com>
2023-12-18 13:11:30 +0200

    feat: set hold for peer review on the resource
    - if peer review is not enabled on the associated identifier

--- app/controllers/stash_datacite/resources_controller.rb
+++ app/controllers/stash_datacite/resources_controller.rb
@@ -29,6 +29,7 @@ module StashDatacite
           check_required_fields(@resource)
           @review = Resource::Review.new(@resource)
           @resource.has_geolocation = @review.geolocation_data?
+          @resource.hold_for_peer_review = true unless @resource.identifier.allow_review?
           @resource.save!
         end
       end
