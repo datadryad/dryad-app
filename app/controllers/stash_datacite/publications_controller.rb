b1b92b892318051ddf020f7901d4ffbdd4468a86
Audrey Hamelers <audrey@datadryad.org>
2024-01-10 13:37:39 +0100

    Closes https://github.com/CDL-Dryad/dryad-product-roadmap/issues/3015

--- app/controllers/stash_datacite/publications_controller.rb
+++ app/controllers/stash_datacite/publications_controller.rb
@@ -77,8 +77,10 @@ module StashDatacite
         @msid = manage_internal_datum(identifier: @se_id, data_type: 'manuscriptNumber', value: parsed_msid)
       end
 
-      # if the newly-set journal wants PPR by default, set the PPR value for this resource
-      @resource.update(hold_for_peer_review: @se_id.journal&.default_to_ppr)
+      if @resource.identifier.allow_review? && @resource.previous_curated_resource.nil? && @resource.curation_start_date.nil?
+        # if the newly-set journal wants PPR by default, set the PPR value for this resource
+        @resource.update(hold_for_peer_review: @se_id.journal&.default_to_ppr)
+      end
 
       save_doi
     end
