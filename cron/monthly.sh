788a11cd13d7e82c9e6c85cc79fa0ad716264a7a
sfisher <scott.fisher@ucop.edu>
2024-01-23 10:16:04 -0800

    remove log processing/parsing from the crons

--- cron/monthly.sh
+++ cron/monthly.sh
@@ -14,6 +14,7 @@ export REPORT_DIR="/apps/dryad/apps/ui/shared/cron/counter-json"
 # export FORCE_SUBMISSION="`date --date="$(date +%Y-%m-15) - 1 month" "+%Y-%m"`"
 
 # run the script with the above settings (RAILS_ENV, REPORT_DIR, FORCE_SUBMISSION)
+# this script is no longer used since we don't routinely update and force reports based on logs/json files
 # bundle exec rails counter:datacite_pusher >> /apps/dryad/apps/ui/shared/cron/logs/counter-uploader.log 2>&1
 
 # Clean outdated content from the database and temporary S3 store
