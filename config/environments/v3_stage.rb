6bc3998471a6f4053141cc21119d73a85ebeb881
dragos <dragos@creativepill.com>
2024-03-27 15:00:12 +0200

    feat: setup log rotation on v3 environments
    - keeping the last 5 logs with a max of 10megabytes for now

--- config/environments/v3_stage.rb
+++ config/environments/v3_stage.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  logger = ActiveSupport::Logger.new(Rails.root.join("log", "v3_stage.log"), 5, 10.megabytes)
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
