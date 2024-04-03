c5443d2c3bc7aa527e6a4c5f7f7f85d78ba14fee
Ryan Scherle <ryan@scherle.org>
2024-04-03 18:11:31 -0400

    fix rspec

--- spec/requests/rack_attack_spec.rb
+++ spec/requests/rack_attack_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe 'Rack::Attack', type: :request do
     it 'throttles download of zip files' do
       target_url = '/stash/downloads/download_resource/BOGUS'
       freeze_time do
-        APP_CONFIG[:rate_limit][:zip_downloads].times do
+        APP_CONFIG[:rate_limit][:zip_downloads_per_hour].times do
           get target_url, headers: headers
           expect(response).to have_http_status(:not_found)
         end
