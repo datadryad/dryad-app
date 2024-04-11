4b239574cbdcd39546905ed5c8882f0a2f18d14b
Audrey Hamelers <audrey@datadryad.org>
2024-04-11 14:21:49 +0200

    Closes https://github.com/datadryad/dryad-product-roadmap/issues/3300

--- spec/factories/stash_engine/resources.rb
+++ spec/factories/stash_engine/resources.rb
@@ -61,8 +61,8 @@ FactoryBot.define do
       create(:description, resource_id: resource.id)
       create(:right, resource: resource)
       create(:contributor, resource: resource)
-      resource.subjects << create(:subject, subject_scheme: 'fos')
-      3.times { resource.subjects << create(:subject) }
+      resource.subjects << create(:subject, subject: Faker::Lorem.unique.word, subject_scheme: 'fos')
+      3.times { resource.subjects << create(:subject, subject: Faker::Lorem.unique.word) }
     end
 
     trait :submitted do
