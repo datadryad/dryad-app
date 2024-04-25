1564397201e4cb2314525e19651b0e71579bd262
Audrey Hamelers <audrey@datadryad.org>
2024-04-25 19:22:30 +0200

    add editable fields and forms to table

--- app/controllers/stash_engine/tenant_admin_controller.rb
+++ app/controllers/stash_engine/tenant_admin_controller.rb
@@ -3,7 +3,8 @@ module StashEngine
     helper SortableTableHelper
     before_action :require_user_login
     before_action :require_superuser
-    before_action :setup_paging, only: %i[index]
+    before_action :setup_paging, only: :index
+    before_action :load, only: %i[popup edit]
 
     def index
       setup_sponsors
@@ -26,6 +27,28 @@ module StashEngine
       @tenants = @tenants.page(@page).per(@page_size)
     end
 
+    def popup
+      strings = { campus_contacts: 'contacts', partner_display: 'member display', ror_orgs: 'ROR organizations', enabled: 'active membership' }
+      @desc = strings[@field.to_sym]
+      respond_to(&:js)
+    end
+
+    def edit
+      valid = %i[partner_display enabled]
+      update = edit_params.slice(*valid)
+      update[:campus_contacts] = edit_params[:campus_contacts].split("\n").map(&:strip).to_json if edit_params[:campus_contacts]
+      @tenant.update(update)
+
+      if edit_params[:ror_orgs]&.present?
+        @tenant.tenant_ror_orgs.destroy_all
+        orgs = edit_params[:ror_orgs].split("\n")
+        orgs.each { |o| StashEngine::TenantRorOrg.create(ror_id: o.strip, tenant_id: @tenant.id) }
+        @tenant.reload
+      end
+
+      respond_to(&:js)
+    end
+
     private
 
     def setup_paging
@@ -41,6 +64,15 @@ module StashEngine
       @sponsors = [OpenStruct.new(id: '', name: '*Select institution*')]
       @sponsors << StashEngine::Tenant.sponsored.map { |t| OpenStruct.new(id: t.id, name: t.short_name) }
       @sponsors.flatten!
+    end
+
+    def load
+      @tenant = authorize Tenant.find(params[:id]), :load?
+      @field = params[:field]
+    end
+
+    def edit_params
+      params.permit(:id, :field, :campus_contacts, :partner_display, :enabled, :ror_orgs)
     end
 
   end
