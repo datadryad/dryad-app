70f2ea2af33350e4a41bf6a51c4a8040411e8ab5
Audrey Hamelers <audrey@datadryad.org>
2024-05-07 23:38:27 +0200

    remove model relationships

--- app/models/stash_engine/funder_role.rb
+++ app/models/stash_engine/funder_role.rb
@@ -16,9 +16,5 @@
 #
 module StashEngine
   class FunderRole < ApplicationRecord
-    self.table_name = 'stash_engine_funder_roles'
-    belongs_to :user
-
-    scope :admins, -> { where(role: 'admin') }
   end
 end
--- app/models/stash_engine/journal_role.rb
+++ app/models/stash_engine/journal_role.rb
@@ -12,13 +12,5 @@
 #
 module StashEngine
   class JournalRole < ApplicationRecord
-    self.table_name = 'stash_engine_journal_roles'
-    belongs_to :user
-    # must contain either a journal or a journal_organization
-    belongs_to :journal, optional: true
-    belongs_to :journal_organization, optional: true
-
-    scope :admins, -> { where(role: 'admin') }
-    scope :org_admins, -> { where(role: 'org_admin') }
   end
 end
