6ff99ad40e5a99bd0568a78124c6fe8abc7d0db7
Ryan Scherle <ryan@scherle.org>
2024-06-04 17:09:29 +0000

    env setup for local testing with remote DB

---
+++ config/deploy/local.rb
@@ -0,0 +1,9 @@
+
+set :rails_env, 'local'
+set :special_login, ' TEST_LOGIN=true '
+
+# To override the default host, set $SERVER_HOSTS, e.g.
+#    $ SERVER_HOSTS='localhost' bundle exec cap development deploy
+set :server_hosts, ENV["SERVER_HOSTS"]&.split(' ') || ['secundus.datadryad.org']
+role %i[app web], fetch(:server_hosts), user: 'ec2-user'
+
