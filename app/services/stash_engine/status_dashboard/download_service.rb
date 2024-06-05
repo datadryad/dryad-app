7e1e0ed89014e5a2933c827b5d990f291176661e
Audrey Hamelers <audrey@datadryad.org>
2024-06-05 18:52:02 +0200

    change download check to use existing available_in_storage method

--- app/services/stash_engine/status_dashboard/download_service.rb
+++ app/services/stash_engine/status_dashboard/download_service.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'httparty'
-
 module StashEngine
   module StatusDashboard
 
@@ -12,12 +10,10 @@ module StashEngine
         # Get a small dataset
         identifier = StashEngine::Identifier.publicly_viewable.where.not(storage_size: nil).order(:storage_size).first
         record_status(online: false, message: 'No dataset available for download to perform test') unless identifier.present?
-        file = identifier.latest_resource_with_public_download.data_files.order(:upload_file_size).first
-        url = file.s3_permanent_presigned_url(head_only: true)
-        resp = HTTParty.head(url, follow_redirect: true, maintain_method_across_redirects: true)
-        online = resp.code == 200
-        msg = "Download service is reporting an HTTP #{resp.code}!" unless online
-        msg += resp.body if !online && resp.body.present?
+        resource = identifier.latest_resource_with_public_download
+        online = resource.repo_queue_states.last.available_in_storage?
+        msg = "Download service is reporting files for #{resource.identifier_str} are not available in storage" unless online
+        msg += "Identifier ID: #{identifier.id}  Resource ID: #{resource.id}" unless online
         record_status(online: online, message: msg)
         online
       rescue StandardError => e
