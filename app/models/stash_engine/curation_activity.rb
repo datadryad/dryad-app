a797edde057f0eb9fb9578c6192bcd3b96d365ab
Audrey Hamelers <audrey@datadryad.org>
2024-06-06 19:55:19 +0200

    array method fix

--- app/models/stash_engine/curation_activity.rb
+++ app/models/stash_engine/curation_activity.rb
@@ -302,7 +302,7 @@ module StashEngine
       # Do not send an invitation to users who have no email address or have an
       # existing invitation for the identifier
       existing_invites = StashEngine::OrcidInvitation.where(identifier_id: resource.identifier_id).pluck(:email).uniq
-      authors = resource.authors.where.not(author_email: existing_invites).where.not(author_email: nil)
+      authors = resource.authors.where.not(author_email: existing_invites).where.not(author_email: nil).to_a
       authors = authors.delete_if { |au| au&.author_email.blank? }
 
       return if authors.empty?
