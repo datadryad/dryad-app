f2cf88586fc1c38fad0838ddef0ac09c312122ca
Alin Vetian <vetianalin@gmail.com>
2024-07-17 18:52:18 +0300

    added withdrawn functionality and email notification, added final delete notification

--- app/services/stash_engine/delete_notifications_service.rb
+++ app/services/stash_engine/delete_notifications_service.rb
@@ -79,15 +79,64 @@ module StashEngine
       true
     end
 
+    # Send withdrawn email notification
+    # - email is sent once at 1 year
+    # - the resource is set to Withdrawn status
+    def send_withdrawn_notification
+      StashEngine::Resource.latest_per_dataset.joins(:last_curation_activity).joins(:process_date)
+        .where(stash_engine_curation_activities: { status: %w[peer_review action_required] })
+        .where(stash_engine_process_dates: { last_status_date: 1.year.ago.beginning_of_day..1.year.ago.end_of_day })
+        .each do |resource|
+
+        reminder_flag = 'withdrawn_email_notice'
+        last_reminder = resource.curation_activities.where('note LIKE ?', "%#{reminder_flag}%")&.last
+        next if last_reminder.present?
+
+        status_updated = create_activity(reminder_flag, resource, status: 'withdrawn',
+          note: "#{reminder_flag} - notification that this item was set to `withdrawn`")
+
+        if status_updated
+          log("Mailing submitter about setting dataset to withdrawn. #{resource_log_text(resource)}")
+          StashEngine::ResourceMailer.send_set_to_withdrawn_notification(resource).deliver_now
+        end
+      rescue StandardError => e
+        p "    Exception! #{e.message}"
+      end
+      true
+    end
+
+    # Send final withdrawn email notification
+    # - email is sent once, 9 months after the resource was withdraw
+    # - the email is sent only if resource was never published
+    def send_final_withdrawn_notification
+      StashEngine::Resource.latest_per_dataset.joins(:last_curation_activity).joins(:process_date)
+        .where(stash_engine_curation_activities: { status: 'withdrawn' })
+        .where('stash_engine_process_dates.last_status_date <= ?', 9.months.ago.end_of_day)
+        .each do |resource|
+        next if resource.curation_activities.pluck(:status).uniq.include?('published')
+
+        reminder_flag = 'final_withdrawn_email_notice'
+        last_reminder = resource.curation_activities.where('note LIKE ?', "%#{reminder_flag}%")&.last
+        next if last_reminder.present?
+
+        log("Mailing submitter as final withdrawn notification. #{resource_log_text(resource)}")
+        StashEngine::ResourceMailer.send_final_withdrawn_notification(resource).deliver_now
+        create_activity(reminder_flag, resource)
+      rescue StandardError => e
+        p "    Exception! #{e.message}"
+      end
+      true
+    end
+
     private
 
-    def create_activity(flag, resource)
-      status = resource.last_curation_activity.status
+    def create_activity(flag, resource, status: nil, note: nil)
+      status ||= resource.last_curation_activity.status
       StashEngine::CurationActivity.create(
         resource_id: resource.id,
         user_id: 0,
         status: status,
-        note: "#{flag} - reminded submitter that this item is still `#{status}`"
+        note: note || "#{flag} - reminded submitter that this item is still `#{status}`"
       )
     end
 
@@ -99,8 +148,12 @@ module StashEngine
 
     def log_data_for_status(status, resource)
       text = "Mailing submitter about deletion of #{status} dataset. "
-      text += " Identifier: #{resource.identifier_id}, Resource: #{resource.id} updated #{resource.updated_at}"
+      text += resource_log_text(resource)
       log(text)
+    end
+
+    def resource_log_text(resource)
+      " Identifier: #{resource.identifier_id}, Resource: #{resource.id} updated #{resource.updated_at}"
     end
   end
 end
--- spec/services/stash_engine/delete_notifications_service_spec.rb
+++ spec/services/stash_engine/delete_notifications_service_spec.rb
@@ -23,7 +23,7 @@ module StashEngine
       context 'when status date is sooner then one month' do
         it 'does not send notification email' do
           Timecop.travel(1.day.from_now)
-          expect(ResourceMailer).to receive(:in_progress_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:in_progress_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_in_progress_reminders
@@ -33,7 +33,7 @@ module StashEngine
       context 'when status date is older then one year' do
         it 'does not send notification email' do
           Timecop.travel(13.months.from_now)
-          expect(ResourceMailer).to receive(:in_progress_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:in_progress_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_in_progress_reminders
@@ -55,7 +55,7 @@ module StashEngine
           resource.last_curation_activity.update!(created_at: 1.months.ago, updated_at: 1.months.ago)
           subject.send(:create_activity, 'in_progress_deletion_notice', resource)
 
-          expect(ResourceMailer).to receive(:in_progress_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:in_progress_delete_notification).never
 
           [1.second, 1.day, 2.week, 1.month - 1.day].each do |period|
             Timecop.travel(period) do
@@ -68,7 +68,7 @@ module StashEngine
           resource.last_curation_activity.update!(created_at: 1.months.ago, updated_at: 1.months.ago)
           subject.send(:create_activity, 'in_progress_deletion_notice', resource)
 
-          expect(ResourceMailer).to receive(:in_progress_delete_notification).once
+          expect(StashEngine::ResourceMailer).to receive(:in_progress_delete_notification).once
           Timecop.travel(1.month) do
             subject.send_in_progress_reminders
           end
@@ -87,7 +87,7 @@ module StashEngine
       context 'when status date is sooner then one month' do
         it 'does not send notification email' do
           Timecop.travel(1.day.from_now)
-          expect(ResourceMailer).to receive(:action_required_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:action_required_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_action_required_reminders
@@ -97,7 +97,7 @@ module StashEngine
       context 'when status date is older then one year' do
         it 'does not send notification email' do
           Timecop.travel(13.months.from_now)
-          expect(ResourceMailer).to receive(:action_required_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:action_required_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_action_required_reminders
@@ -121,7 +121,7 @@ module StashEngine
           resource.last_curation_activity.update!(created_at: 2.months.ago)
           subject.send(:create_activity, 'action_required_deletion_notice', resource)
 
-          expect(ResourceMailer).to receive(:action_required_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:action_required_delete_notification).never
 
           [1.second, 1.day, 2.week, 1.month - 1.day].each do |period|
             Timecop.travel(period) do
@@ -134,7 +134,7 @@ module StashEngine
           resource.last_curation_activity.update!(created_at: 2.months.ago)
           subject.send(:create_activity, 'action_required_deletion_notice', resource)
 
-          expect(ResourceMailer).to receive(:action_required_delete_notification).once
+          expect(StashEngine::ResourceMailer).to receive(:action_required_delete_notification).once
           Timecop.travel(2.month + 1.day) do
             subject.send_action_required_reminders
           end
@@ -155,7 +155,7 @@ module StashEngine
           resource.last_curation_activity.update!(created_at: 1.day.ago)
         end
         it 'does not send notification email' do
-          expect(ResourceMailer).to receive(:peer_review_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:peer_review_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_peer_review_reminders
@@ -168,7 +168,7 @@ module StashEngine
         end
 
         it 'does not send notification email' do
-          expect(ResourceMailer).to receive(:peer_review_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:peer_review_delete_notification).never
           expect(subject).to receive(:create_activity).never
 
           subject.send_peer_review_reminders
@@ -191,7 +191,7 @@ module StashEngine
           Timecop.travel(6.months.from_now) do
             subject.send(:create_activity, 'peer_review_deletion_notice', resource)
           end
-          expect(ResourceMailer).to receive(:peer_review_delete_notification).never
+          expect(StashEngine::ResourceMailer).to receive(:peer_review_delete_notification).never
 
           [1.second, 1.day, 2.week, 1.month - 1.day].each do |period|
             Timecop.travel(6.months.from_now + period) do
@@ -205,9 +205,213 @@ module StashEngine
             subject.send(:create_activity, 'peer_review_deletion_notice', resource)
           end
 
-          expect(ResourceMailer).to receive(:peer_review_delete_notification).once
+          expect(StashEngine::ResourceMailer).to receive(:peer_review_delete_notification).once
           Timecop.travel(7.month.from_now) do
             subject.send_peer_review_reminders
+          end
+        end
+      end
+    end
+
+    describe '#send_withdrawn_notification' do
+      let!(:curation_activity) { create(:curation_activity, status: 'peer_review', resource_id: resource.id) }
+
+      before do
+        allow(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).and_return(true)
+        resource.last_curation_activity.update!(status: 'peer_review')
+      end
+
+      context 'when status date is sooner then one year' do
+        it 'does not send notification email' do
+          Timecop.travel(11.months.from_now)
+          expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+          expect(subject).to receive(:create_activity).never
+
+          subject.send_withdrawn_notification
+        end
+      end
+
+      context 'when status date is older then 1 year' do
+        it "creates withdrawn activity notification at 1 year" do
+          Timecop.travel(1.year.from_now)
+
+          expect(subject).to receive(:create_activity).with('withdrawn_email_notice', resource, {
+            note: "withdrawn_email_notice - notification that this item was set to `withdrawn`",
+            status: "withdrawn"
+          }).once
+
+          subject.send_withdrawn_notification
+        end
+
+        it "sends notification email at 1 year" do
+          Timecop.travel(1.year.from_now)
+
+          expect(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).with(resource).with(no_args)
+
+          subject.send_withdrawn_notification
+        end
+
+        it 'sends only one email ever' do
+          Timecop.travel(1.year.from_now) do
+            subject.send(:create_activity, 'withdrawn_email_notice', resource)
+          end
+          expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+
+          [1.second, 1.day, 2.week, 1.month, 10.years].each do |period|
+            Timecop.travel(1.year.from_now + period) do
+              subject.send_withdrawn_notification
+            end
+          end
+        end
+      end
+    end
+
+    describe '#send_withdrawn_notification' do
+      context 'for in peer_review resource' do
+        let!(:curation_activity) { create(:curation_activity, status: 'peer_review', resource_id: resource.id) }
+
+        before do
+          allow(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).and_return(true)
+          resource.last_curation_activity.update!(status: 'peer_review')
+        end
+
+        context 'when status date is sooner then one year' do
+          it 'does not send notification email' do
+            Timecop.travel(11.months.from_now)
+            expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+            expect(subject).to receive(:create_activity).never
+
+            subject.send_withdrawn_notification
+          end
+        end
+
+        context 'when status date is older then 1 year' do
+          it "creates withdrawn activity notification at 1 year" do
+            Timecop.travel(1.year.from_now)
+
+            expect(subject).to receive(:create_activity).with('withdrawn_email_notice', resource, {
+              note: "withdrawn_email_notice - notification that this item was set to `withdrawn`",
+              status: "withdrawn"
+            }).once
+
+            subject.send_withdrawn_notification
+          end
+
+          it "sends notification email at 1 year" do
+            Timecop.travel(1.year.from_now)
+
+            expect(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).with(resource).with(no_args)
+
+            subject.send_withdrawn_notification
+          end
+
+          it 'sends only one email ever' do
+            Timecop.travel(1.year.from_now) do
+              subject.send(:create_activity, 'withdrawn_email_notice', resource)
+            end
+            expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+
+            [1.second, 1.day, 2.week, 1.month, 10.years].each do |period|
+              Timecop.travel(1.year.from_now + period) do
+                subject.send_withdrawn_notification
+              end
+            end
+          end
+        end
+      end
+
+      context 'for in action_required resource' do
+        let!(:curation_activity) { create(:curation_activity, status: 'action_required', resource_id: resource.id) }
+
+        before do
+          allow(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).and_return(true)
+          resource.last_curation_activity.update!(status: 'action_required')
+        end
+
+        context 'when status date is sooner then one year' do
+          it 'does not send notification email' do
+            Timecop.travel(11.months.from_now)
+            expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+            expect(subject).to receive(:create_activity).never
+
+            subject.send_withdrawn_notification
+          end
+        end
+
+        context 'when status date is older then 1 year' do
+          it "creates withdrawn activity notification at 1 year" do
+            Timecop.travel(1.year.from_now)
+
+            expect(subject).to receive(:create_activity).with('withdrawn_email_notice', resource, {
+              note: "withdrawn_email_notice - notification that this item was set to `withdrawn`",
+              status: "withdrawn"
+            }).once
+
+            subject.send_withdrawn_notification
+          end
+
+          it "sends notification email at 1 year" do
+            Timecop.travel(1.year.from_now)
+
+            expect(StashEngine::ResourceMailer).to receive_message_chain(:send_set_to_withdrawn_notification, :deliver_now).with(resource).with(no_args)
+
+            subject.send_withdrawn_notification
+          end
+
+          it 'sends only one email ever' do
+            Timecop.travel(1.year.from_now) do
+              subject.send(:create_activity, 'withdrawn_email_notice', resource)
+            end
+            expect(StashEngine::ResourceMailer).to receive(:send_set_to_withdrawn_notification).never
+
+            [1.second, 1.day, 2.week, 1.month, 10.years].each do |period|
+              Timecop.travel(1.year.from_now + period) do
+                subject.send_withdrawn_notification
+              end
+            end
+          end
+        end
+      end
+    end
+
+    describe '#send_final_withdrawn_notification' do
+      let!(:curation_activity) { create(:curation_activity, status: 'withdrawn', resource_id: resource.id) }
+
+      before do
+        allow(StashEngine::ResourceMailer).to receive_message_chain(:send_final_withdrawn_notification, :deliver_now).and_return(true)
+        resource.last_curation_activity.update!(status: 'withdrawn')
+      end
+
+      context 'when status date is sooner then 9 months' do
+        it 'does not send notification email' do
+          Timecop.travel(9.months.from_now-1.day)
+          expect(StashEngine::ResourceMailer).to receive(:send_final_withdrawn_notification).never
+          expect(subject).to receive(:create_activity).never
+
+          subject.send_final_withdrawn_notification
+        end
+      end
+
+      context 'when status date is older then 9 months' do
+        it "sends notification email at 9 months" do
+          Timecop.travel(9.months.from_now)
+
+          expect(StashEngine::ResourceMailer).to receive_message_chain(:send_final_withdrawn_notification, :deliver_now).with(resource).with(no_args)
+          expect(subject).to receive(:create_activity).with('final_withdrawn_email_notice', resource).once
+
+          subject.send_final_withdrawn_notification
+        end
+
+        it 'sends only one email ever' do
+          Timecop.travel(9.months.from_now) do
+            subject.send(:create_activity, 'final_withdrawn_email_notice', resource)
+          end
+          expect(StashEngine::ResourceMailer).to receive(:send_final_withdrawn_notification).never
+
+          [1.second, 1.day, 2.week, 1.month, 10.years].each do |period|
+            Timecop.travel(9.months.from_now + period) do
+              subject.send_final_withdrawn_notification
+            end
           end
         end
       end
