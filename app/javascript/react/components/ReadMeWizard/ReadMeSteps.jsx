bd2a4880cf756ba39520af1ae44e757f42300e26
Audrey Hamelers <audrey@datadryad.org>
2024-07-29 20:28:39 +0200

    adjust auto variable lists and prompt text

--- app/javascript/react/components/ReadMeWizard/ReadMeSteps.jsx
+++ app/javascript/react/components/ReadMeWizard/ReadMeSteps.jsx
@@ -2,7 +2,13 @@
 import React from 'react';
 import MarkdownEditor from '../MarkdownEditor';
 
-const formatList = (fileList) => fileList.map((f) => `#### File: ${f.name}\n**Description:**&nbsp;\n\n##### Variables\n${f.variables.map((v) => `* ${v}: `).join('\n')}`).join('\n\n');
+const formatList = (fileList) => fileList.map((f) => {
+  let l = `#### File: ${f.name}\n**Description:**&nbsp;\n\n`;
+  if (Object.hasOwn(f, 'variables')) {
+    l += `##### Variables\n${f.variables.length === 0 ? '* \n' : f.variables.map((v) => `* ${v}: `).join('\n')}`;
+  }
+  return l;
+}).join('\n\n');
 
 export const secTitles = ['Data description', 'Files and variables', 'Code/software', 'Access information'];
 
@@ -20,19 +26,27 @@ export default function ReadMeSteps({
 
   const sections = {
     1: {
-      desc: 'Provide a short description of the experimental efforts for which the data was collected.',
+      desc: <p>Provide a short description of the experimental efforts for which the data was collected.</p>,
       content: content.step1 || '',
     },
     2: {
-      desc: 'Provide a comprehensive list of data files, and define all variables and abbreviations used, including units of measurement. List how you indicate missing values (blank cells, "NA", etc.) if applicable. Please add descriptions below, and add any folders and files inside compressed archives (.zip, .gz) to the list. ',
+      desc:
+      <>
+        <p>Provide a comprehensive list of data files and variables. Starting information has been imported from your uploaded files.</p>
+        <ul>
+          <li>Add any folders and data files inside compressed archives (.zip, .gz), and any missing variables</li>
+          <li>Describe files and define all variables and abbreviations used, including units of measurement</li>
+          <li>List how you indicate missing values (blank cells, &quot;NA&quot;, etc.) if applicable</li>
+        </ul>
+      </>,
       content: content.step2 || formatList(fileList),
     },
     3: {
-      desc: 'What free or open software is needed to view your data? Describe the code software, including versions and loaded packages that you used to run files, and the workflow that you used if the relationship of files to software is not clear. If code or scripts are included with your submission, describe them here.',
+      desc: <p>What free or open software is needed to view your data? Describe the code software, including versions and loaded packages that you used to run files, and the workflow that you used if the relationship of files to software is not clear. If code or scripts are included with your submission, describe them here.</p>,
       content: content.step3 || '',
     },
     4: {
-      desc: 'If applicable, provide links to other publicly accessible locations of the data. If your data was derived from another source(s), list the source(s) and include license information.',
+      desc: <p>If applicable, provide links to other publicly accessible locations of the data. If your data was derived from another source(s), list the source(s) and include license information.</p>,
       content: content.step5 || 'Other publicly accessible locations of the data:\n\n* \n\nData was derived from the following sources:\n\n* ',
     },
   };
@@ -48,7 +62,7 @@ export default function ReadMeSteps({
         ))}
       </div>
       <h2><label htmlFor="readme_step_editor">{secTitles[step - 1]}</label></h2>
-      <p>{sections[step].desc}</p>
+      <div>{sections[step].desc}</div>
       <MarkdownEditor
         id="readme_step_editor"
         {...(step === 1 ? {initialValue: '', replaceValue: sections[step].content} : {initialValue: sections[step].content})}
