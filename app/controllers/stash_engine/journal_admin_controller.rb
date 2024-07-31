564dd0d8c678f6c2073a129834baa08f142810f7
Audrey Hamelers <audrey@datadryad.org>
2024-07-31 15:53:59 +0200

    rubocop

--- app/controllers/stash_engine/journal_admin_controller.rb
+++ app/controllers/stash_engine/journal_admin_controller.rb
@@ -13,8 +13,8 @@ module StashEngine
       if params[:q]
         q = params[:q]
         # search the query in any searchable field
-        @journals = @journals.left_outer_joins(:issns).distinct.where('LOWER(title) LIKE LOWER(?) OR stash_engine_journal_issns.id LIKE ?',
-                                                             "%#{q.strip}%", "%#{q.strip}%")
+        @journals = @journals.left_outer_joins(:issns).distinct
+          .where('LOWER(title) LIKE LOWER(?) OR stash_engine_journal_issns.id LIKE ?', "%#{q.strip}%", "%#{q.strip}%")
       end
 
       ord = helpers.sortable_table_order(whitelist: %w[title issns payment_plan_type default_to_ppr])
