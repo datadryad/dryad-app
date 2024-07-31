52847eae0dc5f2ee9e429cc7aaf9fade49a45594
Audrey Hamelers <audrey@datadryad.org>
2024-07-31 12:27:02 +0200

    fix manuscript dates not appearing, more attempts to reduce load of journals page

--- app/controllers/stash_engine/journals_controller.rb
+++ app/controllers/stash_engine/journals_controller.rb
@@ -6,9 +6,10 @@ module StashEngine
     def index
       params.permit(:q)
       params[:sort] = 'title' if params[:sort].blank?
-      @metadata_journals = Journal.joins(:manuscripts).where("stash_engine_manuscripts.created_at > '#{1.year.ago.iso8601}'").distinct.map(&:id)
+      @metadata_journals = StashEngine::Journal.joins(:manuscripts)
+        .where("stash_engine_manuscripts.created_at > '#{1.year.ago.iso8601}'").select(:id).distinct.map(&:id)
       setup_api_journals
-      sponsoring_journals = Journal.where.not(payment_plan_type: [nil, '']).map(&:id)
+      sponsoring_journals = StashEngine::Journal.where.not(payment_plan_type: [nil, '']).select(:id).map(&:id)
       display_journals = @metadata_journals | sponsoring_journals | @api_journals
 
       ord = helpers.sortable_table_order(whitelist: %w[title issn allow_blackout payment_plan_type name parent_org_id sponsor_id default_to_ppr])
@@ -26,17 +27,11 @@ module StashEngine
     private
 
     def setup_api_journals
-      api_journals = StashEngine::User.joins('inner join oauth_applications on owner_id = stash_engine_users.id')
-        .joins(
-          "inner join stash_engine_roles on stash_engine_users.id = stash_engine_roles.user_id and role_object_type='StashEngine::Journal'"
-        ).select('stash_engine_roles.role_object_id').distinct.map(&:role_object_id)
-      api_journals2 = StashEngine::User.joins('inner join oauth_applications on owner_id = stash_engine_users.id')
-        .joins(
-          "inner join stash_engine_roles on stash_engine_users.id = stash_engine_roles.user_id
-          and role_object_type = 'StashEngine::JournalOrganization'"
-        ).joins(
-          'inner join stash_engine_journals on sponsor_id = stash_engine_roles.role_object_id'
-        ).select('stash_engine_journals.id').distinct.map(&:id)
+      api_journals = StashEngine::Journal.joins(:users).joins('inner join oauth_applications on owner_id = stash_engine_users.id')
+        .select(:id).distinct.map(&:id)
+      api_journals2 = StashEngine::JournalOrganization.joins(:users).joins('inner join oauth_applications on owner_id = stash_engine_users.id')
+        .joins('inner join stash_engine_journals on sponsor_id = stash_engine_roles.role_object_id')
+        .select('stash_engine_journals.id').distinct.map(&:id)
       @api_journals = api_journals | api_journals2
     end
   end
