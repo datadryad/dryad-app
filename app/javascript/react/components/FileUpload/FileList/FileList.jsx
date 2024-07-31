9cf32a22744c2f6d63bec308a6f035988258ec18
Audrey Hamelers <hamelers@ebi.ac.uk>
2024-07-31 22:07:43 +0200

    upload table style & accessibility adjustment

--- app/javascript/react/components/FileUpload/FileList/FileList.jsx
+++ app/javascript/react/components/FileUpload/FileList/FileList.jsx
@@ -12,7 +12,7 @@ const file_list = ({
       <p>Total size: {totalSize}{readmeSize && <em> (README: {readmeSize})</em>}</p>
     </div>
     <BadList chosenFiles={chosenFiles} />
-    <div className="table-wrapper c-uploadtable-wrapper">
+    <div className="table-wrapper c-uploadtable-wrapper" role='region' tabindex='0' aria-labelledby="filelist_id">
       <table className="c-uploadtable">
         <thead>
           <tr>
