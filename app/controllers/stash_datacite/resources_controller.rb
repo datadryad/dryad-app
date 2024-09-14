38d3ab0fd579597819954bda8da4f12c04dc82b7
Audrey Hamelers <audrey@datadryad.org>
2024-09-14 18:09:03 +0200

    NoMethodError fixes

--- app/controllers/stash_datacite/resources_controller.rb
+++ app/controllers/stash_datacite/resources_controller.rb
@@ -56,8 +56,9 @@ module StashDatacite
 
       StashEngine.repository.submit(resource_id: @resource_id)
 
-      @resource.curation_activities << StashEngine::CurationActivity.create(status: 'processing', note: 'Repository processing data',
-                                                                            user_id: current_user.id)
+      @resource.curation_activities << StashEngine::CurationActivity.create(
+        status: 'processing', note: 'Repository processing data', user_id: current_user&.id || 0
+      )
 
       @resource.reload
 
