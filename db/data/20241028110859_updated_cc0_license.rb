bd49fa4bdc1775f11f9af10589f979f8824ce5ed
Alin Vetian <vetianalin@gmail.com>
2024-10-28 18:46:09 +0200

    fixed data migration

--- db/data/20241028110859_updated_cc0_license.rb
+++ db/data/20241028110859_updated_cc0_license.rb
@@ -4,7 +4,6 @@ class UpdatedCc0License < ActiveRecord::Migration[7.0]
   def up
     StashDatacite::Right.where(rights: 'CC0 1.0 Universal (CC0 1.0) Public Domain Dedication')
                         .update_all(rights: 'Creative Commons Zero v1.0 Universal', rights_uri: 'https://spdx.org/licenses/CC0-1.0.html')
-    aaa
   end
 
   def down
