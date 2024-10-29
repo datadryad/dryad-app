5ea23c1e25d79cfa3d8a604b05e7cbcfb1d1c28c
Audrey Hamelers <audrey@datadryad.org>
2024-10-29 18:04:20 +0100

    dupe check exception fix

--- app/controllers/stash_engine/resources_controller.rb
+++ app/controllers/stash_engine/resources_controller.rb
@@ -180,14 +180,14 @@ module StashEngine
         other_submissions = other_submissions.latest_per_dataset.where.not(identifier_id: @resource.identifier_id)
         primary_article = @resource.related_identifiers.find_by(work_type: 'primary_article')&.related_identifier
         manuscript = @resource.resource_publication.manuscript_number
-        dupes = other_submissions.where(title: @resource.title).select(:id, :title).to_a
+        dupes = other_submissions.where(title: @resource.title)&.select(:id, :title).to_a
         if primary_article.present?
           dupes.concat(other_submissions.joins(:related_identifiers)
-              .where(related_identifiers: { work_type: 'primary_article', related_identifier: primary_article }).select(:id, :title).to_a)
+              .where(related_identifiers: { work_type: 'primary_article', related_identifier: primary_article })&.select(:id, :title).to_a)
         end
         if manuscript.present?
           dupes.concat(
-            other_submissions.joins(:resource_publication).find_by(resource_publication: { manuscript_number: manuscript }).select(:id, :title).to_a
+            other_submissions.joins(:resource_publication).find_by(resource_publication: { manuscript_number: manuscript })&.select(:id, :title).to_a
           )
         end
       end
