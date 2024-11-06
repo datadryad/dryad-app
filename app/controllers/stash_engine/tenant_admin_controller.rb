bf6ccbb62af7720cc44ad5f2a2ef01bc90130fcb
Audrey Hamelers <audrey@datadryad.org>
2024-11-06 18:36:07 +0100

    edit more tenant fields

--- app/controllers/stash_engine/tenant_admin_controller.rb
+++ app/controllers/stash_engine/tenant_admin_controller.rb
@@ -29,31 +29,26 @@ module StashEngine
 
     def popup
       strings = { campus_contacts: 'contacts', partner_display: 'member display', ror_orgs: 'ROR organizations', enabled: 'active membership',
-                  logo: 'logo' }
+                  logo: 'logo', short_name: 'member name', long_name: 'full member name', authentication: 'authentication strategy' }
       @desc = strings[@field.to_sym]
       respond_to(&:js)
     end
 
     def edit
-      valid = %i[partner_display enabled]
+      valid = %i[partner_display enabled short_name long_name]
       update = edit_params.slice(*valid)
       update[:campus_contacts] = edit_params[:campus_contacts].split("\n").map(&:strip).to_json if edit_params.key?(:campus_contacts)
+      if edit_params.key?(:authentication)
+        auth = {
+          strategy: edit_params[:authentication][:strategy],
+          ranges: edit_params[:authentication][:ranges].present? ? edit_params[:authentication][:ranges].split("\n").map(&:strip) : nil,
+          entity_id: edit_params[:authentication][:entity_id].presence || nil,
+          entity_domain: edit_params[:authentication][:entity_domain].presence || nil
+        }
+        update[:authentication] = auth.compact.to_json
+      end
       @tenant.update(update)
-
-      if edit_params.key?(:logo)
-        @tenant.logo = StashEngine::Logo.new unless @tenant.logo.present?
-        @tenant.logo.data = edit_params[:logo]
-        @tenant.logo.save
-        @tenant.reload
-      end
-
-      if edit_params.key?(:ror_orgs)
-        @tenant.tenant_ror_orgs.destroy_all
-        orgs = edit_params[:ror_orgs].split("\n")
-        orgs.each { |o| StashEngine::TenantRorOrg.create(ror_id: o.strip, tenant_id: @tenant.id) }
-        @tenant.reload
-      end
-
+      update_associations
       respond_to(&:js)
     end
 
@@ -79,8 +74,25 @@ module StashEngine
       @field = params[:field]
     end
 
+    def update_associations
+      if edit_params.key?(:logo)
+        @tenant.logo = StashEngine::Logo.new unless @tenant.logo.present?
+        @tenant.logo.data = edit_params[:logo]
+        @tenant.logo.save
+      end
+
+      if edit_params.key?(:ror_orgs)
+        @tenant.tenant_ror_orgs.destroy_all
+        orgs = edit_params[:ror_orgs].split("\n")
+        orgs.each { |o| StashEngine::TenantRorOrg.create(ror_id: o.strip, tenant_id: @tenant.id) }
+      end
+
+      @tenant.reload
+    end
+
     def edit_params
-      params.permit(:id, :field, :logo, :campus_contacts, :partner_display, :enabled, :ror_orgs)
+      params.permit(:id, :field, :short_name, :long_name, :logo, :campus_contacts, :partner_display, :enabled, :ror_orgs,
+                    authentication: %i[strategy ranges entity_id entity_domain])
     end
 
   end
