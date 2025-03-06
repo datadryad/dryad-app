581f1b7bed6b2fd9049dbe902e76198b34a0cc45
Alin Vetian <vetianalin@gmail.com>
2025-03-06 17:58:21 +0200

    updated delete resource service

--- app/services/stash_engine/delete_datasets_service.rb
+++ app/services/stash_engine/delete_datasets_service.rb
@@ -1,21 +1,25 @@
 module StashEngine
   class DeleteDatasetsService
-    attr_reader :resource, :current_user
+    attr_reader :resource, :current_user, :add_delete_note
 
-    def initialize(resource, current_user = nil)
+    def initialize(resource, current_user: nil, add_delete_note: true)
       @resource = resource
       @current_user = current_user
+      @add_delete_note = add_delete_note
     end
 
     def call
-      last = resource.previous_resource
-      if last
+      prev = resource.previous_resource
+      identifier = resource.identifier
+      if prev && add_delete_note
         user_id = current_user&.id || 0
         note = "#{(user_id == 0 && 'System cleanup') || 'User'} deleted unsubmitted version #{resource.version_number}"
-        StashEngine::CurationActivity.create(resource_id: last.id, status: last.current_curation_status, user_id: user_id, note: note)
+        StashEngine::CurationActivity.create(resource_id: prev.id, status: prev.current_curation_status, user_id: user_id, note: note)
       end
       success = resource.destroy
-      last.identifier.update(latest_resource_id: last.id) if success && last.identifier
+      if success && identifier && identifier.latest_resource_id == resource.id
+        identifier.update(latest_resource_id: prev.id)
+      end
 
       success
     end
