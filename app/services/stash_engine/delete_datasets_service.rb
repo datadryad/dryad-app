9d348d4de00feba20ce03518ab74501a8d01b8a0
Alin Vetian <vetianalin@gmail.com>
2025-03-06 15:53:53 +0200

    removed bad line of code

--- app/services/stash_engine/delete_datasets_service.rb
+++ app/services/stash_engine/delete_datasets_service.rb
@@ -13,7 +13,6 @@ module StashEngine
         user_id = current_user&.id || 0
         note = "#{(user_id == 0 && 'System cleanup') || 'User'} deleted unsubmitted version #{resource.version_number}"
         StashEngine::CurationActivity.create(resource_id: last.id, status: last.current_curation_status, user_id: user_id, note: note)
-        resource.create(resource_id: last.id, status: last.current_curation_status, user_id: user_id, note: note)
       end
       success = resource.destroy
       last.identifier.update(latest_resource_id: last.id) if success && last.identifier
