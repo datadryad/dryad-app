6a7fd8df45ac245cbfcd8710b8dd321fcc5d8d4f
Audrey Hamelers <audrey@datadryad.org>
2025-03-19 15:44:02 +0100

    modify invoicer fix

--- lib/stash/payments/invoicer.rb
+++ lib/stash/payments/invoicer.rb
@@ -61,10 +61,8 @@ module Stash
         invoice = create_invoice(customer_id)
         create_invoice_overages(over, customer_id, invoice.id)
         invoice.send_invoice
-        resource.curation_activities << StashEngine::CurationActivity.new(
-          note: "New overage invoice sent with ID: #{invoice.id}",
-          status: resource.current_curation_status,
-          user_id: 0
+        resource.curation_activities << StashEngine::CurationActivity.create(
+          note: "New overage invoice sent with ID: #{invoice.id}", status: resource.current_curation_status, user_id: 0
         )
       end
 
