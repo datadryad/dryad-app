946e38818ac44cd67216fc9297124603055cfc96
Audrey Hamelers <audrey@datadryad.org>
2025-03-26 18:20:24 +0100

    intelligently remove partial marks without breaking markdown tags

--- app/javascript/react/components/MarkdownEditor/Button.jsx
+++ app/javascript/react/components/MarkdownEditor/Button.jsx
@@ -251,6 +251,47 @@ function List({type, editor, active}) {
   );
 }
 
+function Toggle({
+  type, editor, active, disabled,
+}) {
+  const callEditorCommand = () => {
+    const view = editor()?.ctx.get(editorViewCtx);
+    const {dispatch, state} = view;
+    const {
+      doc, selection, schema, tr,
+    } = state;
+    const {from: start, to: end} = selection;
+    if (doc.rangeHasMark(start, end === start ? end + 1 : end, schema.marks[type])) {
+      const {parent} = doc.resolve(start);
+      let from = start;
+      let to = end;
+      while (from > 0 && !doc.textBetween(from - 1, start).replace(/[ ]/g, '').length) {
+        from -= 1;
+      }
+      while (to < parent.childCount && !doc.textBetween(end, to + 1).replace(/[ ]/g, '').length) {
+        to += 1;
+      }
+      tr.setSelection(TextSelection.create(doc, from, to));
+      dispatch(tr);
+    }
+    editor()?.action(callCommand(commands[type].key));
+    view.focus();
+  };
+
+  return (
+    <button
+      type="button"
+      className={active ? 'active' : undefined}
+      disabled={disabled}
+      title={labels[type]}
+      aria-label={labels[type]}
+      role="menuitem"
+      onClick={callEditorCommand}
+    >{icons[type]}
+    </button>
+  );
+}
+
 function Table({
   active, editor, mdEditor, activeEditor, editorId,
 }) {
@@ -442,6 +483,9 @@ function Button({
   if (activeEditor === 'visual') {
     if (type === 'link') return <LinkMenu active={active} editor={editor} editorId={editorId} />;
     if (type.includes('list')) return <List active={active} editor={editor} type={type} />;
+    if (['strong', 'emphasis', 'inlineCode', 'strike_through'].includes(type)) {
+      return <Toggle active={active} editor={editor} type={type} disabled={disabled} />;
+    }
   }
   const sharedProps = {
     active, editor, mdEditor, activeEditor,
