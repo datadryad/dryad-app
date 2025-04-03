d65382c67b3ea1c060e91a02ca04d317a0dd3552
Audrey Hamelers <audrey@datadryad.org>
2025-04-03 19:58:02 +0200

    remove import_info from old payment calculation

--- db/data/20250403150344_populate_old_payment_system_flag.rb
+++ db/data/20250403150344_populate_old_payment_system_flag.rb
@@ -4,10 +4,9 @@ class PopulateOldPaymentSystemFlag < ActiveRecord::Migration[8.0]
   def up
     StashEngine::Identifier.where(
       publication_date: nil,
-      payment_id: nil
+      payment_id: nil,
+      payment_type: ['unknown', '', nil]
     ).where.not(pub_state: 'withdrawn')
-      .where.not(import_info: 'published')
-      .where.not(payment_type: 'waiver')
       .joins(:latest_resource)
       .where(stash_engine_resources: { accepted_agreement: true })
       .update_all(old_payment_system: true)
