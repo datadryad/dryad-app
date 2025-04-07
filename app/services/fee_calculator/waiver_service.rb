a67f4477fb23fd06edb912291baf27fcd8c5b64d
Alin Vetian <vetianalin@gmail.com>
2025-04-07 19:28:39 +0300

    add waiver ass non payer

---
+++ app/services/fee_calculator/waiver_service.rb
@@ -0,0 +1,17 @@
+module FeeCalculator
+  class WaiverService < BaseService
+
+    def call
+      verify_max_storage_size
+      add_zero_fee(:storage_fee)
+      add_storage_fee_label
+      @sum_options.merge(total: @sum)
+    end
+
+    private
+
+    def storage_fee_label
+      PRODUCT_NAME_MAPPER[:individual_storage_fee]
+    end
+  end
+end
--- spec/requests/fee_calculator_controller_spec.rb
+++ spec/requests/fee_calculator_controller_spec.rb
@@ -332,6 +332,30 @@ RSpec.describe 'FeeCalculatorController', type: :request do
         end
       end
     end
+
+    describe '#waiver fee' do
+      let(:type) { 'waiver' }
+      let(:options) { {} }
+      let(:identifier) { create(:identifier, payment_type: 'waiver') }
+      let(:resource) { create(:resource, identifier: identifier) }
+
+      before do
+        get resource_fee_calculator_url(resource), params: options
+      end
+
+      it 'does not fail' do
+        expect(response).to have_http_status(:ok)
+        expect(json_response).to eq({ 'some_fee' => 12 })
+      end
+
+      it 'calls new with the correct type' do
+        expect(FeeCalculatorService).to have_received(:new).with('waiver')
+      end
+
+      it 'calls calculate on the service instance with the correct params' do
+        expect(service_instance).to have_received(:calculate).with(options, resource: resource)
+      end
+    end
   end
 
   describe 'examples in documentation table' do
