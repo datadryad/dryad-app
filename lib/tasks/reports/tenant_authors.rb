d051e3283382c3bca6bf17121d683d2f21503d17
Alin Vetian <vetianalin@gmail.com>
2025-05-26 18:11:30 +0300

    added tenant authors report

---
+++ lib/tasks/reports/tenant_authors.rb
@@ -0,0 +1,62 @@
+require 'csv'
+
+module Tasks
+  module Reports
+    class TenantAuthors
+      attr_reader :tenant
+
+      def initialize(tenant)
+        @tenant = tenant
+      end
+
+      def call
+        CSV.open(File.join(Rails.root.join('reports'), "#{tenant.id}-authors-report-#{Time.now.strftime('%Y-%m-%d')}.csv"), 'wb') do |csv|
+          csv << [
+            'Email', 'Author name', 'Dataset', 'DOI', 'Tenant', 'Metrics', 'Date first published', 'Date first submitted',
+            'Status', 'Affiliations', 'Journal', 'Funders'
+          ]
+
+          StashEngine::Resource.latest_per_dataset.joins(tenant: :ror_orgs)
+            .includes(
+              { authors: :affiliations },
+              :tenant,
+              :journal,
+              :contributors,
+              :curation_activities,
+              identifier: [:process_date, :counter_stat]
+            ).where(stash_engine_ror_orgs: { ror_id: ror_ids })
+            .each do |resource|
+            resource.authors.each do |author|
+              identifier = resource.identifier
+              email = author.author_email || StashEngine::User.where(orcid: author.author_orcid)&.first&.email
+              csv << [
+                email, author.author_full_name, resource&.title,
+                identifier.identifier, resource.tenant&.long_name, metrics(identifier.counter_stat),
+                identifier.publication_date,
+                identifier.process_date.processing,
+                resource.curation_activities.order(:created_at).last.status,
+                author.affiliations.map(&:long_name).join(', '),
+                resource.journal&.title,
+                resource.contributors.where(contributor_type: 'funder').map(&:contributor_name).join(', ')
+              ]
+            end
+          end
+          true
+        end
+      end
+
+      private
+
+      def ror_ids
+        tenant.ror_ids
+      end
+
+      def metrics(counter_stat)
+        c = counter_stat.unique_investigation_count
+        r = counter_stat.unique_request_count
+        views = c.blank? || c < r ? (r || 0) : c
+        "#{views} views, #{r || 0} downloads, #{counter_stat.citation_count || 0} citations"
+      end
+    end
+  end
+end
