1ccaa0320d98af482e88b80396aefd2f89905ef5
Alin Vetian <vetianalin@gmail.com>
2025-05-26 18:19:11 +0300

    code updates

--- lib/tasks/reports/tenant_authors.rb
+++ lib/tasks/reports/tenant_authors.rb
@@ -11,26 +11,13 @@ module Tasks
 
       def call
         CSV.open(File.join(Rails.root.join('reports'), "#{tenant.id}-authors-report-#{Time.now.strftime('%Y-%m-%d')}.csv"), 'wb') do |csv|
-          csv << [
-            'Email', 'Author name', 'Dataset', 'DOI', 'Tenant', 'Metrics', 'Date first published', 'Date first submitted',
-            'Status', 'Affiliations', 'Journal', 'Funders'
-          ]
+          csv << header
 
-          StashEngine::Resource.latest_per_dataset.joins(tenant: :ror_orgs)
-            .includes(
-              { authors: :affiliations },
-              :tenant,
-              :journal,
-              :contributors,
-              :curation_activities,
-              identifier: [:process_date, :counter_stat]
-            ).where(stash_engine_ror_orgs: { ror_id: ror_ids })
-            .each do |resource|
+          items_query.each do |resource|
             resource.authors.each do |author|
               identifier = resource.identifier
-              email = author.author_email || StashEngine::User.where(orcid: author.author_orcid)&.first&.email
               csv << [
-                email, author.author_full_name, resource&.title,
+                author_email(author), author.author_full_name, resource&.title,
                 identifier.identifier, resource.tenant&.long_name, metrics(identifier.counter_stat),
                 identifier.publication_date,
                 identifier.process_date.processing,
@@ -51,11 +38,34 @@ module Tasks
         tenant.ror_ids
       end
 
+      def header
+        [
+          'Email', 'Author name', 'Dataset', 'DOI', 'Tenant', 'Metrics', 'Date first published', 'Date first submitted',
+          'Status', 'Affiliations', 'Journal', 'Funders'
+        ]
+      end
+
+      def items_query
+        StashEngine::Resource.latest_per_dataset.joins(tenant: :ror_orgs)
+          .includes(
+            { authors: :affiliations },
+            :tenant,
+            :journal,
+            :contributors,
+            :curation_activities,
+            identifier: %i[process_date counter_stat]
+          ).where(stash_engine_ror_orgs: { ror_id: ror_ids })
+      end
+
       def metrics(counter_stat)
         c = counter_stat.unique_investigation_count
         r = counter_stat.unique_request_count
         views = c.blank? || c < r ? (r || 0) : c
         "#{views} views, #{r || 0} downloads, #{counter_stat.citation_count || 0} citations"
+      end
+
+      def author_email(author)
+        author.author_email || StashEngine::User.where(orcid: author.author_orcid)&.first&.email
       end
     end
   end
