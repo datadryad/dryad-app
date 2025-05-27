e5217076bbb8e6b61840e2af9e3bf917707fb177
Audrey Hamelers <audrey@datadryad.org>
2025-05-27 15:57:57 +0200

    better staged vs permanent s3 checks

--- app/models/stash_engine/generic_file.rb
+++ app/models/stash_engine/generic_file.rb
@@ -237,14 +237,14 @@ module StashEngine
     end
 
     def uploaded_success_url
-      dl_url = s3_staged_presigned_url if file_state.nil? || file_state == 'created'
+      dl_url = s3_staged_presigned_url unless digest? || storage_version_id.present?
       dl_url ||= public_download_url
       dl_url ||= url
       dl_url
     end
 
     def uploaded
-      return Stash::Aws::S3.new.exists?(s3_key: s3_staged_path) if url.nil? && (file_state.nil? || file_state == 'created')
+      return Stash::Aws::S3.new.exists?(s3_key: s3_staged_path) unless digest? || storage_version_id.present?
 
       uploaded_success_url.present?
     end
