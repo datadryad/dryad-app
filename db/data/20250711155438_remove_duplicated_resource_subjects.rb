ace673814edd0025694be34b88ff590866c78fd8
Alin Vetian <vetianalin@gmail.com>
2025-07-14 18:06:25 +0300

    fixed migration

--- db/data/20250711155438_remove_duplicated_resource_subjects.rb
+++ db/data/20250711155438_remove_duplicated_resource_subjects.rb
@@ -2,16 +2,27 @@
 
 class RemoveDuplicatedResourceSubjects < ActiveRecord::Migration[8.0]
   def up
-    duplicates = StashDatacite::ResourcesSubjects.group(:resource_id, :subject_id).having('COUNT(*) > 1').pluck(:resource_id, :subject_id)
-    pp "Deleting duplicates for #{duplicates.count} resources ..."
+    ActiveRecord::Base.transaction do
+      before_count = StashDatacite::ResourcesSubjects.select(:resource_id, :subject_id).distinct.count
 
-    duplicates.each do |duplicate|
-      ids = StashDatacite::ResourcesSubjects.where(resource_id: duplicate[0], subject_id: duplicate[1]).ids
-      StashDatacite::ResourcesSubjects.where(id: ids[1..-1]).delete_all
+      duplicates = StashDatacite::ResourcesSubjects.group(:resource_id, :subject_id).having('COUNT(*) > 1').pluck(:resource_id, :subject_id)
+      pp "Deleting duplicates for #{duplicates.count} resources ..."
+
+      duplicates.each do |duplicate|
+        ids = StashDatacite::ResourcesSubjects.where(resource_id: duplicate[0], subject_id: duplicate[1]).ids
+        StashDatacite::ResourcesSubjects.where(id: ids[1..-1]).delete_all
+      end
+
+      after_count = StashDatacite::ResourcesSubjects.select(:resource_id, :subject_id).distinct.count
+      if before_count != after_count
+        pp "Rollback: distinct pair count changed (#{before_count} → #{after_count})"
+        raise ActiveRecord::Rollback
+      end
     end
+
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    # raise ActiveRecord::IrreversibleMigration
   end
 end
