6228833a1933c6219e41067d09889ace36244b58
Audrey Hamelers <audrey@datadryad.org>
2025-07-28 19:30:28 +0200

    rspec update

--- spec/factories/stash_datacite/contributor_groupings.rb
+++ spec/factories/stash_datacite/contributor_groupings.rb
@@ -18,7 +18,6 @@ FactoryBot.define do
   factory :contributor_grouping, class: StashDatacite::ContributorGrouping do
 
     contributor_name    { 'National Institutes of Health' }
-    contributor_type    { 'funder' }
     identifier_type     { 'crossref_funder_id' }
     name_identifier_id  { 'http://dx.doi.org/10.13039/100000002' }
     json_contains       { JSON.parse(File.read(File.join(Rails.root, 'spec/fixtures/nih_group.json'))) }
