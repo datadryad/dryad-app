03c37fe1d20a2ef98362f70554b2f8806e29c372
Audrey Hamelers <audrey@datadryad.org>
2025-07-30 15:12:43 +0200

    digest lookup deleting old files

--- app/models/stash_engine/resource.rb
+++ app/models/stash_engine/resource.rb
@@ -1133,8 +1133,8 @@ module StashEngine
       digest_type = 'sha-256'
       sums = Stash::Checksums.get_checksums([digest_type], StringIO.new(content))
       digest = sums.get_checksum(digest_type)
-      old_file = data_files.present_files.where(upload_file_name: filename).first&.original_deposit_file
-      return if digest == old_file&.digest
+      old_file = data_files.present_files.where(upload_file_name: filename).first
+      return if digest == old_file&.original_deposit_file&.digest
 
       # remove old file
       old_file.smart_destroy! if old_file&.digest
