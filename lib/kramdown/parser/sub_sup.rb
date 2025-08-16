7e22566cc52349b92b10772111a590fe7450ad2f
Audrey Hamelers <audrey@datadryad.org>
2025-08-16 11:45:58 +0200

    fix parser exception

--- lib/kramdown/parser/sub_sup.rb
+++ lib/kramdown/parser/sub_sup.rb
@@ -12,26 +12,33 @@ module Kramdown
       SUPTAGSTART = /\^([^^]*)\^/
       SUBTAGSTART = /(?<!~)~(?!~)([^~]*)(?<!~)~(?!~)/
 
-      def subsupparse(regex, tag)
+      def subsupparse(regex, char, tag)
         line = @src.current_line_number
+        saved_pos = @src.save_pos
 
         if @src[1].blank?
           add_text(@src.scan(regex))
         else
           @src.pos += 1
           el = Kramdown::Element.new(:html_element, tag, {}, category: :span, line: line)
-          parse_spans(el, regex)
-          @tree.children << el
+          found = parse_spans(el, regex)
+          if found
+            @tree.children << el
+          else
+            @src.revert_pos(saved_pos)
+            add_text(char)
+          end
           @src.pos += 1
         end
       end
 
       def parse_superscript
-        subsupparse(/\^/, 'sup')
+        subsupparse(/\^/, '^', 'sup')
       end
 
       def parse_subscript
-        subsupparse(/~/, 'sub')
+        subsupparse(/~/, '~', 'sub')
+      end
       end
 
       define_parser(:superscript, SUPTAGSTART, '\^') unless has_parser?(:superscript)
