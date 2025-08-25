f3d1c829dccc555730687565fd2b236bfcef2b8f
Alin Vetian <vetianalin@gmail.com>
2025-08-25 17:56:21 +0300

    do not send emails to the user

--- db/data/20250820130739_add_file_deleted_at_flag2.rb
+++ db/data/20250820130739_add_file_deleted_at_flag2.rb
@@ -4,9 +4,10 @@ class AddFileDeletedAtFlag2 < ActiveRecord::Migration[8.0]
   def up
     removed_files_note = 'remove_abandoned_datasets CRON - removing data files from abandoned dataset'
     StashEngine::Resource.joins(:curation_activities).where(stash_engine_curation_activities: {note: removed_files_note}).each do |resource|
-      if resource == resource.identifier.latest_resource
+      if resource.id == resource.identifier.latest_resource.id
         # create a new version to mark all files as deleted
         new_res = DuplicateResourceService.new(resource, StashEngine::User.system_user).call
+        new_res.update skip_emails: true
         new_res.generic_files.update(file_deleted_at: Time.current, file_state: 'deleted')
         new_res.current_state = 'submitted'
 
@@ -27,6 +28,6 @@ class AddFileDeletedAtFlag2 < ActiveRecord::Migration[8.0]
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    # raise ActiveRecord::IrreversibleMigration
   end
 end
