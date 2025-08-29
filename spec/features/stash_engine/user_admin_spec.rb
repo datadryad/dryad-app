a4dc9a9b6048612682794a83511d5eb7fa7b2165
Audrey Hamelers <audrey@datadryad.org>
2025-08-29 20:29:01 +0200

    flaky test fix attempt

--- spec/features/stash_engine/user_admin_spec.rb
+++ spec/features/stash_engine/user_admin_spec.rb
@@ -109,7 +109,8 @@ RSpec.feature 'UserAdmin', type: :feature do
         it 'allows the system admin to add the journal role form and set role' do
           find_button('Add a journal role').click
           expect(page).to have_text('Journal role')
-          fill_in 'searchselect-stash_engine_user_roles_attributes__6__role_object_id___input', with: @journal.title
+          find('#searchselect-stash_engine_user_roles_attributes__6__role_object_id___input').base.send_keys(@journal.title[0..4])
+          expect(page).not_to have_css('li.fa-circle-notch')
           find("li[data-value='#{@journal.id}']").click
           find('#stash_engine_user_roles_attributes_6_role_admin').set(true)
           find('input[name=commit]').click
