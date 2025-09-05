914b4ad1c9c232e5e4dffa14e00b81b3ffaa35b3
Alin Vetian <vetianalin@gmail.com>
2025-09-05 15:26:43 +0300

    rubocop

--- lib/tasks/args_parser.rb
+++ lib/tasks/args_parser.rb
@@ -1,5 +1,4 @@
 require 'optparse'
-# rubocop:disable Lint/EmptyBlock
 module Tasks
   module ArgsParser
 
@@ -7,9 +6,9 @@ module Tasks
       options = OpenStruct.new
       return options if attributes.blank?
 
-      args = ARGV.drop_while { |a| a != "--" }[1..] || []
+      args = ARGV.drop_while { |a| a != '--' }[1..] || []
       args.each_slice(2) do |key, value|
-        next unless key&.start_with?("--")
+        next unless key&.start_with?('--')
 
         my_key = key.sub(/^--/, '').to_sym
         next unless my_key.in?(attributes)
@@ -21,4 +20,3 @@ module Tasks
     end
   end
 end
-# rubocop:enable Lint/EmptyBlock
