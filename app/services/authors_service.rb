b50f2a7f669a158ea09d445da5ae5153d3a35c92
Alin Vetian <vetianalin@gmail.com>
2025-09-16 10:28:06 +0300

    add script to populate missing ORCID, add conflict ORCID emails report, real time ORCID populate

---
+++ app/services/authors_service.rb
@@ -0,0 +1,88 @@
+require 'csv'
+
+class AuthorsService
+  attr_reader :author
+
+  def initialize(author=nil)
+    @author = author
+  end
+
+  def check_orcid
+    return if author.blank?
+    return if author.author_email.blank?
+    return if author.author_orcid.present?
+
+    user_with_orcid = StashEngine::User.where('LOWER(email) = LOWER(?), orcid NOT IN ("", NULL)', author.author_email)&.first
+    return unless user_with_orcid
+
+    author.update(author_orcid: user_with_orcid.orcid)
+  end
+
+  def fix_missing_orchid
+    conflicts = conflicts_list
+    grouped_orcids = combined_orcids.group_by { |email, _| email }.transform_values do |entries|
+      entries
+        .group_by { |_, data| data[:source] }
+        .transform_values { |arr| arr.map { |_, data| data[:orcid] }.uniq }
+    end
+
+    StashEngine::Author.where(author_orcid: [nil, '']).find_each do |author|
+      next if conflicts[author.author_email].present?
+      next if grouped_orcids[author.author_email].blank?
+
+      if (orcid = grouped_orcids[author.author_email][:user]&.first || grouped_orcids[author.author_email][:author]&.first)
+        pp "updating #{author.author_email} with ORCID #{orcid}"
+        # author.update_column(:author_orcid, orcid) # update_column skips validations/callbacks
+      end
+    end
+  end
+
+  def generate_orchid_conflicts_report
+    CSV.open(File.join(REPORTS_DIR, 'orchid_conflicts.csv'), 'w') do |csv|
+      csv << ['Email', 'Authors ORCID', 'Users ORCID']
+      conflicts_list.each_pair do |email, orcids|
+        csv << [
+          email,
+          orcids[:authors].join("\n"),
+          orcids[:users].join("\n")
+        ]
+      end
+    end
+  end
+
+  private
+
+  def conflicts_list
+    return @conflicts_list if @conflicts_list.present?
+
+    grouped = combined_orcids.group_by { |email, _data| email }
+    conflicts = grouped.select do |_email, entries|
+      entries.map { |_, data| data[:orcid] }.uniq.size > 1
+    end
+
+    @conflicts_list = conflicts.transform_values do |entries|
+      {
+        authors: entries.select { |_, d| d[:source] == :author }.map { |_, d| d[:orcid] }.uniq,
+        users: entries.select { |_, d| d[:source] == :user }.map { |_, d| d[:orcid] }.uniq
+      }
+    end
+  end
+
+  def combined_orcids
+    return @combined_orcids if @combined_orcids.present?
+
+    author_map = StashEngine::Author
+      .where.not(author_email: [nil, ''])
+      .where.not(author_orcid: [nil, ''])
+      .pluck(:author_email, :author_orcid)
+      .map { |email, orcid| [email, { source: :author, orcid: orcid }] }
+
+    user_map = StashEngine::User
+      .where.not(email: [nil, ''])
+      .where.not(orcid: [nil, ''])
+      .pluck(:email, :orcid)
+      .map { |email, orcid| [email, { source: :user, orcid: orcid }] }
+
+    @combined_orcids = author_map + user_map
+  end
+end
