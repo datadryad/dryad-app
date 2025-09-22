6e24c9339e40890fc60064f391a2bc3261882211
Alin Vetian <vetianalin@gmail.com>
2025-09-22 18:09:27 +0300

    fixed feature specs calling localhost API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
-
+  config.ignore_localhost = true
   config.default_cassette_options = {
     record: :once,
     match_requests_on: %i[method uri body]
