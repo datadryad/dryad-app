b92a7e245ed15aede8074ffe44075b3ecc71a87f
Alin Vetian <vetianalin@gmail.com>
2025-10-01 19:16:06 +0300

    allow landing page to load in iFrame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true
 
+  private
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options') # remove default
+    response.headers['X-Frame-Options'] = 'ALLOWALL' # or 'SAMEORIGIN'
+  end
 end
