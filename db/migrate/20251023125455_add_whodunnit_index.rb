c9b58acbe428cb32aa39f0e4df7febea95e51d34
Audrey Hamelers <audrey@datadryad.org>
2025-10-23 15:19:39 +0200

    change name for matchingness

--- db/migrate/20251023125455_add_whodunnit_index.rb
+++ db/migrate/20251023125455_add_whodunnit_index.rb
@@ -1,11 +1,11 @@
 class AddWhodunnitIndex < ActiveRecord::Migration[8.0]
   def up
     execute <<-SQL
-      ALTER TABLE paper_trail_versions ADD INDEX paper_trail_whodunnit_idx (whodunnit), ALGORITHM=INPLACE, LOCK=NONE;
+      ALTER TABLE paper_trail_versions ADD INDEX index_paper_trail_versions_on_whodunnit (whodunnit), ALGORITHM=INPLACE, LOCK=NONE;
     SQL
   end
 
   def down
-    remove_index :paper_trail_versions, column: :whodunnit, name: "paper_trail_whodunnit_idx"
+    remove_index :paper_trail_versions, column: :whodunnit, name: "index_paper_trail_versions_on_whodunnit"
   end
 end
