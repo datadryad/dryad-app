83bee82494559d7bfca97b54a128056ceb348c36
Audrey Hamelers <audrey@datadryad.org>
2025-11-28 19:51:24 +0100

    tests

--- spec/mocks/rsolr.rb
+++ spec/mocks/rsolr.rb
@@ -23,6 +23,10 @@ module Mocks
       end
     end
 
+    def mock_solr_frontend!
+      stub_request(:get, %r{solr/dryad}).to_return(status: 200, body: detailed_results, headers: {})
+    end
+
     def mock_rors_solr!(include_rors: [])
       @include_rors = include_rors
 
@@ -53,6 +57,44 @@ module Mocks
           'start' => 1,
           'docs' =>
             [{ 'dc_identifier_s' => "doi:#{@include_identifier&.identifier || '10.5061/dryad.abc123'}" }] } }.to_json
+    end
+
+    def detailed_results
+      results = Array.new(20) do
+        {
+          'dc_identifier_s' => 'doi:10.5061/dryad.1r7m0',
+          'dc_title_s' => Faker::Lorem.sentence(word_count: 10),
+          'dc_creator_sm' => Array.new(rand(1..5)) { "#{Faker::Name.last_name}, #{Faker::Name.first_name}" },
+          'dc_description_s' => Faker::Lorem.paragraph,
+          'dc_subject_sm' => Array.new(rand(4..8)) { Faker::Lorem.word },
+          'dct_issued_dt' => Faker::Time.between_dates(from: Date.today - 2.years, to: Date.today)
+        }
+      end
+      {
+        'response' => {
+          'numFound' => 20,
+          'start' => 0,
+          'docs' => results
+        },
+        'facet_counts' => {
+          'facet_fields' => {
+            'ror_ids_sm' =>
+            ["https://ror.org/#{Faker::Number.number(digits: 7)}", 6,
+             "https://ror.org/#{Faker::Number.number(digits: 7)}", 5],
+            'dryad_author_affiliation_id_sm' =>
+            ["https://ror.org/#{Faker::Number.number(digits: 7)}", 6,
+             "https://ror.org/#{Faker::Number.number(digits: 7)}", 5],
+            'dryad_related_publication_issn_s' =>
+            ["#{Faker::Number.number(digits: 4)}-#{Faker::Number.number(digits: 4)}", 4,
+             "#{Faker::Number.number(digits: 4)}-#{Faker::Number.number(digits: 4)}", 3],
+            'solr_year_i' =>
+            [Date.today.year, 6, (Date.today - 1.year).year, 5],
+            'dryad_dataset_file_ext_sm' => ['md', 10, 'csv', 6, 'tsv', 5],
+            'dc_subject_sm' =>
+            [Faker::Lorem.word, 5, Faker::Lorem.word, 5, Faker::Lorem.word, 3]
+          }
+        }
+      }.to_json
     end
 
     def ror_results
