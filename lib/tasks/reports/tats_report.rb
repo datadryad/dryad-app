786c96c6f2e1608e8099f798af9c4331188c891c
Alin Vetian <vetianalin@gmail.com>
2025-12-16 16:30:32 +0200

    rubocop

--- lib/tasks/reports/tats_report.rb
+++ lib/tasks/reports/tats_report.rb
@@ -24,36 +24,36 @@ module Tasks
         CSV.open(File.join(Rails.root.join('reports'), @report_name), 'wb') do |csv|
           csv << csv_header
           items.each do |ident|
-            service = TimeInStatus.new(identifier: ident, return_in: :days)
-            submitted_time = service.time_in_status %w[submitted]
-            curation_time = service.time_in_status %w[curation], include_statuses: %w[in_progress], action_taken_by: :dryad
-            aar_time = service.time_in_status %w[action_required], include_statuses: %w[in_progress], action_taken_by: :author
-            total_time = (submitted_time + curation_time + aar_time).round(2)
-
-            csv << [
-              ident.id,
-              ident.to_s,
-              ident.latest_resource.title,
-              filesize(ident.storage_size),
-              ident.storage_size,
-              ident.latest_resource.tenant&.short_name,
-              ident.journal&.title,
-              ident.journal&.sponsor&.name,
-              ident.most_recent_curator&.name,
-              submitted_time,
-              (submitted_time * 100 / total_time.to_f).round(2),
-              curation_time,
-              (curation_time * 100 / total_time.to_f).round(2),
-              aar_time,
-              (aar_time * 100 / total_time.to_f).round(2),
-              total_time
-            ]
+            add_identifier_to_csv csv, ident
           end
         end
         true
       end
 
       private
+
+      def add_identifier_to_csv(csv, ident)
+        service = TimeInStatus.new(identifier: ident, return_in: :days)
+        submitted_time = service.time_in_status %w[submitted]
+        curation_time = service.time_in_status %w[curation], include_statuses: %w[in_progress], action_taken_by: :dryad
+        aar_time = service.time_in_status %w[action_required], include_statuses: %w[in_progress], action_taken_by: :author
+        total_time = (submitted_time + curation_time + aar_time).round(2)
+
+        csv << [
+          ident.id,
+          ident.to_s,
+          ident.latest_resource.title,
+          filesize(ident.storage_size), ident.storage_size,
+          ident.latest_resource.tenant&.short_name,
+          ident.journal&.title,
+          ident.journal&.sponsor&.name,
+          ident.most_recent_curator&.name,
+          submitted_time, (submitted_time * 100 / total_time.to_f).round(2),
+          curation_time, (curation_time * 100 / total_time.to_f).round(2),
+          aar_time, (aar_time * 100 / total_time.to_f).round(2),
+          total_time
+        ]
+      end
 
       def csv_header
         [
