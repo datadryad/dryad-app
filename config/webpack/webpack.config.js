3ba18a51254c536c2f18a0fd63c4e2a786194ecc
Audrey Hamelers <audrey@datadryad.org>
2026-01-09 18:32:25 +0100

    upgrade to v8

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -3,6 +3,9 @@ const webpackConfig = generateWebpackConfig()
 
 
 const customConfig = {
+  output: {
+    publicPath: 'auto'
+  },
   resolve: {
     fallback: { "util": false },
     extensions: ['.jsx', '.mjs', '.js', '.sass', '.scss', '.css', '.module.sass', '.module.scss', '.module.css', '.png', '.svg', '.gif', '.jpeg', '.jpg']
