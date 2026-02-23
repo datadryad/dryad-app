055dd6dffc1e0acada03ed13ffbc80f9fd6753e6
Audrey Hamelers <audrey@datadryad.org>
2026-02-23 17:27:58 +0100

    cache solr xml result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,9 @@ class SearchController < ApplicationController
   include StashEngine::SharedSecurityController
 
   def search
-    service = StashApi::SolrSearchService.new(query: params[:q], filters: params.except(:q, :action, :controller))
-
     respond_to do |format|
       format.html do
+        service = StashApi::SolrSearchService.new(query: params[:q], filters: params.except(:q, :action, :controller))
         result = service.search(page: page, per_page: per_page, fields: fields, facet: true)
         if (error = service.error)
           render status: error.status, plain: error.message and return
@@ -17,13 +16,7 @@ class SearchController < ApplicationController
         @profiles = profiles
       end
       format.xml do
-        service = StashApi::SolrSearchService.new(query: params[:q], filters: params.except(:q, :action, :controller, :id).merge(sort: 'date desc'))
-        result = service.search(page: page, per_page: 10, fields: fields, facet: true, wt: :xml)
-        if (error = service.error)
-          render status: error.status, plain: error.message and return
-        end
-
-        render xml: atom_xml(result), content_type: 'application/atom+xml'
+        render xml: atom_xml(xml_result), content_type: 'application/atom+xml'
       end
     end
   end
@@ -130,6 +123,17 @@ class SearchController < ApplicationController
   def per_page
     @page_size = 10 if params[:page_size].blank? || params[:page_size].to_i == 0
     @page_size ||= params[:page_size].to_i
+  end
+
+  def xml_result
+    filters = URI.encode_www_form(params.except(:q, :action, :controller, :id, :page_size, :format).to_unsafe_hash.sort.to_h)
+    Rails.cache.fetch("xml_search/#{filters}", expires_in: 12.hours) do
+      service = StashApi::SolrSearchService.new(query: params[:q], filters: params.except(:q, :action, :controller, :id).merge(sort: 'date desc'))
+      service.search(page: page, per_page: 10, fields: fields, facet: true, wt: :xml)
+      if (error = service.error)
+        render status: error.status, plain: error.message and return
+      end
+    end
   end
 
   def atom_xml(result)
