1d61cb2f9c5de5f6b9a7d43996cc0895e3705913
Alin Vetian <vetianalin@gmail.com>
2026-03-12 13:35:59 +0200

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,7 +57,7 @@ end
 
 after :deploy, 'puma:restart_if_exists'
 after :deploy, 'sidekiq:restart_if_exists'
-after :deploy, :index_help_center
+
 
 namespace :git do
   desc "Add the version file so that we can display the git version in the footer"
@@ -108,6 +108,7 @@ namespace :sidekiq do
       if test("[ -f /etc/systemd/system/#{service}.service ]") ||
         test("systemctl list-unit-files | grep -q #{service}.service")
         execute :sudo, :systemctl, :restart, "#{service}.service"
+        after :deploy, :index_help_center
       else
         info "Puma service #{service} not found, skipping restart"
       end
