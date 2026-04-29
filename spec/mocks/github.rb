13eebcb559bfbef5995919bedddaa2d51a22c0da
Audrey Hamelers <audrey@datadryad.org>
2026-04-29 20:26:49 +0200

    rspec

--- spec/mocks/github.rb
+++ spec/mocks/github.rb
@@ -1,6 +1,25 @@
 module Mocks
   module Github
     def mock_github!
+      default = OpenSSL::PKey::RSA.new(2048)
+      double = class_double(OpenSSL::PKey::RSA).as_stubbed_const
+      allow(double).to receive(:new).and_return(default)
+      stub_request(:get, 'https://api.github.com/app/installations')
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Host' => 'api.github.com',
+            'User-Agent' => 'Ruby'
+          }
+        ).to_return(status: 200, body: [{ id: 0o003 }].to_json)
+      stub_request(:post, 'https://api.github.com/app/installations/0003/access_tokens')
+        .with(
+          headers: {
+            'Accept' => '*/*',
+            'Host' => 'api.github.com',
+            'User-Agent' => 'Ruby'
+          }
+        ).to_return(status: 200, body: { token: 'ajhafhsjfhsd' }.to_json)
       stub_request(:get, 'https://api.github.com/repos/datadryad/dryad-product-roadmap/issues/1234')
         .with(
           headers: {
