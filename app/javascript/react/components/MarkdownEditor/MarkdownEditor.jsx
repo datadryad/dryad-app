e75b039ca37debbac69e113d53e94a8c7ca25f22
Audrey Hamelers <audrey@datadryad.org>
2026-05-21 14:53:35 +0200

    remove image upload from default buttons

--- app/javascript/react/components/MarkdownEditor/MarkdownEditor.jsx
+++ app/javascript/react/components/MarkdownEditor/MarkdownEditor.jsx
@@ -112,7 +112,7 @@ function MilkdownCore({
 }
 
 const defaultButtons = ['heading', 'strong', 'emphasis', 'superscript', 'subscript', 'inlineCode',
-  'spacer', 'blockquote', 'code_block', 'link', 'image', 'spacer', 'table', 'spacer',
+  'spacer', 'blockquote', 'code_block', 'link', 'spacer', 'table', 'spacer',
   'list_menu', 'spacer', 'undo', 'redo'];
 
 function MilkdownEditor({
