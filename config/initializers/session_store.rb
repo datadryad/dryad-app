0dbddc9869d8f07925691bc1b9997834febffb8f
Alin Vetian <vetianalin@gmail.com>
2026-05-21 16:53:42 +0300

    change session store to active record

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store,
+Rails.application.config.session_store :active_record_store,
                                        key: '_dash2_session',
                                        same_site: :lax, # <-- lax because of OAuth redirects
                                        httponly: true,
