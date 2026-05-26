81b12cd6490f9134828b47dcd35be22b2931a0c9
Alin Vetian <vetianalin@gmail.com>
2026-05-26 15:07:28 +0300

    Merge pull request #2960 from datadryad/5037-failing-tests-due-to-seleniumwebdriverchrome-2
    Fix chrome webdriver issues

--- spec/features/stash_engine/admin_searches_spec.rb
+++ spec/features/stash_engine/admin_searches_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature 'AdminSearch', type: :feature do
   include Mocks::Stripe
   include Mocks::DataFile
 
-  before(:each) do
+  before do
     mock_aws!
     mock_solr!
     mock_salesforce!
@@ -41,7 +41,7 @@ RSpec.feature 'AdminSearch', type: :feature do
   end
 
   context :saved_search do
-    before(:each) do
+    before do
       # rubocop:disable Layout/LineLength
       @properties = '{"fields":["doi","authors","submitter"],"filters":{"member":"","status":"","curator":"","journal":{"value":"","label":""},"sponsor":"","funder":{"value":"","label":""},"affiliation":{"value":"","label":""},"updated_at":{"start_date":"","end_date":""},"submit_date":{"start_date":"","end_date":""},"publication_date":{"start_date":"","end_date":""},"identifiers":""},"search_string":""}'
       # rubocop:enable Layout/LineLength
@@ -62,9 +62,10 @@ RSpec.feature 'AdminSearch', type: :feature do
     end
 
     context :search_properties do
-      before(:each) do
+      before do
         sign_in(@superuser, false)
       end
+
       it 'does not show saved search', js: true do
         visit stash_url_helpers.admin_dashboard_path
         expect(page).to have_text('Admin dashboard')
@@ -72,6 +73,7 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(find('#submitter')).not_to be_checked
         expect(find('thead')).not_to have_text('Submitter')
       end
+
       it 'shows saved search', js: true do
         visit stash_url_helpers.admin_dashboard_path(search: 1)
         expect(page).to have_text('Admin dashboard')
@@ -81,6 +83,7 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(find('#submitter')).to be_checked
         expect(find('thead')).to have_text('Submitter')
       end
+
       it 'shows default saved search', js: true do
         @superuser.admin_searches.first.update(default: true)
         visit stash_url_helpers.admin_dashboard_path
@@ -91,6 +94,7 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(find('#submitter')).to be_checked
         expect(find('thead')).to have_text('Submitter')
       end
+
       it 'edits saved search', js: true do
         visit stash_url_helpers.admin_dashboard_path(search: 1)
         expect(page).to have_text('Admin dashboard')
@@ -99,6 +103,7 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(page).to have_text('Display fields')
         check 'metrics'
         click_button('Apply')
+        expect(page).to have_button('Save search changes')
         click_button('Save search changes')
         visit stash_url_helpers.admin_dashboard_path(search: 1)
         expect(page).to have_text('Admin dashboard')
@@ -108,6 +113,7 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(find('#metrics')).to be_checked
         expect(find('thead')).to have_text('Metrics')
       end
+
       it 'replaces search default', js: true do
         @superuser.admin_searches.first.update(default: true)
         visit stash_url_helpers.admin_dashboard_path
@@ -133,27 +139,30 @@ RSpec.feature 'AdminSearch', type: :feature do
     end
 
     context :search_profile do
-      before(:each) do
+      before do
         sign_in(@superuser, false)
       end
+
       it 'has a search share code', js: true do
         visit stash_url_helpers.saved_searches_path
         expect(find('#admin_searches_list')).to have_text('First saved search')
         expect(@superuser.admin_searches.first.share_code).not_to eq(nil)
         expect(page).to have_link(nil, href: admin_dashboard_path(share: @superuser.admin_searches.first.share_code, clear: true))
       end
+
       it 'edits search details', js: true do
         visit stash_url_helpers.saved_searches_path
         expect(find('#admin_searches_list')).to have_text('First saved search')
-        within(find('#admin_searches_list li:first-child')) do
+        within('#admin_searches_list li:first-child') do
           click_button 'Edit search description'
         end
         fill_in('title', with: 'Edited search')
-        within(find('#admin_searches_list li:first-child')) do
+        within('#admin_searches_list li:first-child') do
           click_button 'Save'
         end
         expect(find('#admin_searches_list')).to have_text('Edited search')
       end
+
       it 'replaces search default', js: true do
         @superuser.admin_searches << StashEngine::AdminSearch.create(title: 'First default search', properties: @properties, default: true)
         visit stash_url_helpers.saved_searches_path
@@ -161,21 +170,23 @@ RSpec.feature 'AdminSearch', type: :feature do
         expect(find('#admin_searches_list')).to have_text('First saved search')
         expect(find('#admin_searches_list')).to have_text('First default search')
         expect(find('#admin_searches_list li:last-child')).to have_text('Default')
-        within(find('#admin_searches_list li:first-child')) do
+        within('#admin_searches_list li:first-child') do
           click_button 'Edit search description'
         end
         check 'default'
-        within(find('#admin_searches_list li:first-child')) do
+        within('#admin_searches_list li:first-child') do
           click_button 'Save'
         end
         expect(find('#admin_searches_list li:first-child')).to have_text('Default')
         expect(find('#admin_searches_list li:last-child')).not_to have_text('Default')
       end
+
       it 'removes saved search', js: true do
         visit stash_url_helpers.saved_searches_path
         expect(find('#admin_searches_list')).to have_text('First saved search')
+        expect(page).to have_button('Delete saved search: First saved search')
         click_button 'Delete saved search: First saved search'
-        expect(page).not_to have_css('#admin_searches_list li')
+        expect(page).to have_no_css('#admin_searches_list li')
       end
     end
   end
