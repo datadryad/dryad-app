af1acc4326c310ef860fd63fb1bbabf638a25a43
Audrey Hamelers <audrey@datadryad.org>
2026-06-18 16:38:19 +0200

    rspec

--- app/controllers/stash_engine/journals_controller.rb
+++ app/controllers/stash_engine/journals_controller.rb
@@ -9,7 +9,7 @@ module StashEngine
 
       ord = helpers.sortable_table_order(whitelist: %w[title payment_plan sponsor_id root_org_id default_to_ppr integrated_at])
 
-      @journals = Journal.with_sponsorship.where("payment_plan is not null or integrated_at >= #{2.years.ago}").order(ord, title: :asc)
+      @journals = Journal.with_sponsorship.where("payment_plan is not null or integrated_at >= '#{2.years.ago}'").order(ord, title: :asc)
         .preload(:sponsor).preload(:issns)
 
       respond_to(&:html)
