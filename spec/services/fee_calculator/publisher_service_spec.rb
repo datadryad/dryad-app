b431d8d80a8e8c6cf916c144015017cb795e019c
Alin Vetian <vetianalin@gmail.com>
2026-06-26 18:13:23 +0300

    updates specs

--- spec/services/fee_calculator/publisher_service_spec.rb
+++ spec/services/fee_calculator/publisher_service_spec.rb
@@ -209,7 +209,7 @@ module FeeCalculator
               let(:new_files_size) { 100_000_000_000 }
 
               it {
-                is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 464, invoice_fee: 199, total: 663, storage_fee_label: 'Large data fee' })
+                is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 464, total: 464, storage_fee_label: 'Large data fee' })
               }
             end
 
@@ -331,9 +331,9 @@ module FeeCalculator
             context 'when files_size changes from free tier to another' do
               let(:new_files_size) { 100_000_000_000 }
 
-              it {
-                is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 464, invoice_fee: 199, total: 663, storage_fee_label: 'Large data fee' })
-              }
+                it {
+                  is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 464, total: 464, storage_fee_label: 'Large data fee' })
+                }
             end
 
             context 'when files_size changes from non free tier to another' do
@@ -341,8 +341,7 @@ module FeeCalculator
               let(:new_files_size) { 900_000_000_000 }
 
               it {
-                is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 3_883, invoice_fee: 199, total: 4_082,
-                                    storage_fee_label: 'Large data fee' })
+                is_expected.to eq({ service_fee: 0, dpc_fee: 0, storage_fee: 3_883, total: 3_883, storage_fee_label: 'Large data fee' })
               }
             end
 
@@ -369,7 +368,7 @@ module FeeCalculator
         let(:resource) { create(:resource, identifier: identifier, total_file_size: new_files_size) }
         let(:contributor) do
           create(:contributor, contributor_name: 'National Cancer Institute',
-                               contributor_type: 'funder', resource_id: resource.id)
+                 contributor_type: 'funder', resource_id: resource.id)
         end
         let!(:funder) { create(:funder, name: contributor.contributor_name, enabled: true) }
 
