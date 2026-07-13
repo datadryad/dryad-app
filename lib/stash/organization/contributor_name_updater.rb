e007f694291686c5c1d1b55ec1f6ebfe669006a7
Alin Vetian <vetianalin@gmail.com>
2026-07-13 16:27:34 +0300

    rubocop

--- lib/stash/organization/contributor_name_updater.rb
+++ lib/stash/organization/contributor_name_updater.rb
@@ -10,7 +10,7 @@ module Stash
 
         index = 0
         StashDatacite::Contributor.joins(:ror_org)
-          .where(stash_engine_ror_orgs: {status: StashEngine::RorOrg.statuses[:active]})
+          .where(stash_engine_ror_orgs: { status: StashEngine::RorOrg.statuses[:active] })
           .where('stash_engine_ror_orgs.name != dcs_contributors.contributor_name OR dcs_contributors.contributor_name is NULL').find_each do |record|
           puts "Updating contributor with id: #{record.id} from \"#{record.contributor_name}\" to \"#{record.ror_org.name}\""
           record.update(contributor_name: record.ror_org.name)
