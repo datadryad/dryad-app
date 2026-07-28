619b7c7ee948e0b7ab1e5ceb2682eab57f2fd288
Alin Vetian <vetianalin@gmail.com>
2026-07-28 15:59:20 +0300

    typos

--- app/models/concerns/stash_engine/payment_methods.rb
+++ app/models/concerns/stash_engine/payment_methods.rb
@@ -9,7 +9,7 @@ module StashEngine
     # if payer has a LDF limit set and it is reached:
     #  - institution should pay DPC
     #  - user pays LDF calculated as per institution
-    # returns false if the user chooses to pay wia invoice
+    # returns false if the user chooses to pay via invoice
     def user_must_pay?
       return false if old_system_valid_payer?
       return false if latest_resource.resource_type&.resource_type == 'collection'
@@ -62,8 +62,8 @@ module StashEngine
     end
 
     def payment_needed?
-      # if payment is via invoice, then user_nust_pay returns false
-      # since the user does not have to apy anything right now
+      # if payment is via invoice, then user_must_pay returns false
+      # since the user does not have to pay anything right now
       return false unless payments.last.pay_with_invoice? || user_must_pay?
       return false if old_payment_system
 
