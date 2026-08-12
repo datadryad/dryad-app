7b804b27edbebe3111c1937c3c7449844e7720f8
Alin Vetian <vetianalin@gmail.com>
2026-08-12 19:10:00 +0300

    Centralize Stripe API settings

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,9 +10,6 @@ class PaymentsController < ApplicationController
   layout 'stash_engine/application'
 
   def create
-    Stripe.api_key = APP_CONFIG.payments.key
-    Stripe.api_version = '2026-05-27.dahlia'
-
     payment_service = PaymentsService.new(current_user, @resource, create_params)
 
     begin
