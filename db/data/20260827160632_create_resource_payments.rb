2775ed932962cb90021f1f5029a3b190896f6b29
Audrey Hamelers <audrey@datadryad.org>
2026-09-02 16:24:41 +0200

    check for existing invoice payment record

--- db/data/20260827160632_create_resource_payments.rb
+++ db/data/20260827160632_create_resource_payments.rb
@@ -4,7 +4,7 @@ class CreateResourcePayments < ActiveRecord::Migration[8.0]
   def up
     # create resource payments records for old invoices
     StashEngine::Identifier.where("stash_engine_identifiers.payment_type = 'stripe' and payment_id like 'in_%'").distinct.each do |id|
-      next if id.payments.present?
+      next if id.payments.where(invoice_id: id.payment_id).exists?
 
       res = id.resources.with_public_metadata.first
       ResourcePayment.create(
