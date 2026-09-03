15bc1c4ebed9b28d49616ac1f107fcb0e68bcd05
Alin Vetian <vetianalin@gmail.com>
2026-09-03 14:57:35 +0300

    Update volume and age report

--- app/services/time_in_status.rb
+++ app/services/time_in_status.rb
@@ -6,7 +6,7 @@ class TimeInStatus
   #  :hours => hours
   #  :days  => days
   #  nil    => seconds
-  def initialize(identifier: nil, resource: nil, return_in: nil)
+  def initialize(identifier: nil, resource: nil, return_in: nil, up_to_date: nil)
     @activities = if resource.present?
                     resource.curation_activities
                   elsif identifier.present?
@@ -15,39 +15,51 @@ class TimeInStatus
                     StashEngine::CurationActivity.none
                   end
     @return_in = return_in
+    @up_to_date = up_to_date
   end
 
   # include_statuses are always related to action_taken_by if this latest is set
   # action_taken_by should be one of :dryad OR :author
   def time_in_status(statuses, include_statuses: nil, action_taken_by: nil)
+    cas = curation_activities_snapshot.where.not(status: 'processing')
     total_time = 0
     enter_time = nil
 
-    @activities.each do |ca|
+    cas.each do |ca|
       if ca.status.in?(statuses)
         enter_time = ca.created_at if enter_time.nil?
         next
       end
 
-      if include_statuses && enter_time && ca.status.in?(include_statuses)
-        if action_taken_by.present?
-          if (action_taken_by == :dryad && ca.user.min_app_admin?) ||
-            (action_taken_by == :author && !ca.user.min_app_admin?)
-            enter_time = ca.created_at if enter_time.nil?
-            next
-          end
-        else
-          enter_time = ca.created_at if enter_time.nil?
-          next
-        end
-      end
-
+      next if enter_time && is_in_included_statuses?(ca, include_statuses, action_taken_by)
       next unless enter_time
 
       total_time += ca.created_at.to_i - enter_time.to_i
       enter_time = nil
     end
     readable_time total_time
+  end
+
+  def last_time_in_status(status, include_statuses: nil, action_taken_by: nil)
+    cas = curation_activities_snapshot.where.not(status: 'processing')
+    enter_time = nil
+    last_status_ca = cas.where(status: status).last
+    return unless last_status_ca
+
+    last_status_date = last_status_ca.created_at
+    last_status_date = @up_to_date if last_status_ca == cas.last && @up_to_date
+
+    cas.where(created_at: ..last_status_ca.created_at).each do |ca|
+      # same status
+      if ca.status == status
+        enter_time = ca.created_at if enter_time.nil?
+        next
+      end
+      next if enter_time && is_in_included_statuses?(ca, include_statuses, action_taken_by)
+
+      enter_time = nil
+    end
+    readable_time last_status_date.to_i - enter_time.to_i
   end
 
   def readable_time(time)
@@ -61,5 +73,25 @@ class TimeInStatus
     else
       time.to_f.round(2)
     end
+  end
+
+  private
+
+  def is_in_included_statuses?(ca, include_statuses, action_taken_by)
+    # not in included statuses
+    return false unless ca.status.in?(include_statuses.to_a)
+
+    # action actor is missing
+    return true if action_taken_by.blank?
+
+    # action is taken by proper actor
+    (action_taken_by == :dryad && ca.user.min_app_admin?) ||
+      (action_taken_by == :author && !ca.user.min_app_admin?)
+  end
+
+  def curation_activities_snapshot
+    return @activities unless @up_to_date
+
+    @activities.where(created_at: ..@up_to_date)
   end
 end
