95b633b0b7e5a14541c0afe6984c263021b4f77f
Alin Vetian <vetianalin@gmail.com>
2026-09-14 16:38:56 +0300

    fix strange error loging

--- lib/middleware/auth_failure_logger.rb
+++ lib/middleware/auth_failure_logger.rb
@@ -9,11 +9,20 @@ module Middleware
 
       status, headers, body = @app.call(env)
 
-      AuthFailureService.new(request, nil, get_params(request)).create(:api_unauthorized) if request.path == '/oauth/token' && status == 401
+      if request.path == '/oauth/token' && status == 401
+        AuthFailureService.new(
+          request,
+          nil,
+          get_params(request)
+        ).create(:api_unauthorized)
+      end
 
       [status, headers, body]
     rescue StandardError => e
       Rails.logger.error("AuthFailureLogger error: #{e.message}")
+      Rails.logger.error(e.backtrace.join("\n"))
+
+      raise
     end
 
     def get_params(request)
