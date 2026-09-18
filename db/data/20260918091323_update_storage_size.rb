f4f6f503b91463ec5b16d9feb40f77c0b387a3cf
Audrey Hamelers <audrey@datadryad.org>
2026-09-18 11:30:06 +0200

    update data schema

--- db/data/20260918091323_update_storage_size.rb
+++ db/data/20260918091323_update_storage_size.rb
@@ -3,7 +3,7 @@
 class UpdateStorageSize < ActiveRecord::Migration[8.0]
   def up
     # some of these will be legitimately the same, but this also won't hurt anything
-    StashEngine::Identifier.joins(:latest_resource).where('storage_size = total_file_size') each do |id|
+    StashEngine::Identifier.joins(:latest_resource).where('storage_size = total_file_size').each do |id|
       next if id.resources.count == 1
 
       total_dataset_size = id.resources.sum { |r| r.data_files.created.sum(&:upload_file_size) }
